3578277740215304842e3a4546dbf94d20897f72
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-05-27 11:56:43 +0000

    Experimental support vor EMX/GCC 0.9d as build environment. IBM VAC++ 3 remains default.

--- include/pdwin32.mk
+++ include/pdwin32.mk
@@ -2,14 +2,28 @@
 # PD-Win32 Project
 #
 #
-#       Create: cdmckill@novice.uwaterloo.ca
-#               January 4th, 1998
+#      Create: cdmckill@novice.uwaterloo.ca, January 4th, 1998
+#      Modified: phaller@gmx.net, May 27th, 1999
 #
 #
 
+# defines
+#    DEBUG - build a debug version instead of release version
+#    CCENV:
+#      VAC3 - use IBM VisualAge for C++ 3 compiler environment (default)
+#      EMX - use GNU/EMX compiler environment
+
+# include compiler environment
+!ifndef CCENV
+CCENV=VAC3
+!endif
 
 !ifdef DEBUG
-!include $(PDWIN32_INCLUDE)/pdwin32.dbg
+!include $(PDWIN32_INCLUDE)/pdwin32.$(CCENV).dbg
 !else
-!include $(PDWIN32_INCLUDE)/pdwin32.rel
+!include $(PDWIN32_INCLUDE)/pdwin32.$(CCENV).rel
 !endif
+
+# include system tools
+!include $(PDWIN32_INCLUDE)/pdwin32.tools
+
