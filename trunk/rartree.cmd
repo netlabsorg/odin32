849818b75fbd1c00d48683d840cbfa0f857a1044
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-06-02 16:00:38 +0000

    a

--- trunk/rartree.cmd
+++ trunk/rartree.cmd
@@ -1,3 +1,3 @@
-: $Id: rartree.cmd,v 1.1 1999-05-24 20:19:05 ktk Exp $ 
+: $Id: rartree.cmd,v 1.2 1999-06-02 16:00:37 cbratschi Exp $ (CTR was here)
 
 rar a -r -m4 -x*.obj -x*.dll -x*.pch -x*.lib -x*.exe -x*.sys -x*.map -x*.sym -x*.vtg %1 *
