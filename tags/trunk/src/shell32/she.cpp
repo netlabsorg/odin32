9453f11ea4c186a9f847a0f72cfa62b06ecec05a
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-06-09 15:22:27 +0000

    Fix: source splitup

---
+++ tags/trunk/src/shell32/she.cpp
@@ -0,0 +1,45 @@
+/*
+ * Win32 SHELL32 for OS/2
+ * Copyright 1999 Patrick Haller
+ * Project Odin Software License can be found in LICENSE.TXT
+ */
+
+
+/*****************************************************************************
+ * Includes                                                                  *
+ *****************************************************************************/
+
+#include <os2win.h>
+#include <shellapi.h>
+#include <winreg.h>
+#include "shell32.h"
+
+#include <stdarg.h>
+//#include <builtin.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <misc.h>
+#include <nameid.h>
+#include <unicode.h>
+
+
+/*****************************************************************************
+ * Types & Defines                                                           *
+ *****************************************************************************/
+
+
+/*****************************************************************************
+ * Name      :
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    : UNTESTED STUB
+ *
+ * Author    : Patrick Haller [Tue, 1999/06/01 09:00]
+ *****************************************************************************/
+
+
