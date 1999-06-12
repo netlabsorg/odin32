ad67da0dd0002402f4d67ba4f040c19a4b86d1c3
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-06-12 18:19:14 +0000

    added draglist dummy control and exported its APIs

---
+++ trunk/src/comctl32/draglist.c
@@ -0,0 +1,43 @@
+/*
+ * Drag List control
+ *
+ * Copyright 1999 Eric Kohl
+ *
+ * NOTES
+ *   This is just a dummy control. An author is needed! Any volunteers?
+ *   I will only improve this control once in a while.
+ *     Eric <ekohl@abo.rhein-zeitung.de>
+ *
+ * TODO:
+ *   - Everything.
+ */
+
+#include "commctrl.h"
+
+
+BOOL WINAPI MakeDragList (HWND hwndLB)
+{
+//    FIXME (commctrl, "(0x%x)\n", hwndLB);
+
+
+    return FALSE;
+}
+
+
+VOID WINAPI DrawInsert (HWND hwndParent, HWND hwndLB, INT nItem)
+{
+//    FIXME (commctrl, "(0x%x 0x%x %d)\n", hwndParent, hwndLB, nItem);
+
+
+}
+
+
+INT WINAPI LBItemFromPt (HWND hwndLB, POINT pt, BOOL bAutoScroll)
+{
+//    FIXME (commctrl, "(0x%x %ld x %ld %s)\n",
+//	   hwndLB, pt.x, pt.y, bAutoScroll ? "TRUE" : "FALSE");
+
+
+    return -1;
+}
+
