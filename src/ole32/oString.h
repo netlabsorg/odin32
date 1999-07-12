44257e0abdc6378fe3990329bde646ca83368049
davidr <davidr@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-12 00:17:20 +0000

    Added Unicode variant of string management class. Added RunningObjectTable definitions.

--- src/ole32/oString.h
+++ src/ole32/oString.h
@@ -2,14 +2,15 @@
 #if !defined(OSTRING_INCLUDED)
 #define OSTRING_INCLUDED
 
-class oStringA
+class oStringBase
 {
-private:
-    LPSTR	m_strBuf;	// Buffer for string data.
-    
+protected:
+    char *	m_strBuf;	// Buffer.
+
     long	m_bufLen;	// Current allocated length
-    				// of buffer
+    				// of buffer (bytes)
     				
+    long	m_bytePerChar;	// Number of bytes per character.
     long	m_strLen;	// Length of string 
     				// incl. zero terminator
 
@@ -17,36 +18,76 @@ private:
 
     static long blockSize;
 
-protected:
+    oStringBase(long bytePerChar);
+    virtual ~oStringBase();
+
     void freeBuf();
     void getBuf(long newLen);
     void adjBuf(long newLen);
     long calcBufLen(long newLen);
+};
 
+class oStringA : public oStringBase
+{
 public:
     oStringA(void);
     oStringA(int defLen);
     oStringA(LPCWSTR pUnicode);
     oStringA(LPCSTR pAscii);
+    oStringA(REFCLSID pclsId);
     oStringA(const oStringA &ref);
-    virtual ~oStringA();
 
     operator LPSTR();
 
-    // Equality
+    // Assignment
     oStringA operator =  (const oStringA & string);
     oStringA operator =  (LPCSTR pAscii);
     oStringA operator =  (LPCWSTR pUnicode);
+    oStringA operator =  (REFCLSID pClsId);
 
     // Concatenation
     oStringA operator +  (const oStringA & string);
     oStringA operator +  (LPCSTR pAscii);
     oStringA operator +  (LPCWSTR pUnicode);
 
-    // Concatenation
+    // Concatenation & Assignment.
     oStringA operator += (const oStringA & string);
     oStringA operator += (LPCSTR pAscii);
     oStringA operator += (LPCWSTR pUnicode);
+
+};
+
+class oStringW : public oStringBase
+{
+public:
+    oStringW(void);
+    oStringW(int defLen);
+    oStringW(LPCWSTR pUnicode);
+    oStringW(const wchar_t * pUnicode);
+    oStringW(LPCSTR pAscii);
+    oStringW(REFCLSID pclsId);
+    oStringW(const oStringW &ref);
+
+    operator LPWSTR();
+
+    // Assignment
+    oStringW operator =  (const oStringW & string);
+    oStringW operator =  (LPCSTR pAscii);
+    oStringW operator =  (LPCWSTR pUnicode);
+    oStringW operator =  (const wchar_t * pUnicode);
+    oStringW operator =  (REFCLSID pClsId);
+
+    // Concatenation
+    oStringW operator +  (const oStringW & string);
+    oStringW operator +  (LPCSTR pAscii);
+    oStringW operator +  (LPCWSTR pUnicode);
+    oStringW operator +  (const wchar_t * pUnicode);
+
+    // Concatenation & Assignment.
+    oStringW operator += (const oStringW & string);
+    oStringW operator += (LPCSTR pAscii);
+    oStringW operator += (LPCWSTR pUnicode);
+    oStringW operator += (const wchar_t * pUnicode);
 
 };
 
