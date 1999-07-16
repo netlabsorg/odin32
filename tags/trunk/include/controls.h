ec6232670659a7c585550b85ba1cdc1cbd9cde6f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-16 17:28:56 +0000

    *** empty log message ***

---
+++ tags/trunk/include/controls.h
@@ -0,0 +1,21 @@
+/*
+ * Win32 common controls
+ *
+ * Copyright (c) 1999 Christoph Bratschi
+ *
+ */
+
+#ifndef __CONTROLS_H
+#define __CONTROLS_H
+
+#include "wingdi.h"
+
+#define GetInfoPtr(hwnd) (GetWindowLongA(hwnd,0))
+#define SetInfoPtr(hwnd,data) \
+  SetWindowLongA(hwnd,0,data)
+
+void CONTROLS_Register();
+void CONTROLS_Unregister();
+
+#endif // __CONTROLS_H
+
