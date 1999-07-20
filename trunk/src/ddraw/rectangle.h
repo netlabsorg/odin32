b6d0019ffcc3579315edb6b32cdbd53736086916
hugh <hugh@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-20 22:16:51 +0000

    Changhed from Isequence to DPA for managing rectangles and surfaces

--- trunk/src/ddraw/rectangle.h
+++ trunk/src/ddraw/rectangle.h
@@ -33,56 +33,9 @@ Coord
   width() const,
   height() const;
 
-/*--------------------------------- Synonyms ---------------------------------
-
-Coord
-  bottom ( ) const,
-  left   ( ) const,
-  right  ( ) const,
-  top    ( ) const;
-*/
-
 private:
   long lTop,lLeft;
   long lBottom,lRight;
 }; // class DDRectangle
-
-DDRectangle :: DDRectangle ( Coord y1,
-                    Coord x1,
-                    Coord y2,
-                    Coord x2 )
-{
-  lTop    = y1;
-  lLeft   = x1;
-  lBottom = y2;
-  lRight  = x2;
-}
-
-IBase::Boolean DDRectangle :: operator == ( const DDRectangle &aRect ) const
-  {
-  return ( lTop == aRect.lTop && lLeft == aRect.lLeft
-           &&
-           lBottom == aRect.lBottom && lRight == aRect.lRight);
-  }
-IBase::Boolean DDRectangle :: operator != ( const DDRectangle& aRect ) const
-  {
-  return !( *this == aRect );
-  }
-IBase::Boolean DDRectangle :: intersects ( const DDRectangle &aRect ) const
-{
-  return ( lTop < aRect.lBottom && lLeft<aRect.lRight
-           &&
-           aRect.lTop < lBottom && aRect.lLeft < lRight );
-}
-
-DDRectangle::Coord DDRectangle :: width (  ) const
-{
-  return ( lRight - lLeft);
-}
-
-DDRectangle::Coord DDRectangle :: height (  ) const
-{
-  return ( lBottom - lTop);
-}
 
 #endif
