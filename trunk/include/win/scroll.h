7e75b362e907196afa228a225017eb0a4d7336a2
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-21 17:19:00 +0000

    *** empty log message ***

--- trunk/include/win/scroll.h
+++ trunk/include/win/scroll.h
@@ -1,14 +1,18 @@
-/* $Id: scroll.h,v 1.1 1999-05-24 20:19:18 ktk Exp $ */
+/* $Id: scroll.h,v 1.2 1999-07-21 17:18:43 sandervl Exp $ */
 
 /*
  * Scroll-bar class extra info
  *
  * Copyright 1993 Martin Ayotte
  * Copyright 1994 Alexandre Julliard
+ * Copyright 1999 Christoph Bratschi
  */
 
 #ifndef __WINE_SCROLL_H
 #define __WINE_SCROLL_H
+
+#define ODINSCROLLBARCLASSNAME "ODIN_SCROLLBAR"
+#define WIN32SCROLLBARCLASSNAME "SCROLLBAR"
 
 #include "windef.h"
 
@@ -28,4 +32,8 @@ extern void SCROLL_DrawScrollBar( HWND hwnd, HDC hdc, INT nBar,
 extern void SCROLL_HandleScrollEvent( HWND hwnd, INT nBar,
                                       UINT msg, POINT pt );
 
+BOOL SCROLLBAR_Register();
+BOOL SCROLLBAR_Unregister();
+
 #endif  /* __WINE_SCROLL_H */
+
