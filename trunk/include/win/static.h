c406d082058ab5bb70476e34fd45261e4dd43c5c
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-21 07:13:47 +0000

    *** empty log message ***

--- trunk/include/win/static.h
+++ trunk/include/win/static.h
@@ -1,10 +1,14 @@
-/* $Id: static.h,v 1.1 1999-05-24 20:19:19 ktk Exp $ */
+/* $Id: static.h,v 1.2 1999-07-21 07:13:47 sandervl Exp $ */
 
 /*
  * Static-class extra info
  *
  * Copyright 1994 Alexandre Julliard
+ * Copyright 1999 Christoph Bratschi
  */
+
+#define ODINSTATICCLASSNAME  "ODIN_STATIC"
+#define WIN32STATICCLASSNAME "STATIC"
 
 #ifndef __WINE_STATIC_H
 #define __WINE_STATIC_H
@@ -14,12 +18,15 @@
   /* Extra info for STATIC windows */
 typedef struct
 {
-    HFONT16  hFont;   /* Control font (or 0 for system font) */
+    HFONT    hFont;   /* Control font (or 0 for system font) */
     WORD     dummy;   /* Don't know what MS-Windows puts in there */
-    HICON16  hIcon;   /* Icon handle for SS_ICON controls */ 
+    HICON    hIcon;   /* Icon handle for SS_ICON controls */
 } STATICINFO;
 
-extern LRESULT WINAPI StaticWndProc( HWND hWnd, UINT uMsg, WPARAM wParam,
-                                     LPARAM lParam );
+extern LRESULT WINAPI StaticWndProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);
+
+BOOL STATIC_Register();
+BOOL STATIC_Unregister();
 
 #endif  /* __WINE_STATIC_H */
+
