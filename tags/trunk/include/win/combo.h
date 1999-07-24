1f81e0e7eafa1b6c06ff72057770ed0f297b5244
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-07-24 12:37:53 +0000

    *** empty log message ***

--- tags/trunk/include/win/combo.h
+++ tags/trunk/include/win/combo.h
@@ -1,4 +1,4 @@
-/* $Id: combo.h,v 1.1 1999-05-24 20:19:09 ktk Exp $ */
+/* $Id: combo.h,v 1.2 1999-07-24 12:37:53 sandervl Exp $ */
 
 /*
  * Combo box definitions
@@ -6,6 +6,10 @@
 
 #ifndef __WINE_COMBO_H
 #define __WINE_COMBO_H
+
+#define LISTBOXCLASSNAME  "LISTBOX"
+#define COMBOLBOXCLASSNAME "COMBOLBOX"
+#define COMBOBOXCLASSNAME "COMBOBOX"
 
 #define ID_CB_LISTBOX           1000
 #define ID_CB_EDIT              1001
@@ -15,33 +19,35 @@
 #define CBF_DROPPED             0x0001
 #define CBF_BUTTONDOWN          0x0002
 #define CBF_NOROLLUP            0x0004
-#define CBF_MEASUREITEM		0x0008
+#define CBF_MEASUREITEM         0x0008
 #define CBF_FOCUSED             0x0010
 #define CBF_CAPTURE             0x0020
 #define CBF_EDIT                0x0040
-#define CBF_NORESIZE		0x0080
-#define CBF_NOTIFY		0x0100
+#define CBF_NORESIZE            0x0080
+#define CBF_NOTIFY              0x0100
 #define CBF_NOREDRAW            0x0200
-#define CBF_SELCHANGE		0x0400
+#define CBF_SELCHANGE           0x0400
+#define CBF_NOEDITNOTIFY        0x1000
 #define CBF_EUI                 0x8000
 
 /* Combo state struct */
 
 typedef struct
 {
-   WND*    	self;
-   HWND  	owner;
-   UINT  	dwStyle;
-   HWND  	hWndEdit;
-   HWND  	hWndLBox;
-   UINT  	wState;
-   HFONT 	hFont;
+   HWND         hwndself;
+   HWND         owner;
+   UINT         dwStyle;
+   HWND         hWndEdit;
+   HWND         hWndLBox;
+   UINT         wState;
+   HFONT        hFont;
    RECT         textRect;
    RECT         buttonRect;
    RECT         droppedRect;
+   INT          droppedIndex;
    INT          fixedOwnerDrawHeight;
-   INT   	droppedWidth;		/* last two are not used unless set */
-   INT   	editHeight;		/* explicitly */
+   INT          droppedWidth;           /* last two are not used unless set */
+   INT          editHeight;             /* explicitly */
 } HEADCOMBO,*LPHEADCOMBO;
 
 /*
@@ -49,14 +55,22 @@ typedef struct
  */
 
 #define CB_GETTYPE( lphc )    ((lphc)->dwStyle & (CBS_DROPDOWNLIST))
-#define CB_DISABLED( lphc )   ((lphc)->self->dwStyle & WS_DISABLED)
+#define CB_DISABLED( lphc )   (GetWindowLongA((lphc)->hwndself,GWL_STYLE) & WS_DISABLED)
 #define CB_OWNERDRAWN( lphc ) ((lphc)->dwStyle & (CBS_OWNERDRAWFIXED | CBS_OWNERDRAWVARIABLE))
 #define CB_HASSTRINGS( lphc ) ((lphc)->dwStyle & CBS_HASSTRINGS)
-#define CB_HWND( lphc )       ((lphc)->self->hwndSelf)
+#define CB_HWND( lphc )       ((lphc)->hwndself)
 
-BOOL 	COMBO_FlipListbox( LPHEADCOMBO, BOOL );
-HWND 	COMBO_GetLBWindow( WND* );
+BOOL    COMBO_FlipListbox( LPHEADCOMBO, BOOL );
+HWND    COMBO_GetLBWindow( HWND );
 LRESULT COMBO_Directory( LPHEADCOMBO, UINT, LPSTR, BOOL );
+
+BOOL LISTBOX_Register();
+BOOL LISTBOX_Unregister();
+BOOL COMBOLBOX_Register();
+BOOL COMBOLBOX_Unregister();
+
+BOOL COMBOBOX_Register();
+BOOL COMBOBOX_Unregister();
 
 #endif /* __WINE_COMBO_H */
 
