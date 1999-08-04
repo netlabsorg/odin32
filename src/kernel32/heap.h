d7ed609bdf502c0160bb2ad6a51d199536aae19a
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-08-04 00:46:14 +0000

    Fix: Win3-Style Profile support ported from WINE. Also removes Open32 dependency

--- src/kernel32/heap.h
+++ src/kernel32/heap.h
@@ -1,4 +1,4 @@
-/* $Id: heap.h,v 1.3 1999-06-10 19:11:30 phaller Exp $ */
+/* $Id: heap.h,v 1.4 1999-08-04 00:46:14 phaller Exp $ */
 
 /*
  * Project Odin Software License can be found in LICENSE.TXT
@@ -8,14 +8,6 @@
 #ifndef __HEAP_H__
 #define __HEAP_H__
 
-HANDLE WIN32API OS2HeapCreate(DWORD  flOptions,
-                              DWORD  dwInitialSize,
-                              DWORD  dwMaximumSize);
-
-LPVOID WIN32API OS2HeapAlloc (HANDLE hHeap,
-                              DWORD  dwFlags,
-                              DWORD  dwBytes);
-
 #ifndef HEAP_NO_SERIALIZE
   #define HEAP_NO_SERIALIZE 1
 #endif
@@ -23,6 +15,40 @@ LPVOID WIN32API OS2HeapAlloc (HANDLE hHeap,
 #ifndef HEAP_ZERO_MEMORY
   #define HEAP_ZERO_MEMORY  8
 #endif
+
+
+LPVOID  WIN32API HeapAlloc     (HANDLE hHeap, DWORD dwFlags, DWORD dwBytes);
+LPVOID  WIN32API HeapReAlloc   (HANDLE hHeap, DWORD dwFlags, LPVOID lpMem, DWORD dwBytes);
+BOOL    WIN32API HeapFree      (HANDLE hHeap, DWORD dwFlags, LPVOID lpMem);
+HANDLE  WIN32API HeapCreate    (DWORD flOptions, DWORD dwInitialSize, DWORD dwMaximumSize);
+BOOL    WIN32API HeapDestroy   (HANDLE hHeap);
+DWORD   WIN32API HeapSize      (HANDLE hHeap, DWORD arg2, PVOID  arg3);
+DWORD   WIN32API HeapCompact   (HANDLE hHeap, DWORD dwFlags);
+BOOL    WIN32API HeapValidate  (HANDLE hHeap, DWORD dwFlags, LPCVOID lpMem);
+BOOL    WIN32API HeapUnlock    (HANDLE hHeap);
+BOOL    WIN32API HeapLock      (HANDLE hHeap);
+BOOL    WIN32API HeapWalk      (HANDLE hHeap, void *lpEntry);
+HANDLE  WIN32API GetProcessHeap(VOID);
+HLOCAL  WIN32API LocalAlloc    (UINT fuFlags, DWORD cbBytes);
+HLOCAL  WIN32API LocalDiscard  (HLOCAL hMem);
+UINT    WIN32API LocalFlags    (HLOCAL hMem);
+HLOCAL  WIN32API LocalFree     (HLOCAL hMem);
+HLOCAL  WIN32API LocalHandle   (PCVOID lpMem);
+BOOL    WIN32API LocalUnlock   (HLOCAL hMem);
+HLOCAL  WIN32API LocalReAlloc  (HLOCAL hMem, DWORD cbBytes, UINT fuFlags);
+UINT    WIN32API LocalSize     (HLOCAL hMem);
+PVOID   WIN32API LocalLock     (HLOCAL hMem);
+UINT    WIN32API LocalShrink   (HLOCAL hMem, UINT   cbNewSize);
+UINT    WIN32API LocalCompact  (UINT cbNewSize);
+HGLOBAL WIN32API GlobalAlloc   (UINT fuFlags, DWORD dwBytes);
+HGLOBAL WIN32API GlobalFree    (HGLOBAL arg1);
+HGLOBAL WIN32API GlobalHandle  (LPCVOID arg1);
+UINT    WIN32API GlobalFlags   (HGLOBAL arg1);
+DWORD   WIN32API GlobalCompact (DWORD dwMinFree);
+PVOID   WIN32API GlobalLock    (HGLOBAL arg1);
+HGLOBAL WIN32API GlobalReAlloc (HGLOBAL arg1, DWORD arg2, UINT  arg3);
+DWORD   WIN32API GlobalSize    (HGLOBAL arg1);
+BOOL    WIN32API GlobalUnlock  (HGLOBAL arg1);
 
 
 #endif
