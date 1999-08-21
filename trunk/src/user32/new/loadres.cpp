3b373c8c533c8505d54f9949bd7f4c85c543411b
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-08-21 19:10:59 +0000

    LoadBitmap changes

--- trunk/src/user32/new/loadres.cpp
+++ trunk/src/user32/new/loadres.cpp
@@ -1,10 +1,14 @@
-/* $Id: loadres.cpp,v 1.7 1999-08-20 20:09:50 sandervl Exp $ */
+/* $Id: loadres.cpp,v 1.8 1999-08-21 19:10:59 sandervl Exp $ */
 
 /*
  * Win32 resource API functions for OS/2
  *
  * Copyright 1998 Sander van Leeuwen
  *
+ * Parts based on Wine code (objects\bitmap.c):
+ *
+ * Copyright 1993 Alexandre Julliard
+ *           1998 Huw D M Davies
  *
  * Project Odin Software License can be found in LICENSE.TXT
  *
@@ -14,6 +18,9 @@
 #include <winres.h>
 #include <heapstring.h>
 #include <oslibres.h>
+#include <winconst.h>
+#include <win\virtual.h>
+#include "dib.h"
 
 //******************************************************************************
 //******************************************************************************
@@ -158,45 +165,168 @@ HCURSOR WIN32API LoadCursorW(HINSTANCE hinst, LPCWSTR lpszCursor)
 }
 //******************************************************************************
 //******************************************************************************
-HBITMAP WIN32API LoadBitmapA(HINSTANCE hinst, LPCSTR lpszBitmap)
+BOOL IsSystemBitmap(ULONG *id)
 {
- HBITMAP rc;
+   switch(*id)
+   {
+      	case OBM_UPARROW_W:
+      	case OBM_DNARROW_W:
+      	case OBM_RGARROW_W:
+      	case OBM_LFARROW_W:
+      	case OBM_RESTORE_W:
+      	case OBM_RESTORED_W:
+      	case OBM_UPARROWD_W:
+      	case OBM_DNARROWD_W:
+      	case OBM_RGARROWD_W:
+      	case OBM_LFARROWD_W:
+      	case OBM_OLD_UPARROW_W:
+      	case OBM_OLD_DNARROW_W:
+      	case OBM_OLD_RGARROW_W:
+      	case OBM_OLD_LFARROW_W:
+      	case OBM_CHECK_W:
+      	case OBM_CHECKBOXES_W:
+      	case OBM_BTNCORNERS_W:
+      	case OBM_COMBO_W:
+      	case OBM_REDUCE_W:
+      	case OBM_REDUCED_W:
+      	case OBM_ZOOM_W:
+      	case OBM_ZOOMD_W:
+      	case OBM_SIZE_W:
+      	case OBM_CLOSE_W:
+      	case OBM_MNARROW_W:
+      	case OBM_UPARROWI_W:
+      	case OBM_DNARROWI_W:
+      	case OBM_RGARROWI_W:
+      	case OBM_LFARROWI_W:
+		return TRUE;
 
-  if(HIWORD(lpszBitmap) != 0) 
-  {  	//convert string name identifier to numeric id
-    	dprintf(("lpszBitmap [%s]\n", lpszBitmap));
-    
-    	lpszBitmap = (LPCSTR)ConvertNameId(hinst, (char *)lpszBitmap);
-  }
-  else  dprintf(("lpszBitmap %08xh\n", (int)lpszBitmap));
+	//TODO: Not supported by Open32. Replacement may not be accurate
+      	case OBM_OLD_CLOSE_W:
+		*id = OBM_CLOSE_W;
+		return TRUE;
 
-  rc = O32_LoadBitmap(hinst, lpszBitmap);
+      	case OBM_BTSIZE_W:
+		*id = OBM_SIZE_W;
+		return TRUE;
 
-  dprintf(("LoadBitmapA returned %08xh\n", rc));
-  
-  return(rc);
+      	case OBM_OLD_REDUCE_W:
+		*id = OBM_REDUCE_W;
+		return TRUE;
+
+      	case OBM_OLD_ZOOM_W:
+		*id = OBM_ZOOM_W;
+		return TRUE;
+
+      	case OBM_OLD_RESTORE_W:
+		*id = OBM_RESTORE_W;
+		return TRUE;
+
+     	default:
+		return FALSE;
+   }
 }
 //******************************************************************************
 //******************************************************************************
+HANDLE LoadBitmapA(HINSTANCE hinst, LPCSTR lpszName, int cxDesired, int cyDesired, 
+                   UINT fuLoad)
+{
+    HBITMAP hbitmap = 0;
+    HDC hdc;
+    HRSRC hRsrc;
+    HGLOBAL handle;
+    char *ptr = NULL;
+    BITMAPINFO *info, *fix_info=NULL;
+    HGLOBAL hFix;
+    int size;
+
+    if (!(fuLoad & LR_LOADFROMFILE)) {
+      if (!(hRsrc = FindResourceA( hinst, lpszName, RT_BITMAPA ))) return 0;
+      if (!(handle = LoadResource( hinst, hRsrc ))) return 0;
+
+      if ((info = (BITMAPINFO *)LockResource( handle )) == NULL) return 0;
+    }
+    else
+    {
+        if (!(ptr = (char *)VIRTUAL_MapFileA( lpszName ))) return 0;
+        info = (BITMAPINFO *)(ptr + sizeof(BITMAPFILEHEADER));
+    }
+
+    //TODO: This has to be removed once pe2lx stores win32 resources!!!
+    if (info->bmiHeader.biSize != sizeof(BITMAPCOREHEADER) && 
+        info->bmiHeader.biSize != sizeof(BITMAPINFOHEADER)) 
+    {//assume it contains a file header first
+  	info = (BITMAPINFO *)((char *)info + sizeof(BITMAPFILEHEADER));
+    }
+
+    size = DIB_BitmapInfoSize(info, DIB_RGB_COLORS);
+    if ((hFix = GlobalAlloc(0, size))) fix_info = (BITMAPINFO *)GlobalLock(hFix);
+    if (fix_info) {
+      BYTE pix;
+
+      memcpy(fix_info, info, size);
+      pix = *((LPBYTE)info+DIB_BitmapInfoSize(info, DIB_RGB_COLORS));
+      DIB_FixColorsToLoadflags(fix_info, fuLoad, pix);
+      if ((hdc = GetDC(0)) != 0) {
+        char *bits = (char *)info + size;
+	if (fuLoad & LR_CREATEDIBSECTION) {
+          DIBSECTION dib;
+	  hbitmap = CreateDIBSection(hdc, fix_info, DIB_RGB_COLORS, NULL, 0, 0);
+          GetObjectA(hbitmap, sizeof(DIBSECTION), &dib);
+          SetDIBits(hdc, hbitmap, 0, dib.dsBm.bmHeight, bits, info, 
+                    DIB_RGB_COLORS);
+        }
+        else {
+          hbitmap = CreateDIBitmap( hdc, &fix_info->bmiHeader, CBM_INIT,
+                                      bits, fix_info, DIB_RGB_COLORS );
+	}
+        ReleaseDC( 0, hdc );
+      }
+      GlobalUnlock(hFix);
+      GlobalFree(hFix);
+    }
+    if (fuLoad & LR_LOADFROMFILE) UnmapViewOfFile( ptr );
+    return hbitmap;
+}
+//******************************************************************************
+//TODO: No support for RT_NEWBITMAP
+//******************************************************************************
+HBITMAP WIN32API LoadBitmapA(HINSTANCE hinst, LPCSTR lpszBitmap)
+{
+ HBITMAP hBitmap = 0;
+
+  if(IsSystemBitmap((ULONG *)&lpszBitmap)) {
+	hBitmap = O32_LoadBitmap(hinst, lpszBitmap);
+  }
+  else {
+	hBitmap = LoadBitmapA(hinst, lpszBitmap, 0, 0, 0);
+  }
+  dprintf(("LoadBitmapA returned %08xh\n", hBitmap));
+  
+  return(hBitmap);
+}
+//******************************************************************************
+//TODO: No support for RT_NEWBITMAP
+//******************************************************************************
 HBITMAP WIN32API LoadBitmapW(HINSTANCE hinst, LPCWSTR lpszBitmap)
 {
- char   *astring = NULL;
- HBITMAP rc;
+ HBITMAP hBitmap;
 
-    if(HIWORD(lpszBitmap) != 0) {//convert string name identifier to numeric id
-	 astring = UnicodeToAsciiString((LPWSTR)lpszBitmap);
-         dprintf(("lpszBitmap %s\n", astring));
+  if(IsSystemBitmap((ULONG *)&lpszBitmap)) {
+	hBitmap = O32_LoadBitmap(hinst, (LPCSTR)lpszBitmap);
+  }
+  else {
+    	if(HIWORD(lpszBitmap) != 0) {
+		 lpszBitmap = (LPWSTR)UnicodeToAsciiString((LPWSTR)lpszBitmap);
+    	}
+	hBitmap = LoadBitmapA(hinst, (LPSTR)lpszBitmap, 0, 0, 0);
+  }
 
-	 lpszBitmap = (LPWSTR)ConvertNameId(hinst, (char *)astring);
-    }
-    else dprintf(("lpszBitmap %d\n", (int)lpszBitmap));
+  if(HIWORD(lpszBitmap) != 0)
+	FreeAsciiString((LPSTR)lpszBitmap);
 
-    rc = O32_LoadBitmap(hinst, (char *)lpszBitmap);
-    if(astring)
-	FreeAsciiString(astring);
-
-    dprintf(("LoadBitmapW returned %d\n", rc));
-    return(rc);
+  dprintf(("LoadBitmapW returned %08xh\n", hBitmap));
+  
+  return(hBitmap);
 }
 //******************************************************************************
 //TODO: Far from complete, but works for loading resources from exe
@@ -209,9 +339,21 @@ HANDLE WIN32API LoadImageA(HINSTANCE hinst, LPCSTR lpszName, UINT uType,
 
   dprintf(("LoadImageA NOT COMPLETE (%d,%d)\n", cxDesired, cyDesired));
 
+  if (fuLoad & LR_DEFAULTSIZE) {
+        if (uType == IMAGE_ICON) {
+	    if (!cxDesired) cxDesired = GetSystemMetrics(SM_CXICON);
+	    if (!cyDesired) cyDesired = GetSystemMetrics(SM_CYICON);
+	} 
+	else if (uType == IMAGE_CURSOR) {
+            if (!cxDesired) cxDesired = GetSystemMetrics(SM_CXCURSOR);
+	    if (!cyDesired) cyDesired = GetSystemMetrics(SM_CYCURSOR);
+	}
+  }
+  if (fuLoad & LR_LOADFROMFILE) fuLoad &= ~LR_SHARED;
+
   switch(uType) {
 	case IMAGE_BITMAP:
-		hRet = (HANDLE)LoadBitmapA(hinst, lpszName);
+		hRet = (HANDLE)LoadBitmapA(hinst, lpszName, cxDesired, cyDesired, fuLoad);
 		break;
 	case IMAGE_CURSOR:
 		hRet = (HANDLE)LoadCursorA(hinst, lpszName);
@@ -235,6 +377,18 @@ HANDLE WIN32API LoadImageW(HINSTANCE hinst, LPCWSTR lpszName, UINT uType,
  HANDLE hRet = 0;
 
   dprintf(("LoadImageW NOT COMPLETE (%d,%d)\n", cxDesired, cyDesired));
+
+  if (fuLoad & LR_DEFAULTSIZE) {
+        if (uType == IMAGE_ICON) {
+	    if (!cxDesired) cxDesired = GetSystemMetrics(SM_CXICON);
+	    if (!cyDesired) cyDesired = GetSystemMetrics(SM_CYICON);
+	} 
+	else if (uType == IMAGE_CURSOR) {
+            if (!cxDesired) cxDesired = GetSystemMetrics(SM_CXCURSOR);
+	    if (!cyDesired) cyDesired = GetSystemMetrics(SM_CYCURSOR);
+	}
+  }
+  if (fuLoad & LR_LOADFROMFILE) fuLoad &= ~LR_SHARED;
 
   switch(uType) {
 	case IMAGE_BITMAP:
