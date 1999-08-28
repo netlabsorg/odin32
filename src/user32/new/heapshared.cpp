374cfe68879d08799d281c888297337da7398a82
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-08-28 19:33:04 +0000

    Window handle changes (now allocated in shared array) + postmessage changes + fixes

--- src/user32/new/heapshared.cpp
+++ src/user32/new/heapshared.cpp
@@ -1,6 +1,10 @@
-/* $Id: heapshared.cpp,v 1.1 1999-08-28 14:09:57 sandervl Exp $ */
+/* $Id: heapshared.cpp,v 1.2 1999-08-28 19:32:46 sandervl Exp $ */
 /*
  * Shared heap functions for OS/2
+ *
+ * Initially commit 16 kb, add more when required
+ *
+ * NOTE: Hardcoded limit of 256 KB per process (increase when required)
  *
  * Copyright 1999 Sander van Leeuwen (sandervl@xs4all.nl)
  *
@@ -11,10 +15,9 @@
 #include <misc.h>
 #include "heapshared.h"
 
-#define INITIAL_HEAPSIZE	(64*1024)
-
-Heap_t  sharedHeap = 0;
-PVOID   pSharedMem = NULL;
+       Heap_t  sharedHeap = 0;
+static PVOID   pSharedMem = NULL;
+static BYTE    pageBitmap[MAX_HEAPPAGES] = {0};
 
 void * _LNK_CONV getmoreShared(Heap_t pHeap, size_t *size, int *clean);
 void _LNK_CONV releaseShared(Heap_t pHeap, void *block, size_t size);
@@ -26,18 +29,26 @@ BOOL InitializeSharedHeap()
  APIRET rc;
 
   dprintf(("USER32: InitializeSharedHeap"));
-  rc = DosAllocSharedMem(&pSharedMem, NULL, INITIAL_HEAPSIZE, PAG_READ|PAG_WRITE|PAG_COMMIT|OBJ_GIVEABLE);
+  rc = DosAllocSharedMem(&pSharedMem, NULL, MAX_HEAPSIZE, PAG_READ|PAG_WRITE|OBJ_GETTABLE);
   if(rc != 0) {
 	dprintf(("InitializeSharedHeap: DosAllocSharedMem failed with %d", rc));
 	return FALSE;
   }
-  sharedHeap = _ucreate(pSharedMem, INITIAL_HEAPSIZE, _BLOCK_CLEAN, 
+  rc = DosSetMem(pSharedMem, INCR_HEAPSIZE, PAG_READ|PAG_WRITE|PAG_COMMIT);
+  if(rc != 0) {
+	dprintf(("InitializeSharedHeap: DosSetMem failed with %d", rc));
+	return FALSE;
+  }
+  sharedHeap = _ucreate(pSharedMem, INCR_HEAPSIZE, _BLOCK_CLEAN, 
                         _HEAP_REGULAR|_HEAP_SHARED,
                         getmoreShared, releaseShared);
 
   if(sharedHeap == NULL) {
 	dprintf(("InitializeSharedHeap: _ucreate failed!"));
 	return FALSE;
+  }
+  for(int i=0;i<INCR_HEAPSIZE/PAGE_SIZE;i++) {
+  	pageBitmap[i] = 1; //mark as committed
   }
   return TRUE;
 }
@@ -64,29 +75,71 @@ void  *_smalloc(size_t size)
 }
 //******************************************************************************
 //******************************************************************************
+ULONG GetPageRangeFree(ULONG pageoffset)
+{
+  for(int i=pageoffset;i<MAX_HEAPPAGES;i++) {
+	if(pageBitmap[i] == 1) {
+		break;
+	}
+  }
+  return i-pageoffset;
+}
+//******************************************************************************
+//******************************************************************************
 void * _LNK_CONV getmoreShared(Heap_t pHeap, size_t *size, int *clean)
 {
  APIRET rc;
  ULONG newsize;
  PVOID newblock;
 
-  /* round the size up to a multiple of 64K */
-  *size = (*size / 65536) * 65536 + 65536;
+  /* round the size up to a multiple of 4K */
+  *size = (*size / 4096) * 4096 + 4096;
+  *size = max(*size, INCR_HEAPSIZE);
 
-  rc = DosAllocSharedMem(&newblock, NULL, *size, PAG_READ|PAG_WRITE|PAG_COMMIT|OBJ_GIVEABLE);
-  if(rc != 0) {
-	dprintf(("getmoreShared: DosAllocSharedMem failed with %d", rc));
-	return NULL;
+  for(int i=0;i<MAX_HEAPPAGES;i++) 
+  {
+	int nrpagesfree = GetPageRangeFree(i);
+	if(nrpagesfree >= *size/PAGE_SIZE) {
+  		newblock = (PVOID)((ULONG)pSharedMem + i*PAGE_SIZE);
+  		rc = DosSetMem(newblock, *size, PAG_READ|PAG_WRITE|PAG_COMMIT);
+  		if(rc != 0) {
+			dprintf(("getmoreShared: DosSetMem failed with %d", rc));
+			return NULL;
+  		}
+  		for(int j=i;j<i+*size/PAGE_SIZE;i++) {
+  			pageBitmap[i] = 1; //mark as committed
+  		}
+
+		*clean = _BLOCK_CLEAN;
+		dprintf(("USER32: getmoreShared %x %d", newblock, *size));
+		return newblock;
+	}
+	if(nrpagesfree)
+		i += nrpagesfree-1;
   }
-  *clean = _BLOCK_CLEAN;
-
-  return newblock;
+  dprintf(("USER32: getmoreShared NOTHING LEFT (%d)", *size));
+  return NULL;
 }
 //******************************************************************************
 //******************************************************************************
 void _LNK_CONV releaseShared(Heap_t pHeap, void *block, size_t size)
 {
-  DosFreeMem(block);
+ ULONG pagenr;
+
+  dprintf(("USER32: releaseShared %x %d", block, size));
+  DosSetMem(block, size, PAG_READ|PAG_WRITE|PAG_DECOMMIT);
+
+  pagenr  = (ULONG)block - (ULONG)pSharedMem;
+  pagenr /= PAGE_SIZE;
+  for(int i=pagenr;i<pagenr+size/PAGE_SIZE;i++) {
+  	pageBitmap[i] = 0; //mark as decommitted
+  }
+}
+//******************************************************************************
+//******************************************************************************
+DWORD  HeapGetSharedMemBase()
+{
+  return (DWORD) pSharedMem;
 }
 //******************************************************************************
 //******************************************************************************
--- src/user32/new/heapshared.h
+++ src/user32/new/heapshared.h
@@ -1,4 +1,4 @@
-/* $Id: heapshared.h,v 1.1 1999-08-28 14:09:58 sandervl Exp $ */
+/* $Id: heapshared.h,v 1.2 1999-08-28 19:32:46 sandervl Exp $ */
 /*
  * Shared heap functions for OS/2
  *
@@ -12,10 +12,16 @@
 #include <stdlib.h>
 #include <umalloc.h>
 
+#define PAGE_SIZE		4096
+#define MAX_HEAPSIZE            (256*1024)
+#define MAX_HEAPPAGES		(MAX_HEAPSIZE/PAGE_SIZE)
+#define INCR_HEAPSIZE		(16*1024)
+
 extern Heap_t sharedHeap;
 
 BOOL   InitializeSharedHeap();
 void   DestroySharedHeap();
+DWORD  HeapGetSharedMemBase();
 
 void  *_smalloc(size_t size);
 
