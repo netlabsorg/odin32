c66e72666e214054a8d69012dc9f49a7fe53a36a
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-02 18:54:13 +0000

    Add: credits for us :)

---
+++ include/win/authors.h
@@ -0,0 +1,28 @@
+#ifndef __WINE_AUTHORS_H
+#define __WINE_AUTHORS_H
+
+static const char * const SHELL_People[] =
+{
+    "Christoph Bratschi",
+    "Edgar Buerkle",
+    "Yuri Dario",
+    "Przemyslaw Dobrowolski",
+    "Daniela Engert",
+    "Peter Fitzsimmons",
+    "Patrick Haller",
+    "Achim Hasenmueller",
+    "Sander van Leeuwen",
+    "Felix Maschek",
+    "Chris McKillop",
+    "Markus Montkowski",
+    "Knut Stange Osmundsen",
+    "Rene Pronk",
+    "David Raison",
+    "Joel Troster",
+    "Jens Wiessner",
+    "Vince Vielhaber",
+    "<<< all the WINE people >>>",
+    NULL
+};
+
+#endif  /* __WINE_AUTHORS_H */
