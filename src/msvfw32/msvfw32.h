4b19cc413e43700c4d0c355d80f50b33540926a2
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-04 09:09:03 +0000

    Creation

---
+++ src/msvfw32/msvfw32.h
@@ -0,0 +1,14 @@
+/* $Id: msvfw32.h,v 1.1 1999-09-04 09:08:44 sandervl Exp $ */
+/* Definitions for the MS Video32 DLL (MSVFW32.DLL)
+ *
+ * Copyright 1999 Jens Wiessner
+ */
+
+#ifndef __ODIN_MSVFW32_H
+#define __ODIN_MSVFW32_H
+
+INT         WINAPI lstrncmpiA(LPCSTR,LPCSTR,INT);
+
+#endif /* __ODIN_MSVFW32_H */
+
+
