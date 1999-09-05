11b7a918cd5330f054e1e6d67dcc0e6ceae03f20
dengert <dengert@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-05 16:08:36 +0000

    redefined macro

--- tags/trunk/src/user32/new/open32wbase.h
+++ tags/trunk/src/user32/new/open32wbase.h
@@ -1,4 +1,4 @@
-/* $Id: open32wbase.h,v 1.4 1999-09-05 16:06:21 dengert Exp $ */
+/* $Id: open32wbase.h,v 1.5 1999-09-05 16:08:36 dengert Exp $ */
 /*
  * Open32 Window fake Base Class for OS/2
  *
@@ -14,42 +14,42 @@
 
 extern "C" {
 void _System WinSetDAXData (HWND, PVOID);
-ULONG _System DosBeep (ULONG, ULONG);
+ULONG _System PIEPS (ULONG, ULONG);
 }
 
-#define DosBeep(x,y)
+#define PIEPS(x,y)
 
 #pragma pack(1)
 
 class fakeOpen32WinBaseClass
 {
    public:
-      virtual BOOL      ni08(void) { DosBeep (1000,10); return FALSE; }
-      virtual BOOL      ni10(void) { DosBeep (1100,10); return FALSE; }
-      virtual BOOL      ni18(void) { DosBeep (1200,10); return FALSE; }
-      virtual BOOL      ni20(void) { DosBeep (1300,10); return FALSE; }
-      virtual BOOL      ni28(void) { DosBeep (1400,10); return FALSE; }
-      virtual BOOL      ni30(void) { DosBeep (1500,10); return FALSE; }
-      virtual BOOL      ni38(void) { DosBeep (1600,10); return FALSE; }
-      virtual BOOL      ni40(void) { DosBeep (1700,10); return FALSE; }
-      virtual BOOL      ni48(void) { DosBeep (1800,10); return FALSE; }
-      virtual BOOL      ni50(void) { DosBeep (1900,10); return FALSE; }
-      virtual BOOL      ni58(void) { DosBeep (2000,10); return FALSE; }
-      virtual BOOL      ni60(void) { DosBeep (2100,10); return FALSE; }
-      virtual BOOL      ni68(void) { DosBeep (2200,10); return FALSE; }
-      virtual BOOL      ni70(void) { DosBeep (2300,10); return FALSE; }
-      virtual BOOL      ni78(void) { DosBeep (2400,10); return FALSE; }
-      virtual BOOL      ni80(void) { DosBeep (2500,10); return FALSE; }
-      virtual BOOL      ni88(void) { DosBeep (2600,10); return FALSE; }
+      virtual BOOL      ni08(void) { PIEPS (1000,10); return FALSE; }
+      virtual BOOL      ni10(void) { PIEPS (1100,10); return FALSE; }
+      virtual BOOL      ni18(void) { PIEPS (1200,10); return FALSE; }
+      virtual BOOL      ni20(void) { PIEPS (1300,10); return FALSE; }
+      virtual BOOL      ni28(void) { PIEPS (1400,10); return FALSE; }
+      virtual BOOL      ni30(void) { PIEPS (1500,10); return FALSE; }
+      virtual BOOL      ni38(void) { PIEPS (1600,10); return FALSE; }
+      virtual BOOL      ni40(void) { PIEPS (1700,10); return FALSE; }
+      virtual BOOL      ni48(void) { PIEPS (1800,10); return FALSE; }
+      virtual BOOL      ni50(void) { PIEPS (1900,10); return FALSE; }
+      virtual BOOL      ni58(void) { PIEPS (2000,10); return FALSE; }
+      virtual BOOL      ni60(void) { PIEPS (2100,10); return FALSE; }
+      virtual BOOL      ni68(void) { PIEPS (2200,10); return FALSE; }
+      virtual BOOL      ni70(void) { PIEPS (2300,10); return FALSE; }
+      virtual BOOL      ni78(void) { PIEPS (2400,10); return FALSE; }
+      virtual BOOL      ni80(void) { PIEPS (2500,10); return FALSE; }
+      virtual BOOL      ni88(void) { PIEPS (2600,10); return FALSE; }
       virtual BOOL      clientArea(PRECTL pRectl) { pRectl=pRectl; return FALSE; }
       virtual BOOL      redraw() { return bRedraw; }
       virtual VOID      setRedraw( BOOL bOn = TRUE ) { bRedraw = bOn; }
-      virtual BOOL      niA8(void) { DosBeep (2700,10); return FALSE; }
-      virtual BOOL      niB0(void) { DosBeep (2800,10); return FALSE; }
-      virtual BOOL      niB8(void) { DosBeep (2900,10); return FALSE; }
-      virtual BOOL      niC0(void) { DosBeep (3000,10); return FALSE; }
-      virtual BOOL      niC8(void) { DosBeep (3100,10); return FALSE; }
-      virtual BOOL      niD0(void) { DosBeep (3200,10); return FALSE; }
+      virtual BOOL      niA8(void) { PIEPS (2700,10); return FALSE; }
+      virtual BOOL      niB0(void) { PIEPS (2800,10); return FALSE; }
+      virtual BOOL      niB8(void) { PIEPS (2900,10); return FALSE; }
+      virtual BOOL      niC0(void) { PIEPS (3000,10); return FALSE; }
+      virtual BOOL      niC8(void) { PIEPS (3100,10); return FALSE; }
+      virtual BOOL      niD0(void) { PIEPS (3200,10); return FALSE; }
 
       char              type;
       ULONG             filler1[1];
