c49e5c999bd398f5af9a6ad79dfa69eba7365bfc
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-08 07:30:10 +0000

    New options. Will now handle 'internal' functions correctly.

--- tags/trunk/tools/impdef/ImpDef.h
+++ tags/trunk/tools/impdef/ImpDef.h
@@ -1,4 +1,4 @@
-/* $Id: ImpDef.h,v 1.1 1999-09-05 02:25:48 bird Exp $ */
+/* $Id: ImpDef.h,v 1.2 1999-09-08 07:30:10 bird Exp $ */
 /*
  * ImpDef - Create export file which use internal names and ordinals.
  *
@@ -11,9 +11,16 @@
     {
         BOOL fSimilarToExported;   /* Merges the names of the exported and    *
                                     * internal function when possible/needed. */
+        BOOL ulOrdStartInternalFunctions;
+                                   /* Ordinal number where internal functions *
+                                    * starts. API functions have lower        *
+                                    * ordinal values that this number.        */
         BOOL fRemoveOS2APIPrefix;  /* For API exports any OS2 prefix is       *
                                     * removed (APIs have ordinals less than   *
                                     * ORD_START_INTERNAL_FUNCTIONS).          */
+        BOOL fInternalFnExportStdCallsIntName;
+                                   /* Fix! Export internal names for internal *
+                                    * stdcall functions.                      */
     } OPTIONS, *POPTIONS;
 #endif
 
