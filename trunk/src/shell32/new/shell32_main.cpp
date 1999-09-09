77f77ef1e69eec174cbd109cc714912125f845dc
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-09 15:44:24 +0000

    .

--- trunk/src/shell32/new/shell32_main.cpp
+++ trunk/src/shell32/new/shell32_main.cpp
@@ -301,21 +301,26 @@ DWORD WINAPI SHGetFileInfoW(LPCWSTR path,DWORD dwFileAttributes,
 	return 0;
 }
 
+
 /*************************************************************************
  * ExtractIconA				[SHELL32.133]
  */
 HICON WINAPI ExtractIconA( HINSTANCE hInstance, LPCSTR lpszExeFileName,
 	UINT nIconIndex )
-{   HGLOBAL16 handle = InternalExtractIcon16(hInstance,lpszExeFileName,nIconIndex, 1);
+{
+#if 0
+//@@@PH
+HGLOBAL handle = InternalExtractIconA(hInstance,lpszExeFileName,nIconIndex, 1);
     TRACE_(shell)("\n");
     if( handle )
     {
-	HICON16* ptr = (HICON16*)GlobalLock16(handle);
-	HICON16  hIcon = *ptr;
+	HICON* ptr = (HICON*)GlobalLock(handle);
+	HICON  hIcon = *ptr;
 
-	GlobalFree16(handle);
+	GlobalFree(handle);
 	return hIcon;
     }
+#endif
     return 0;
 }
 
@@ -538,24 +543,25 @@ BOOL WINAPI AboutDlgProc( HWND hWnd, UINT msg, WPARAM wParam,
 
     case WM_LBTRACKPOINT:
 	hWndCtl = GetDlgItem(hWnd, IDC_LISTBOX);
-	if( (INT16)GetKeyState16( VK_CONTROL ) < 0 )
+	if( (INT)GetKeyState( VK_CONTROL ) < 0 )
       { if( DragDetect( hWndCtl, *((LPPOINT)&lParam) ) )
         { INT idx = SendMessageA( hWndCtl, LB_GETCURSEL, 0, 0 );
 		if( idx != -1 )
           { INT length = SendMessageA( hWndCtl, LB_GETTEXTLEN, (WPARAM)idx, 0 );
-		    HGLOBAL16 hMemObj = GlobalAlloc16( GMEM_MOVEABLE, length + 1 );
-		    char* pstr = (char*)GlobalLock16( hMemObj );
+		    HGLOBAL hMemObj = GlobalAlloc( GMEM_MOVEABLE, length + 1 );
+		    char* pstr = (char*)GlobalLock( hMemObj );
 
 		    if( pstr )
-            { HCURSOR16 hCursor = LoadCursor16( 0, MAKEINTRESOURCE16(OCR_DRAGOBJECT) );
+            { HCURSOR hCursor = LoadCursorA( 0, (LPCSTR)OCR_DRAGOBJECT );
 			SendMessageA( hWndCtl, LB_GETTEXT, (WPARAM)idx, (LPARAM)pstr );
 			SendMessageA( hWndCtl, LB_DELETESTRING, (WPARAM)idx, 0 );
 			UpdateWindow( hWndCtl );
-			if( !DragObject16((HWND16)hWnd, (HWND16)hWnd, DRAGOBJ_DATA, 0, (WORD)hMemObj, hCursor) )
+   //@@@PH DragObject16 experimentally replaced by DragObject
+			if( !DragObject(hWnd, hWnd, DRAGOBJ_DATA, hMemObj, hCursor) )
 			    SendMessageA( hWndCtl, LB_ADDSTRING, (WPARAM)-1, (LPARAM)pstr );
 		    }
             if( hMemObj )
-              GlobalFree16( hMemObj );
+              GlobalFree( hMemObj );
 		}
 	    }
 	}
@@ -584,7 +590,7 @@ BOOL WINAPI AboutDlgProc( HWND hWnd, UINT msg, WPARAM wParam,
 	if( wParam == hWnd )
       { LPDRAGINFO lpDragInfo = (LPDRAGINFO)PTR_SEG_TO_LIN((SEGPTR)lParam);
 	    if( lpDragInfo && lpDragInfo->wFlags == DRAGOBJ_DATA && lpDragInfo->hList )
-        { char* pstr = (char*)GlobalLock16( (HGLOBAL16)(lpDragInfo->hList) );
+        { char* pstr = (char*)GlobalLock( (HGLOBAL)(lpDragInfo->hList) );
 		if( pstr )
           { static char __appendix_str[] = " with";
 
