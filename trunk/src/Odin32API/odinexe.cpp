9697922e33dd8e0f1a91e2d50055eef113c99a6d
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-18 17:46:28 +0000

    Creation of source files with entrypoints for Win32 apps compiled in OS/2

---
+++ trunk/src/Odin32API/odinexe.cpp
@@ -0,0 +1,36 @@
+/*
+ * Win32 Odin32 Application executable entrypoint
+ *
+ * Copyright 1999 Sander van Leeuwen (sandervl@xs4all.nl)
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#include <os2.h>
+#include <win32type.h>
+#include <odinlx.h>
+#include <exceptions.h>
+
+//Win32 app entrypoint:
+int WIN32API WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow);
+
+extern "C" {
+ //Win32 resource table (produced by wrc)
+ extern DWORD _Resource_PEResTab;
+}
+//******************************************************************************
+//******************************************************************************
+int main(int argc, char *argv[])
+{
+ WINEXCEPTION_FRAME exceptFrame;
+
+  OS2SetExceptionHandler(&exceptFrame);
+  RegisterLxExe(WinMain, (PVOID)&_Resource_PEResTab);
+  OS2UnsetExceptionHandler(&exceptFrame);
+}
+//******************************************************************************
+//******************************************************************************
