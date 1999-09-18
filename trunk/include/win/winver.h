53c57d89e08be424b751c598a16c325b3746fb0c
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-18 15:55:45 +0000

    Header changes for win32 apps compiled in OS/2

--- trunk/include/win/winver.h
+++ trunk/include/win/winver.h
@@ -1,4 +1,4 @@
-/* $Id: winver.h,v 1.3 1999-08-22 22:54:26 sandervl Exp $ */
+/* $Id: winver.h,v 1.4 1999-09-18 15:55:45 sandervl Exp $ */
 
 #ifndef __WINE_WINVER_H
 #define __WINE_WINVER_H
@@ -13,14 +13,23 @@ extern "C" {
 
 DWORD       WINAPI VerFindFileA(DWORD,LPSTR,LPSTR,LPSTR,LPSTR,PUINT,LPSTR,PUINT);
 DWORD       WINAPI VerFindFileW(DWORD,LPWSTR,LPWSTR,LPWSTR,LPWSTR,PUINT,LPWSTR,PUINT);
+#define     VerFindFile WINELIB_NAME_AW(VerFindFile)
+
 DWORD       WINAPI VerInstallFileA(DWORD,LPSTR,LPSTR,LPSTR,LPSTR,LPSTR,LPSTR,PUINT);
 DWORD       WINAPI VerInstallFileW(DWORD,LPWSTR,LPWSTR,LPWSTR,LPWSTR,LPWSTR,LPWSTR,PUINT);
+#define     VerInstallFile WINELIB_NAME_AW(VerInstallFile)
+
 DWORD       WINAPI VerQueryValueA(LPVOID,LPSTR,LPVOID*,PUINT);
 DWORD       WINAPI VerQueryValueW(LPVOID,LPWSTR,LPVOID*,PUINT);
+#define     VerQueryValue WINELIB_NAME_AW(VerQueryValue)
+
 DWORD       WINAPI GetFileVersionInfoSizeA(LPSTR,LPDWORD);
 DWORD       WINAPI GetFileVersionInfoSizeW(LPWSTR,LPDWORD);
+#define     GetFileVersionInfoSize WINELIB_NAME_AW(GetFileVersionInfoSize)
+
 BOOL        WINAPI GetFileVersionInfoA(LPSTR,DWORD,DWORD,LPVOID);
 BOOL        WINAPI GetFileVersionInfoW(LPWSTR,DWORD,DWORD,LPVOID);
+#define     GetFileVersionInfo WINELIB_NAME_AW(GetFileVersionInfo)
 
 // enable C linkage to avoid parameter mangling
 #ifdef __cplusplus__
