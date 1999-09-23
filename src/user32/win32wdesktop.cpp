cdd81bc4f1f35be66732147c036f3fc75eb6caab
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-23 10:33:59 +0000

    Added desktop window class. Instance created when user32 is loaded

---
+++ src/user32/win32wdesktop.cpp
@@ -0,0 +1,91 @@
+/* $Id: win32wdesktop.cpp,v 1.1 1999-09-23 10:33:59 sandervl Exp $ */
+/*
+ * Win32 Desktop Window for OS/2
+ *
+ *
+ * Copyright 1999 Sander van Leeuwen (sandervl@xs4all.nl)
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#include <os2win.h>
+#include <misc.h>
+#include <win32wbase.h>
+#include <win32wdesktop.h>
+#include "oslibwin.h"
+#include "win32wndhandle.h"
+
+Win32Desktop *windowDesktop = 0;
+
+//******************************************************************************
+//******************************************************************************
+BOOL CreateWin32Desktop()
+{
+  windowDesktop = new Win32Desktop();
+  if(windowDesktop == NULL) {
+	dprintf(("Unable to create desktop window!!!"));
+	return FALSE;
+  }
+  return TRUE;
+}
+//******************************************************************************
+//******************************************************************************
+void DestroyDesktopWindow()
+{
+  if(windowDesktop) {
+	delete windowDesktop;
+	windowDesktop = 0;
+  }
+}
+//******************************************************************************
+//******************************************************************************
+Win32Desktop::Win32Desktop() : Win32BaseWindow(OBJTYPE_WINDOW)
+{
+  OSLibWinQueryWindowRect(OSLIB_HWND_DESKTOP, &rectWindow, RELATIVE_TO_SCREEN);
+  OS2Hwnd      = OSLIB_HWND_DESKTOP;
+  OS2HwndFrame = OSLIB_HWND_DESKTOP;
+  rectClient   = rectWindow;
+
+  /* Find the window class */
+  windowClass = Win32WndClass::FindClass(-1, (LPSTR)DESKTOP_CLASS_ATOM);
+}
+//******************************************************************************
+//******************************************************************************
+Win32Desktop::~Win32Desktop()
+{
+}
+//******************************************************************************
+//Dummy window procedure. Does nothing.
+//******************************************************************************
+LRESULT WIN32API DesktopWndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam )
+{
+    return 0;
+}
+//******************************************************************************
+//******************************************************************************
+BOOL DESKTOP_Register()
+{
+    WNDCLASSA wndClass;
+
+    ZeroMemory(&wndClass,sizeof(WNDCLASSA));
+    wndClass.style         = CS_GLOBALCLASS;
+    wndClass.lpfnWndProc   = (WNDPROC)DesktopWndProc;
+    wndClass.cbClsExtra    = 0;
+    wndClass.cbWndExtra    = 0;
+    wndClass.hCursor       = (HCURSOR)IDC_ARROWA;
+    wndClass.hbrBackground = 0;
+    wndClass.lpszClassName = DESKTOP_CLASS_NAMEA;
+
+    return RegisterClassA(&wndClass);
+}
+//******************************************************************************
+//******************************************************************************
+BOOL DESKTOP_Unregister()
+{
+    if (GlobalFindAtomA(DESKTOP_CLASS_NAMEA))
+            return UnregisterClassA(DESKTOP_CLASS_NAMEA,(HINSTANCE)NULL);
+    else    return FALSE;
+}
+//******************************************************************************
+//******************************************************************************
