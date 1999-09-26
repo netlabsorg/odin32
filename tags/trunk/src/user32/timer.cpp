a0842e432f1dba8cba864dea88a3f13109c68b65
dengert <dengert@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-26 16:09:04 +0000

    SetTimer/KillTimer implemented

---
+++ tags/trunk/src/user32/timer.cpp
@@ -0,0 +1,231 @@
+/* $Id: timer.cpp,v 1.3 1999-09-26 16:09:04 dengert Exp $ */
+
+/*
+ * timer functions for USER32
+ *
+ * Copyright 1993 Alexandre Julliard
+ * Copyright 1999 Daniela Engert (dani@ngrt.de)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+
+#define INCL_WIN
+#define INCL_DOSSEMAPHORES
+#include <os2.h>
+#include <os2sel.h>
+#include <stdlib.h>
+#include "win32type.h"
+#include <winconst.h>
+#include <misc.h>
+#include <win32wbase.h>
+#include "oslibutil.h"
+#include "timer.h"
+
+#ifndef OPEN32API
+#define OPEN32API _System
+#endif
+
+#define WM_TIMER_W 0x0113
+typedef VOID (CALLBACK *TIMERPROC)(HWND hwnd, UINT msg, UINT id, DWORD dwTime);
+
+typedef struct tagTIMER
+{
+    enum {free = 0, UserTimer, SystemTimer} inUse;
+    HWND           hwnd;
+    UINT           id;
+    HWND           PMhwnd;
+    ULONG          PMid;
+    TIMERPROC      proc;
+} TIMER;
+
+#define NB_TIMERS            34
+#define NB_RESERVED_TIMERS    2  /* for SetSystemTimer */
+
+#define SYS_TIMER_RATE  54925
+
+static TIMER TimersArray[NB_TIMERS];
+
+HMTX hSemTimer;
+
+inline void EnterCriticalSection (void)
+{
+   if (hSemTimer == NULLHANDLE)
+      DosCreateMutexSem (NULL, &hSemTimer, 0L, 1);
+   else
+      DosRequestMutexSem (hSemTimer, SEM_INDEFINITE_WAIT);
+}
+
+inline void LeaveCriticalSection (void)
+{
+   DosReleaseMutexSem (hSemTimer);
+}
+
+BOOL TIMER_HandleTimer (PQMSG pMsg)
+{
+    int i;
+    TIMER *pTimer;
+    HWND  PMhwnd = pMsg->hwnd;
+    ULONG PMid   = (ULONG)(pMsg->mp1);
+
+    for (i = 0, pTimer = TimersArray; i < NB_TIMERS; i++, pTimer++)
+        if (pTimer->inUse && (pTimer->PMhwnd == PMhwnd) && (pTimer->PMid == PMid))
+            break;
+
+    if (i == NB_TIMERS)  /* no matching timer found */
+        return (TRUE);   /* eat message */
+
+    pMsg->mp1 = MPFROMLONG (pTimer->id);
+    if (!pTimer->proc)
+        return (FALSE);  /* forward message */
+
+    if (!WinInSendMsg (GetThreadHAB()))
+        pTimer->proc (pTimer->hwnd, WM_TIMER_W, pTimer->id, pMsg->time);
+
+    return (TRUE);
+}
+
+static UINT TIMER_SetTimer (HWND hwnd, UINT id, UINT timeout, TIMERPROC proc, BOOL sys)
+{
+    int i;
+    TIMER *pTimer;
+    Win32BaseWindow *wnd = Win32BaseWindow::GetWindowFromHandle(hwnd);
+
+    if (hwnd && !wnd) return 0;
+
+    EnterCriticalSection ();
+
+    /* Check if there's already a timer with the same hwnd and id */
+
+    for (i = 0, pTimer = TimersArray; i < NB_TIMERS; i++, pTimer++)
+        if (pTimer->inUse && (pTimer->hwnd == hwnd) && (pTimer->id == id))
+            break;
+
+    if (i == NB_TIMERS)  /* no matching timer found */
+    {
+        /* Find a free timer */
+
+        for (i = 0, pTimer = TimersArray; i < NB_TIMERS; i++, pTimer++)
+            if (!pTimer->inUse) break;
+
+        if ((i >= NB_TIMERS) ||
+            (!sys && (i >= NB_TIMERS-NB_RESERVED_TIMERS)))
+        {
+            LeaveCriticalSection();
+            return 0;
+        }
+
+        if (!hwnd) id = i + 1;
+
+        /* Add the timer */
+
+        pTimer->inUse   = sys ? TIMER::SystemTimer : TIMER::UserTimer;
+        pTimer->hwnd    = hwnd;
+        pTimer->id      = id;
+        pTimer->proc    = proc;
+        pTimer->PMhwnd  = hwnd ? wnd->getOS2WindowHandle() : NULLHANDLE;
+        pTimer->PMid    = WinStartTimer (GetThreadHAB(), pTimer->PMhwnd,
+                                         i + 1, timeout);
+
+        if (!pTimer->PMid) id = pTimer->id = 0;
+    } else {
+        WinStartTimer (GetThreadHAB(), pTimer->PMhwnd, pTimer->PMid, timeout);
+    }
+
+    LeaveCriticalSection();
+
+    if (!id) return TRUE;
+    else return id;
+}
+
+static BOOL TIMER_KillTimer (HWND hwnd, UINT id, BOOL sys)
+{
+    int i;
+    TIMER * pTimer;
+
+    EnterCriticalSection();
+
+    /* Find the timer */
+
+    for (i = 0, pTimer = TimersArray; i < NB_TIMERS; i++, pTimer++)
+        if (pTimer->inUse &&
+            (pTimer->hwnd == hwnd) && (pTimer->id == id)) break;
+
+    if ((i >= NB_TIMERS) ||
+        (!sys && (i >= NB_TIMERS-NB_RESERVED_TIMERS)) ||
+        (!sys && (pTimer->inUse != TIMER::UserTimer)) ||
+        (sys && (pTimer->inUse != TIMER::SystemTimer)) )
+    {
+        LeaveCriticalSection();
+        return FALSE;
+    }
+
+    /* Delete the timer */
+
+    WinStopTimer (GetThreadHAB(), pTimer->PMhwnd, pTimer->PMid);
+
+    LeaveCriticalSection();
+
+    return TRUE;
+}
+
+/***********************************************************************
+ *           SetTimer32   (USER32.511)
+ */
+UINT WIN32API SetTimer (HWND hwnd, UINT id, UINT timeout, TIMERPROC proc)
+{
+    UINT rc;
+    USHORT sel = RestoreOS2FS();
+
+    dprintf(("USER32: SetTimer %04x %d %d %08lx", hwnd, id, timeout, (LONG)proc));
+
+    rc = TIMER_SetTimer (hwnd, id, timeout, proc, FALSE);
+    SetFS(sel);
+    return (rc);
+}
+
+/***********************************************************************
+ *           SetSystemTimer32   (USER32.509)
+ */
+UINT WIN32API SetSystemTimer (HWND hwnd, UINT id, UINT timeout, TIMERPROC proc)
+{
+    UINT rc;
+    USHORT sel = RestoreOS2FS();
+
+    dprintf(("USER32: SetSystemTimer %04x %d %d %08lx", hwnd, id, timeout, (LONG)proc));
+
+    rc = TIMER_SetTimer (hwnd, id, timeout, proc, TRUE);
+    SetFS(sel);
+    return (rc);
+}
+
+/***********************************************************************
+ *           KillTimer32   (USER32.354)
+ */
+BOOL WIN32API KillTimer (HWND hwnd, UINT id)
+{
+    BOOL rc;
+    USHORT sel = RestoreOS2FS();
+
+    dprintf(("USER32: KillTimer %04x %d", hwnd, id));
+
+    rc = TIMER_KillTimer (hwnd, id, FALSE);
+    SetFS(sel);
+    return (rc);
+}
+
+/***********************************************************************
+ *           KillSystemTimer32   (USER32.353)
+ */
+BOOL WIN32API KillSystemTimer (HWND hwnd, UINT id)
+{
+    BOOL rc;
+    USHORT sel = RestoreOS2FS();
+
+    dprintf(("USER32: KillSystemTimer %04x %d", hwnd, id));
+
+    rc = TIMER_KillTimer (hwnd, id, TRUE);
+    SetFS(sel);
+    return (rc);
+}
+
---
+++ tags/trunk/src/user32/timer.h
@@ -0,0 +1,18 @@
+/* $Id: timer.h,v 1.1 1999-09-26 16:09:04 dengert Exp $ */
+/*
+ * public timer functions
+ *
+ *
+ * Copyright 1999      Daniela Engert (dani@ngrt.de)
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+extern BOOL TIMER_HandleTimer (PQMSG pMsg);
+
+#endif //__TIMER_H__
+
