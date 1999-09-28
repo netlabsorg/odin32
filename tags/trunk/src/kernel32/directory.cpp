219abe9fd74065e5b299f51e12fff90079e88786
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-09-28 01:18:45 +0000

    Fix: source cleanup, introduction ov WIN32.DIR.SYSTEM and WIN32.DIR.WINDOWS as overrrides

--- tags/trunk/src/kernel32/directory.cpp
+++ tags/trunk/src/kernel32/directory.cpp
@@ -1,4 +1,4 @@
-/* $Id: directory.cpp,v 1.4 1999-06-10 20:47:58 phaller Exp $ */
+/* $Id: directory.cpp,v 1.5 1999-09-28 01:17:01 phaller Exp $ */
 
 /*
  * Win32 Directory functions for OS/2
@@ -9,131 +9,303 @@
  * Project Odin Software License can be found in LICENSE.TXT
  *
  */
+
+
+/*****************************************************************************
+ * Includes                                                                  *
+ *****************************************************************************/
+
+#include <odin.h>
+#include <odinwrap.h>
 #include <os2win.h>
 #include <stdlib.h>
-#include "unicode.h"
+#include <unicode.h>
+#include <heapstring.h>
 
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetCurrentDirectoryA(UINT arg1, LPSTR arg2)
-{
-    dprintf(("KERNEL32:  GetCurrentDirectory\n"));
-    return O32_GetCurrentDirectory(arg1, arg2);
-}
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetCurrentDirectoryW(UINT nBufferLength, LPWSTR lpBuffer)
-{
-    char *asciidir = (char *)malloc(nBufferLength+1);
-     int  rc;
 
-    dprintf(("KERNEL32:  OS2GetCurrentDirectoryW\n"));
+ODINDEBUGCHANNEL(KERNEL32-DIRECTORY)
 
-    rc = O32_GetCurrentDirectory(nBufferLength, asciidir);
-    if(rc != 0) {
-        AsciiToUnicode(asciidir, lpBuffer);
-    }
-    free(asciidir);
-    return(rc);
-}
-//******************************************************************************
 
-//******************************************************************************
-BOOL WIN32API SetCurrentDirectoryA(LPCSTR arg1)
-{
-    dprintf(("KERNEL32:  SetCurrentDirectory to %s\n", arg1));
-    return O32_SetCurrentDirectory((LPSTR)arg1);
-}
-//******************************************************************************
-//******************************************************************************
-BOOL WIN32API SetCurrentDirectoryW(LPCWSTR lpPathName)
-{
- char *asciipath;
- BOOL  rc;
+/*****************************************************************************
+ * Name      : GetCurrentDirectoryA
+ * Purpose   : query the current directory
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
 
-    dprintf(("KERNEL32:  OS2SetCurrentDirectoryW\n"));
-    asciipath = UnicodeToAsciiString((LPWSTR)lpPathName);
-    rc = O32_SetCurrentDirectory(asciipath);
-    FreeAsciiString(asciipath);
-    return(rc);
-}
-//******************************************************************************
-//******************************************************************************
-BOOL WIN32API CreateDirectoryA( LPCSTR arg1, PSECURITY_ATTRIBUTES  arg2)
+ODINFUNCTION2(UINT, GetCurrentDirectoryA, UINT,  nBufferLength,
+                                          LPSTR, lpBuffer)
 {
-    dprintf(("KERNEL32:  OS2CreateDirectoryA %s\n", arg1));
-    return O32_CreateDirectory(arg1, arg2);
+  return O32_GetCurrentDirectory(nBufferLength, lpBuffer);
 }
-//******************************************************************************
-//******************************************************************************
-BOOL WIN32API CreateDirectoryW(LPCWSTR arg1, PSECURITY_ATTRIBUTES arg2)
-{
- BOOL  rc;
- char *astring;
 
-    dprintf(("KERNEL32:  OS2CreateDirectoryW"));
-    astring = UnicodeToAsciiString((LPWSTR)arg1);
-    rc = O32_CreateDirectory(astring, arg2);
-    FreeAsciiString(astring);
-    return(rc);
-}
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetSystemDirectoryA(LPSTR arg1, UINT arg2)
+
+/*****************************************************************************
+ * Name      : GetCurrentDirectoryW
+ * Purpose   : query the current directory
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(UINT, GetCurrentDirectoryW, UINT,   nBufferLength,
+                                          LPWSTR, lpBuffer)
 {
-    dprintf(("KERNEL32:  GetSystemDirectory\n"));
+  char *asciidir = (char *)malloc(nBufferLength+1);
+  int  rc;
+
+  rc = O32_GetCurrentDirectory(nBufferLength, asciidir);
+  if(rc != 0)
+    AsciiToUnicode(asciidir, lpBuffer);
+  free(asciidir);
+  return(rc);
+}
+
+
+/*****************************************************************************
+ * Name      : SetCurrentDirectoryA
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION1(BOOL,SetCurrentDirectoryA,LPCSTR,lpPathName)
+{
+  return O32_SetCurrentDirectory((LPSTR)lpPathName);
+}
+
+
+/*****************************************************************************
+ * Name      : SetCurrentDirectoryW
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION1(BOOL,SetCurrentDirectoryW,LPCWSTR,lpPathName)
+{
+  char *asciipath;
+  BOOL  rc;
+
+  asciipath = UnicodeToAsciiString((LPWSTR)lpPathName);
+  rc = SetCurrentDirectoryA(asciipath);
+  FreeAsciiString(asciipath);
+  return(rc);
+}
+
+
+/*****************************************************************************
+ * Name      : CreateDirectoryA
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(BOOL,CreateDirectoryA,LPCSTR,              arg1,
+                                    PSECURITY_ATTRIBUTES,arg2)
+{
+  return O32_CreateDirectory(arg1, arg2);
+}
+
+
+/*****************************************************************************
+ * Name      : CreateDirectoryW
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(BOOL,CreateDirectoryW,LPCWSTR,             arg1,
+                                    PSECURITY_ATTRIBUTES,arg2)
+{
+  BOOL  rc;
+  char *astring;
+
+  astring = UnicodeToAsciiString((LPWSTR)arg1);
+  rc = CreateDirectoryA(astring, arg2);
+  FreeAsciiString(astring);
+  return(rc);
+}
+
+
+/*****************************************************************************
+ * Name      : GetSystemDirectoryA
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(UINT,GetSystemDirectoryA,LPSTR,arg1,
+                                       UINT, arg2)
+{
+  LPSTR lpstrEnv = getenv("WIN32.DIR.SYSTEM");          /* query environment */
+
+  if (lpstrEnv != NULL)
+  {
+    lstrcpynA(arg1,                   /* copy environment variable to buffer */
+              lpstrEnv,
+              arg2);
+    return (lstrlenA(arg1));                /* return number of copies bytes */
+  }
+  else
+                               /* if no override by environment is available */
     return O32_GetSystemDirectory(arg1, arg2);
 }
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetSystemDirectoryW(LPWSTR lpBuffer, UINT uSize)
-{
- char *asciibuffer = (char *)malloc(uSize+1);
- UINT  rc;
 
-    dprintf(("KERNEL32:  OS2GetSystemDirectoryW\n"));
-    rc = O32_GetSystemDirectory(asciibuffer, uSize);
-    if(rc)      AsciiToUnicode(asciibuffer, lpBuffer);
-    free(asciibuffer);
-    return(rc);
-}
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetWindowsDirectoryA( LPSTR arg1, UINT  arg2)
-{
-    dprintf(("KERNEL32:  GetWindowsDirectory\n"));
-    return O32_GetWindowsDirectory(arg1, arg2);
-}
-//******************************************************************************
-//******************************************************************************
-UINT WIN32API GetWindowsDirectoryW(LPWSTR lpBuffer, UINT uSize)
-{
- char *asciibuffer = (char *)malloc(uSize+1);
- UINT  rc;
 
-    dprintf(("KERNEL32:  OS2GetWindowsDirectoryW"));
-    rc = O32_GetWindowsDirectory(asciibuffer, uSize);
-    AsciiToUnicode(asciibuffer, lpBuffer);
-    free(asciibuffer);
-    return(rc);
-}
-//******************************************************************************
-//******************************************************************************
-BOOL WIN32API RemoveDirectoryA( LPCSTR arg1)
-{
-    dprintf(("KERNEL32:  OS2RemoveDirectoryA\n"));
-    return O32_RemoveDirectory(arg1);
-}
-//******************************************************************************
-//******************************************************************************
-BOOL WIN32API RemoveDirectoryW(LPCWSTR lpPathName)
-{
- char *asciipath;
- BOOL  rc;
+/*****************************************************************************
+ * Name      : GetSystemDirectoryW
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
 
-    dprintf(("KERNEL32:  OS2RemoveDirectoryW"));
-    asciipath = UnicodeToAsciiString((LPWSTR)lpPathName);
-    rc = O32_RemoveDirectory(asciipath);
-    FreeAsciiString(asciipath);
-    return(rc);
+ODINFUNCTION2(UINT,GetSystemDirectoryW,LPWSTR,lpBuffer,
+                                       UINT,  uSize)
+{
+  char *asciibuffer = (char *)malloc(uSize+1);
+  UINT  rc;
+
+  rc = GetSystemDirectoryA(asciibuffer, uSize);
+  if(rc) AsciiToUnicode(asciibuffer, lpBuffer);
+  free(asciibuffer);
+  return(rc);
 }
+
+
+/*****************************************************************************
+ * Name      : GetWindowsDirectoryA
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(UINT,GetWindowsDirectoryA,LPSTR,arg1,
+                                        UINT, arg2)
+{
+  LPSTR lpstrEnv = getenv("WIN32.DIR.WINDOWS");         /* query environment */
+
+  if (lpstrEnv != NULL)
+  {
+    lstrcpynA(arg1,                   /* copy environment variable to buffer */
+              lpstrEnv,
+              arg2);
+    return (lstrlenA(arg1));                /* return number of copies bytes */
+  }
+  else
+                               /* if no override by environment is available */
+    return O32_GetWindowsDirectory(arg1,
+                                   arg2);
+}
+
+
+/*****************************************************************************
+ * Name      : GetWindowsDirectoryW
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION2(UINT,GetWindowsDirectoryW,LPWSTR,lpBuffer,
+                                        UINT,  uSize)
+{
+  char *asciibuffer = (char *)malloc(uSize+1);
+  UINT  rc;
+
+  rc = GetWindowsDirectoryA(asciibuffer, uSize);
+  AsciiToUnicode(asciibuffer, lpBuffer);
+  free(asciibuffer);
+  return(rc);
+}
+
+
+/*****************************************************************************
+ * Name      : RemoveDirectoryA
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION1(BOOL,RemoveDirectoryA,LPCSTR,arg1)
+{
+  return O32_RemoveDirectory(arg1);
+}
+
+
+/*****************************************************************************
+ * Name      : RemoveDirectoryW
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    :
+ *
+ * Author    : Patrick Haller [Wed, 1999/09/28 20:44]
+ *****************************************************************************/
+
+ODINFUNCTION1(BOOL,RemoveDirectoryW,LPCWSTR,lpPathName)
+{
+  char *asciipath;
+  BOOL  rc;
+
+  asciipath = UnicodeToAsciiString((LPWSTR)lpPathName);
+  rc = RemoveDirectoryA(asciipath);
+  FreeAsciiString(asciipath);
+  return(rc);
+}
+
