efac0fddc8dc761c1a8e54e7adc8f68d4f256c31
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-01 16:02:32 +0000

    Added stubs + forwarders

--- tags/trunk/src/msvcrt/msvcrt.cpp
+++ tags/trunk/src/msvcrt/msvcrt.cpp
@@ -1,4 +1,4 @@
-/* $Id: msvcrt.cpp,v 1.1 1999-09-28 13:26:06 sandervl Exp $ */
+/* $Id: msvcrt.cpp,v 1.2 1999-10-01 16:02:32 sandervl Exp $ */
 
 /*
  * The Visual C RunTime DLL
@@ -10,6 +10,7 @@
 
 
 #include <os2win.h>
+#include <wchar.h>
 #include "debugtools.h"
 #include <debugdefs.h>
 
@@ -60,6 +61,156 @@ INT CDECL MSVCRT__EH_prolog(DWORD ret)
 INT CDECL MSVCRT___CxxFrameHandler(DWORD ret)
 {
   dprintf(("MSVCRT: __CxxFrameHandler not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _getws    (MSVCRT.261)
+ */
+wchar_t * CDECL MSVCRT__getws( wchar_t *s )
+{
+  dprintf(("MSVCRT: _getws not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _ismbbkprint    (MSVCRT.284)
+ */
+int CDECL MSVCRT__ismbbkprint( unsigned int ch )
+{
+  dprintf(("MSVCRT: _ismbbkprint not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _ismbcalnum    (MSVCRT.290)
+ */
+int CDECL MSVCRT__ismbcalnum( unsigned int ch )
+{
+  dprintf(("MSVCRT: _ismbcalnum not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _ismbcgraph    (MSVCRT.293)
+ */
+int CDECL MSVCRT__ismbcgraph( unsigned int ch )
+{
+  dprintf(("MSVCRT: _ismbcgraph not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _ismbcpunct    (MSVCRT.302)
+ */
+int CDECL MSVCRT__ismbcpunct( unsigned int ch )
+{
+  dprintf(("MSVCRT: _ismbcpunct not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _putws    (MSVCRT.407)
+ */
+int CDECL MSVCRT__putws( const wchar_t *s )
+{
+  dprintf(("MSVCRT: _putws not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _strncoll    (MSVCRT.453)
+ */
+int CDECL MSVCRT__strncoll( const char *s1, const char *s2, size_t n )
+{
+  dprintf(("MSVCRT: _strncoll not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  _strnicoll    (MSVCRT.455)
+ */
+int CDECL MSVCRT__strnicoll( const char *s1, const char *s2, size_t n )
+{
+  dprintf(("MSVCRT: _strnicoll not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  fgetws    (MSVCRT.597)
+ */
+wchar_t * CDECL MSVCRT_fgetws( wchar_t *s, int n, FILE *strm )
+{
+  dprintf(("MSVCRT: fgetws\n"));
+  return (fgetws(s, n, strm));
+}
+
+
+/*********************************************************************
+ *                  fputws    (MSVCRT.605)
+ */
+int CDECL MSVCRT_fputws( const wchar_t *s, FILE *strm )
+{
+  dprintf(("MSVCRT: fputws\n"));
+  return (fputws(s, strm));
+}
+
+
+/*********************************************************************
+ *                  getwc    (MSVCRT.621)
+ */
+wint_t CDECL MSVCRT_getwc( FILE * strm)
+{
+  dprintf(("MSVCRT: getwc\n"));
+  return (getwc(strm));
+}
+
+
+/*********************************************************************
+ *                  getwchar    (MSVCRT.622)
+ */
+wint_t CDECL MSVCRT_getwchar( void )
+{
+  dprintf(("MSVCRT: getwchar\n"));
+  return (getwchar());
+}
+
+
+/*********************************************************************
+ *                  putwc    (MSVCRT.675)
+ */
+wint_t CDECL MSVCRT_putwc( wint_t t, FILE * strm)
+{
+  dprintf(("MSVCRT: putwc not implemented.\n"));
+  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+  return FALSE;
+}
+
+
+/*********************************************************************
+ *                  putwchar    (MSVCRT.676)
+ */
+wint_t CDECL MSVCRT_putwchar( wint_t t)
+{
+  dprintf(("MSVCRT: putwchar not implemented.\n"));
   SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
   return FALSE;
 }
