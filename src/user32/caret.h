e2a68990ab2ec515109a753777d8ee34584de900
dengert <dengert@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-06 10:36:39 +0000

    adjust caret pos on window height change

--- src/user32/caret.h
+++ src/user32/caret.h
@@ -1,4 +1,4 @@
-/* $Id: caret.h,v 1.1 1999-09-28 08:00:56 dengert Exp $ */
+/* $Id: caret.h,v 1.2 1999-10-06 10:36:39 dengert Exp $ */
 
 /*
  * Caret functions for USER32
@@ -8,4 +8,6 @@
  */
 
 extern void recreateCaret (HWND hwndFocus);
+extern BOOL WIN32API SetCaretPos (int x, int y);
+extern BOOL WIN32API GetCaretPos (PPOINT pPoint);
 
