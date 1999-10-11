70e11767541314a3f42809465178a7602e77ac54
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-11 16:04:52 +0000

    pmframe finished

--- src/user32/pmframe.h
+++ src/user32/pmframe.h
@@ -1,4 +1,4 @@
-/* $Id: pmframe.h,v 1.1 1999-10-08 16:13:08 cbratschi Exp $ */
+/* $Id: pmframe.h,v 1.2 1999-10-11 16:04:51 cbratschi Exp $ */
 /*
  * Win32 Frame Managment Code for OS/2
  *
@@ -9,6 +9,7 @@
 #define __PMFRAME_H__
 
 PVOID FrameSubclassFrameWindow(Win32BaseWindow *win32wnd);
-VOID  FrameSetBorderSize(Win32BaseWindow *win32wnd);
+VOID  FrameSetBorderSize(Win32BaseWindow *win32wnd,BOOL resize);
+UINT  FrameGetDefSizeBorderSize(VOID);
 
 #endif
