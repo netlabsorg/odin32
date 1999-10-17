a1d91e1461328ca4ebaa17adda6c25f26862d2e0
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-17 15:28:44 +0000

    *** empty log message ***

--- include/win/winpos.h
+++ include/win/winpos.h
@@ -1,4 +1,4 @@
-/* $Id: winpos.h,v 1.1 1999-05-24 20:19:22 ktk Exp $ */
+/* $Id: winpos.h,v 1.2 1999-10-17 15:26:12 sandervl Exp $ */
 
 /*
  * *DeferWindowPos() structure and definitions
@@ -9,8 +9,10 @@
 #ifndef __WINE_WINPOS_H
 #define __WINE_WINPOS_H
 
+#ifndef __WIN32OS2__
 #include "win.h"
 #include "wine/winuser16.h" /* for WINDOWPOS16 */
+#endif //__WIN32OS2__
 
 #define DWP_MAGIC  ((INT)('W' | ('P' << 8) | ('O' << 16) | ('S' << 24)))
 
@@ -27,25 +29,5 @@ typedef struct
     HWND      hwndParent;
     WINDOWPOS winPos[1];
 } DWP;
-
-extern BOOL WINPOS_RedrawIconTitle( HWND hWnd );
-extern BOOL WINPOS_ShowIconTitle( WND* pWnd, BOOL bShow );
-extern void   WINPOS_GetMinMaxInfo( WND* pWnd, POINT *maxSize,
-                                    POINT *maxPos, POINT *minTrack,
-                                    POINT *maxTrack );
-extern UINT WINPOS_MinMaximize( WND* pWnd, UINT16 cmd, LPRECT16 lpPos);
-extern BOOL WINPOS_SetActiveWindow( HWND hWnd, BOOL fMouse,
-                                      BOOL fChangeFocus );
-extern BOOL WINPOS_ChangeActiveWindow( HWND hwnd, BOOL mouseMsg );
-extern LONG WINPOS_SendNCCalcSize(HWND hwnd, BOOL calcValidRect,
-                                  RECT *newWindowRect, RECT *oldWindowRect,
-                                  RECT *oldClientRect, WINDOWPOS *winpos,
-                                  RECT *newClientRect );
-extern LONG WINPOS_HandleWindowPosChanging16(WND *wndPtr, WINDOWPOS16 *winpos);
-extern LONG WINPOS_HandleWindowPosChanging(WND *wndPtr, WINDOWPOS *winpos);
-extern INT16 WINPOS_WindowFromPoint( WND* scopeWnd, POINT16 pt, WND **ppWnd );
-extern void WINPOS_CheckInternalPos( WND* wndPtr );
-extern BOOL WINPOS_ActivateOtherWindow(WND* pWnd);
-extern BOOL WINPOS_CreateInternalPosAtom(void);
 
 #endif  /* __WINE_WINPOS_H */
