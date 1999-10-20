1f2fcf3203debac733ae9f89535ccf2122afdaa0
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-20 06:38:10 +0000

    compile fixes

---
+++ trunk/src/user32/controls.h
@@ -0,0 +1,36 @@
+/* $Id: controls.h,v 1.1 1999-10-20 06:38:08 sandervl Exp $ */
+/*
+ * Win32 common controls
+ *
+ * Copyright (c) 1999 Christoph Bratschi
+ *
+ */
+
+#ifndef __CONTROLS_H
+#define __CONTROLS_H
+
+#include "win32wbase.h"
+
+#define GetInfoPtr(hwnd) (GetWindowLongA(hwnd,0))
+#define SetInfoPtr(hwnd,data) \
+  SetWindowLongA(hwnd,0,data)
+
+void CONTROLS_Register();
+void CONTROLS_Unregister();
+
+#define BUTTON_CONTROL  	0
+#define STATIC_CONTROL  	1
+#define SCROLLBAR_CONTROL	2
+#define LISTBOX_CONTROL		3
+#define COMBOLBOX_CONTROL	4
+#define COMBOBOX_CONTROL	5
+#define EDIT_CONTROL		6
+#define MDICLIENT_CONTROL	7
+#define DIALOG_CONTROL		8
+#define DESKTOP_CONTROL 	9
+#define MAX_CONTROLS 		10
+
+BOOL WIDGETS_IsControl(Win32BaseWindow *window, int control);
+
+
+#endif // __CONTROLS_H
