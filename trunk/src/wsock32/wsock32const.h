3d5b6a9a1ff7e5756d64fd4676c31367727df20c
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-25 23:17:20 +0000

    Fix: invalid structure alignment for asynchronous operations

--- trunk/src/wsock32/wsock32const.h
+++ trunk/src/wsock32/wsock32const.h
@@ -1,4 +1,4 @@
-/* $Id: wsock32const.h,v 1.2 1999-10-20 20:11:03 phaller Exp $ */
+/* $Id: wsock32const.h,v 1.3 1999-10-25 23:17:20 phaller Exp $ */
 
 /* WSOCK32.H--definitions & conversions for Odin's wsock32.dll.
  * Unused/unneeded Microsoft declarations removed.
@@ -172,6 +172,47 @@
  * to a WSAAsyncSelect().
  */
 #define OS2WSAGETSELECTERROR(lParam)           HIWORD(lParam)
+
+
+/*
+ * Structures returned by network data base library, taken from the
+ * BSD file netdb.h.  All addresses are supplied in host order, and
+ * returned in network order (suitable for use in system calls).
+ */
+
+struct  Whostent {
+        char     * h_name;           /* official name of host */
+        char     *  * h_aliases;  /* alias list */
+        short   h_addrtype;             /* host address type */
+        short   h_length;               /* length of address */
+        char     *  * h_addr_list; /* list of addresses */
+#define h_addr  h_addr_list[0]          /* address, for backward compat */
+};
+
+/*
+ * It is assumed here that a network number
+ * fits in 32 bits.
+ */
+struct  Wnetent {
+        char     * n_name;           /* official name of net */
+        char     *  * n_aliases;  /* alias list */
+        short   n_addrtype;             /* net address type */
+        u_long  n_net;                  /* network # */
+};
+
+struct  Wservent {
+        char     * s_name;           /* official service name */
+        char     *  * s_aliases;  /* alias list */
+        short   s_port;                 /* port # */
+        char     * s_proto;          /* protocol to use */
+};
+
+struct  Wprotoent {
+        char     * p_name;           /* official protocol name */
+        char     *  * p_aliases;  /* alias list */
+        short   p_proto;                /* protocol # */
+};
+
 
 #endif  /* _WINSOCK32CONST_ */
 
