0d804081e3b016624a856f54501e66dcd3deeab3
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-10-27 09:33:48 +0000

    Fix: debug info

--- trunk/src/shell32/resource.asm
+++ trunk/src/shell32/resource.asm
@@ -1,7 +1,7 @@
 ;/* This file is generated with wrc version 1.0.14 (08-Aug-1999). Do not edit! */
 ;/* Source : shres.rc */
 ;/* Cmdline: wrc -s -I. -ID:\IBMCPP\include -I..\..\include -I..\..\include\win -o resource.asm shres.rc */
-;/* Date   : Tue Oct 19 20:15:43 1999 */
+;/* Date   : Mon Oct 25 10:23:09 1999 */
 
 	.386p
 	.model flat
@@ -11,7 +11,7 @@
 _Resource_PEResTab:
 	public	_Resource_PEResTab
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 6
 	dd	3
@@ -28,7 +28,7 @@ _Resource_PEResTab:
 	dd	(L16 - _Resource_PEResTab) or 80000000h
 L3:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 16
 	dd	1
@@ -65,7 +65,7 @@ L3:
 	dd	(L3_16 - _Resource_PEResTab) or 80000000h
 L4:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	3, 0
 	dd	(_Resource_4_MENU_001_name - _Resource_PEResTab) or 80000000h
@@ -76,7 +76,7 @@ L4:
 	dd	(L4_MENU_SHV_FILE - _Resource_PEResTab) or 80000000h
 L5:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	2, 0
 	dd	(_Resource_5_SHBRSFORFOLDER_MSGBOX_name - _Resource_PEResTab) or 80000000h
@@ -85,7 +85,7 @@ L5:
 	dd	(L5_SHELL_ABOUT_MSGBOX - _Resource_PEResTab) or 80000000h
 L6:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 2
 	dd	1
@@ -94,7 +94,7 @@ L6:
 	dd	(L6_2 - _Resource_PEResTab) or 80000000h
 L14:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 5
 	dd	0
@@ -109,210 +109,210 @@ L14:
 	dd	(L14_34 - _Resource_PEResTab) or 80000000h
 L16:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	1
 	dd	(L16_1 - _Resource_PEResTab) or 80000000h
 L3_1:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_1_0 - _Resource_PEResTab
 L3_2:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_2_0 - _Resource_PEResTab
 L3_3:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_3_0 - _Resource_PEResTab
 L3_4:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_4_0 - _Resource_PEResTab
 L3_5:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_5_0 - _Resource_PEResTab
 L3_6:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_6_0 - _Resource_PEResTab
 L3_7:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_7_0 - _Resource_PEResTab
 L3_8:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_8_0 - _Resource_PEResTab
 L3_9:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_9_0 - _Resource_PEResTab
 L3_10:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_10_0 - _Resource_PEResTab
 L3_11:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_11_0 - _Resource_PEResTab
 L3_12:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_12_0 - _Resource_PEResTab
 L3_13:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_13_0 - _Resource_PEResTab
 L3_14:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_14_0 - _Resource_PEResTab
 L3_15:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_15_0 - _Resource_PEResTab
 L3_16:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L3_16_0 - _Resource_PEResTab
 L4_MENU_001:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L4_MENU_001_0 - _Resource_PEResTab
 L4_MENU_002:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L4_MENU_002_0 - _Resource_PEResTab
 L4_MENU_SHV_FILE:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L4_MENU_SHV_FILE_0 - _Resource_PEResTab
 L5_SHBRSFORFOLDER_MSGBOX:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L5_SHBRSFORFOLDER_MSGBOX_0 - _Resource_PEResTab
 L5_SHELL_ABOUT_MSGBOX:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000409h
 	dd	L5_SHELL_ABOUT_MSGBOX_1033 - _Resource_PEResTab
 L6_1:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L6_1_0 - _Resource_PEResTab
 L6_2:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L6_2_0 - _Resource_PEResTab
 L14_0:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L14_0_0 - _Resource_PEResTab
 L14_3:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L14_3_0 - _Resource_PEResTab
 L14_8:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L14_8_0 - _Resource_PEResTab
 L14_15:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L14_15_0 - _Resource_PEResTab
 L14_34:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
 	dd	L14_34_0 - _Resource_PEResTab
 L16_1:
 	dd	0
-	dd	0380cc35fh
+	dd	03814217dh
 	dd	0
 	dw	0, 1
 	dd	000000000h
@@ -461,7 +461,7 @@ L14_34_0:
 	dd	0
 L16_1_0:
 	dd	_Resource_VerInf_1_0_data - _Resource_PEResTab
-	dd	812
+	dd	760
 	dd	0
 	dd	0
 _Resource_4_MENU_001_name:
@@ -623,7 +623,7 @@ _Resource_Dlg_SHBRSFORFOLDER_MSGBOX_0_data:
 
 	align	4
 _Resource_VerInf_1_0_data:
-	db	02ch, 003h, 034h, 000h, 000h, 000h, 056h, 000h
+	db	0f8h, 002h, 034h, 000h, 000h, 000h, 056h, 000h
 	db	053h, 000h, 05fh, 000h, 056h, 000h, 045h, 000h
 	db	052h, 000h, 053h, 000h, 049h, 000h, 04fh, 000h
 	db	04eh, 000h, 05fh, 000h, 049h, 000h, 04eh, 000h
@@ -634,12 +634,12 @@ _Resource_VerInf_1_0_data:
 	db	03fh, 000h, 000h, 000h, 00ah, 000h, 000h, 000h
 	db	001h, 000h, 001h, 000h, 001h, 000h, 000h, 000h
 	db	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
-	db	000h, 000h, 000h, 000h, 08ch, 002h, 000h, 000h
+	db	000h, 000h, 000h, 000h, 058h, 002h, 000h, 000h
 	db	000h, 000h, 053h, 000h, 074h, 000h, 072h, 000h
 	db	069h, 000h, 06eh, 000h, 067h, 000h, 046h, 000h
 	db	069h, 000h, 06ch, 000h, 065h, 000h, 049h, 000h
 	db	06eh, 000h, 066h, 000h, 06fh, 000h, 000h, 000h
-	db	068h, 002h, 000h, 000h, 000h, 000h, 030h, 000h
+	db	034h, 002h, 000h, 000h, 000h, 000h, 030h, 000h
 	db	034h, 000h, 030h, 000h, 039h, 000h, 030h, 000h
 	db	034h, 000h, 045h, 000h, 034h, 000h, 000h, 000h
 	db	03ch, 000h, 012h, 000h, 001h, 000h, 043h, 000h
@@ -656,75 +656,68 @@ _Resource_VerInf_1_0_data:
 	db	000h, 000h, 000h, 000h, 04fh, 000h, 064h, 000h
 	db	069h, 000h, 06eh, 000h, 020h, 000h, 054h, 000h
 	db	065h, 000h, 061h, 000h, 06dh, 000h, 000h, 000h
-	db	060h, 000h, 01ch, 000h, 001h, 000h, 046h, 000h
+	db	02ah, 000h, 001h, 000h, 001h, 000h, 046h, 000h
 	db	069h, 000h, 06ch, 000h, 065h, 000h, 044h, 000h
 	db	065h, 000h, 073h, 000h, 063h, 000h, 072h, 000h
 	db	069h, 000h, 070h, 000h, 074h, 000h, 069h, 000h
 	db	06fh, 000h, 06eh, 000h, 000h, 000h, 000h, 000h
-	db	047h, 000h, 065h, 000h, 06eh, 000h, 065h, 000h
-	db	072h, 000h, 069h, 000h, 063h, 000h, 020h, 000h
-	db	045h, 000h, 078h, 000h, 061h, 000h, 06dh, 000h
-	db	070h, 000h, 06ch, 000h, 065h, 000h, 020h, 000h
-	db	041h, 000h, 070h, 000h, 070h, 000h, 06ch, 000h
-	db	069h, 000h, 063h, 000h, 061h, 000h, 074h, 000h
-	db	069h, 000h, 06fh, 000h, 06eh, 000h, 000h, 000h
-	db	028h, 000h, 004h, 000h, 001h, 000h, 046h, 000h
-	db	069h, 000h, 06ch, 000h, 065h, 000h, 056h, 000h
-	db	065h, 000h, 072h, 000h, 073h, 000h, 069h, 000h
-	db	06fh, 000h, 06eh, 000h, 000h, 000h, 000h, 000h
-	db	030h, 000h, 02eh, 000h, 030h, 000h, 000h, 000h
-	db	042h, 000h, 011h, 000h, 001h, 000h, 049h, 000h
-	db	06eh, 000h, 074h, 000h, 065h, 000h, 072h, 000h
-	db	06eh, 000h, 061h, 000h, 06ch, 000h, 04eh, 000h
-	db	061h, 000h, 06dh, 000h, 065h, 000h, 000h, 000h
-	db	04fh, 000h, 064h, 000h, 069h, 000h, 06eh, 000h
-	db	033h, 000h, 032h, 000h, 020h, 000h, 02dh, 000h
-	db	020h, 000h, 053h, 000h, 048h, 000h, 045h, 000h
-	db	04ch, 000h, 04ch, 000h, 033h, 000h, 032h, 000h
-	db	000h, 000h, 000h, 000h, 04ah, 000h, 013h, 000h
-	db	001h, 000h, 04ch, 000h, 065h, 000h, 067h, 000h
-	db	061h, 000h, 06ch, 000h, 043h, 000h, 06fh, 000h
-	db	070h, 000h, 079h, 000h, 072h, 000h, 069h, 000h
-	db	067h, 000h, 068h, 000h, 074h, 000h, 000h, 000h
+	db	000h, 000h, 000h, 000h, 028h, 000h, 004h, 000h
+	db	001h, 000h, 046h, 000h, 069h, 000h, 06ch, 000h
+	db	065h, 000h, 056h, 000h, 065h, 000h, 072h, 000h
+	db	073h, 000h, 069h, 000h, 06fh, 000h, 06eh, 000h
+	db	000h, 000h, 000h, 000h, 030h, 000h, 02eh, 000h
+	db	030h, 000h, 000h, 000h, 042h, 000h, 011h, 000h
+	db	001h, 000h, 049h, 000h, 06eh, 000h, 074h, 000h
+	db	065h, 000h, 072h, 000h, 06eh, 000h, 061h, 000h
+	db	06ch, 000h, 04eh, 000h, 061h, 000h, 06dh, 000h
+	db	065h, 000h, 000h, 000h, 04fh, 000h, 064h, 000h
+	db	069h, 000h, 06eh, 000h, 033h, 000h, 032h, 000h
+	db	020h, 000h, 02dh, 000h, 020h, 000h, 053h, 000h
+	db	048h, 000h, 045h, 000h, 04ch, 000h, 04ch, 000h
+	db	033h, 000h, 032h, 000h, 000h, 000h, 000h, 000h
+	db	04ah, 000h, 013h, 000h, 001h, 000h, 04ch, 000h
+	db	065h, 000h, 067h, 000h, 061h, 000h, 06ch, 000h
 	db	043h, 000h, 06fh, 000h, 070h, 000h, 079h, 000h
 	db	072h, 000h, 069h, 000h, 067h, 000h, 068h, 000h
-	db	074h, 000h, 020h, 000h, 028h, 000h, 043h, 000h
-	db	029h, 000h, 020h, 000h, 031h, 000h, 039h, 000h
-	db	039h, 000h, 039h, 000h, 000h, 000h, 000h, 000h
-	db	02ah, 000h, 001h, 000h, 001h, 000h, 04ch, 000h
-	db	065h, 000h, 067h, 000h, 061h, 000h, 06ch, 000h
-	db	054h, 000h, 072h, 000h, 061h, 000h, 064h, 000h
-	db	065h, 000h, 06dh, 000h, 061h, 000h, 072h, 000h
-	db	06bh, 000h, 073h, 000h, 000h, 000h, 000h, 000h
+	db	074h, 000h, 000h, 000h, 043h, 000h, 06fh, 000h
+	db	070h, 000h, 079h, 000h, 072h, 000h, 069h, 000h
+	db	067h, 000h, 068h, 000h, 074h, 000h, 020h, 000h
+	db	028h, 000h, 043h, 000h, 029h, 000h, 020h, 000h
+	db	031h, 000h, 039h, 000h, 039h, 000h, 039h, 000h
 	db	000h, 000h, 000h, 000h, 02ah, 000h, 001h, 000h
-	db	001h, 000h, 04fh, 000h, 072h, 000h, 069h, 000h
-	db	067h, 000h, 069h, 000h, 06eh, 000h, 061h, 000h
-	db	06ch, 000h, 046h, 000h, 069h, 000h, 06ch, 000h
-	db	065h, 000h, 06eh, 000h, 061h, 000h, 06dh, 000h
-	db	065h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
-	db	042h, 000h, 011h, 000h, 001h, 000h, 050h, 000h
-	db	072h, 000h, 06fh, 000h, 064h, 000h, 075h, 000h
-	db	063h, 000h, 074h, 000h, 04eh, 000h, 061h, 000h
-	db	06dh, 000h, 065h, 000h, 000h, 000h, 000h, 000h
-	db	04fh, 000h, 064h, 000h, 069h, 000h, 06eh, 000h
-	db	033h, 000h, 032h, 000h, 020h, 000h, 02dh, 000h
-	db	020h, 000h, 053h, 000h, 048h, 000h, 045h, 000h
-	db	04ch, 000h, 04ch, 000h, 033h, 000h, 032h, 000h
-	db	000h, 000h, 000h, 000h, 02ch, 000h, 004h, 000h
+	db	001h, 000h, 04ch, 000h, 065h, 000h, 067h, 000h
+	db	061h, 000h, 06ch, 000h, 054h, 000h, 072h, 000h
+	db	061h, 000h, 064h, 000h, 065h, 000h, 06dh, 000h
+	db	061h, 000h, 072h, 000h, 06bh, 000h, 073h, 000h
+	db	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
+	db	02ah, 000h, 001h, 000h, 001h, 000h, 04fh, 000h
+	db	072h, 000h, 069h, 000h, 067h, 000h, 069h, 000h
+	db	06eh, 000h, 061h, 000h, 06ch, 000h, 046h, 000h
+	db	069h, 000h, 06ch, 000h, 065h, 000h, 06eh, 000h
+	db	061h, 000h, 06dh, 000h, 065h, 000h, 000h, 000h
+	db	000h, 000h, 000h, 000h, 042h, 000h, 011h, 000h
 	db	001h, 000h, 050h, 000h, 072h, 000h, 06fh, 000h
 	db	064h, 000h, 075h, 000h, 063h, 000h, 074h, 000h
-	db	056h, 000h, 065h, 000h, 072h, 000h, 073h, 000h
-	db	069h, 000h, 06fh, 000h, 06eh, 000h, 000h, 000h
-	db	031h, 000h, 02eh, 000h, 030h, 000h, 000h, 000h
-	db	044h, 000h, 000h, 000h, 000h, 000h, 056h, 000h
-	db	061h, 000h, 072h, 000h, 046h, 000h, 069h, 000h
-	db	06ch, 000h, 065h, 000h, 049h, 000h, 06eh, 000h
-	db	066h, 000h, 06fh, 000h, 000h, 000h, 000h, 000h
-	db	024h, 000h, 004h, 000h, 000h, 000h, 054h, 000h
-	db	072h, 000h, 061h, 000h, 06eh, 000h, 073h, 000h
-	db	06ch, 000h, 061h, 000h, 074h, 000h, 069h, 000h
-	db	06fh, 000h, 06eh, 000h, 000h, 000h, 000h, 000h
-	db	009h, 004h, 0e4h, 004h
+	db	04eh, 000h, 061h, 000h, 06dh, 000h, 065h, 000h
+	db	000h, 000h, 000h, 000h, 04fh, 000h, 064h, 000h
+	db	069h, 000h, 06eh, 000h, 033h, 000h, 032h, 000h
+	db	020h, 000h, 02dh, 000h, 020h, 000h, 053h, 000h
+	db	048h, 000h, 045h, 000h, 04ch, 000h, 04ch, 000h
+	db	033h, 000h, 032h, 000h, 000h, 000h, 000h, 000h
+	db	02ch, 000h, 004h, 000h, 001h, 000h, 050h, 000h
+	db	072h, 000h, 06fh, 000h, 064h, 000h, 075h, 000h
+	db	063h, 000h, 074h, 000h, 056h, 000h, 065h, 000h
+	db	072h, 000h, 073h, 000h, 069h, 000h, 06fh, 000h
+	db	06eh, 000h, 000h, 000h, 031h, 000h, 02eh, 000h
+	db	030h, 000h, 000h, 000h, 044h, 000h, 000h, 000h
+	db	000h, 000h, 056h, 000h, 061h, 000h, 072h, 000h
+	db	046h, 000h, 069h, 000h, 06ch, 000h, 065h, 000h
+	db	049h, 000h, 06eh, 000h, 066h, 000h, 06fh, 000h
+	db	000h, 000h, 000h, 000h, 024h, 000h, 004h, 000h
+	db	000h, 000h, 054h, 000h, 072h, 000h, 061h, 000h
+	db	06eh, 000h, 073h, 000h, 06ch, 000h, 061h, 000h
+	db	074h, 000h, 069h, 000h, 06fh, 000h, 06eh, 000h
+	db	000h, 000h, 000h, 000h, 009h, 004h, 0e4h, 004h
 
 	align	4
 _Resource_Ico_1_0_data:
