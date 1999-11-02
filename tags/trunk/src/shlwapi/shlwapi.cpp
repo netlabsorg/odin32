1a07b00de19d56ffb630b4430e2ca755cadd3983
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-11-02 20:54:12 +0000

    Add: new SHLWAPI dll created

---
+++ tags/trunk/src/shlwapi/shlwapi.cpp
@@ -0,0 +1,35 @@
+/* $Id: shlwapi.cpp,v 1.1 1999-11-02 20:53:37 phaller Exp $ */
+
+/*
+ * Win32 URL-handling APIs for OS/2
+ *
+ * Copyright 1999      Patrick Haller <phaller@gmx.net>
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ * Copyright 1996,1998 Marcus Meissner
+ * Copyright 1996 Jukka Iivonen
+ * Copyright 1997 Uwe Bonnes
+ * Copyright 1999 Jens Wiessner
+ */
+
+
+#include <odin.h>
+#include <odinwrap.h>
+
+
+ODINDEBUGCHANNEL(SHLWAPI)
+
+
+/*****************************************************************************
+ * Name      :
+ * Purpose   :
+ * Parameters:
+ * Variables :
+ * Result    :
+ * Remark    :
+ * Status    : UNTESTED STUB
+ *
+ * Author    : Patrick Haller [Tue, 1998/06/16 23:00]
+ *****************************************************************************/
+
