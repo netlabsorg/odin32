10a50c710e4987b42bc8a080fb1b06d796d6920c
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-11-09 17:06:14 +0000

    don't draw line end

--- trunk/src/gdi32/gdi32.cpp
+++ trunk/src/gdi32/gdi32.cpp
@@ -1,4 +1,4 @@
-/* $Id: gdi32.cpp,v 1.13 1999-11-05 09:17:27 sandervl Exp $ */
+/* $Id: gdi32.cpp,v 1.14 1999-11-09 17:06:14 cbratschi Exp $ */
 
 /*
  * GDI32 apis
@@ -13,11 +13,14 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <math.h>
 #include "misc.h"
 #include "callback.h"
 #include "unicode.h"
 #include "dibsect.h"
 
+#define ROUND_FLOAT(x) ((INT)((x < 0) ? x-0.5:x+0.5))
+#define sqr(x) (pow(x,2))
 
 typedef struct _POLYTEXTA
 {
@@ -804,10 +807,72 @@ BOOL WIN32API EnumEnhMetaFile( HDC arg1, HENHMETAFILE arg2, ENHMFENUMPROC arg3, 
 }
 //******************************************************************************
 //******************************************************************************
-BOOL WIN32API LineTo( HDC arg1, int arg2, int  arg3)
+POINT OS2ToWin32LineEnd(POINT startPt,INT nXEnd,INT nYEnd)
 {
-    dprintf(("GDI32: OS2LineTo"));
-    return O32_LineTo(arg1, arg2, arg3);
+  POINT pt;
+
+  if (startPt.x != nXEnd || startPt.y != nYEnd)
+  {
+    if (nXEnd == startPt.x)
+    {
+      pt.x = nXEnd;
+      pt.y = (nYEnd > startPt.y) ? nYEnd-1:nYEnd+1;
+    } else if (nYEnd == startPt.y)
+    {
+      pt.x = (nXEnd > startPt.x) ? nXEnd-1:nXEnd+1;
+      pt.y = nYEnd;
+    } else
+    {
+      DOUBLE len = sqrt(sqr(nXEnd-startPt.x)+sqr(nYEnd-startPt.y));
+      DOUBLE lenDif = (len-1)/len;
+      INT w = nXEnd-startPt.x,h = nYEnd-startPt.y;
+
+      pt.x = startPt.x+ROUND_FLOAT(w*lenDif);
+      pt.y = startPt.y+ROUND_FLOAT(h*lenDif);
+    }
+  } else
+  {
+    pt.x = nXEnd;
+    pt.y = nYEnd;
+  }
+
+  return pt;
+}
+
+VOID DrawSingleLinePoint(HDC hdc,POINT pt)
+{
+  LOGPEN penInfo;
+
+  if (!GetObjectA(GetCurrentObject(hdc,OBJ_PEN),sizeof(penInfo),(LPVOID)&penInfo)) return;
+  if (penInfo.lopnWidth.x <= 1 && penInfo.lopnWidth.y <= 1) SetPixel(hdc,pt.x,pt.y,penInfo.lopnColor); else
+  {
+    INT x = pt.x-penInfo.lopnWidth.x/2;
+    INT y = pt.y-penInfo.lopnWidth.y/2;
+    Ellipse(hdc,x,y,x+penInfo.lopnWidth.x,y+penInfo.lopnWidth.y);
+  }
+}
+
+BOOL WIN32API LineTo( HDC hdc, int nXEnd, int  nYEnd)
+{
+  POINT oldPt,pt;
+
+  dprintf(("GDI32: OS2LineTo"));
+
+  //CB: Open32 draws a pixel too much!
+  GetCurrentPositionEx(hdc,&oldPt);
+  pt = OS2ToWin32LineEnd(oldPt,nXEnd,nYEnd);
+
+  BOOL rc;
+
+  if (oldPt.x == pt.x && oldPt.y == pt.y)
+  {
+    DrawSingleLinePoint(hdc,pt);
+
+    rc = TRUE;
+  } else rc = O32_LineTo(hdc,pt.x,pt.y);
+  MoveToEx(hdc,nXEnd,nYEnd,NULL);
+
+  return rc;
 }
 //******************************************************************************
 //******************************************************************************
@@ -1491,13 +1556,20 @@ BOOL WIN32API LPtoDP( HDC arg1, PPOINT arg2, int  arg3)
 }
 //******************************************************************************
 //******************************************************************************
-BOOL WIN32API LineDDA( int arg1, int arg2, int arg3, int arg4, LINEDDAPROC lpLineFunc, LPARAM lpData)
+BOOL WIN32API LineDDA( int nXStart, int nYStart, int nXEnd, int nYEnd, LINEDDAPROC lpLineFunc, LPARAM lpData)
 {
  BOOL                 rc;
  LineDDAProcCallback *callback = new LineDDAProcCallback(lpLineFunc, lpData);
+ POINT startPt,endPt;
 
   dprintf(("GDI32: OS2LineDDA\n"));
-  rc = O32_LineDDA(arg1, arg2, arg3, arg4, callback->GetOS2Callback(), (LPARAM)callback);
+
+  //CB: don't know if Open32 reports the last pixel, but all other line functions do
+  startPt.x = nXStart;
+  startPt.y = nYStart;
+  endPt = OS2ToWin32LineEnd(startPt,nXEnd,nYEnd);
+
+  rc = O32_LineDDA(startPt.x,startPt.y,endPt.x,endPt.y,callback->GetOS2Callback(),(LPARAM)callback);
   if(callback)
         delete callback;
   return(rc);
@@ -1622,10 +1694,11 @@ BOOL WIN32API PolyPolygon( HDC arg1, const POINT * arg2, const INT * arg3, UINT 
 }
 //******************************************************************************
 //******************************************************************************
-BOOL WIN32API PolyPolyline( HDC arg1, const POINT * arg2, const DWORD * arg3, DWORD  arg4)
+BOOL WIN32API PolyPolyline( HDC hdc, const POINT * lppt, const DWORD * lpdwPolyPoints, DWORD cCount)
 {
     dprintf(("GDI32: OS2PolyPolyline"));
-    return O32_PolyPolyline(arg1, arg2, arg3, arg4);
+
+    return O32_PolyPolyline(hdc,lppt,lpdwPolyPoints,cCount);
 }
 //******************************************************************************
 //******************************************************************************
@@ -1636,17 +1709,62 @@ BOOL WIN32API Polygon( HDC arg1, const POINT * arg2, int  arg3)
 }
 //******************************************************************************
 //******************************************************************************
-BOOL WIN32API Polyline( HDC arg1, const POINT * arg2, int  arg3)
+BOOL WIN32API Polyline( HDC hdc, const POINT *lppt, int cPoints)
 {
     dprintf(("GDI32: OS2Polyline"));
-    return O32_Polyline(arg1, arg2, arg3);
+
+    if (cPoints == 0) return TRUE;
+    if (cPoints < 0)
+    {
+      SetLastError(ERROR_INVALID_PARAMETER);
+
+      return FALSE;
+    }
+
+    if (cPoints == 1)
+    {
+      DrawSingleLinePoint(hdc,*lppt); //CB: check metafile recording
+
+      return TRUE;
+    }
+
+    //CB: Open32 draw a pixel too much!
+    POINT *points = (POINT*)lppt;
+    POINT lastPt = lppt[cPoints-1];
+    BOOL rc;
+
+    points[cPoints-1] = OS2ToWin32LineEnd(lppt[cPoints-2],lastPt.x,lastPt.y);
+    rc = O32_Polyline(hdc,lppt,cPoints);
+    points[cPoints-1] = lastPt;
+
+    return rc;
 }
 //******************************************************************************
 //******************************************************************************
-BOOL WIN32API PolylineTo( HDC arg1, const POINT * arg2, DWORD  arg3)
+BOOL WIN32API PolylineTo( HDC hdc, const POINT * lppt, DWORD cCount)
 {
     dprintf(("GDI32: OS2PolylineTo"));
-    return O32_PolylineTo(arg1, arg2, arg3);
+
+    if (cCount == 0) return TRUE;
+
+    if (cCount == 1)
+    {
+      DrawSingleLinePoint(hdc,*lppt);
+
+      return TRUE; //CB: check metafile recording
+    }
+
+    //CB: Open32 draw a pixel too much!
+    POINT *points = (POINT*)lppt;
+    POINT lastPt = lppt[cCount-1];
+    BOOL rc;
+
+    points[cCount-1] = OS2ToWin32LineEnd(lppt[cCount-2],lastPt.x,lastPt.y);
+    rc = O32_PolylineTo(hdc,lppt,cCount);
+    points[cCount-1] = lastPt;
+    MoveToEx(hdc,lastPt.x,lastPt.y,NULL);
+
+    return rc;
 }
 //******************************************************************************
 //******************************************************************************
