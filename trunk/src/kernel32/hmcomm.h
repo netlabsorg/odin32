13594647342d0bfd8287f84e61b55acfd939f428
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-11-26 08:17:40 +0000

    first hmcomm revision

---
+++ trunk/src/kernel32/hmcomm.h
@@ -0,0 +1,23 @@
+/* $Id */
+
+/*
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ * Win32 COM device access class
+ *
+ * 1999 Achim Hasenmueller <achimha@innotek.de>
+ *
+ */
+
+#ifndef _HM_COMM_H_
+#define _HM_COMM_H_
+
+class HMDeviceCommHandler : public HMDeviceHandler
+{
+
+
+};
+
+
+
+#endif // _HM_COMM_H_
