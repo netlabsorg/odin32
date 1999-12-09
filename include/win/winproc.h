baf4dff0ab21c824204385aa0c3d5aa6a8e4721c
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-09 09:49:41 +0000

    winproc changes

--- include/win/winproc.h
+++ include/win/winproc.h
@@ -1,4 +1,4 @@
-/* $Id: winproc.h,v 1.1 1999-05-24 20:19:22 ktk Exp $ */
+/* $Id: winproc.h,v 1.2 1999-12-09 09:49:41 sandervl Exp $ */
 
 /*
  * Window procedure callbacks definitions
@@ -14,7 +14,6 @@
 typedef enum
 {
     WIN_PROC_INVALID,
-    WIN_PROC_16,
     WIN_PROC_32A,
     WIN_PROC_32W
 } WINDOWPROCTYPE;
@@ -30,51 +29,24 @@ typedef void *HWINDOWPROC;  /* Really a pointer to a WINDOWPROC */
 
 typedef struct
 {
-    WPARAM16	wParam;
-    LPARAM	lParam;
-    LRESULT	lResult;
-} MSGPARAM16;
-
-typedef struct
-{
     WPARAM    wParam;
     LPARAM	lParam;
     LRESULT	lResult;
 } MSGPARAM;
 
 extern BOOL WINPROC_Init(void);
-extern WNDPROC16 WINPROC_GetProc( HWINDOWPROC proc, WINDOWPROCTYPE type );
-extern BOOL WINPROC_SetProc( HWINDOWPROC *pFirst, WNDPROC16 func,
+extern WNDPROC WINPROC_GetProc( HWINDOWPROC proc, WINDOWPROCTYPE type );
+extern BOOL WINPROC_SetProc( HWINDOWPROC *pFirst, WNDPROC func,
                                WINDOWPROCTYPE type, WINDOWPROCUSER user );
 extern void WINPROC_FreeProc( HWINDOWPROC proc, WINDOWPROCUSER user );
 extern WINDOWPROCTYPE WINPROC_GetProcType( HWINDOWPROC proc );
 
-extern INT WINPROC_MapMsg32ATo32W( HWND hwnd, UINT msg, WPARAM wParam,
-                                     LPARAM *plparam );
-extern INT WINPROC_MapMsg32WTo32A( HWND hwnd, UINT msg, WPARAM wParam,
-                                     LPARAM *plparam );
-extern INT WINPROC_MapMsg16To32A( UINT16 msg16, WPARAM16 wParam16,
-                                    UINT *pmsg32, WPARAM *pwparam32,
-                                    LPARAM *plparam );
-extern INT WINPROC_MapMsg16To32W( HWND16, UINT16 msg16, WPARAM16 wParam16,
-                                    UINT *pmsg32, WPARAM *pwparam32,
-                                    LPARAM *plparam );
-extern INT WINPROC_MapMsg32ATo16( HWND hwnd, UINT msg32,
-                                    WPARAM wParam32, UINT16 *pmsg16,
-                                    WPARAM16 *pwparam16, LPARAM *plparam );
-extern INT WINPROC_MapMsg32WTo16( HWND hwnd, UINT msg32,
-                                    WPARAM wParam32, UINT16 *pmsg16,
-                                    WPARAM16 *pwparam16, LPARAM *plparam );
-extern void WINPROC_UnmapMsg32ATo32W( HWND hwnd, UINT msg, WPARAM wParam,
-                                      LPARAM lParam );
-extern void WINPROC_UnmapMsg32WTo32A( HWND hwnd, UINT msg, WPARAM wParam,
-                                      LPARAM lParam );
-extern LRESULT WINPROC_UnmapMsg16To32A( HWND16 hwnd, UINT msg, WPARAM wParam,
-                                        LPARAM lParam, LRESULT result );
-extern LRESULT WINPROC_UnmapMsg16To32W( HWND16 hwnd, UINT msg, WPARAM wParam,
-                                        LPARAM lParam, LRESULT result );
-extern void WINPROC_UnmapMsg32ATo16( HWND hwnd, UINT msg, WPARAM wParam,
-                                     LPARAM lParam, MSGPARAM16* pm16 );
-extern void WINPROC_UnmapMsg32WTo16( HWND hwnd, UINT msg, WPARAM wParam,
-                                     LPARAM lParam, MSGPARAM16* pm16 );
+LRESULT WINPROC_CallProc32ATo32W( WNDPROC func, HWND hwnd,
+                                  UINT msg, WPARAM wParam,
+                                  LPARAM lParam );
+
+LRESULT WINPROC_CallProc32WTo32A( WNDPROC func, HWND hwnd,
+                                  UINT msg, WPARAM wParam,
+                                  LPARAM lParam );
+
 #endif  /* __WINE_WINPROC_H */
