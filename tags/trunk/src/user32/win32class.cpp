d110136d77574deff79bbbf9b4b0f218728062d3
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-09 11:00:02 +0000

    replaced strncpy with lstrcpynA

--- tags/trunk/src/user32/win32class.cpp
+++ tags/trunk/src/user32/win32class.cpp
@@ -1,4 +1,4 @@
-/* $Id: win32class.cpp,v 1.8 1999-12-09 10:59:37 sandervl Exp $ */
+/* $Id: win32class.cpp,v 1.9 1999-12-09 11:00:02 sandervl Exp $ */
 /*
  * Win32 Window Class Managment Code for OS/2
  *
@@ -279,7 +279,7 @@ BOOL Win32WndClass::getClassInfo(WNDCLASSEXW *wndclass)
 ULONG Win32WndClass::getClassName(LPSTR lpszClassName, ULONG cchClassName)
 {
   if(HIWORD(classNameA)) {
-        lstrncpyA(lpszClassName, classNameA, cchClassName-1);
+        lstrcpynA(lpszClassName, classNameA, cchClassName-1);
         return strlen(lpszClassName);
   }
   *(ULONG *)lpszClassName = classAtom;
