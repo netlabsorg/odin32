cd7af73eb4afb41727f3a309e34479360cebcc88
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-18 19:50:04 +0000

    WINE 991212 changes

--- tags/trunk/include/win/rebar.h
+++ tags/trunk/include/win/rebar.h
@@ -1,4 +1,4 @@
-/* $Id: rebar.h,v 1.1 1999-05-24 20:19:18 ktk Exp $ */
+/* $Id: rebar.h,v 1.2 1999-12-18 19:50:04 achimha Exp $ */
 
 /*
  * Rebar class extra info
@@ -58,6 +58,7 @@ typedef struct tagREBAR_INFO
     HCURSOR  hcurHorz;    /* handle to the EW cursor */
     HCURSOR  hcurVert;    /* handle to the NS cursor */
     HCURSOR  hcurDrag;    /* handle to the drag cursor */
+    INT      iVersion     /* version number */
 
     REBAR_BAND *bands;      /* pointer to the array of rebar bands */
 
