b2f52f863171fd2841bdc9b2adb83603f0e7d1f6
davidr <davidr@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-22 23:27:14 +0000

    Added FUNCFLAGS enum

--- include/win/wine/obj_oleaut.h
+++ include/win/wine/obj_oleaut.h
@@ -1,4 +1,4 @@
-/* $Id: obj_oleaut.h,v 1.5 1999-08-22 22:52:08 sandervl Exp $ */
+/* $Id: obj_oleaut.h,v 1.6 1999-12-22 23:27:14 davidr Exp $ */
 /*
  * Defines the COM interfaces and APIs related to OLE automation support.
  */
@@ -411,6 +411,24 @@ typedef union tagBINDPTR
 	VARDESC  *lpvardesc;
 	ITypeComp *lptcomp;
 } BINDPTR, *LPBINDPTR;
+
+typedef enum tagFUNCFLAGS
+{
+	FUNCFLAG_FRESTRICTED    = 0x1,
+	FUNCFLAG_FSOURCE        = 0x2,
+	FUNCFLAG_FBINDABLE      = 0x4,
+	FUNCFLAG_FREQUESTEDIT   = 0x8,
+	FUNCFLAG_FDISPLAYBIND   = 0x10,
+	FUNCFLAG_FDEFAULTBIND   = 0x20,
+	FUNCFLAG_FHIDDEN        = 0x40,
+	FUNCFLAG_FUSESGETLASTERROR      = 0x80,
+	FUNCFLAG_FDEFAULTCOLLELEM       = 0x100,
+	FUNCFLAG_FUIDEFAULT     = 0x200,
+	FUNCFLAG_FNONBROWSABLE  = 0x400,
+	FUNCFLAG_FREPLACEABLE   = 0x800,
+	FUNCFLAG_FIMMEDIATEBIND = 0x1000
+}   FUNCFLAGS;
+
 
 typedef enum tagVARFLAGS
 {
