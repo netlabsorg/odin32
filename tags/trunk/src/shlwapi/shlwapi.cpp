ba8a7ab8a0468b53211bbae78bbe3728fbc70446
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-28 23:17:04 +0000

    Added more forwarders to shell32 apis

--- tags/trunk/src/shlwapi/shlwapi.cpp
+++ tags/trunk/src/shlwapi/shlwapi.cpp
@@ -1,4 +1,4 @@
-/* $Id: shlwapi.cpp,v 1.1 1999-11-02 20:53:37 phaller Exp $ */
+/* $Id: shlwapi.cpp,v 1.2 1999-12-28 23:17:04 sandervl Exp $ */
 
 /*
  * Win32 URL-handling APIs for OS/2
@@ -16,20 +16,92 @@
 
 #include <odin.h>
 #include <odinwrap.h>
+#include <os2sel.h>
 
+#include <string.h>
+#include <ctype.h>
+#include <wctype.h>
+#define HAVE_WCTYPE_H
+#include <odin.h>
+
+#define ICOM_CINTERFACE 1
+#define CINTERFACE 1
+
+#include "debugtools.h"
+#include "winnls.h"
+#include "winversion.h"
+#include "winreg.h"
+#include "crtdll.h"
+
+#include <heapstring.h>
+#include <misc.h>
+#include <win\shell.h>
+#include <win\winerror.h>
 
 ODINDEBUGCHANNEL(SHLWAPI)
 
 
-/*****************************************************************************
- * Name      :
- * Purpose   :
- * Parameters:
- * Variables :
- * Result    :
- * Remark    :
- * Status    : UNTESTED STUB
- *
- * Author    : Patrick Haller [Tue, 1998/06/16 23:00]
- *****************************************************************************/
+
+/*
+   shlwapi functions that have found their way in because most of
+   shlwapi is unimplemented and doesn't have a home.
+
+   FIXME: move to a more appropriate file( when one exists )
+*/
+
+ /* SHGetValue: Gets a value from the registry */
+
+ODINFUNCTION6(DWORD,SHGetValueA,HKEY,     hkey,
+                                LPCSTR,   pSubKey,
+                                LPCSTR,   pValue,
+                                LPDWORD,  pwType,
+                                LPVOID,   pvData,
+                                LPDWORD,  pbData)
+{
+    dprintf(("(%p),stub!\n", pSubKey));
+
+	return ERROR_SUCCESS;  /* return success */
+}
+
+ODINFUNCTION6(DWORD,SHGetValueW,HKEY,     hkey,
+                                LPCWSTR,  pSubKey,
+                                LPCWSTR,  pValue,
+                                LPDWORD,  pwType,
+                                LPVOID,   pvData,
+                                LPDWORD,  pbData)
+{
+    dprintf(("(%p),stub!\n", pSubKey));
+
+	return ERROR_SUCCESS;  /* return success */
+}
+
+/* gets a user-specific registry value. */
+
+ODINFUNCTION8(LONG,SHRegGetUSValueA,LPCSTR,   pSubKey,
+                                    LPCSTR,   pValue,
+                                    LPDWORD,  pwType,
+                                    LPVOID,   pvData,
+                                    LPDWORD,  pbData,
+                                    BOOL,     fIgnoreHKCU,
+                                    LPVOID,   pDefaultData,
+                                    DWORD,    wDefaultDataSize)
+{
+    FIXME("(%p),stub!\n", pSubKey);
+
+	return ERROR_SUCCESS;  /* return success */
+}
+
+ODINFUNCTION8(LONG,SHRegGetUSValueW,LPCWSTR, pSubKey,
+                                    LPCWSTR, pValue,
+                                    LPDWORD, pwType,
+                                    LPVOID,  pvData,
+                                    LPDWORD, pbData,
+                                    BOOL,    flagIgnoreHKCU,
+                                    LPVOID,  pDefaultData,
+                                    DWORD,   wDefaultDataSize)
+{
+    dprintf(("(%p),stub!\n", pSubKey));
+
+	return ERROR_SUCCESS;  /* return success */
+}
 
