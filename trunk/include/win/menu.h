067cd97aa8974b24f32c43255d83976b03a3e757
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
1999-12-30 17:35:38 +0000

    *** empty log message ***

--- trunk/include/win/menu.h
+++ trunk/include/win/menu.h
@@ -1,5 +1,3 @@
-/* $Id: menu.h,v 1.1 1999-05-24 20:19:14 ktk Exp $ */
-
 /*
  * Menu definitions
  */
@@ -9,14 +7,23 @@
 
 #include "win.h"
 
+#define POPUPMENUCLASSNAME "#32768"
+
+LRESULT WINAPI PopupMenuWndProc( HWND hwnd, UINT message, WPARAM wParam,
+                                 LPARAM lParam );
+
 extern BOOL MENU_Init(void);
 extern HMENU MENU_GetSysMenu(HWND hWndOwner, HMENU hSysPopup);
 extern UINT MENU_GetMenuBarHeight( HWND hwnd, UINT menubarWidth,
                                      INT orgX, INT orgY );
-extern BOOL MENU_PatchResidentPopup( HQUEUE16, WND* );
-extern void MENU_TrackMouseMenuBar( WND *wnd, INT ht, POINT pt );
-extern void MENU_TrackKbdMenuBar( WND *wnd, UINT wParam, INT vkey );
+extern BOOL MENU_PatchResidentPopup( HQUEUE, HWND );
+extern void MENU_TrackMouseMenuBar( HWND hwnd, INT ht, POINT pt );
+extern void MENU_TrackKbdMenuBar( HWND hwnd, UINT wParam, INT vkey );
 extern UINT MENU_DrawMenuBar( HDC hDC, LPRECT lprect,
                                 HWND hwnd, BOOL suppress_draw );
+extern UINT MENU_FindSubMenu( HMENU *hmenu, HMENU hSubTarget );
+
+BOOL POPUPMENU_Register();
+BOOL POPUPMENU_Unregister();
 
 #endif /* __WINE_MENU_H */
