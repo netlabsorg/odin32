718d9eb2653499da4f46f1e07413647fc14fb6d1
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-01-05 19:52:37 +0000

    JW: twain update

--- trunk/src/twain_32/twain_32.cpp
+++ trunk/src/twain_32/twain_32.cpp
@@ -1,4 +1,4 @@
-/* $Id: twain_32.cpp,v 1.1 2000-01-04 22:19:01 sandervl Exp $ */
+/* $Id: twain_32.cpp,v 1.2 2000-01-05 19:52:37 sandervl Exp $ */
 
 /*
  * TWAIN_32 implementation
@@ -16,14 +16,11 @@
 #include <string.h>
 
 #include <memory.h>
-
+#include <misc.h>
 #include <twain.h>
 
-#include "misc.h"
-
-
-TW_UINT16 (APIENTRY *TWAINOS2_DSM_Entry)( pTW_IDENTITY, pTW_IDENTITY,
-	   TW_UINT32, TW_UINT16, TW_UINT16, TW_MEMREF) = 0;
+extern TW_UINT16 (APIENTRY *TWAINOS2_DSM_Entry)( pTW_IDENTITY, pTW_IDENTITY,
+	   TW_UINT32, TW_UINT16, TW_UINT16, TW_MEMREF);
 
 
 //******************************************************************************
@@ -36,28 +33,22 @@ TW_UINT16 FAR PASCAL DSM_Entry( pTW_IDENTITY pOrigin,
                                 TW_MEMREF    pData)
 {
     int returnval;
-    static HINSTANCE hInstance = 0;
-    hInstance = LoadLibraryA("TWAINOS2.DLL");
-    if(hInstance) 
-    {   
-        *(VOID **)&TWAINOS2_DSM_Entry=(void*)GetProcAddress(hInstance, (LPCSTR)"DSM_Entry");
-    }
-    WriteLog("TWAIN_32: DSM_Entry called with parameters:");
-    WriteLog("TWAIN_32: pOrigin.Id: %08xh ", pOrigin->Id);
-//    WriteLog("TWAIN_32: pOrigin.Version: %08xh ", pOrigin->Version);
-//    WriteLog("TWAIN_32: pOrigin.ProtocolMajor: %08xh ", pOrigin->ProtocolMajor);
-//    WriteLog("TWAIN_32: pOrigin.ProtocolMinor: %08xh ", pOrigin->ProtocolMinor);
-    WriteLog("TWAIN_32: pOrigin.SupportedGroups: %08xh ", pOrigin->SupportedGroups);
-    WriteLog("TWAIN_32: pOrigin.Manufacturer: %s ", pOrigin->Manufacturer);
-    WriteLog("TWAIN_32: pOrigin.ProductFamily: %s ", pOrigin->ProductFamily);
-    WriteLog("TWAIN_32: pOrigin.Productname: %s ", pOrigin->ProductName);
-    WriteLog("TWAIN_32: pDest: %08xh ", pDest);
-    WriteLog("TWAIN_32: Data Group: %04xh ", DG);
-    WriteLog("TWAIN_32: Data Attribute Type: %04xh ", DAT);
-    WriteLog("TWAIN_32: Message: %04xh ", MSG);
-    WriteLog("TWAIN_32: pData: %08xh ", pData);
-    returnval = TWAINOS2_DSM_Entry( pOrigin, pDest, DG, DAT, MSG, pData);	
-    WriteLog("TWAIN_32: DSM_Entry returned: %04xh \n\n", returnval);
+    dprintf(("TWAIN_32: DSM_Entry called with parameters:"));
+    dprintf(("TWAIN_32: pOrigin.Id: %08xh ", pOrigin->Id));
+//    dprintf(("TWAIN_32: pOrigin.Version: %08xh ", pOrigin->Version));
+    dprintf(("TWAIN_32: pOrigin.ProtocolMajor: %08xh ", pOrigin->ProtocolMajor));
+    dprintf(("TWAIN_32: pOrigin.ProtocolMinor: %08xh ", pOrigin->ProtocolMinor));
+    dprintf(("TWAIN_32: pOrigin.SupportedGroups: %08xh ", pOrigin->SupportedGroups));
+    dprintf(("TWAIN_32: pOrigin.Manufacturer: %s ", pOrigin->Manufacturer));
+    dprintf(("TWAIN_32: pOrigin.ProductFamily: %s ", pOrigin->ProductFamily));
+    dprintf(("TWAIN_32: pOrigin.Productname: %s ", pOrigin->ProductName));
+    dprintf(("TWAIN_32: pDest: %08xh ", pDest));
+    dprintf(("TWAIN_32: Data Group: %04xh ", DG));
+    dprintf(("TWAIN_32: Data Attribute Type: %04xh ", DAT));
+    dprintf(("TWAIN_32: Message: %04xh ", MSG));
+    dprintf(("TWAIN_32: pData: %08xh ", pData));
+    returnval = TWAINOS2_DSM_Entry( pOrigin, pDest, DG, DAT, MSG, pData);
+    dprintf(("TWAIN_32: DSM_Entry returned: %04xh \n\n", returnval));
     if (returnval == 3)
     {
        MessageBoxA(0, "No DataSource found.", "ODIN TWAIN_32", MB_OK);
