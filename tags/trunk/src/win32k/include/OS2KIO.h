8a8818375b9906d55c4510f7e8bf2a96375bc368
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-01-17 16:52:25 +0000

    Finished porototyping.

--- tags/trunk/src/win32k/include/OS2KIO.h
+++ tags/trunk/src/win32k/include/OS2KIO.h
@@ -1,4 +1,4 @@
-/* $Id: OS2KIO.h,v 1.1 2000-01-14 19:21:12 bird Exp $
+/* $Id: OS2KIO.h,v 1.2 2000-01-17 16:52:25 bird Exp $
  *
  * OS/2 kernel IO, typedefs and macros.
  *
@@ -9,12 +9,23 @@
 #ifndef _OS2KIO_h_
 #define _OS2KIO_h_
 
-APIRET KRNLCALL IOSftOpen(PSZ pszFilename, ULONG ul2, ULONG ul3,
-                          PSFN phFile, ULONG ul5);
+APIRET KRNLCALL IOSftOpen(PSZ pszFilename,
+                          ULONG flOpenFlags,  /* probably similar to DosOpen */
+                          ULONG fsOpenMode,   /* probably similar to DosOpen */
+                          PSFN phFile,
+                          PULONG pulsomething); /* EA? */
 APIRET KRNLCALL IOSftClose(SFN hFile);
-APIRET KRNLCALL IOSftTransPath(
-APIRET KRNLCALL IOSftReadAt(SFN hFile, PULONG pulActual, PVOID pvBuffer, ULONG flFlags, ULONG cbBuffer);
-APIRET KRNLCALL IOSftWriteAt(SFN hFile, PULONG pulActual, PVOID pvBuffer, ULONG flFlags, ULONG cbBuffer);
-APIRET KRNLCALL IOSft(1,2,3,4,5);
+APIRET KRNLCALL IOSftTransPath(PSZ pszPath /* IN and OUT? */);
+APIRET KRNLCALL IOSftReadAt(SFN hFile,
+                            PULONG pulActual,
+                            PVOID pvBuffer,
+                            ULONG flFlags,
+                            ULONG cbBuffer);
+APIRET KRNLCALL IOSftWriteAt(SFN hFile,
+                            PULONG pulActual,
+                            PVOID pvBuffer,
+                            ULONG flFlags,
+                            ULONG cbBuffer);
+/* APIRET KRNLCALL IOSft(1,2,3,4,5); */
 
 #endif
