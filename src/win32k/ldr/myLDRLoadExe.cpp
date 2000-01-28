672a541c0430d066f2f4bf28ff3f0cc3d5651e68
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-01-28 15:28:43 +0000

    Added module header on the obsolete files.

--- src/win32k/ldr/myLDRLoadExe.cpp
+++ src/win32k/ldr/myLDRLoadExe.cpp
@@ -1,9 +1,18 @@
+/* $Id: myLDRLoadExe.cpp,v 1.2 2000-01-28 15:28:42 bird Exp $
+ *
+ * Obsolete
+ *
+ * Copyright (c) 1998 knut st. osmundsen
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
 #include <ldr.h>
 
-int LDRCALL myLDRLoadExe(PCHAR pExeName,PULONG p2)
+int LDRCALL myLDRLoadExe(PCHAR pExeName,PULONG pPTDA) /* ? */
 {
-	//kernel_printf(("_LDRLoadExe: %s - param2:0x%x", pExeName, p2));
-	return _LDRLoadExe(pExeName,p2);
+    //kernel_printf(("_LDRLoadExe: %s - param2:0x%x", pExeName, p2));
+    return _LDRLoadExe(pExeName,p2);
 }
 
 
