a6f5f005854f1f99ada38e780a8c9a8f97e4868f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-01-28 12:06:58 +0000

    *** empty log message ***

--- trunk/include/os2sel.h
+++ trunk/include/os2sel.h
@@ -1,4 +1,4 @@
-/* $Id: os2sel.h,v 1.5 1999-08-17 19:30:49 phaller Exp $ */
+/* $Id: os2sel.h,v 1.6 2000-01-28 12:06:58 sandervl Exp $ */
 /*
  *
  * Project Odin Software License can be found in LICENSE.TXT
@@ -11,5 +11,8 @@ unsigned short _System GetFS       ();
 unsigned short _System RestoreOS2FS();
 void           _System SetFS       (unsigned short selector);
 unsigned short _System SetReturnFS (unsigned short selector);
+
+//SvL: Checks if thread FS & exception structure are valid
+int            _System CheckCurFS();
 
 #endif //__OS2SEL_H__
