7aee4ae1a707d7215e2c752fa9b55a8a5d1a675e
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-01-31 22:51:28 +0000

    Add: PathSkipRootAW added

--- trunk/src/shlwapi/shlwapi.cpp
+++ trunk/src/shlwapi/shlwapi.cpp
@@ -1,4 +1,4 @@
-/* $Id: shlwapi.cpp,v 1.3 1999-12-30 00:24:29 phaller Exp $ */
+/* $Id: shlwapi.cpp,v 1.4 2000-01-31 22:50:51 phaller Exp $ */
 
 /*
  * Win32 URL-handling APIs for OS/2
@@ -161,3 +161,58 @@ ODINFUNCTION5(LONG,    SHRegGetBoolUSValueW,
   return ERROR_SUCCESS;  /* return success */
 }
 
+
+/*****************************************************************************
+ * Name      : LPSTR PathSkipRootA
+ * Purpose   : Parses a path, ignoring the drive letter or UNC server/share path parts.
+ * Parameters: LPCSTR pszPath
+ * Variables :
+ * Result    : unknown
+ * Remark    : SHLWAPI.PathSkipRootA
+ * Status    : UNTESTED
+ *
+ * Author    : Patrick Haller [Mon, 2000/01/31 23:02]
+ *****************************************************************************/
+
+ODINFUNCTION1(LPSTR, PathSkipRootA, LPCSTR, pszPath)
+{
+  // check if "driveletter:\"
+  if (pszPath[1] == ':')
+    return (LPSTR)(pszPath + 2);
+  
+  // check if UNC-style path
+  if ( (pszPath[0] == '\\') &&
+      (pszPath[1] == '\\') )
+  {
+    LPSTR pszTemp = strchr(pszPath + 2, '\\');
+    if (NULL != pszTemp)
+      // return root part, skip server/share
+      return (LPSTR)pszTemp++;
+    else
+      // UNC syntax validation, return pszPath
+      return (LPSTR)pszTemp;
+  }
+  
+  // else ...
+  return (LPSTR)pszPath;
+}
+
+
+/*****************************************************************************
+ * Name      : LPWSTR PathSkipRootW
+ * Purpose   : Parses a path, ignoring the drive letter or UNC server/share path parts.
+ * Parameters: LPCWSTR pszPath
+ * Variables :
+ * Result    : unknown
+ * Remark    : SHLWAPI.PathSkipRootW
+ * Status    : UNTESTED
+ *
+ * Author    : Patrick Haller [Mon, 2000/01/31 23:02]
+ *****************************************************************************/
+
+ODINFUNCTION1(LPWSTR, PathSkipRootW, LPCWSTR, pszPath)
+{
+  dprintf(("not implemented"));
+  
+  return (LPWSTR)pszPath;
+}
