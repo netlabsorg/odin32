5421d38552eb80b71110d9e56440c89f0576ade9
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-03 19:04:01 +0000

    added

---
+++ tags/trunk/include/cpuhlp.h
@@ -0,0 +1,41 @@
+/* $Id: cpuhlp.h,v 1.1 2000-02-03 19:04:01 sandervl Exp $ */
+
+#ifndef _CPUHLP_H_
+#define _CPUHLP_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BIT(a)	(1<<a)
+
+#define CPUID_FPU_PRESENT		BIT(0)
+#define CPUID_VMMODE_EXTENSIONS		BIT(1)
+#define CPUID_DBG_EXTENSIONS		BIT(2)
+#define CPUID_PAGE_SIZE_EXTENSIONS	BIT(3)
+#define CPUID_TIME_STAMP_COUNTER	BIT(4)
+#define CPUID_K86_MODEL_REGS		BIT(5)
+#define CPUID_MACHINE_CHECK_EXCEPTION	BIT(7)
+#define CPUID_CMPXCHG8B_INSTRUCTION	BIT(8)
+#define CPUID_APIC			BIT(9)
+#define CPUID_MEMORY_TYPE_RANGE_REGS	BIT(12)
+#define CPUID_GLOBAL_PAGING_EXTENSIONS	BIT(13)
+#define CPUID_CONDITIONAL_MOVE		BIT(15)
+#define CPUID_MMX			BIT(23)
+
+BOOL  SupportsCPUID();
+
+void  GetCPUVendorString(char *vendor);
+DWORD GetCPUFeatures();
+DWORD GetCPUSignature();
+
+void  InitSystemInfo(int nrcpus);
+
+extern DWORD CPUFeatures;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CPUHLP_H_ */
+
