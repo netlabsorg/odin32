de16700a7c22c2ed12606be56014d5cdfc08138d
hugh <hugh@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-04 19:29:17 +0000

    some minor dsound fixes

---
+++ trunk/src/dsound/asmutil.h
@@ -0,0 +1,21 @@
+/*
+ * asmutil.asm function definition
+ *
+ * Copyright 1998 Sander van Leeuwen
+ *           1999 Markus Montkowski
+ */
+
+#ifndef __ASMUTIL_H__
+#define __ASMUTIL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern ULONG __cdecl GetMsCount( QWORD* pqwT1, ULONG ulFreq);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
