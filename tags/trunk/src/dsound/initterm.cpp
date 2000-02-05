c6247724593b7457e5627027b44db24adf92b16c
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-05 02:19:44 +0000

    Added version resource

--- tags/trunk/src/dsound/initterm.cpp
+++ tags/trunk/src/dsound/initterm.cpp
@@ -1,4 +1,4 @@
-/* $Id: initterm.cpp,v 1.8 1999-11-14 22:12:15 hugh Exp $ */
+/* $Id: initterm.cpp,v 1.9 2000-02-05 01:58:50 sandervl Exp $ */
 
 /*
  * DLL entry point
@@ -32,26 +32,39 @@
 #include <string.h>
 #include <odin.h>
 #include <win32type.h>
+#include <winconst.h>
 #include <odinlx.h>
 #include <misc.h>       /*PLF Wed  98-03-18 23:18:15*/
-#include "initterm.h"
 
 extern "C" {
 void CDECL _ctordtorInit( void );
 void CDECL _ctordtorTerm( void );
+
+ //Win32 resource table (produced by wrc)
+ extern DWORD _Resource_PEResTab;
 }
 
-char  dsoundPath[CCHMAXPATH] = "";
+char dsoundPath[CCHMAXPATH] = "";
 
-/*-------------------------------------------------------------------*/
-/* A clean up routine registered with DosExitList must be used if    */
-/* runtime calls are required and the runtime is dynamically linked. */
-/* This will guarantee that this clean up routine is run before the  */
-/* library DLL is terminated.                                        */
-/*-------------------------------------------------------------------*/
-static void APIENTRY cleanup(ULONG reason);
+//******************************************************************************
+//******************************************************************************
+BOOL WINAPI LibMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID fImpLoad)
+{
+   switch (fdwReason)
+   {
+   case DLL_PROCESS_ATTACH:
+	return TRUE;
 
+   case DLL_THREAD_ATTACH:
+   case DLL_THREAD_DETACH:
+	return TRUE;
 
+   case DLL_PROCESS_DETACH:
+	_ctordtorTerm();
+	return TRUE;
+   }
+   return FALSE;
+}
 /****************************************************************************/
 /* _DLL_InitTerm is the function that gets called by the operating system   */
 /* loader when it loads and frees this DLL for each process that accesses   */
@@ -75,30 +88,20 @@ unsigned long SYSTEM _DLL_InitTerm(unsigned long hModule, unsigned long
    switch (ulFlag) {
       case 0 :
       {
-         _ctordtorInit();
-
-   DosQueryModuleName(hModule, CCHMAXPATH, dsoundPath);
+   	 DosQueryModuleName(hModule, CCHMAXPATH, dsoundPath);
          char *endofpath = strrchr(dsoundPath, '\\');
          if(endofpath) *(endofpath+1) = 0;
+         _ctordtorInit();
 
          CheckVersionFromHMOD(PE2LX_VERSION, hModule); /*PLF Wed  98-03-18 05:28:48*/
 
-         /*******************************************************************/
-         /* A DosExitList routine must be used to clean up if runtime calls */
-         /* are required and the runtime is dynamically linked.             */
-         /*******************************************************************/
-
-   if(RegisterLxDll(hModule, 0, 0) == FALSE)
-    return 0UL;
-
-         rc = DosExitList(0x0000F000|EXLST_ADD, cleanup);
-         if(rc)
-                return 0UL;
+	 if(RegisterLxDll(hModule, LibMain, (PVOID)&_Resource_PEResTab) == FALSE) 
+		return 0UL;
 
          break;
       }
       case 1 :
-   UnregisterLxDll(hModule);
+	 UnregisterLxDll(hModule);
          break;
       default  :
          return 0UL;
@@ -109,11 +112,5 @@ unsigned long SYSTEM _DLL_InitTerm(unsigned long hModule, unsigned long
    /***********************************************************/
    return 1UL;
 }
-
-
-static void APIENTRY cleanup(ULONG ulReason)
-{
-   _ctordtorTerm();
-   DosExitList(EXLST_EXIT, cleanup);
-   return ;
-}
+//******************************************************************************
+//******************************************************************************
