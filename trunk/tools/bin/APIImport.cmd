78be3b4ee9168b9eea4b72ab76071d4248f4a32a
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-14 14:31:35 +0000

    msvcrt directory contains tree dlls. wnetap32 fix is now here.

--- trunk/tools/bin/APIImport.cmd
+++ trunk/tools/bin/APIImport.cmd
@@ -1,4 +1,4 @@
-/* $Id: APIImport.cmd,v 1.1 2000-02-11 14:21:19 bird Exp $
+/* $Id: APIImport.cmd,v 1.2 2000-02-14 14:31:35 bird Exp $
  *
  * Helper script which invokes APIImport.exe with the correct .def file.
  *
@@ -10,13 +10,27 @@
     call RxFuncAdd 'SysFileTree', 'RexxUtil', 'SysFileTree';
 
     sDllName = filespec('name', directory());
-    call MakeTempDeffile sDllName, 'APIImport.def';
 
     parse source sD1 sD2 sSrc;
 
     sSrc = filespec('drive', sSrc) || filespec('path', sSrc);
-    sSrc||'APIImport.exe APIImport.def';
-
+    sAPIImport= sSrc||'APIImport.exe';
+    if (sDllName = 'msvcrt') then
+    do
+        call MakeTempDeffile sDllName, 'APIImport.def';
+        sAPIImport || ' APIImport.def';
+        call MakeTempDeffile sDllName||'20', 'APIImport20.def';
+        sAPIImport || ' APIImport20.def';
+        call MakeTempDeffile sDllName||'40', 'APIImport40.def';
+        sAPIImport || ' APIImport40.def';
+    end
+    else
+    do
+        if (translate(sDllName) = 'WNETAP32') then
+            sDllName = 'netapi32';
+        call MakeTempDeffile sDllName, 'APIImport.def';
+        sAPIImport || ' APIImport.def';
+    end
     exit(rc);
 
 
@@ -29,7 +43,7 @@ MakeTempDeffile: procedure;
     if (pos('READY', rc) <> 1) then
     do
         rc = SysFileTree('*.def', 'asFiles', 'FO');
-        if rc == 0 then
+        if rc = 0 then
         do
             do i = 1 to asFiles.0
                 if translate(substr(asFiles.i, length(asFiles.i) - 7, 7)) <> 'EXP.DEF' then
