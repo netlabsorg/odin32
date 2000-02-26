552f106524423940a63f621f9732c31dfba4b720
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-26 00:46:31 +0000

    More elf definitions and declarations.

--- tags/trunk/src/win32k/elf2lx/elf2lx.cpp
+++ tags/trunk/src/win32k/elf2lx/elf2lx.cpp
@@ -1,8 +1,8 @@
-/* $Id: elf2lx.cpp,v 1.1 1999-11-10 01:45:31 bird Exp $
+/* $Id: elf2lx.cpp,v 1.2 2000-02-26 00:46:30 bird Exp $
  *
  * Elf2Lx - implementation.
  *
- * Copyright (c) 1999 knut st. osmundsen
+ * Copyright (c) 1999-2000 knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
  *
  * Project Odin Software License can be found in LICENSE.TXT
  *
