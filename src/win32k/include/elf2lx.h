821e1f55703ea2875090caf5da14c4721d3b5bc3
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-02-27 02:16:04 +0000

    Started thinking thru writing...

--- src/win32k/include/elf2lx.h
+++ src/win32k/include/elf2lx.h
@@ -1,4 +1,4 @@
-/* $Id: elf2lx.h,v 1.2 2000-02-26 00:46:31 bird Exp $
+/* $Id: elf2lx.h,v 1.3 2000-02-27 02:16:04 bird Exp $
  *
  * Elf2Lx - Declaration.
  *
@@ -12,15 +12,53 @@
 #define _ELF2LX_H_
 
 
+/**
+ * Elf to LX converter class.
+ * @version
+ * @verdesc
+ * @author      knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
+ * @approval
+ */
 class Elf2Lx : public ModuleBase
 {
 public:
+    /** @cat Constructor/Destructor */
     Elf2Lx(SFN hFile);
-    ULONG   init(PCSZ pszFilename)
+    ~Elf2Lx();
+
+    /** @cat Public Main methods */
+    ULONG  init(PCSZ pszFilename);
+    ULONG  read(ULONG offLXFile, PVOID pvBuffer, ULONG cbToRead, ULONG flFlags, PMTE pMTE);
+    ULONG  applyFixups(PMTE pMTE, ULONG iObject, ULONG iPageTable, PVOID pvPage,
+                       ULONG ulPageAddress, PVOID pvPTDA); /*(ldrEnum32bitRelRecs)*/
+    #ifndef RING0
+    ULONG  writeFile(PCSZ pszLXFilename);
+    #endif
+
+    /** @cat public Helper methods */
+    VOID   dumpVirtualLxFile();
+    BOOL   queryIsModuleName(PCSZ pszFilename);
+
+    /** @cat Static helper methods */
+    static BOOL validHeader(Elf32_Ehdr *pEhdr);
 
 
 private:
+    /** @cat private data members. */
 
+    /**
+     * @cat Elf structures
+     */
+    Elf32_Ehdr *       pEhdr;
+    Elf32_Phdr *       paPhdrs;
+
+    /**
+     * @cat LX structures
+     */
+    struct e32_exe     LXHdr;           /* Lxheader */
+
+    struct o32_obj *   paObjTab;        /* Pointer to object table - if null check cObjects > 0 and generate it using makeObjectTable */
+    struct o32_map *   paObjPageTab;    /* Pointer to object page table - if null check cObjects > 0 and generate it using makeObjectPageTable */
 };
 
 
