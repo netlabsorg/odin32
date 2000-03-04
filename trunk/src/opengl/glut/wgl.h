a12094b36c43c6fde4df598156113ce21c11ce69
jeroen <jeroen@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-04 19:37:40 +0000

    *** empty log message ***

--- trunk/src/opengl/glut/wgl.h
+++
@@ -1,9 +0,0 @@
-/* $Id: wgl.h,v 1.2 2000-02-09 08:46:22 jeroen Exp $ */
-
-typedef ULONG HGC;
-
-BOOL APIENTRY wglDeleteContext(HGC hgc);
-HDC  APIENTRY wglGetCurrentDC(void);
-HGC  APIENTRY wglGetCurrentContext(void);
-BOOL APIENTRY wglMakeCurrent(HDC hdc,HGC hgc);
-HGC  APIENTRY wglCreateContext(HDC hdc);
