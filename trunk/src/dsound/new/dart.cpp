a2ec3e9691a4b253a40b9738998140dacdc6318e
mike <mike@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-06 13:11:41 +0000

    *** empty log message ***

---
+++ trunk/src/dsound/new/dart.cpp
@@ -0,0 +1,354 @@
+/*
+ *  Kevin Langman
+ *
+ *  The Dart functions are here to avoid the conficts with the
+ *  the OS/2 mcios2.h, meerror.h, os2medef.h and mmsystem.h
+ *
+ */
+
+#define INCL_DOS
+#define INCL_OS2MM
+#include <os2wrap.h>
+#include <os2mewrap.h>
+
+#include <stdlib.h>
+#include <string.h>
+
+#define DART_DSOUND
+#include "dart.h"
+#include "dsmixer.h"
+
+#include <misc.h>
+
+static MCI_MIXSETUP_PARMS *MixSetup_Global;
+static MCI_MIX_BUFFER     *pMixBuffs;
+static long               lLastBuff;
+static char               *pDSoundBuff;
+
+USHORT               usDeviceID;                 /* Amp Mixer device id          */
+MCI_MIX_BUFFER       MixBuffers[NUM_DART_BUFFS]; /* Device buffers               */
+MCI_MIXSETUP_PARMS   MixSetupParms;              /* Mixer parameters             */
+MCI_BUFFER_PARMS     BufferParms;                /* Device buffer parms          */
+
+
+#define ULONG_LOWD(ul)  (*(USHORT *)((ULONG *)(&ul)))     /* Low  Word */
+
+//******************************************************************************
+//******************************************************************************
+LONG APIENTRY OS2_Dart_Update(ULONG ulStatus,PMCI_MIX_BUFFER pBuffer,ULONG ulFlags)
+{
+   //MCI_STATUS_PARMS Status;
+   ULONG           rc;
+
+   if ((ulFlags == MIX_WRITE_COMPLETE) ||
+       ((ulFlags == (MIX_WRITE_COMPLETE | MIX_STREAM_ERROR))&&
+       (ulStatus == ERROR_DEVICE_UNDERRUN)))
+   {
+      lLastBuff++;
+      if (lLastBuff == NUM_DART_BUFFS)
+         lLastBuff = 0;
+
+      /* Now mix sound from all playing secondary SoundBuffers into the primary buffer */
+      MixCallback(BUFFER_SIZE/NUM_DART_BUFFS);
+
+      /* Fill The next buff from the DSound Buffer */
+      memcpy( pMixBuffs[lLastBuff].pBuffer, &pDSoundBuff[lLastBuff*(BUFFER_SIZE/NUM_DART_BUFFS)], BUFFER_SIZE/NUM_DART_BUFFS );
+
+      /* Send the NEXT Buffer to Dart for playing! */
+      rc = MixSetup_Global->pmixWrite(MixSetup_Global->ulMixHandle, &pMixBuffs[lLastBuff], 1 );
+      //dprintf(("DSOUND-DART: Playing Next Buffer %d", rc));
+   }
+
+   return TRUE;
+}
+
+
+long Dart_Open_Device(USHORT *pusDeviceID, void **vpMixBuffer, void **vpMixSetup,
+                      void **vpBuffParms,  void **ppvBuffer)
+{
+
+   MCI_AMP_OPEN_PARMS  AmpOpenParms;
+   //MCI_WAVE_SET_PARMS WaveSetParms;
+   ULONG   rc;
+   short   device = 0;
+
+   dprintf(("DSOUND-DART: Dart_Open_Device"));
+
+   *vpMixBuffer    = &MixBuffers;
+   *vpMixSetup     = &MixSetupParms;
+   *vpBuffParms    = &BufferParms;
+
+   pMixBuffs = &MixBuffers[0];
+   lLastBuff = 0;
+
+   /* Is there a way to avoid the use of the MixSetup_Global ????? */
+   MixSetup_Global = &MixSetupParms;
+   /****************************************************************/
+
+   // If the DSOUND_DEVICE is set then use that number as the device for DART.
+   // this will allow people with many sound cards to use the card of their choice
+   // for an instance of a DSOUND enabled app!
+   if (getenv("DSOUND_DEVICE") != NULL) {
+      device = atoi(getenv("DSOUND_DEVICE"));
+   }
+
+   /* Open the AmpMix Device and start processing the buffer! */
+   memset(&AmpOpenParms, 0, sizeof(MCI_AMP_OPEN_PARMS));
+   AmpOpenParms.usDeviceID    = 0;
+   AmpOpenParms.pszDeviceType = (PSZ)MCI_DEVTYPE_AUDIO_AMPMIX;
+
+   rc = mciSendCommand(0, MCI_OPEN, MCI_WAIT | MCI_OPEN_TYPE_ID, (PVOID)&AmpOpenParms, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_OPEN %d", rc));
+      return DSERR_GENERIC;
+   }
+   *pusDeviceID = AmpOpenParms.usDeviceID;
+   usDeviceID   = AmpOpenParms.usDeviceID;
+
+   /* setup playback parameters */
+   memset(&MixSetupParms, 0, sizeof(MCI_MIXSETUP_PARMS));
+   MixSetupParms.ulBitsPerSample  = 16;
+   MixSetupParms.ulFormatTag      = MCI_WAVE_FORMAT_PCM;
+   MixSetupParms.ulSamplesPerSec  = 22500;
+   MixSetupParms.ulChannels       = 2;
+   MixSetupParms.ulFormatMode     = MCI_PLAY;
+   MixSetupParms.ulDeviceType     = MCI_DEVTYPE_WAVEFORM_AUDIO;
+   MixSetupParms.pmixEvent        = OS2_Dart_Update;
+
+   rc = mciSendCommand(usDeviceID, MCI_MIXSETUP, MCI_WAIT | MCI_MIXSETUP_INIT,
+                      (PVOID)&MixSetupParms, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_MIXSETUP (Constructor) %d", rc));
+      return DSERR_GENERIC;
+   }
+
+   /* Create the Audio Buffer */
+   // OK... Since DSound only uses 1 buffer and uses the GetPosition API to
+   // figure out where it can and can't write, I have emulating this by
+   // using 16 1kb buffers and do locking by tracking what buffer is currently
+   // playing. This maybe less CPU friendly then other methods but it's the best
+   // my little brain could come up with!!
+
+   MixSetupParms.ulBufferSize = BUFFER_SIZE/NUM_DART_BUFFS;
+
+//   BufferParms->ulStructLength = sizeof(MCI_BUFFER_PARMS);
+   BufferParms.ulNumBuffers   = NUM_DART_BUFFS;
+   BufferParms.ulBufferSize   = MixSetupParms.ulBufferSize;
+   BufferParms.pBufList       = MixBuffers;
+
+   rc = mciSendCommand(usDeviceID, MCI_BUFFER, MCI_WAIT | MCI_ALLOCATE_MEMORY,
+                                (PVOID)&BufferParms, 0);
+   if ( ULONG_LOWD(rc) != MCIERR_SUCCESS ) {
+      dprintf(("DSOUND-DART: MCI_BUFFER (Alloc) %d", rc));
+      mciSendCommand(*pusDeviceID, MCI_CLOSE, MCI_WAIT, NULL, 0);
+      return DSERR_OUTOFMEMORY;
+   }
+
+   /* Clear the Buffer */
+   // Todo:  I don't know if Dart expects Signed or Unsigned data... This will
+   // ok until I look in to this... Also it may be that Dart uses signed for
+   // 8bit data and unsigned for 16it.. Anyhow the worst that can happen by setting
+   // the buffer to 0 is a click on Dart_Play and a Clink when real sound is written
+   // to the buffer!
+   for (device = 0; device < NUM_DART_BUFFS; device++) {
+      memset(MixBuffers[device].pBuffer, 0, BUFFER_SIZE/NUM_DART_BUFFS);
+   }
+
+   // Allocate memory for the DSound "Holder" buffer.
+   // TODO: Move this to the Constructor for OS2PrimBuff
+   //       so that the Deconstructor can be used to free the memory!
+   *(char**)ppvBuffer = (char*)malloc(BUFFER_SIZE);
+   if (*ppvBuffer == NULL) {
+      return DSERR_OUTOFMEMORY;
+   }
+   pDSoundBuff = (char*)(*ppvBuffer);
+
+   dprintf(("DSOUND-DART: Dart_Open_Device Exiting"));
+
+   return DS_OK;
+}
+
+long Dart_Close_Device(USHORT usDeviceID, void *vpMixBuffer, void *vpMixSetup,
+                       void *vpBuffParms )
+{
+   MCI_MIX_BUFFER      *MixBuffer;
+   MCI_MIXSETUP_PARMS  *MixSetup;
+   MCI_BUFFER_PARMS    *BufferParms;
+
+   dprintf(("DSOUND-DART: Dart_Close_Device"));
+
+   MixBuffer   = (MCI_MIX_BUFFER*)vpMixBuffer;
+   MixSetup    = (MCI_MIXSETUP_PARMS*)vpMixSetup;
+   BufferParms = (MCI_BUFFER_PARMS*)vpBuffParms;
+
+    if (MixBuffer->pBuffer) {
+       mciSendCommand(usDeviceID,MCI_BUFFER,MCI_WAIT|MCI_DEALLOCATE_MEMORY, BufferParms,0);
+       MixBuffer->pBuffer=NULL;
+    }
+    if (usDeviceID) {
+        mciSendCommand(usDeviceID,MCI_CLOSE,MCI_WAIT, NULL, 0);
+        usDeviceID=0;
+    }
+
+   dprintf(("DSOUND-DART: Dart_Close_Device returning DS_OK"));
+   return DS_OK;
+}
+
+
+long Dart_GetPosition(USHORT usDeviceID, LONG *pulPosition)
+{
+   dprintf(("DSOUND-DART: Dart_GetPosition"));
+
+   *pulPosition = (lLastBuff * (BUFFER_SIZE/NUM_DART_BUFFS)) + (BUFFER_SIZE/NUM_DART_BUFFS);
+   if (*pulPosition > BUFFER_SIZE)
+      *pulPosition = 0;
+
+   dprintf(("DSOUND-DART: Returning %d", *pulPosition));
+
+   return DS_OK;
+}
+
+long Dart_SetFormat(USHORT *pusDeviceID, void *vpMixSetup, void *vpBufferParms, void *vpMixBuffer, LONG lBPS, LONG lSPS, LONG lChannels )
+{
+   ULONG   rc;
+   MCI_MIXSETUP_PARMS  *MixSetup;
+   MCI_BUFFER_PARMS    *BufferParms;
+   MCI_MIX_BUFFER      *MixBuffer;
+   MCI_AMP_OPEN_PARMS  AmpOpenParms;
+   short               device = 0;
+
+   MixSetup    = (MCI_MIXSETUP_PARMS*)vpMixSetup;
+   BufferParms = (MCI_BUFFER_PARMS*)vpBufferParms;
+   MixBuffer   = (MCI_MIX_BUFFER*)vpMixBuffer;
+
+   dprintf(("DSOUND-DART: Dart_SetFormat"));
+
+   // If the DSOUND_DEVICE is set then use that number as the device for DART.
+   // this will allow people with many sound cards to use the card of there choice
+   // for an instance of a DSOUND enabled app!
+   if (getenv("DSOUND_DEVICE") != NULL) {
+      device = atoi(getenv( "DSOUND_DEVICE" ));
+   }
+
+   /* Dealloc to avoid the 5511 error */
+   rc = mciSendCommand(*pusDeviceID, MCI_BUFFER, MCI_WAIT | MCI_DEALLOCATE_MEMORY,
+                       BufferParms, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_DEALLOCATE_MEMORY (SetFormat) %d", rc));
+      return DSERR_GENERIC;
+   }
+
+   rc = mciSendCommand(*pusDeviceID, MCI_CLOSE, MCI_WAIT, NULL, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_CLOSE (SetFormat) %d", rc));
+      return(DSERR_GENERIC);
+   }
+
+   /* Reopen the MixAmp Device and start processing the buffer! */
+   memset(&AmpOpenParms,0,sizeof(MCI_AMP_OPEN_PARMS));
+   AmpOpenParms.usDeviceID    = 0;
+   AmpOpenParms.pszDeviceType = (PSZ)MAKEULONG(MCI_DEVTYPE_AUDIO_AMPMIX, (USHORT)device);
+
+   rc = mciSendCommand(0, MCI_OPEN, MCI_WAIT | MCI_OPEN_TYPE_ID, (PVOID)&AmpOpenParms, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_OPEN %d", rc));
+      return DSERR_GENERIC;
+   }
+   *pusDeviceID = AmpOpenParms.usDeviceID;
+
+   /* setup playback parameters */
+   memset(MixSetup, 0, sizeof(MCI_MIXSETUP_PARMS));
+   MixSetup->ulBitsPerSample  = lBPS;
+   MixSetup->ulFormatTag      = MCI_WAVE_FORMAT_PCM;
+   MixSetup->ulSamplesPerSec  = lSPS;
+   MixSetup->ulChannels       = lChannels;
+   MixSetup->ulFormatMode     = MCI_PLAY;
+   MixSetup->ulDeviceType     = MCI_DEVTYPE_WAVEFORM_AUDIO;
+   MixSetup->pmixEvent        = OS2_Dart_Update;
+
+   rc = mciSendCommand(*pusDeviceID, MCI_MIXSETUP, MCI_WAIT | MCI_MIXSETUP_INIT,
+                       (PVOID)MixSetup, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_MIXSETUP (SetFormat) %d", rc));
+      return DSERR_GENERIC;
+   }
+
+   memset(BufferParms, 0, sizeof(MCI_BUFFER_PARMS));
+   memset(MixBuffer,   0, sizeof(MCI_MIX_BUFFER) * NUM_DART_BUFFS);
+   BufferParms->ulStructLength = sizeof(MCI_BUFFER_PARMS);
+   BufferParms->ulNumBuffers   = NUM_DART_BUFFS;
+   BufferParms->ulBufferSize   = BUFFER_SIZE/NUM_DART_BUFFS;
+   BufferParms->pBufList       = MixBuffer;
+   MixBuffer->pBuffer          = NULL;
+
+   rc = mciSendCommand(*pusDeviceID, MCI_BUFFER, MCI_WAIT | MCI_ALLOCATE_MEMORY,
+                       (PVOID)BufferParms, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_BUFFER_ALLOCATE_MEMORY (SetFormat) %d", rc));
+      mciSendCommand(*pusDeviceID, MCI_CLOSE, MCI_WAIT, NULL, 0);
+      memset(MixBuffer, 0, sizeof(MCI_MIX_BUFFER) * NUM_DART_BUFFS);
+      return DSERR_OUTOFMEMORY;
+   }
+
+   /* Clear the Buffer */
+   // Todo:  I don't know if Dart expects Signed or Unsigned data... This will
+   // ok until I look in to this... Also it may be that Dart uses signed for
+   // 8bit data and unsigned for 16it.. Anyhow the worst that can happen by setting
+   // the buffer to 0 is a click on Dart_Play and a Clink when real sound is written
+   // to the buffer!
+   for (int i=0; i<NUM_DART_BUFFS; i++) {
+      memset(MixBuffer[i].pBuffer, 0, BUFFER_SIZE/NUM_DART_BUFFS);
+   }
+
+   return DS_OK;
+}
+
+
+long Dart_Stop(USHORT usDeviceID)
+{
+   ULONG   rc;
+
+   dprintf(("DSOUND-DART: Dart_Stop"));
+
+//   rc = mciSendCommand(usDeviceID, MCI_PAUSE, MCI_WAIT, NULL, 0);
+   rc = mciSendCommand(usDeviceID, MCI_STOP, MCI_WAIT, NULL, 0);
+   if (rc != MCIERR_SUCCESS) {
+      dprintf(("DSOUND-DART: MCI_PAUSE %d", rc));
+      return DSERR_GENERIC;
+   }
+   return DS_OK;
+}
+
+long Dart_Play(USHORT usDeviceID, void *vpMixSetup, void *vpMixBuffer, long playing)
+{
+   ULONG   rc;
+   MCI_MIXSETUP_PARMS  *MixSetup;
+   MCI_MIX_BUFFER      *MixBuffer;
+
+   MixSetup  = (MCI_MIXSETUP_PARMS*)vpMixSetup;
+   MixBuffer = (MCI_MIX_BUFFER*)vpMixBuffer;
+
+   dprintf(("DSOUND-DART: Dart_Play"));
+
+   if (playing == TRUE) {
+      rc = mciSendCommand(usDeviceID, MCI_RESUME, MCI_WAIT, NULL, 0);
+      if (rc != MCIERR_SUCCESS) {
+         dprintf(("DSOUND-DART: MCI_RESUME %d", rc));
+         return DSERR_GENERIC;
+      }
+   } else { //if (playing==FALSE)
+      dprintf(("DSOUND-DART: Playback started!!!!"));
+
+      /* Mix the first buffer before playing */
+      MixCallback(BUFFER_SIZE/NUM_DART_BUFFS);
+      memcpy(pMixBuffs[lLastBuff].pBuffer, &pDSoundBuff[lLastBuff*(BUFFER_SIZE/NUM_DART_BUFFS)], BUFFER_SIZE/NUM_DART_BUFFS);
+
+      USHORT  sel = RestoreOS2FS();
+      /* Note: the call to pmixWrite trashes the FS selector, we have to save */
+      /* and then restore FS!!! Otherwise exception handling will be broken.  */
+      MixSetupParms.pmixWrite(MixSetupParms.ulMixHandle, MixBuffers, NUM_DART_BUFFS);
+      SetFS(sel);
+   }
+
+   return DS_OK;
+}
---
+++ trunk/src/dsound/new/resource.asm
@@ -0,0 +1,160 @@
+;/* This file is generated with wrc version 1.0.14 (08-Aug-1999). Do not edit! */
+;/* Source : dsound.rc */
+;/* Cmdline: wrc -s -I. -ID:\IBMCPP\include -I..\..\include -I..\..\include\win -o resource.asm dsound.rc */
+;/* Date   : Thu Feb 24 23:50:09 2000 */
+
+	.386p
+	.model flat
+	.data
+
+	align	4
+_Resource_PEResTab:
+	public	_Resource_PEResTab
+	dd	0
+	dd	038b5b5a1h
+	dd	0
+	dw	0, 1
+	dd	16
+	dd	(L16 - _Resource_PEResTab) or 80000000h
+L16:
+	dd	0
+	dd	038b5b5a1h
+	dd	0
+	dw	0, 1
+	dd	1
+	dd	(L16_1 - _Resource_PEResTab) or 80000000h
+L16_1:
+	dd	0
+	dd	038b5b5a1h
+	dd	0
+	dw	0, 1
+	dd	000000000h
+	dd	L16_1_0 - _Resource_PEResTab
+_Resource_ResourceDirectory:
+	public	_Resource_ResourceDirectory
+L16_1_0:
+	dd	_Resource_VerInf_1_0_data - _Resource_PEResTab
+	dd	804
+	dd	0
+	dd	0
+
+LResTabEnd:
+
+;/* Resource binary data */
+
+	align	4
+_Resource_VerInf_1_0_data:
+	db	024h, 003h, 034h, 000h, 000h, 000h, 056h, 000h
+	db	053h, 000h, 05fh, 000h, 056h, 000h, 045h, 000h
+	db	052h, 000h, 053h, 000h, 049h, 000h, 04fh, 000h
+	db	04eh, 000h, 05fh, 000h, 049h, 000h, 04eh, 000h
+	db	046h, 000h, 04fh, 000h, 000h, 000h, 000h, 000h
+	db	0bdh, 004h, 0efh, 0feh, 000h, 000h, 001h, 000h
+	db	000h, 000h, 001h, 000h, 000h, 000h, 000h, 000h
+	db	000h, 000h, 001h, 000h, 000h, 000h, 000h, 000h
+	db	03fh, 000h, 000h, 000h, 00ah, 000h, 000h, 000h
+	db	001h, 000h, 001h, 000h, 001h, 000h, 000h, 000h
+	db	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
+	db	000h, 000h, 000h, 000h, 084h, 002h, 000h, 000h
+	db	000h, 000h, 053h, 000h, 074h, 000h, 072h, 000h
+	db	069h, 000h, 06eh, 000h, 067h, 000h, 046h, 000h
+	db	069h, 000h, 06ch, 000h, 065h, 000h, 049h, 000h
+	db	06eh, 000h, 066h, 000h, 06fh, 000h, 000h, 000h
+	db	060h, 002h, 000h, 000h, 000h, 000h, 030h, 000h
+	db	034h, 000h, 030h, 000h, 039h, 000h, 030h, 000h
+	db	034h, 000h, 045h, 000h, 034h, 000h, 000h, 000h
+	db	03ch, 000h, 012h, 000h, 001h, 000h, 043h, 000h
+	db	06fh, 000h, 06dh, 000h, 06dh, 000h, 065h, 000h
+	db	06eh, 000h, 074h, 000h, 073h, 000h, 000h, 000h
+	db	04fh, 000h, 064h, 000h, 069h, 000h, 06eh, 000h
+	db	033h, 000h, 032h, 000h, 020h, 000h, 053h, 000h
+	db	079h, 000h, 073h, 000h, 074h, 000h, 065h, 000h
+	db	06dh, 000h, 020h, 000h, 044h, 000h, 06ch, 000h
+	db	06ch, 000h, 000h, 000h, 034h, 000h, 00ah, 000h
+	db	001h, 000h, 043h, 000h, 06fh, 000h, 06dh, 000h
+	db	070h, 000h, 061h, 000h, 06eh, 000h, 079h, 000h
+	db	04eh, 000h, 061h, 000h, 06dh, 000h, 065h, 000h
+	db	000h, 000h, 000h, 000h, 04fh, 000h, 064h, 000h
+	db	069h, 000h, 06eh, 000h, 020h, 000h, 054h, 000h
+	db	065h, 000h, 061h, 000h, 06dh, 000h, 000h, 000h
+	db	04ch, 000h, 012h, 000h, 001h, 000h, 046h, 000h
+	db	069h, 000h, 06ch, 000h, 065h, 000h, 044h, 000h
+	db	065h, 000h, 073h, 000h, 063h, 000h, 072h, 000h
+	db	069h, 000h, 070h, 000h, 074h, 000h, 069h, 000h
+	db	06fh, 000h, 06eh, 000h, 000h, 000h, 000h, 000h
+	db	044h, 000h, 053h, 000h, 04fh, 000h, 055h, 000h
+	db	04eh, 000h, 044h, 000h, 020h, 000h, 053h, 000h
+	db	079h, 000h, 073h, 000h, 074h, 000h, 065h, 000h
+	db	06dh, 000h, 020h, 000h, 064h, 000h, 06ch, 000h
+	db	06ch, 000h, 000h, 000h, 02ah, 000h, 005h, 000h
+	db	001h, 000h, 046h, 000h, 069h, 000h, 06ch, 000h
+	db	065h, 000h, 056h, 000h, 065h, 000h, 072h, 000h
+	db	073h, 000h, 069h, 000h, 06fh, 000h, 06eh, 000h
+	db	000h, 000h, 000h, 000h, 034h, 000h, 02eh, 000h
+	db	030h, 000h, 034h, 000h, 000h, 000h, 000h, 000h
+	db	02eh, 000h, 007h, 000h, 001h, 000h, 049h, 000h
+	db	06eh, 000h, 074h, 000h, 065h, 000h, 072h, 000h
+	db	06eh, 000h, 061h, 000h, 06ch, 000h, 04eh, 000h
+	db	061h, 000h, 06dh, 000h, 065h, 000h, 000h, 000h
+	db	044h, 000h, 053h, 000h, 04fh, 000h, 055h, 000h
+	db	04eh, 000h, 044h, 000h, 000h, 000h, 000h, 000h
+	db	054h, 000h, 018h, 000h, 001h, 000h, 04ch, 000h
+	db	065h, 000h, 067h, 000h, 061h, 000h, 06ch, 000h
+	db	043h, 000h, 06fh, 000h, 070h, 000h, 079h, 000h
+	db	072h, 000h, 069h, 000h, 067h, 000h, 068h, 000h
+	db	074h, 000h, 000h, 000h, 043h, 000h, 06fh, 000h
+	db	070h, 000h, 079h, 000h, 072h, 000h, 069h, 000h
+	db	067h, 000h, 068h, 000h, 074h, 000h, 020h, 000h
+	db	028h, 000h, 043h, 000h, 029h, 000h, 020h, 000h
+	db	031h, 000h, 039h, 000h, 039h, 000h, 039h, 000h
+	db	02dh, 000h, 032h, 000h, 030h, 000h, 030h, 000h
+	db	030h, 000h, 000h, 000h, 02ah, 000h, 001h, 000h
+	db	001h, 000h, 04ch, 000h, 065h, 000h, 067h, 000h
+	db	061h, 000h, 06ch, 000h, 054h, 000h, 072h, 000h
+	db	061h, 000h, 064h, 000h, 065h, 000h, 06dh, 000h
+	db	061h, 000h, 072h, 000h, 06bh, 000h, 073h, 000h
+	db	000h, 000h, 000h, 000h, 000h, 000h, 000h, 000h
+	db	03eh, 000h, 00bh, 000h, 001h, 000h, 04fh, 000h
+	db	072h, 000h, 069h, 000h, 067h, 000h, 069h, 000h
+	db	06eh, 000h, 061h, 000h, 06ch, 000h, 046h, 000h
+	db	069h, 000h, 06ch, 000h, 065h, 000h, 06eh, 000h
+	db	061h, 000h, 06dh, 000h, 065h, 000h, 000h, 000h
+	db	044h, 000h, 053h, 000h, 04fh, 000h, 055h, 000h
+	db	04eh, 000h, 044h, 000h, 02eh, 000h, 044h, 000h
+	db	04ch, 000h, 04ch, 000h, 000h, 000h, 000h, 000h
+	db	040h, 000h, 010h, 000h, 001h, 000h, 050h, 000h
+	db	072h, 000h, 06fh, 000h, 064h, 000h, 075h, 000h
+	db	063h, 000h, 074h, 000h, 04eh, 000h, 061h, 000h
+	db	06dh, 000h, 065h, 000h, 000h, 000h, 000h, 000h
+	db	04fh, 000h, 064h, 000h, 069h, 000h, 06eh, 000h
+	db	033h, 000h, 032h, 000h, 020h, 000h, 02dh, 000h
+	db	020h, 000h, 044h, 000h, 053h, 000h, 04fh, 000h
+	db	055h, 000h, 04eh, 000h, 044h, 000h, 000h, 000h
+	db	02eh, 000h, 005h, 000h, 001h, 000h, 050h, 000h
+	db	072h, 000h, 06fh, 000h, 064h, 000h, 075h, 000h
+	db	063h, 000h, 074h, 000h, 056h, 000h, 065h, 000h
+	db	072h, 000h, 073h, 000h, 069h, 000h, 06fh, 000h
+	db	06eh, 000h, 000h, 000h, 034h, 000h, 02eh, 000h
+	db	030h, 000h, 034h, 000h, 000h, 000h, 000h, 000h
+	db	044h, 000h, 000h, 000h, 000h, 000h, 056h, 000h
+	db	061h, 000h, 072h, 000h, 046h, 000h, 069h, 000h
+	db	06ch, 000h, 065h, 000h, 049h, 000h, 06eh, 000h
+	db	066h, 000h, 06fh, 000h, 000h, 000h, 000h, 000h
+	db	024h, 000h, 004h, 000h, 000h, 000h, 054h, 000h
+	db	072h, 000h, 061h, 000h, 06eh, 000h, 073h, 000h
+	db	06ch, 000h, 061h, 000h, 074h, 000h, 069h, 000h
+	db	06fh, 000h, 06eh, 000h, 000h, 000h, 000h, 000h
+	db	009h, 004h, 0e4h, 004h
+
+	align	4
+_Resource_ResourceDescriptor:
+	public	_Resource_ResourceDescriptor
+_Resource_ResourceTable:
+	dd	_Resource_PEResTab
+_Resource_NumberOfResources:
+	dd	1
+_Resource_ResourceSectionSize:
+	dd	LResTabEnd - _Resource_PEResTab
+_Resource_ResourcesEntries:
+	dd	_Resource_ResourceDirectory
+	end
