d6603c87f4d68947f13c1109cb6c76368d74f84c
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-09 20:46:35 +0000

    *** empty log message ***

--- trunk/src/win32k/Check.cmd
+++ trunk/src/win32k/Check.cmd
@@ -1,8 +1,9 @@
-/* $Id: Check.cmd,v 1.1 2000-03-09 20:30:43 bird Exp $
+/* $Id: Check.cmd,v 1.2 2000-03-09 20:46:35 bird Exp $
  *
  * Dirty hack to get around the nmake feature of passing on MAKEFLAGS.
  * This simply executes it's arguments and returns the errorcode.
  */
     parse arg sArgs
+    '@echo off';
     sArgs;
     exit RC ;
