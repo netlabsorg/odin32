74ca1257aa8c1d2fc79b2a16a962ca4b26c19fef
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-17 16:07:53 +0000

    WSAAsyncSelect fix

---
+++ trunk/src/wsock32/dbglocal.h
@@ -0,0 +1,43 @@
+/* $Id: dbglocal.h,v 1.1 2000-03-17 16:07:13 sandervl Exp $ */
+
+/*
+ * debug logging functions for OS/2
+ *
+ * Overrides main dprintf macros
+ *
+ * Copyright 2000 Sander van Leeuwen
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ */
+#ifndef __DBGLOCAL_H__
+#define __DBGLOCAL_H__
+
+#ifdef DEBUG
+//Parses environment variable for selective enabling/disabling of logging
+void ParseLogStatus();
+
+#define DBG_ENVNAME        "dbg_wsock32"
+
+#define DBG_initterm       0
+#define DBG_relaywin       1
+#define DBG_wsock32        2
+#define DBG_unknown        3
+#define DBG_async          4
+#define DBG_asyncthread    5
+#define DBG_MAXFILES       6
+
+extern USHORT DbgEnabled[DBG_MAXFILES];
+
+#ifdef dprintf
+#undef dprintf
+#endif
+
+#define dprintf(a)      if(DbgEnabled[DBG_LOCALLOG] == 1) WriteLog a
+
+#else
+
+#define ParseLogStatus()
+
+#endif //DEBUG
+
+#endif
