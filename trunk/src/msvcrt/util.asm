1a8716912ae50d990b137188d2baa0463b24368d
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-21 19:53:25 +0000

    EB: Added log statement to _eh_prolog

--- trunk/src/msvcrt/util.asm
+++ trunk/src/msvcrt/util.asm
@@ -1,12 +1,30 @@
         .386p
-	.model flat
+ .model flat
+
+CONST32 segment dword use32 public 'CONST'
+     align 04h
+label1  db "MSVCRT: EH_prolog",0ah, 0h
+CONST32 ends
+
         .code
 
-
-        align   4
+        extrn   WriteLog:proc
         public  _MSVCRT__EH_prolog
 
+        align   4
 _MSVCRT__EH_prolog:
+   push        eax
+   push        ecx
+   push        edx
+   pushfd
+   push        offset FLAT:label1
+   call        WriteLog
+   add         esp,04h
+   popfd
+   pop         edx
+   pop         ecx
+   pop         eax
+
    push        0FFh
    push        eax
    mov         eax,fs:[00000000]
