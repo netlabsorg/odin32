9d383d0179b1272c00fe151e9d70f77bc6706fc2
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-22 18:46:20 +0000

    wsock32 rewrite (no pmwsock dependancy)

---
+++ tags/trunk/src/wsock32/asyncthread.h
@@ -0,0 +1,74 @@
+/* $Id: asyncthread.h,v 1.1 2000-03-22 18:46:15 sandervl Exp $ */
+
+/*
+ * Async thread help functions
+ *
+ * Copyright 2000 Sander van Leeuwen (sandervl@xs4all.nl)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ */
+#ifndef __ASYNCTHREAD_H__
+#define __ASYNCTHREAD_H__
+
+#include <vmutex.h>
+
+#define	ASYNCCNV	_Optlink
+
+typedef void (* ASYNCCNV ASYNCTHREADPROC)(void *);
+
+enum AsyncRequestType {
+  ASYNC_GETHOSTBYNAME,
+  ASYNC_GETHOSTBYADDR,
+  ASYNC_GETPROTOBYNAME,
+  ASYNC_GETPROTOBYNUMBER,
+  ASYNC_GETSERVBYNAME,
+  ASYNC_GETSERVBYPORT
+} ;
+
+typedef struct _ASYNCTHREADPARM
+{
+	BOOL 		fActive;
+	BOOL            fCancelled;
+	AsyncRequestType request;
+	LHANDLE		hAsyncTaskHandle;
+        HANDLE          hThread; //handle of thread that started the async request
+	ASYNCTHREADPROC asyncProc;
+        VMutex         *parmmutex;
+	HWND 		hwnd;
+	DWORD		msg;
+	LPSTR		buf;
+	DWORD		buflen;
+	union {
+		struct {
+			LPSTR	name;
+		} gethostbyname;
+		struct {
+			LPSTR	addr;
+			int	len;
+			int	type;
+		} gethostbyaddr;
+		struct {
+			LPSTR	name;
+		} getprotobyname;
+		struct {
+			int	number;
+		} getprotobynumber;
+		struct {
+			LPSTR	name;
+			LPSTR   proto;
+		} getservbyname;
+		struct {
+			int	port;
+			LPSTR   proto;
+		} getservbyport;
+        } u;
+        _ASYNCTHREADPARM *next;
+} ASYNCTHREADPARM, *PASYNCTHREADPARM;
+
+ULONG QueueAsyncJob(ASYNCTHREADPROC asyncproc, PASYNCTHREADPARM pThreadParm);
+void  RemoveFromQueue(PASYNCTHREADPARM pThreadParm);
+
+void  EnableAsyncEvent(SOCKET s, ULONG flags);
+BOOL  QueryAsyncEvent(SOCKET s, HWND *pHwnd, int *pMsg, ULONG *plEvent);
+
+#endif  //__ASYNCTHREAD_H__
