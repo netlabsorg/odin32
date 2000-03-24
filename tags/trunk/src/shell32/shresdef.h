82ad1c5ea8b4f842116b3d11fbdb38f7a623df26
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-24 17:17:30 +0000

    bug fixes

--- tags/trunk/src/shell32/shresdef.h
+++ tags/trunk/src/shell32/shresdef.h
@@ -1,4 +1,4 @@
-/* $Id: shresdef.h,v 1.2 2000-01-08 04:37:02 phaller Exp $ */
+/* $Id: shresdef.h,v 1.3 2000-03-24 17:17:30 cbratschi Exp $ */
 
 /*
  * Win32 SHELL32 for OS/2
@@ -21,23 +21,23 @@
 /*
  * columntitles for the shellview
  */
-#define IDS_SHV_COLUMN1		7
-#define IDS_SHV_COLUMN2		8
-#define IDS_SHV_COLUMN3		9
-#define IDS_SHV_COLUMN4		10
-#define IDS_SHV_COLUMN5		11
-#define IDS_SHV_COLUMN6		12
-#define IDS_SHV_COLUMN7		13
+#define IDS_SHV_COLUMN1         7
+#define IDS_SHV_COLUMN2         8
+#define IDS_SHV_COLUMN3         9
+#define IDS_SHV_COLUMN4         10
+#define IDS_SHV_COLUMN5         11
+#define IDS_SHV_COLUMN3DV       12
+#define IDS_SHV_COLUMN4DV       13
 
-#define IDS_DESKTOP		20
-#define IDS_MYCOMPUTER		21
+#define IDS_DESKTOP             20
+#define IDS_MYCOMPUTER          21
 
-#define IDS_SELECT		22
-#define IDS_OPEN		23
-#define IDS_VIEW_LARGE		24
-#define IDS_VIEW_SMALL		25
-#define IDS_VIEW_LIST		26
-#define IDS_VIEW_DETAILS	27
+#define IDS_SELECT              22
+#define IDS_OPEN                23
+#define IDS_VIEW_LARGE          24
+#define IDS_VIEW_SMALL          25
+#define IDS_VIEW_LIST           26
+#define IDS_VIEW_DETAILS        27
 
 
 #endif /* __WINE_SHELL_RES_H */
