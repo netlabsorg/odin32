72db6074113c497dd7861129ec11075f5cf26c57
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-25 23:50:11 +0000

    Temporary checkin.

--- trunk/tools/dbginfo/kHll.cpp
+++ trunk/tools/dbginfo/kHll.cpp
@@ -1,4 +1,4 @@
-/* $Id: kHll.cpp,v 1.1 2000-03-25 21:09:59 bird Exp $
+/* $Id: kHll.cpp,v 1.2 2000-03-25 23:50:11 bird Exp $
  *
  * kHll - Implementation of the class kHll.
  *        That class is used to create HLL debuginfo.
@@ -31,10 +31,133 @@
 #include <stdio.h>
 #include <string.h>
 #include <stddef.h>
+#include <assert.h>
 
 #include "hll.h"
+#include "kList.h"
 #include "kHll.h"
 
 
+
+kHLLPubSym::kHLLPubSym(
+        const char *        pszName,
+        unsigned long       off,
+        unsigned short      iObj,
+        unsigned short      iType
+        );
+
+
+kHLLPubSym::~kHLLPubSym();
+
+
+int kHLLPubSym::write(FILE *phFile)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Constructor - Creates an empty HLL object.
+ */
+kHll::kHll()
+{
+}
+
+
+/**
+ * Destructor.
+ */
+kHll::~kHll()
+{
+}
+
+
+/**
+ * Adds an LX object to the HLL info.
+ * @returns   Object handle. NULL on error.
+ * @param     pszName   Object name.
+ * @param     cb        Size of object.
+ */
+const void *    kHll::addObject(
+                    const char *        pszName,
+                    unsigned long int   cb
+                    )
+{
+    pszName = pszName;
+    cb = cb;
+    return NULL;
+}
+
+
+/**
+ * Adds a module.
+ * @returns  Object handle. NULL on error.
+ * @param    pszName    Module name
+ * @param    pvLib      Library module handle.
+ * @param    cObjects   Number of objects in the array.
+ * @param    paObjects  Pointer to an array of objects.
+ */
+const void *    kHll::addModule(
+                    const char *        pszName,
+                    const void *        pvLib,
+                    unsigned            cObject,
+                    PMODOBJECT          paObjects)
+{
+    return NULL;
+}
+
+
+
+/**
+ * Adds a public symbol.
+ * @returns   Handle to the symbol. NULL on error.
+ * @param     pszName  Symbol name.
+ * @param     off      Offset into the LX Object of the symbol.
+ * @param     iObject  LX Object index.
+ * @param     pvType   Type handle. NULL if not type.
+ */
+const void *    kHll::addPublicSymbol(
+                    const char *        pszName,
+                    unsigned long int   off,
+                    unsigned short int  iObject,
+                    const void *        pvType
+                    )
+{
+    PHLLPUBLICSYM    pPubSym;
+
+    assert(pszName != NULL);
+    pPubSym = (PHLLPUBLICSYM)malloc(sizeof(HLLPUBLICSYM) + strlen(pszName));
+    if (pPubSym != NULL)
+    {
+        strcpy(pPubSym->hll.achName, pszName);
+        pPubSym->hll.cchName = strlen(pszName);
+        pPubSym->hll.iObj = iObject;
+        pPubSym->hll.off = off;
+        pPubSym->hll.iType = pvType == NULL ? 0 : -1; //FIXME/TODO: Types->getIndex(pvType); check if 0 or -1.
+        PublicSymbols.insert(pPubSym);
+        free(pPubSym);
+    }
+
+    return NULL;
+}
+
+
+
+BOOL            kHll::write(
+                     const char *pszFilename
+                     )
+{
+    return FALSE;
+}
 
 
--- trunk/tools/dbginfo/kHll.h
+++ trunk/tools/dbginfo/kHll.h
@@ -1,4 +1,4 @@
-/* $Id: kHll.h,v 1.1 2000-03-25 21:10:00 bird Exp $
+/* $Id: kHll.h,v 1.2 2000-03-25 23:50:11 bird Exp $
  *
  * kHll - Declarations of the class kHll.
  *        That class is used to create HLL debuginfo.
@@ -14,10 +14,60 @@
 #define _kHll_h_
 
 
+/*******************************************************************************
+*   Structures and Typedefs                                                    *
+*******************************************************************************/
+/**
+ * Module object(=segment) description.
+ */
+typedef struct
+{
+    unsigned short  iObject;            /* LX Object index. */
+    unsigned long   cb;                 /* Size of object part. */
+    unsigned long   offset;             /* Offset into the LX Object. */
+} MODOBJECT, *PMODOBJECT;
+
 
 class kHll
+
+/**
+ * HLL Public symbol entry.
+ * @author      knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
+ */
+class kHllPubSymEntry : public kListEntry
 {
 private:
+    PHLLPUBLICSYM       pPubSym;
+
+public:
+    kHLLPubSym(
+        const char *        pszName,
+        unsigned long       off,
+        unsigned short      iObj,
+        unsigned short      iType
+        );
+    ~kHLLPubSym();
+
+    int         write(FILE *phFile);
+};
+
+
+
+/**
+ * HLL Debug Info generator.
+ * @author      knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
+ */
+class kHll
+{
+
+private:
+    /** @cat
+     * Internal data.
+     */
+    kList<kHllModuleEntry>     Modules;
+    kList<kHllPubSymEntry>     PublicSymbols;
+
+
 public:
     /** @cat
      * Constructors and Destructors.
@@ -34,15 +84,16 @@ public:
                         );
     const void *    addModule(
                         const char *        pszName,
-                        const void *
-
-                        ...);
+                        const void *        pvLib,
+                        unsigned            cObject,
+                        PMODOBJECT          paObjects
+                        );
     const void *    addPublicSymbol(
                         const char *        pszName,
                         unsigned long int   off,
                         unsigned short int  iObject,
                         const void *        pvType
-                            );
+                        );
     /*
     const void *    addPublicSymbol(
                         const char *        pszName,
@@ -51,7 +102,16 @@ public:
                         const char *        pszType
                             );
     */
-}
+
+
+    /** @cat
+     * Output.
+     */
+    BOOL            write(
+                        const char *pszFilename
+                        );
+
+};
 
 
 
