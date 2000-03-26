7bd0f18c6df3c71f556af977fba0d75b684bc872
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-26 14:23:21 +0000

    Temporary checkin.

---
+++ tools/dbginfo/Sym2Hll.cpp
@@ -0,0 +1,36 @@
+/* $Id: Sym2Hll.cpp,v 1.1 2000-03-26 14:23:21 bird Exp $
+ *
+ * Sym2Hll - Symbol file to HLL debuginfo converter.
+ *
+ * Copyright (c) 2000 knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+
+/*******************************************************************************
+*   Internal Functions                                                         *
+*******************************************************************************/
+#include <os2.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "hll.h"
+#include "kList.h"
+#include "kHll.h"
+
+
+
+
+int main(int argc, char **argv)
+{
+
+    return -1;
+}
+
+
+#ifdef __IBMCPP__
+#include "klist.cpp"
+#endif
