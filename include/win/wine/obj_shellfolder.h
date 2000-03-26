4db6892ea2f65d5cf022fc3b5059b51b85e213d3
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-03-26 16:32:33 +0000

    *** empty log message ***

--- include/win/wine/obj_shellfolder.h
+++ include/win/wine/obj_shellfolder.h
@@ -1,4 +1,4 @@
-/* $Id: obj_shellfolder.h,v 1.6 1999-11-10 23:35:15 phaller Exp $ */
+/* $Id: obj_shellfolder.h,v 1.7 2000-03-26 16:29:41 cbratschi Exp $ */
 /*
  * Defines the COM interfaces and APIs related to IShellFolder
  *
@@ -9,7 +9,7 @@
 #define __WINE_WINE_OBJ_SHELLFOLDER_H
 
 #include "wine/obj_base.h"
-#include "wine/obj_moniker.h"		/* for LPBC */
+#include "wine/obj_moniker.h"           /* for LPBC */
 #include "wine/obj_enumidlist.h"
 #include "wine/obj_oleaut.h"
 #include "winbase.h"
@@ -22,22 +22,22 @@ extern "C" {
 /****************************************************************************
 *  STRRET (temporary, move it away)
 */
-#define	STRRET_WSTR	0x0000
-#define	STRRET_OFFSETA	0x0001
-#define	STRRET_CSTRA	0x0002
-#define STRRET_ASTR	0X0003
-#define STRRET_OFFSETW	0X0004
-#define STRRET_CSTRW	0X0005
+#define STRRET_WSTR     0x0000
+#define STRRET_OFFSETA  0x0001
+#define STRRET_CSTRA    0x0002
+#define STRRET_ASTR     0X0003
+#define STRRET_OFFSETW  0X0004
+#define STRRET_CSTRW    0X0005
 
 
 typedef struct _STRRET
-{ UINT uType;		/* STRRET_xxx */
+{ UINT uType;           /* STRRET_xxx */
   union
-  { LPWSTR	pOleStr;	/* OLESTR that will be freed */
-    LPSTR	pStr;
-    UINT	uOffset;	/* OffsetINT32o SHITEMID (ANSI) */
-    char	cStr[MAX_PATH];	/* Buffer to fill in */
-    WCHAR	cStrW[MAX_PATH];
+  { LPWSTR      pOleStr;        /* OLESTR that will be freed */
+    LPSTR       pStr;
+    UINT        uOffset;        /* OffsetINT32o SHITEMID (ANSI) */
+    char        cStr[MAX_PATH]; /* Buffer to fill in */
+    WCHAR       cStrW[MAX_PATH];
   }u;
 } STRRET,*LPSTRRET;
 
@@ -62,9 +62,9 @@ typedef struct IEnumExtraSearch IEnumExtraSearch, *LPENUMEXTRASEARCH;
 
 typedef struct
 {
-  GUID	guidSearch;
+  GUID  guidSearch;
   WCHAR wszFriendlyName[80];
-  WCHAR	wszMenuText[80];
+  WCHAR wszMenuText[80];
   WCHAR wszHelpText[MAX_PATH];
   WCHAR wszUrl[2084];
   WCHAR wszIcon[MAX_PATH+10];
@@ -85,60 +85,60 @@ ICOM_DEFINE(IEnumExtraSearch,IUnknown)
 #undef ICOM_INTERFACE
 
 /*** IUnknown methods ***/
-#define IEnumIDList_QueryInterface(p,a,b)	ICOM_CALL2(QueryInterface,p,a,b)
-#define IEnumIDList_AddRef(p)			ICOM_CALL (AddRef,p)
-#define IEnumIDList_Release(p)			ICOM_CALL (Release,p)
+#define IEnumIDList_QueryInterface(p,a,b)       ICOM_CALL2(QueryInterface,p,a,b)
+#define IEnumIDList_AddRef(p)                   ICOM_CALL (AddRef,p)
+#define IEnumIDList_Release(p)                  ICOM_CALL (Release,p)
 /*** IEnumIDList methods ***/
-#define IEnumIDList_Next(p,a,b,c)		ICOM_CALL3(Next,p,a,b,c)
-#define IEnumIDList_Skip(p,a)			ICOM_CALL1(Skip,p,a)
-#define IEnumIDList_Reset(p)			ICOM_CALL(Reset,p)
-#define IEnumIDList_Clone(p,a)			ICOM_CALL1(Clone,p,a)
+#define IEnumIDList_Next(p,a,b,c)               ICOM_CALL3(Next,p,a,b,c)
+#define IEnumIDList_Skip(p,a)                   ICOM_CALL1(Skip,p,a)
+#define IEnumIDList_Reset(p)                    ICOM_CALL(Reset,p)
+#define IEnumIDList_Clone(p,a)                  ICOM_CALL1(Clone,p,a)
 
 /*****************************************************************************
  * IShellFolder::GetDisplayNameOf/SetNameOf uFlags
  */
 typedef enum
-{	SHGDN_NORMAL		= 0,		/* default (display purpose) */
-	SHGDN_INFOLDER		= 1,		/* displayed under a folder (relative)*/
-	SHGDN_FORPARSING	= 0x8000	/* for ParseDisplayName or path */
+{       SHGDN_NORMAL            = 0,            /* default (display purpose) */
+        SHGDN_INFOLDER          = 1,            /* displayed under a folder (relative)*/
+        SHGDN_FORPARSING        = 0x8000        /* for ParseDisplayName or path */
 } SHGNO;
 
 /*****************************************************************************
  * IShellFolder::EnumObjects
  */
 typedef enum tagSHCONTF
-{	SHCONTF_FOLDERS		= 32,	/* for shell browser */
-	SHCONTF_NONFOLDERS	= 64,	/* for default view */
-	SHCONTF_INCLUDEHIDDEN	= 128	/* for hidden/system objects */
+{       SHCONTF_FOLDERS         = 32,   /* for shell browser */
+        SHCONTF_NONFOLDERS      = 64,   /* for default view */
+        SHCONTF_INCLUDEHIDDEN   = 128   /* for hidden/system objects */
 } SHCONTF;
 
 /*****************************************************************************
  * IShellFolder::GetAttributesOf flags
  */
-#define SFGAO_CANCOPY		DROPEFFECT_COPY /* Objects can be copied */
-#define SFGAO_CANMOVE		DROPEFFECT_MOVE /* Objects can be moved */
-#define SFGAO_CANLINK		DROPEFFECT_LINK /* Objects can be linked */
-#define SFGAO_CANRENAME		0x00000010L	/* Objects can be renamed */
-#define SFGAO_CANDELETE		0x00000020L	/* Objects can be deleted */
-#define SFGAO_HASPROPSHEET	0x00000040L	/* Objects have property sheets */
-#define SFGAO_DROPTARGET	0x00000100L	/* Objects are drop target */
-#define SFGAO_CAPABILITYMASK	0x00000177L
-#define SFGAO_LINK		0x00010000L	/* Shortcut (link) */
-#define SFGAO_SHARE		0x00020000L	/* shared */
-#define SFGAO_READONLY		0x00040000L	/* read-only */
-#define SFGAO_GHOSTED		0x00080000L	/* ghosted icon */
-#define SFGAO_HIDDEN            0x00080000L	/* hidden object */
-#define SFGAO_DISPLAYATTRMASK	0x000F0000L
-#define SFGAO_FILESYSANCESTOR	0x10000000L	/* It contains file system folder */
-#define SFGAO_FOLDER		0x20000000L	/* It's a folder. */
-#define SFGAO_FILESYSTEM	0x40000000L	/* is a file system thing (file/folder/root) */
-#define SFGAO_HASSUBFOLDER	0x80000000L	/* Expandable in the map pane */
-#define SFGAO_CONTENTSMASK	0x80000000L
-#define SFGAO_VALIDATE		0x01000000L	/* invalidate cached information */
-#define SFGAO_REMOVABLE		0x02000000L	/* is this removeable media? */
-#define SFGAO_BROWSABLE		0x08000000L	/* is in-place browsable */
-#define SFGAO_NONENUMERATED	0x00100000L	/* is a non-enumerated object */
-#define SFGAO_NEWCONTENT	0x00200000L	/* should show bold in explorer tree */
+#define SFGAO_CANCOPY           DROPEFFECT_COPY /* Objects can be copied */
+#define SFGAO_CANMOVE           DROPEFFECT_MOVE /* Objects can be moved */
+#define SFGAO_CANLINK           DROPEFFECT_LINK /* Objects can be linked */
+#define SFGAO_CANRENAME         0x00000010L     /* Objects can be renamed */
+#define SFGAO_CANDELETE         0x00000020L     /* Objects can be deleted */
+#define SFGAO_HASPROPSHEET      0x00000040L     /* Objects have property sheets */
+#define SFGAO_DROPTARGET        0x00000100L     /* Objects are drop target */
+#define SFGAO_CAPABILITYMASK    0x00000177L
+#define SFGAO_LINK              0x00010000L     /* Shortcut (link) */
+#define SFGAO_SHARE             0x00020000L     /* shared */
+#define SFGAO_READONLY          0x00040000L     /* read-only */
+#define SFGAO_GHOSTED           0x00080000L     /* ghosted icon */
+#define SFGAO_HIDDEN            0x00080000L     /* hidden object */
+#define SFGAO_DISPLAYATTRMASK   0x000F0000L
+#define SFGAO_FILESYSANCESTOR   0x10000000L     /* It contains file system folder */
+#define SFGAO_FOLDER            0x20000000L     /* It's a folder. */
+#define SFGAO_FILESYSTEM        0x40000000L     /* is a file system thing (file/folder/root) */
+#define SFGAO_HASSUBFOLDER      0x80000000L     /* Expandable in the map pane */
+#define SFGAO_CONTENTSMASK      0x80000000L
+#define SFGAO_VALIDATE          0x01000000L     /* invalidate cached information */
+#define SFGAO_REMOVABLE         0x02000000L     /* is this removeable media? */
+#define SFGAO_BROWSABLE         0x08000000L     /* is in-place browsable */
+#define SFGAO_NONENUMERATED     0x00100000L     /* is a non-enumerated object */
+#define SFGAO_NEWCONTENT        0x00200000L     /* should show bold in explorer tree */
 
 /************************************************************************
  *
@@ -209,20 +209,20 @@ ICOM_DEFINE(IShellFolder,IUnknown)
 #undef ICOM_INTERFACE
 
 /*** IUnknown methods ***/
-#define IShellFolder_QueryInterface(p,a,b)		ICOM_CALL2(QueryInterface,p,a,b)
-#define IShellFolder_AddRef(p)				ICOM_CALL (AddRef,p)
-#define IShellFolder_Release(p)				ICOM_CALL (Release,p)
+#define IShellFolder_QueryInterface(p,a,b)              ICOM_CALL2(QueryInterface,p,a,b)
+#define IShellFolder_AddRef(p)                          ICOM_CALL (AddRef,p)
+#define IShellFolder_Release(p)                         ICOM_CALL (Release,p)
 /*** IShellFolder methods ***/
-#define IShellFolder_ParseDisplayName(p,a,b,c,d,e,f)	ICOM_CALL6(ParseDisplayName,p,a,b,c,d,e,f)
-#define IShellFolder_EnumObjects(p,a,b,c)		ICOM_CALL3(EnumObjects,p,a,b,c)
-#define IShellFolder_BindToObject(p,a,b,c,d)		ICOM_CALL4(BindToObject,p,a,b,c,d)
-#define IShellFolder_BindToStorage(p,a,b,c,d)		ICOM_CALL4(BindToStorage,p,a,b,c,d)
-#define IShellFolder_CompareIDs(p,a,b,c)		ICOM_CALL3(CompareIDs,p,a,b,c)
-#define IShellFolder_CreateViewObject(p,a,b,c)		ICOM_CALL3(CreateViewObject,p,a,b,c)
-#define IShellFolder_GetAttributesOf(p,a,b,c)		ICOM_CALL3(GetAttributesOf,p,a,b,c)
-#define IShellFolder_GetUIObjectOf(p,a,b,c,d,e,f)	ICOM_CALL6(GetUIObjectOf,p,a,b,c,d,e,f)
-#define IShellFolder_GetDisplayNameOf(p,a,b,c)		ICOM_CALL3(GetDisplayNameOf,p,a,b,c)
-#define IShellFolder_SetNameOf(p,a,b,c,d,e)		ICOM_CALL5(SetNameOf,p,a,b,c,d,e)
+#define IShellFolder_ParseDisplayName(p,a,b,c,d,e,f)    ICOM_CALL6(ParseDisplayName,p,a,b,c,d,e,f)
+#define IShellFolder_EnumObjects(p,a,b,c)               ICOM_CALL3(EnumObjects,p,a,b,c)
+#define IShellFolder_BindToObject(p,a,b,c,d)            ICOM_CALL4(BindToObject,p,a,b,c,d)
+#define IShellFolder_BindToStorage(p,a,b,c,d)           ICOM_CALL4(BindToStorage,p,a,b,c,d)
+#define IShellFolder_CompareIDs(p,a,b,c)                ICOM_CALL3(CompareIDs,p,a,b,c)
+#define IShellFolder_CreateViewObject(p,a,b,c)          ICOM_CALL3(CreateViewObject,p,a,b,c)
+#define IShellFolder_GetAttributesOf(p,a,b,c)           ICOM_CALL3(GetAttributesOf,p,a,b,c)
+#define IShellFolder_GetUIObjectOf(p,a,b,c,d,e,f)       ICOM_CALL6(GetUIObjectOf,p,a,b,c,d,e,f)
+#define IShellFolder_GetDisplayNameOf(p,a,b,c)          ICOM_CALL3(GetDisplayNameOf,p,a,b,c)
+#define IShellFolder_SetNameOf(p,a,b,c,d,e)             ICOM_CALL5(SetNameOf,p,a,b,c,d,e)
 
 /*****************************************************************************
  * IShellFolder2 interface
@@ -232,34 +232,34 @@ ICOM_DEFINE(IShellFolder,IUnknown)
 /* GetDefaultColumnState */
 typedef enum
 {
-	SHCOLSTATE_TYPE_STR	= 0x00000001,
-	SHCOLSTATE_TYPE_INT	= 0x00000002,
-	SHCOLSTATE_TYPE_DATE	= 0x00000003,
-	SHCOLSTATE_TYPEMASK	= 0x0000000F,
-	SHCOLSTATE_ONBYDEFAULT	= 0x00000010,
-	SHCOLSTATE_SLOW		= 0x00000020,
-	SHCOLSTATE_EXTENDED	= 0x00000040,
-	SHCOLSTATE_SECONDARYUI	= 0x00000080,
-	SHCOLSTATE_HIDDEN	= 0x00000100
+        SHCOLSTATE_TYPE_STR     = 0x00000001,
+        SHCOLSTATE_TYPE_INT     = 0x00000002,
+        SHCOLSTATE_TYPE_DATE    = 0x00000003,
+        SHCOLSTATE_TYPEMASK     = 0x0000000F,
+        SHCOLSTATE_ONBYDEFAULT  = 0x00000010,
+        SHCOLSTATE_SLOW         = 0x00000020,
+        SHCOLSTATE_EXTENDED     = 0x00000040,
+        SHCOLSTATE_SECONDARYUI  = 0x00000080,
+        SHCOLSTATE_HIDDEN       = 0x00000100
 } SHCOLSTATE;
 
 typedef struct
 {
-	GUID	fmtid;
-	DWORD	pid;
+        GUID    fmtid;
+        DWORD   pid;
 } SHCOLUMNID, *LPSHCOLUMNID;
 typedef const SHCOLUMNID* LPCSHCOLUMNID;
 
 /* GetDetailsEx */
-#define PID_FINDDATA		0
-#define PID_NETRESOURCE		1
-#define PID_DESCRIPTIONID	2
+#define PID_FINDDATA            0
+#define PID_NETRESOURCE         1
+#define PID_DESCRIPTIONID       2
 
 typedef struct
 {
-	int	fmt;
-	int	cxChar;
-	STRRET	str;
+        int     fmt;
+        int     cxChar;
+        STRRET  str;
 } SHELLDETAILS, *LPSHELLDETAILS;
 
 #define ICOM_INTERFACE IShellFolder2
@@ -278,28 +278,28 @@ ICOM_DEFINE(IShellFolder2, IShellFolder)
 #undef ICOM_INTERFACE
 
 /*** IUnknown methods ***/
-#define IShellFolder2_QueryInterface(p,a,b)		ICOM_CALL2(QueryInterface,p,a,b)
-#define IShellFolder2_AddRef(p)				ICOM_CALL (AddRef,p)
-#define IShellFolder2_Release(p)			ICOM_CALL (Release,p)
+#define IShellFolder2_QueryInterface(p,a,b)             ICOM_CALL2(QueryInterface,p,a,b)
+#define IShellFolder2_AddRef(p)                         ICOM_CALL (AddRef,p)
+#define IShellFolder2_Release(p)                        ICOM_CALL (Release,p)
 /*** IShellFolder methods ***/
-#define IShellFolder2_ParseDisplayName(p,a,b,c,d,e,f)	ICOM_CALL6(ParseDisplayName,p,a,b,c,d,e,f)
-#define IShellFolder2_EnumObjects(p,a,b,c)		ICOM_CALL3(EnumObjects,p,a,b,c)
-#define IShellFolder2_BindToObject(p,a,b,c,d)		ICOM_CALL4(BindToObject,p,a,b,c,d)
-#define IShellFolder2_BindToStorage(p,a,b,c,d)		ICOM_CALL4(BindToStorage,p,a,b,c,d)
-#define IShellFolder2_CompareIDs(p,a,b,c)		ICOM_CALL3(CompareIDs,p,a,b,c)
-#define IShellFolder2_CreateViewObject(p,a,b,c)		ICOM_CALL3(CreateViewObject,p,a,b,c)
-#define IShellFolder2_GetAttributesOf(p,a,b,c)		ICOM_CALL3(GetAttributesOf,p,a,b,c)
-#define IShellFolder2_GetUIObjectOf(p,a,b,c,d,e,f)	ICOM_CALL6(GetUIObjectOf,p,a,b,c,d,e,f)
-#define IShellFolder2_GetDisplayNameOf(p,a,b,c)		ICOM_CALL3(GetDisplayNameOf,p,a,b,c)
-#define IShellFolder2_SetNameOf(p,a,b,c,d,e)		ICOM_CALL5(SetNameOf,p,a,b,c,d,e)
+#define IShellFolder2_ParseDisplayName(p,a,b,c,d,e,f)   ICOM_CALL6(ParseDisplayName,p,a,b,c,d,e,f)
+#define IShellFolder2_EnumObjects(p,a,b,c)              ICOM_CALL3(EnumObjects,p,a,b,c)
+#define IShellFolder2_BindToObject(p,a,b,c,d)           ICOM_CALL4(BindToObject,p,a,b,c,d)
+#define IShellFolder2_BindToStorage(p,a,b,c,d)          ICOM_CALL4(BindToStorage,p,a,b,c,d)
+#define IShellFolder2_CompareIDs(p,a,b,c)               ICOM_CALL3(CompareIDs,p,a,b,c)
+#define IShellFolder2_CreateViewObject(p,a,b,c)         ICOM_CALL3(CreateViewObject,p,a,b,c)
+#define IShellFolder2_GetAttributesOf(p,a,b,c)          ICOM_CALL3(GetAttributesOf,p,a,b,c)
+#define IShellFolder2_GetUIObjectOf(p,a,b,c,d,e,f)      ICOM_CALL6(GetUIObjectOf,p,a,b,c,d,e,f)
+#define IShellFolder2_GetDisplayNameOf(p,a,b,c)         ICOM_CALL3(GetDisplayNameOf,p,a,b,c)
+#define IShellFolder2_SetNameOf(p,a,b,c,d,e)            ICOM_CALL5(SetNameOf,p,a,b,c,d,e)
 /*** IShellFolder2 methods ***/
-#define IShellFolder2_GetDefaultSearchGUID(p,a)		ICOM_CALL1(GetDefaultSearchGUID,p,a)
-#define IShellFolder2_EnumSearches(p,a)			ICOM_CALL1(EnumSearches,p,a)
-#define IShellFolder2_GetDefaultColumn(p,a,b,c)		ICOM_CALL3(GetDefaultColumn,p,a,b,c)
-#define IShellFolder2_GetDefaultColumnState(p,a,b)	ICOM_CALL2(GetDefaultColumnState,p,a,b)
-#define IShellFolder2_GetDetailsEx(p,a,b,c)		ICOM_CALL3(GetDetailsEx,p,a,b,c)
-#define IShellFolder2_GetDetailsOf(p,a,b,c)		ICOM_CALL3(GetDetailsOf,p,a,b,c)
-#define IShellFolder2_MapNameToSCID(p,a,b)		ICOM_CALL2(MapNameToSCID,p,a,b)
+#define IShellFolder2_GetDefaultSearchGUID(p,a)         ICOM_CALL1(GetDefaultSearchGUID,p,a)
+#define IShellFolder2_EnumSearches(p,a)                 ICOM_CALL1(EnumSearches,p,a)
+#define IShellFolder2_GetDefaultColumn(p,a,b,c)         ICOM_CALL3(GetDefaultColumn,p,a,b,c)
+#define IShellFolder2_GetDefaultColumnState(p,a,b)      ICOM_CALL2(GetDefaultColumnState,p,a,b)
+#define IShellFolder2_GetDetailsEx(p,a,b,c)             ICOM_CALL3(GetDetailsEx,p,a,b,c)
+#define IShellFolder2_GetDetailsOf(p,a,b,c)             ICOM_CALL3(GetDetailsOf,p,a,b,c)
+#define IShellFolder2_MapNameToSCID(p,a,b)              ICOM_CALL2(MapNameToSCID,p,a,b)
 
 /*****************************************************************************
  * IPersistFolder interface
@@ -320,13 +320,13 @@ ICOM_DEFINE(IPersistFolder, IPersist)
 #undef ICOM_INTERFACE
 
 /*** IUnknown methods ***/
-#define IPersistFolder_QueryInterface(p,a,b)	ICOM_CALL2(QueryInterface,p,a,b)
-#define IPersistFolder_AddRef(p)		ICOM_CALL (AddRef,p)
-#define IPersistFolder_Release(p)		ICOM_CALL (Release,p)
+#define IPersistFolder_QueryInterface(p,a,b)    ICOM_CALL2(QueryInterface,p,a,b)
+#define IPersistFolder_AddRef(p)                ICOM_CALL (AddRef,p)
+#define IPersistFolder_Release(p)               ICOM_CALL (Release,p)
 /*** IPersist methods ***/
-#define IPersistFolder_GetClassID(p,a)		ICOM_CALL1(GetClassID,p,a)
+#define IPersistFolder_GetClassID(p,a)          ICOM_CALL1(GetClassID,p,a)
 /*** IPersistFolder methods ***/
-#define IPersistFolder_Initialize(p,a)		ICOM_CALL1(Initialize,p,a)
+#define IPersistFolder_Initialize(p,a)          ICOM_CALL1(Initialize,p,a)
 
 #ifdef __cplusplus
 } /* extern "C" */
