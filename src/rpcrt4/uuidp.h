50d966b6ff2a238cce97fcbbd868b7b5c8f55e23
davidr <davidr@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-02 22:02:59 +0000

    RPCRT4: Remote Procedure Call Runtime

---
+++ src/rpcrt4/uuidp.h
@@ -0,0 +1,14 @@
+/* $Id: uuidp.h,v 1.1 2000-04-02 22:02:59 davidr Exp $ */
+/* Private DCE UUID definition
+ * Based on OSF sources (see below)
+ *
+ * Copyright 2000 D.J.Raison
+ */
+
+#if !defined(_RPCRT4_UUIDP_INCLUDED)
+#define _RPCRT4_UUIDP_INCLUDED
+
+extern	void	UuidInit(void );
+
+
+#endif /* _RPCRT4_UUIDP_INCLUDED */
