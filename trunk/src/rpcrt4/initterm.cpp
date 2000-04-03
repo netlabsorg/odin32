3930625628287158c0a152577a3ee189157306a5
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-03 07:53:26 +0000

    added makefile, fixed initterm (no exitlist)

--- trunk/src/rpcrt4/initterm.cpp
+++ trunk/src/rpcrt4/initterm.cpp
@@ -1,4 +1,4 @@
-/* $Id: initterm.cpp,v 1.1 2000-04-02 22:02:55 davidr Exp $ */
+/* $Id: initterm.cpp,v 1.2 2000-04-03 07:53:26 sandervl Exp $ */
 
 /*
  * DLL entry point
@@ -32,6 +32,7 @@
 #include <string.h>
 #include <odin.h>
 #include <win32type.h>
+#include <winconst.h>
 #include <odinlx.h>
 #include <misc.h>       /*PLF Wed  98-03-18 23:18:15*/
 #include "uuidp.h"
@@ -39,17 +40,30 @@
 extern "C" {
 void CDECL _ctordtorInit( void );
 void CDECL _ctordtorTerm( void );
+
+ //Win32 resource table (produced by wrc)
+ extern DWORD _Resource_PEResTab;
 }
 
-/*-------------------------------------------------------------------*/
-/* A clean up routine registered with DosExitList must be used if    */
-/* runtime calls are required and the runtime is dynamically linked. */
-/* This will guarantee that this clean up routine is run before the  */
-/* library DLL is terminated.                                        */
-/*-------------------------------------------------------------------*/
-static void APIENTRY cleanup(ULONG reason);
+//******************************************************************************
+//******************************************************************************
+BOOL WINAPI LibMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID fImpLoad)
+{
+   switch (fdwReason)
+   {
+   case DLL_PROCESS_ATTACH:
+	return TRUE;
 
+   case DLL_THREAD_ATTACH:
+   case DLL_THREAD_DETACH:
+	return TRUE;
 
+   case DLL_PROCESS_DETACH:
+	_ctordtorTerm();
+	return TRUE;
+   }
+   return FALSE;
+}
 /****************************************************************************/
 /* _DLL_InitTerm is the function that gets called by the operating system   */
 /* loader when it loads and frees this DLL for each process that accesses   */
@@ -81,15 +95,12 @@ unsigned long SYSTEM _DLL_InitTerm(unsigned long hModule, unsigned long
          /* are required and the runtime is dynamically linked.             */
          /*******************************************************************/
 
-	 if(RegisterLxDll(hModule, 0, 0) == FALSE) 
+	 if(RegisterLxDll(hModule, LibMain, (PVOID)&_Resource_PEResTab) == FALSE) 
 		return 0UL;
 
-         rc = DosExitList(0x0000F000|EXLST_ADD, cleanup);
-         if(rc)
-                return 0UL;
-
-	 /* Init the Uuid subsystem */
-	 UuidInit();
+	 //SvL: Must be done here as the socket calls trash FS!
+ 	 /* Init the Uuid subsystem */
+ 	 UuidInit();
 
          break;
       case 1 :
@@ -103,12 +114,4 @@ unsigned long SYSTEM _DLL_InitTerm(unsigned long hModule, unsigned long
    /* A non-zero value must be returned to indicate success.  */
    /***********************************************************/
    return 1UL;
-}
-
-
-static void APIENTRY cleanup(ULONG ulReason)
-{
-   _ctordtorTerm();
-   DosExitList(EXLST_EXIT, cleanup);
-   return ;
 }
