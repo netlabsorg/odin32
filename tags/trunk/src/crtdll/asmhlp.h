586baadcb24e1365184098a78548f8161943637f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-13 18:46:15 +0000

    div fix

--- tags/trunk/src/crtdll/asmhlp.h
+++ tags/trunk/src/crtdll/asmhlp.h
@@ -1,7 +1,9 @@
-// $Id: asmhlp.h,v 1.1 1999-12-21 12:27:11 sandervl Exp $
+// $Id: asmhlp.h,v 1.2 2000-04-13 18:46:15 sandervl Exp $
 #ifndef __ASMHLP_H__
 #define __ASMHLP_H__
 
 void CDECL POP_FPU(double &fpval);
+
+void CDECL SetEDX(ULONG edx);
 
 #endif
