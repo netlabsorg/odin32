8a9a6fceaa67f5e496d5657b56f3b48c269e2c1d
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-16 04:06:11 +0000

    Initial coding...

--- trunk/include/odin32validate.h
+++ trunk/include/odin32validate.h
@@ -1,6 +1,11 @@
-/* $Id: odin32validate.h,v 1.1 2000-04-16 02:59:03 bird Exp $
+/* $Id: odin32validate.h,v 1.2 2000-04-16 04:06:11 bird Exp $
  *
  * Parameter validation macros.
+ *
+ * Note that these are not fully implemented yet. Pointers and memory areas
+ * are not checked if valid yet, since this requires some more effort. So,
+ * for the time being pointers and memory areas are checked agains the
+ * maximum user virtual address space limits.
  *
  * Copyright (c) 2000 knut st. osmundsen (knut.stange.osmundsen@pmsc.no)
  *
@@ -11,46 +16,62 @@
 #define _odin32validate_h_
 
 /**
- * ADDRESS_SPACE_LIMIT holds the largest possible address in the address space + 1.
- * 3GB is the higest possible address in the user address space in OS/2.
+ * ADDRESS_SPACE_LIMIT holds the largest possible address in the user virtual
+ * address virtual space + 1.
+ * 3GB is the higest possible address in the user virtual address space in OS/2.
  */
 #define ADDRESS_SPACE_LIMIT 0xc0000000
 
 
 /**
- * Validates that the value of a pointer is within the virtual address space.
+ * Validates that the specified pointer points at valid.
+ * (Currently it only check that it's within the user virtual address space.)
  * @returns   TRUE if valid.
  *            FALSE if invalid.
  * @param     p     Pointer value.
  */
-#define VALIDFAST_PTR(p) \
+#define VALID_PTR(p) \
             ((char*)(p) >= (char*)0x10000 \
             && (char*)(p) < (char*)ADDRESS_SPACE_LIMIT)
 
 
 /**
- * Validates that a area pointed to is within the virtual address space.
+ * Validates that the area pointed to is valid.
+ * (Currently it only check that it's within the user virtual address space.)
  * @returns   TRUE if valid.
  *            FALSE if invalid.
  * @param     p     Pointer value.
  * @param     cb    Size of data pointer to by the pointer.
  */
-#define VALIDFAST_PTRSIZE(p, cb) \
+#define VALID_PTRSIZE(p, cb) \
             ((char*)(p) >= (char*)0x10000 \
              && (char*)(p) < (char*)ADDRESS_SPACE_LIMIT \
-             && (char*)(p) + (char*)(cb) < (char*)ADDRESS_SPACE_LIMIT)
+             && (char*)(p) + (unsigned)(cb) < (char*)ADDRESS_SPACE_LIMIT)
+
 
 /**
- * Validates that the value of a string pointer is within the virtual address
- * space, and that its length less or equal to the given max length.
+ * Validates that a string pointer is valid and that the entire string is valid.
+ * (Currently it only checks that the first byte of the string is within
+ * the user virtual address space.)
  * @returns   TRUE if valid.
  *            FALSE if invalid.
  * @param     psz       String pointer.
  * @param     cchMax    Max string length
  */
-#define VALIDFAST_PSZLENGTH(psz, cchMax) \
-            (VALIDFAST_PTRSIZE(psz, 1) \
-             && strlen(psz) <= cchMax)
+#define VALID_PSZ(psz) \
+            (VALID_PTRSIZE(psz, 1))
+
+/**
+ * Validates that a string less or equal to a given max length.
+ * Note! the pointer isn't validated. That you'll have to do separatly using
+ * VALID_PSZ(psz) which is defined right above.
+ * @returns   TRUE if valid.
+ *            FALSE if invalid.
+ * @param     psz       String pointer.
+ * @param     cchMax    Max string length
+ */
+#define VALID_PSZMAXSIZE(psz, cchMax) \
+             (strlen(psz) <= cchMax)
 
 
 
