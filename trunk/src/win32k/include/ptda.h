1c53b819a1e3a67800c7fc8b8bc01b006b46a3d8
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-17 02:26:29 +0000

    Some drafts.

--- trunk/src/win32k/include/ptda.h
+++ trunk/src/win32k/include/ptda.h
@@ -1,4 +1,4 @@
-/* $Id: ptda.h,v 1.1 2000-04-17 01:56:49 bird Exp $
+/* $Id: ptda.h,v 1.2 2000-04-17 02:26:29 bird Exp $
  *
  * PTDA access functions.
  *
@@ -11,6 +11,16 @@
 #define _ptda_h_
 
 
+/*******************************************************************************
+*   Structures and Typedefs                                                    *
+*******************************************************************************/
+typedef struct _PTDA
+{
+    char dummy;
+} PTDA, *PPTDA;
 
+PPTDA   ptdaGetCur(void);
+PPTDA   ptdaGet_pPTDAExecChild(PPTDA pPTDA);
+USHORT  ptdaGet_ptda_environ(PPTDA pPTDA);
 
 #endif
