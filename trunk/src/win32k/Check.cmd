b03f3aadd984c2f748717754f80933fe9a0080c1
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-04-17 02:32:34 +0000

    Corrected a bug. MAKEFLAGS is a environment variable. Set this to NULL before executing the sArgs.

--- trunk/src/win32k/Check.cmd
+++ trunk/src/win32k/Check.cmd
@@ -1,9 +1,10 @@
-/* $Id: Check.cmd,v 1.2 2000-03-09 20:46:35 bird Exp $
+/* $Id: Check.cmd,v 1.3 2000-04-17 02:32:34 bird Exp $
  *
  * Dirty hack to get around the nmake feature of passing on MAKEFLAGS.
  * This simply executes it's arguments and returns the errorcode.
  */
     parse arg sArgs
     '@echo off';
+    '@SET MAKEFLAGS='
     sArgs;
     exit RC ;
