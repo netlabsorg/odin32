4765bd1921f3be21c3ff48f22333b992aa442fd0
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-05-12 12:21:59 +0000

    Made it easier to customize.

--- trunk/tools/DailyBuild/odin32env.cmd
+++ trunk/tools/DailyBuild/odin32env.cmd
@@ -1,4 +1,4 @@
-/* $Id: odin32env.cmd,v 1.1 2000-04-27 11:32:25 bird Exp $
+/* $Id: odin32env.cmd,v 1.2 2000-05-12 12:21:59 bird Exp $
  *
  * Sets the build environment.
  *
@@ -7,51 +7,72 @@
  * Project Odin Software License can be found in LICENSE.TXT
  *
  */
-    'SET TOOLKIT=c:\appl\os2\toolkt40';
-    'SET VACPP_SHARED=FALSE'
-    'SET CPPHELP_INI=C:\OS2\SYSTEM'
-    'SET CPPLOCAL=c:\appl\os2\IBMCPP30'
-    'SET CPPMAIN=%CPPLOCAL%'
-    'SET CPPWORK=%CPPLOCAL%'
-    'SET SMINCLUDE=%TOOLKIT%\som\include;%TOOLKIT%\idl;%CPPLOCAL%\INCLUDE\SOM'
-    'SET TMP=c:\temp'
-    'SET HELPNDX=EPMKWHLP.NDX+CPP.NDX+CPPBRS.NDX'
 
-    'SET INCLUDE=%CPPLOCAL%\INCLUDE;%CPPLOCAL%\INCLUDE\SOM;%TOOLKIT%\h;%include%;'
-    'SET LIB=%CPPLOCAL%\LIB;%lib%;'
-    'SET LIB=%TOOLKIT%\SPEECH\LIB;%TOOLKIT%\SAMPLES\MM\LIB;%TOOLKIT%\LIB;%TOOLKIT%\SOM\LIB;%TOOLKIT%\OPENDOC\BASE\LIB;%LIB%';
-    'SET LOCPATH=%CPPLOCAL%\LOCALE;%LOCPATH%'
-    'SET HELP=%CPPLOCAL%\HELP;%HELP%;'
-    'SET BEGINLIBPATH=%CPPLOCAL%\DLL;%BeginLIBPATH%;'
-
-    'SET PATH=%CPPLOCAL%\BIN;d:\knut\ddk\ddktools\toolkits\msc60\binp;d:\knut\cvs\bin;%Path%'
-    'SET PATH=%TOOLKIT%\BETA\BIN;%TOOLKIT%\SOM\COMMON;%TOOLKIT%\SOM\BIN;%TOOLKIT%\OPENDOC\BASE\BIN;%TOOLKIT%\BIN;%Path%;d:\knut\ddk\base\tools';
-
-    'SET DPATH=%CPPLOCAL%\HELP;%CPPLOCAL%;%CPPLOCAL%\LOCALE;%DPATH%'
-    'SET DPATH=%TOOLKIT%\SOM\COMMON\SYSTEM;%TOOLKIT%\SOM\MSG;%DPath%';
-    'SET DPATH=%TOOLKIT%\MSG;%TOOLKIT%\OPENDOC\BASE\MSG;%TOOLKIT%\BOOK;%DPath'
-
-    'SET BOOKSHELF=c:\appl\os2\toolkt40\archived;c:\appl\os2\toolkt40\book;c:\appl\os2\toolkt40\books;c:\appl\os2\ibmcpp30\help;%BOOKSHELF%';
-
-    'SET NLSPATH=C:\MPTN\MSG\NLS\%%N;C:\TCPIP\MSG\ENUS850\%%N;C:\netqos2\%%N;C:\appl\os2\toolkt40\OPENDOC\BASE\LOCALE\EN_US\%%N;C:\appl\os2\toolkt40\MSG\%%N;'
+    sToolkit = 'c:\appl\os2\toolkt40';
+    sVAC     = 'c:\appl\os2\ibmcpp30';
+    sDDKBase = 'd:\knut\ddk\base';
+    sMSC     = 'd:\knut\ddk\ddktools\toolkits\msc60';
+    sCVS     = 'd:\knut\cvs';
+    sEMXBack = 'c:\emx';
+    sEMXForw = 'c:/emx';
+    sMYSQL   = 'c:\emx\mysql'; /* optionally */
 
     'SET CVSROOT=:pserver:bird@www.netlabs.org:d:/netlabs.src/odin32'
     'SET HOME=d:\knut\home'
     'SET USER=bird'
     'SET DEBUG=1'
 
+    /* oopps! this one is problemous for 4OS/2 due the the '%N's... */
+    'SET NLSPATH=C:\MPTN\MSG\NLS\%%N;C:\TCPIP\MSG\ENUS850\%%N;C:\netqos2\%%N;'sToolkit'\OPENDOC\BASE\LOCALE\EN_US\%%N;'sToolkit'\MSG\%%N;'
+
+    /*
+     * Non or minore changes are needed here!
+     */
+    'SET TOOLKIT='sToolkit
+    'SET CPPLOCAL='sVAC
+    'SET DDKBASE='sDDKBase
+    'SET MSC='sMSC
+
+    'SET CPPHELP_INI=C:\OS2\SYSTEM'
+    'SET TMP=%TEMP%'
+    'SET TMPDIR=%TMP%'
+
+    'SET VACPP_SHARED=FALSE'
+    'SET CPPMAIN='sVAC
+    'SET CPPWORK='sVAC
+    'SET SMINCLUDE='sToolkit'\som\include;'sToolkit'\idl;'sVAC'\INCLUDE\SOM'
+    'SET HELPNDX=EPMKWHLP.NDX+CPP.NDX+CPPBRS.NDX'
+
+    'SET INCLUDE='sVAC'\INCLUDE;'sVAC'\INCLUDE\SOM;'sToolkit'\h;%include%;'
+    'SET LIB='sVAC'\LIB;%lib%;'
+    'SET LIB='sToolkit'\SPEECH\LIB;'sToolkit'\SAMPLES\MM\LIB;'sToolkit'\LIB;'sToolkit'\SOM\LIB;'sToolkit'\OPENDOC\BASE\LIB;%LIB%';
+    'SET LOCPATH='sVAC'\LOCALE;%LOCPATH%'
+    'SET HELP='sVAC'\HELP;%HELP%;'
+    'SET BEGINLIBPATH='sVAC'\DLL;%BeginLIBPATH%;'
+
+    'SET PATH='sVAC'\BIN;'sMSC'\binp;'sCVS'\bin;%Path%'
+    'SET PATH='sToolkit'\BETA\BIN;'sToolkit'\SOM\COMMON;'sToolkit'\SOM\BIN;'sToolkit'\OPENDOC\BASE\BIN;'sToolkit'\BIN;%Path%;'sDDKBase'\tools';
+
+    'SET DPATH='sVAC'\HELP;'sVAC';'sVAC'\LOCALE;%DPATH%'
+    'SET DPATH='sToolkit'\SOM\COMMON\SYSTEM;'sToolkit'\SOM\MSG;%DPath%';
+    'SET DPATH='sToolkit'\MSG;'sToolkit'\OPENDOC\BASE\MSG;'sToolkit'\BOOK;%DPath'
+
+    'SET BOOKSHELF='sToolkit'\archived;'sToolkit'\book;'sToolkit'\books;'sVAC'\help;%BOOKSHELF%';
+
+
     /* emx + mysql */
-    'set C_INCLUDE_PATH=C:/emx/include'
-    'set LIBRARY_PATH=C:/emx/lib'
-    'set CPLUS_INCLUDE_PATH=C:/emx/include/cpp;C:/emx/include'
-    'set PROTODIR=C:/emx/include/cpp/gen'
-    'set OBJC_INCLUDE_PATH=C:/emx/include'
-    'set TERM=mono'
-    'set TERMCAP=C:/emx/etc/termcap.dat'
-    'set INFOPATH=C:/emx/info'
-    'set BOOKSHELF=C:\OS2\BOOK;C:\EMX\BOOK;%BOOKSHELF%'
+    'set C_INCLUDE_PATH='sEMXForw'/include'
+    'set LIBRARY_PATH='sEMXForw'/lib'
+    'set CPLUS_INCLUDE_PATH='sEMXForw'/include/cpp;'sEMXForw'/include'
+    'set PROTODIR='sEMXForw'/include/cpp/gen'
+    'set OBJC_INCLUDE_PATH='sEMXForw'/include'
+    'set INFOPATH='sEMXForw'/info'
+    'set BOOKSHELF='sEMXBack'\BOOK;%BOOKSHELF%'
     'set EMXBOOK=emxdev.inf+emxlib.inf+emxgnu.inf+emxbsd.inf'
     'set HELPNDX=emxbook.ndx+%HELPNDX%'
-    'set DPATH=C:\emx\book;%DPATH%'
-    'set PATH=C:\emx\bin;%PATH%;c:\emx\mysql\bin'
+    'set DPATH='sEMXBack'\book;%DPATH%'
+    'set PATH='sEMXBack'\bin;%PATH%;'sMYSQL'\bin'
+    'set TERM=mono'
+    'set TERMCAP='sEMXForw'/etc/termcap.dat'
+
     exit(0);
