0f4caf6d479d5d994e2986954fb88623d0772031
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-05-19 12:08:37 +0000

    header updates

--- trunk/include/win/crtdll.h
+++ trunk/include/win/crtdll.h
@@ -1,4 +1,4 @@
-/* $Id: crtdll.h,v 1.5 2000-02-03 23:31:41 sandervl Exp $ */
+/* $Id: crtdll.h,v 1.6 2000-05-19 12:08:35 sandervl Exp $ */
 
 
 #ifndef __WINE_CRTDLL_H
@@ -26,45 +26,45 @@
 #define CRTDLL_ALPHA		(0x0100|CRTDLL_UPPER|CRTDLL_LOWER)
 
 /* syserr defines */
-char __syserr00[] = "No Error";
-char __syserr01[] = "Input to function out of range (EDOM)";
-char __syserr02[] = "Output of function out of range (ERANGE)";
-char __syserr03[] = "Argument list too long (E2BIG)";
-char __syserr04[] = "Permission denied (EACCES)";
-char __syserr05[] = "Resource temporarily unavailable (EAGAIN)";
-char __syserr06[] = "Bad file descriptor (EBADF)";
-char __syserr07[] = "Resource busy (EBUSY)";
-char __syserr08[] = "No child processes (ECHILD)";
-char __syserr09[] = "Resource deadlock avoided (EDEADLK)";
-char __syserr10[] = "File exists (EEXIST)";
-char __syserr11[] = "Bad address (EFAULT)";
-char __syserr12[] = "File too large (EFBIG)";
-char __syserr13[] = "Interrupted system call (EINTR)";
-char __syserr14[] = "Invalid argument (EINVAL)";
-char __syserr15[] = "Input or output error (EIO)";
-char __syserr16[] = "Is a directory (EISDIR)";
-char __syserr17[] = "Too many open files (EMFILE)";
-char __syserr18[] = "Too many links (EMLINK)";
-char __syserr19[] = "File name too long (ENAMETOOLONG)";
-char __syserr20[] = "Too many open files in system (ENFILE)";
-char __syserr21[] = "No such device (ENODEV)";
-char __syserr22[] = "No such file or directory (ENOENT)";
-char __syserr23[] = "Unable to execute file (ENOEXEC)";
-char __syserr24[] = "No locks available (ENOLCK)";
-char __syserr25[] = "Not enough memory (ENOMEM)";
-char __syserr26[] = "No space left on drive (ENOSPC)";
-char __syserr27[] = "Function not implemented (ENOSYS)";
-char __syserr28[] = "Not a directory (ENOTDIR)";
-char __syserr29[] = "Directory not empty (ENOTEMPTY)";
-char __syserr30[] = "Inappropriate I/O control operation (ENOTTY)";
-char __syserr31[] = "No such device or address (ENXIO)";
-char __syserr32[] = "Operation not permitted (EPERM)";
-char __syserr33[] = "Broken pipe (EPIPE)";
-char __syserr34[] = "Read-only file system (EROFS)";
-char __syserr35[] = "Invalid seek (ESPIPE)";
-char __syserr36[] = "No such process (ESRCH)";
-char __syserr37[] = "Improper link (EXDEV)";
-char __syserr38[] = "No more files (ENMFILE)";
+const char __syserr00[] = "No Error";
+const char __syserr01[] = "Input to function out of range (EDOM)";
+const char __syserr02[] = "Output of function out of range (ERANGE)";
+const char __syserr03[] = "Argument list too long (E2BIG)";
+const char __syserr04[] = "Permission denied (EACCES)";
+const char __syserr05[] = "Resource temporarily unavailable (EAGAIN)";
+const char __syserr06[] = "Bad file descriptor (EBADF)";
+const char __syserr07[] = "Resource busy (EBUSY)";
+const char __syserr08[] = "No child processes (ECHILD)";
+const char __syserr09[] = "Resource deadlock avoided (EDEADLK)";
+const char __syserr10[] = "File exists (EEXIST)";
+const char __syserr11[] = "Bad address (EFAULT)";
+const char __syserr12[] = "File too large (EFBIG)";
+const char __syserr13[] = "Interrupted system call (EINTR)";
+const char __syserr14[] = "Invalid argument (EINVAL)";
+const char __syserr15[] = "Input or output error (EIO)";
+const char __syserr16[] = "Is a directory (EISDIR)";
+const char __syserr17[] = "Too many open files (EMFILE)";
+const char __syserr18[] = "Too many links (EMLINK)";
+const char __syserr19[] = "File name too long (ENAMETOOLONG)";
+const char __syserr20[] = "Too many open files in system (ENFILE)";
+const char __syserr21[] = "No such device (ENODEV)";
+const char __syserr22[] = "No such file or directory (ENOENT)";
+const char __syserr23[] = "Unable to execute file (ENOEXEC)";
+const char __syserr24[] = "No locks available (ENOLCK)";
+const char __syserr25[] = "Not enough memory (ENOMEM)";
+const char __syserr26[] = "No space left on drive (ENOSPC)";
+const char __syserr27[] = "Function not implemented (ENOSYS)";
+const char __syserr28[] = "Not a directory (ENOTDIR)";
+const char __syserr29[] = "Directory not empty (ENOTEMPTY)";
+const char __syserr30[] = "Inappropriate I/O control operation (ENOTTY)";
+const char __syserr31[] = "No such device or address (ENXIO)";
+const char __syserr32[] = "Operation not permitted (EPERM)";
+const char __syserr33[] = "Broken pipe (EPIPE)";
+const char __syserr34[] = "Read-only file system (EROFS)";
+const char __syserr35[] = "Invalid seek (ESPIPE)";
+const char __syserr36[] = "No such process (ESRCH)";
+const char __syserr37[] = "Improper link (EXDEV)";
+const char __syserr38[] = "No more files (ENMFILE)";
 
 /* function prototypes used in crtdll.c */
 extern int LastErrorToErrno(DWORD);
