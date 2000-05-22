ca1ad68ad06999b5e8c388ade4be23fcae3b0a99
cbratschi <cbratschi@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-05-22 17:19:00 +0000

    *** empty log message ***

--- include/win/tab.h
+++ include/win/tab.h
@@ -1,4 +1,4 @@
-/* $Id: tab.h,v 1.3 2000-03-18 16:10:56 cbratschi Exp $ */
+/* $Id: tab.h,v 1.4 2000-05-22 17:18:55 cbratschi Exp $ */
 
 /*
  * Tab control class extra info
@@ -28,6 +28,7 @@ typedef struct tagTAB_INFO
   COMCTL32_HEADER header;
 
   UINT       uNumItem;        /* number of tab items */
+  UINT       uNumRows;	      /* number of tab rows */
   INT        tabHeight;       /* height of the tab row */
   INT        tabWidth;        /* width of tabs */
   HFONT      hFont;           /* handle to the current font */
@@ -38,11 +39,13 @@ typedef struct tagTAB_INFO
   INT        leftmostVisible; /* Used for scrolling, this member contains
                                * the index of the first visible item */
   INT        iSelected;       /* the currently selected item */
+  INT        iHotTracked;     /* the highlighted item under the mouse */
   INT        uFocus;          /* item which has the focus */
   TAB_ITEM*  items;           /* pointer to an array of TAB_ITEM's */
   BOOL       DoRedraw;        /* flag for redrawing when tab contents is changed*/
   BOOL       needsScrolling;  /* TRUE if the size of the tabs is greater than
                                * the size of the control */
+  BOOL	     fSizeSet;	      /* was the size of the tabs explicitly set? */
   HWND       hwndUpDown;      /* Updown control used for scrolling */
 } TAB_INFO;
 
