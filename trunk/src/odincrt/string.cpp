5c559c3b3bd62bdb684f709a9585123bf3e0c09f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-07-06 21:17:02 +0000

    VP fixed strncpy

--- trunk/src/odincrt/string.cpp
+++ trunk/src/odincrt/string.cpp
@@ -1,4 +1,4 @@
-/* $Id: string.cpp,v 1.1 1999-12-09 10:58:15 sandervl Exp $ */
+/* $Id: string.cpp,v 1.2 2000-07-06 21:17:02 sandervl Exp $ */
 /*
  * Project Odin Software License can be found in LICENSE.TXT
  * strncpy replacement (one in RTL lib is buggy; doesn't stop at 0 terminator)
@@ -14,13 +14,11 @@ char * _LNK_CONV __strncpy( char *dest, const char *src, size_t length)
   register char *p1 = dest;
   register char *p2 = (char *)src;
 
-  length--; // pre-decrement to avoid exceeding buffer length
-            // results in better code than (dest > 1)
-
   for (;*p2 && length; length--)
     *p1++ = *p2++;
 
-  *p1 = 0; //CB: copy arg-1, set end 0
+  if ( length > 0 ) // above loop ended due to nul in src
+    *p1 = 0;
 
   return dest;
 }
