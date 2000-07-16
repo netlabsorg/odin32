b69176f7f52e03b217a8d64f973a5eb0e96344dd
nobody <nobody@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-07-16 22:21:20 +0000

    This commit was manufactured by cvs2svn to create branch 'DEVELOPMENT'.

--- bin/dbgnlog.cmd
+++
@@ -1,4 +0,0 @@
-SET NOWIN32LOG=TRUE
-SET PMDPATH=f:\source\odin32\src\advapi32;f:\source\odin32\src\comdlg32;f:\source\odin32\src\gdi32;f:\source\odin32\src\user32;f:\source\odin32\src\kernel32;f:\source\odin32\src\winmm;f:\source\odin32\src\version;f:\source\odin32\src\shell32;f:\source\odin32\src\ole32;f:\source\odin32\src\ddraw;f:\source\odin32\src\dsound;f:\source\odin32\src\comctl32;
-ipmd %1
-
