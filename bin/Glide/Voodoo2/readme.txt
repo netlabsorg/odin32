770b18f404b90927829f6d8e00d09882f9e037e0
nobody <nobody@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-07-16 22:21:23 +0000

    This commit was manufactured by cvs2svn to create branch 'development'.

--- bin/Glide/Voodoo2/readme.txt
+++
@@ -1,2 +0,0 @@
-This is the Glide dll for the 3dfx Voodoo 2 card.
-Copy it into the Odin system directory to use it.
--- include/win/xmalloc.h
+++
@@ -1,7 +0,0 @@
-/* $Id: xmalloc.h,v 1.1 2000-01-06 20:03:04 sandervl Exp $ */
-#ifndef __XMALLOC_H__
-#define __XMALLOC_H__
-
-#define xmalloc malloc
-
-#endif //__XMALLOC_H__
