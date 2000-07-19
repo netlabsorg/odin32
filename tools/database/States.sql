4a53729be92dc9c2d68db5d88880817a195acf60
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-07-19 21:29:36 +0000

    Added column 'weigth' to the state table. This column hold the weigth functions in that state should have when calculating the total completion percentage.

--- tools/database/States.sql
+++ tools/database/States.sql
@@ -1,4 +1,4 @@
--- $Id: States.sql,v 1.2 1999-12-02 12:05:47 bird Exp $
+-- $Id: States.sql,v 1.3 2000-07-19 21:29:36 bird Exp $
 --
 -- Insert states into the state table.
 --
@@ -6,27 +6,28 @@ USE odin32;
 
 DELETE FROM state;
 
+--      refcode color    weight  name                       description
 INSERT INTO state
-    VALUES (0, '#000080', 'Not Implemented', 'Not implemented.');
+    VALUES (0, '#000080',   0,  'Not Implemented',          'Not implemented.');
 INSERT INTO state
-    VALUES (1, '#0000FF', 'Stub', 'Stub-function. Does nothing.');
+    VALUES (1, '#0000FF',   5,  'Stub',                     'Stub-function. Does nothing.');
 INSERT INTO state
-    VALUES (2, '#808000', 'Partially', 'Partially implemented. Not Tested.');
+    VALUES (2, '#808000',  20,  'Partially',                'Partially implemented. Not Tested.');
 INSERT INTO state
-    VALUES (3, '#FFFF00', 'Partially Tested', 'Partially implemented. Tested and approved.');
+    VALUES (3, '#FFFF00',  25,  'Partially Tested',         'Partially implemented. Tested and approved.');
 INSERT INTO state
-    VALUES (4, '#008000', 'Completely', 'Completely implemented. Not tested.');
+    VALUES (4, '#008000',  90,  'Completely',               'Completely implemented. Not tested.');
 INSERT INTO state
-    VALUES (5, '#00FF00', 'Completely Tested', 'Completely implemented. Tested and approved.');
+    VALUES (5, '#00FF00', 100,  'Completely Tested',        'Completely implemented. Tested and approved.');
 INSERT INTO state
-    VALUES (6, '#808080', 'Open32 Partially', 'Partially implemented. Not Tested.');
+    VALUES (6, '#808080',  20,  'Open32 Partially',         'Partially implemented. Not Tested.');
 INSERT INTO state
-    VALUES (7, '#FFFF80', 'Open32 Partially Tested', 'Partially implemented. Tested and approved.');
+    VALUES (7, '#FFFF80',  25,  'Open32 Partially Tested',  'Partially implemented. Tested and approved.');
 INSERT INTO state
-    VALUES (8, '#008080', 'Open32 Completely', 'Completely implemented. Not tested.');
+    VALUES (8, '#008080',  90,  'Open32 Completely',        'Completely implemented. Not tested.');
 INSERT INTO state
-    VALUES (9, '#00FF80', 'Open32 Completely Tested', 'Completely implemented. Tested and approved.');
+    VALUES (9, '#00FF80', 100,  'Open32 Completely Tested', 'Completely implemented. Tested and approved.');
 INSERT INTO state
-    VALUES (99, '#FF0000', 'Unknown', 'The state of this function is unknown.');
+    VALUES (99, '#FF0000', 25,  'Unknown',                  'The state of this function is unknown.');
 
 
