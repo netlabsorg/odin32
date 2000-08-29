61e1f1aa80652b094f6b4d0c9823138ac29e113f
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-08-29 19:47:04 +0000

    k32/W32k (lib) IOCtl APIs refshed up and extended.

--- src/win32k/include/win32k.h
+++ src/win32k/include/win32k.h
@@ -1,4 +1,4 @@
-/* $Id: win32k.h,v 1.4 2000-02-26 20:21:42 bird Exp $
+/* $Id: win32k.h,v 1.4.4.1 2000-08-29 19:47:01 bird Exp $
  *
  * Top level make file for the Win32k library.
  * Contains library and 32-bit IOCtl definition.
@@ -24,7 +24,7 @@
  * K32 category - these are the functions found in the k32 directory.
  */
 #define K32_ALLOCMEMEX          0x01
-
+#define K32_QUERYOTES           0x02
 
 /*
  * Elf category
@@ -35,6 +35,30 @@
 /*******************************************************************************
 *   Structures and Typedefs                                                    *
 *******************************************************************************/
+#pragma pack(1)
+
+/*
+ * Object Table Entry buffer.
+ */
+typedef struct _QueryOTE
+{
+    ULONG   ote_size;                   /* Object virtual size */
+    ULONG   ote_base;                   /* Object base virtual address */
+    ULONG   ote_flags;                  /* Attribute flags */
+    ULONG   ote_pagemap;                /* Object page map index */
+    ULONG   ote_mapsize;                /* Num of entries in obj page map */
+  /*ULONG   ote_reserved;*/
+    USHORT  ote_sel;                    /* Object Selector */
+    USHORT  ote_hob;                    /* Object Handle */
+} QOTE, *PQOTE;
+
+typedef struct _QueryOTEBuffer
+{
+    ULONG   cOTEs;                      /* Number of entries in aOTE. */
+    QOTE    aOTE[1];                    /* Array of OTEs. */
+} QOTEBUFFER, *PQOTEBUFFER;
+
+
 /*
  * K32 category parameter structs
  */
@@ -50,17 +74,27 @@ typedef struct _k32AllocMemEx
     ULONG   rc;                         /* Return code. */
 } K32ALLOCMEMEX, *PK32ALLOCMEMEX;
 
+typedef struct _k32QueryOTEs
+{
+    HMODULE     hMTE;                   /* Module handle. */
+    PQOTEBUFFER pQOte;                  /* Pointer to output buffer. */
+    ULONG       cbQOte;                 /* Size of the buffer pointed to by pQOte  */
+    ULONG       rc;                     /* Return code. */
+} K32QUERYOTES, *PK32QUERYOTES;
 
 
+#pragma pack()
 
-#ifdef INCL_WIN32K_LIB
+#ifndef NO_WIN32K_LIB_FUNCTIONS
 /*******************************************************************************
 *   External Functions                                                         *
 *******************************************************************************/
 APIRET APIENTRY  libWin32kInit(void);
 APIRET APIENTRY  libWin32kTerm(void);
 USHORT APIENTRY  libHelperGetCS(void);
+BOOL   APIENTRY  libWin32kInstalled(void);
 APIRET APIENTRY  DosAllocMemEx(PPVOID ppv, ULONG cb, ULONG flag);
+APIRET APIENTRY  W32kQueryOTEs(HMODULE hMTE, PQOTEBUFFER pQOte, ULONG cbQOte);
 
 
 #endif
