55470db6eb02b49dada97ccb90fe281c13ef869e
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-09-21 12:05:16 +0000

    Removed environemnt...

--- tags/trunk/src/win32k/fhfix.c
+++ tags/trunk/src/win32k/fhfix.c
@@ -1,6 +1,7 @@
 #define INCL_BASE
 #include <os2.h>
 #include <string.h>
+
 int main(int argc, char **argv)
 {
     static char     szArgBuffer[32768];
@@ -35,7 +36,7 @@ int main(int argc, char **argv)
     }
     *psz = '\0';
 
-    if (DosExecPgm(NULL, 0, EXEC_SYNC, szArgBuffer, NULL, &resc, szArgBuffer) != NO_ERROR)
+    if (DosExecPgm(NULL, 0, EXEC_SYNC, szArgBuffer, "\0\0", &resc, szArgBuffer) != NO_ERROR)
     {
         /*complain*/
         return -1;
