866c2e31fcc7f8747f819ef8d5f05c2da1f9a2e0
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-11-14 14:28:47 +0000

    header update

--- trunk/include/winuser32.h
+++ trunk/include/winuser32.h
@@ -1,4 +1,4 @@
-/* $Id: winuser32.h,v 1.2 2000-11-05 18:47:11 sandervl Exp $ */
+/* $Id: winuser32.h,v 1.3 2000-11-14 14:28:47 sandervl Exp $ */
 
 /*
  * Internal functions exported by user32
@@ -9,15 +9,19 @@
 #ifndef __WINUSER32_H__
 #define __WINUSER32_H__
 
+#include <dcdata.h>
+
 ULONG OSLibGetScreenHeight();
 ULONG OSLibGetScreenWidth();
 
 void WIN32API Calculate1PixelDelta(pDCData pHps);
 void WIN32API TestWideLine (pDCData pHps);
-BOOL WIN32API changePageXForm(pDCData pHps, PPOINTL pValue, int x, int y, PPOINTL pPrev);
+BOOL WIN32API changePageXForm(pDCData pHps, POINTL *pValue, int x, int y, POINTL *pPrev);
 BOOL WIN32API setPageXForm(pDCData pHps);
 VOID WIN32API removeClientArea(pDCData pHps);
 LONG WIN32API clientHeight(HWND hwnd, pDCData pHps);
 int  WIN32API setMapMode(pDCData pHps, int mode);
+
+int  WIN32API DIB_GetDIBWidthBytes( int width, int depth );
 
 #endif //__WINUSER32_H__
