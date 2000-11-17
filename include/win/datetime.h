dfaa8305c63c86d317b8c35d635ccbc0b2b36053
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2000-11-17 14:32:50 +0000

    header updates for comctl32

--- include/win/datetime.h
+++ include/win/datetime.h
@@ -1,4 +1,4 @@
-/* $Id: datetime.h,v 1.3 2000-02-25 16:58:33 cbratschi Exp $ */
+/* $Id: datetime.h,v 1.4 2000-11-17 14:32:49 sandervl Exp $ */
 
 /*
  * Date and time picker class extra info
@@ -10,35 +10,16 @@
 #ifndef __WINE_DATETIME_H
 #define __WINE_DATETIME_H
 
-typedef struct tagDATETIME_INFO
-{
-        COMCTL32_HEADER header;
-        HWND hMonthCal;
-        HWND hUpdown;
-        SYSTEMTIME date;
-        BOOL dateValid;
-        HWND hwndCheckbut;
-        RECT rect;
-        RECT checkbox;
-        RECT daytxt;
-        RECT daynumtxt;
-        RECT rmonthtxt;
-        RECT yeartxt;
-        RECT calbutton;
-        int  select;
-        HFONT hFont;
-} DATETIME_INFO, *LPDATETIME_INFO;
 
-extern VOID DATETIME_Register (VOID);
-extern VOID DATETIME_Unregister (VOID);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#define DTHT_NONE     0
-#define DTHT_MCPOPUP  1
-#define DTHT_YEAR     2
-#define DTHT_DAYNUM   3
-#define DTHT_MONTH    4
-#define DTHT_DAY      5
-#define DTHT_CHECKBOX 6
-#define DTHT_GOTFOCUS 128
+VOID DATETIME_Register (VOID);
+VOID DATETIME_Unregister (VOID);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  /* __WINE_DATETIME_H */
