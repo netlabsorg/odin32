6c3bd538c28e2a6faf75fb687fa2cad35f591fc5
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-01-20 18:14:01 +0000

    Moved build level info from versionos2.h.

---
+++ trunk/include/odinbuild.h
@@ -0,0 +1,23 @@
+/* $Id: odinbuild.h,v 1.1 2001-01-20 18:14:01 bird Exp $
+ *
+ * Odin32 version number and build numbers.
+ *
+ * Copyright (c) 2001 knut st. osmundsen (knut.stange.osmundsen@mynd.no)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+
+#ifndef _ODINBUILD_H_
+#define _ODINBUILD_H_
+
+/*
+ * Current Odin version and build number.
+ * Used for bldlevel info.
+ */
+#define ODIN32_VERSION_MAJOR 0
+#define ODIN32_VERSION_MINOR 5
+#define ODIN32_VERSION       "0.5"
+#define ODIN32_BUILD_NR       309        /* This started at 300 on the 17th Des 2000. */
+
+#endif
