0b9f247fc0043e7b74897046cb0ac17e989c89db
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-01-22 04:28:48 +0000

    Daily build - 20010122

--- include/odinbuild.h
+++ include/odinbuild.h
@@ -1,4 +1,4 @@
-/* $Id: odinbuild.h,v 1.1 2001-01-20 18:14:01 bird Exp $
+/* $Id: odinbuild.h,v 1.2 2001-01-22 04:28:48 bird Exp $
  *
  * Odin32 version number and build numbers.
  *
@@ -18,6 +18,7 @@
 #define ODIN32_VERSION_MAJOR 0
 #define ODIN32_VERSION_MINOR 5
 #define ODIN32_VERSION       "0.5"
-#define ODIN32_BUILD_NR       309        /* This started at 300 on the 17th Des 2000. */
+#define ODIN32_BUILD_NR       310        /* This started at 300 on the 17th Des 2000. */
 
 #endif
+
