cdc33a1f26edea884772b1bcccc128373e7813f1
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-02-10 11:16:29 +0000

    Empty headers.

---
+++ trunk/src/win32k/include/OS2KPG.h
@@ -0,0 +1,19 @@
+/* $Id: OS2KPG.h,v 1.1 2001-02-10 11:16:28 bird Exp $
+ *
+ * OS/2 kernel page manager stuff.
+ *
+ * Copyright (c) 2001 knut st. osmundsen (knut.stange.osmundsen@mynd.no)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#ifndef _OS2KPG_H_
+#define _OS2KPG_H_
+
+
+
+
+
+
+#endif
+
