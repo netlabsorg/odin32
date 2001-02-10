74d56d7fdbc8037be8ca29fd1cccdbeaa89478a1
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-02-10 11:16:29 +0000

    Empty headers.

---
+++ src/win32k/include/OS2KSM.h
@@ -0,0 +1,17 @@
+/* $Id: OS2KSM.h,v 1.1 2001-02-10 11:16:29 bird Exp $
+ *
+ * OS/2 kernel swapper manager stuff.
+ *
+ * Copyright (c) 2001 knut st. osmundsen (knut.stange.osmundsen@mynd.no)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#ifndef _OS2KSM_H_
+#define _OS2KSM_H_
+
+
+
+
+
+#endif
