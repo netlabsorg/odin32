6eb7e4539016cc3efe3d95abcfa28275477078a2
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-02-16 23:20:30 +0000

    Temporary checkin - not even compiling.

--- branches/notebook/src/win32k/utils/Dialog.h
+++ branches/notebook/src/win32k/utils/Dialog.h
@@ -1,4 +1,4 @@
-/* $Id: Dialog.h,v 1.2 2001-02-11 23:41:46 bird Exp $
+/* $Id: Dialog.h,v 1.2.2.1 2001-02-16 23:20:29 bird Exp $
  *
  * Dialog IDs.
  *
@@ -20,91 +20,110 @@
 
 
 /*
- * The Win32kCC (main) dialog.
+ * The Win32kCC (main) notebook dialog.
  */
-#define DL_WIN32KCC                 1000
+#define DL_WIN32KCC                 1001
+#define DL_WIN32KCC_OLD             1000
+#define DL_WIN32KCC_NTBK            1002
 
-#define GR_LOGGING                  1010
-#define CB_LOGGING_ENABLED          1011
-#define RB_LOGGING_COM1             1012
-#define RB_LOGGING_COM2             1013
-#define RB_LOGGING_COM3             1014
-#define RB_LOGGING_COM4             1015
+/*
+ * The Logging page.
+ */
+#define DL_LOGGING_PAGE             1100
+#define GR_LOGGING                  1101
+#define CB_LOGGING_ENABLED          1102
+#define RB_LOGGING_COM1             1103
+#define RB_LOGGING_COM2             1104
+#define RB_LOGGING_COM3             1105
+#define RB_LOGGING_COM4             1106
 
+/*
+ * The Loader Page.
+ */
+#define DL_LOADERS_PAGE             1200
+#define GR_LOADERS                  1201
+#define CB_LDR_DISABLE_ALL          1202
 
-#define GR_LOADERS                  1030
-#define CB_LDR_DISABLE_ALL          1031
+#define GR_LDR_PE                   1210
+#define RB_LDR_PE_PURE              1211
+#define RB_LDR_PE_MIXED             1212
+#define RB_LDR_PE_PE                1213
+#define RB_LDR_PE_NOT               1214
+#define TX_LDR_PE_INFOLEVEL         1215
+#define SB_LDR_PE_INFOLEVEL         1216
+#define TX_LDR_PE_MODULES           1217
+#define TX_LDR_PE_MODULES_VAL       1218
+#define CK_LDR_PE_ONEOBJECT         1219
 
-#define GR_LDR_PE                   1040
-#define RB_LDR_PE_PURE              1041
-#define RB_LDR_PE_MIXED             1042
-#define RB_LDR_PE_PE                1043
-#define RB_LDR_PE_NOT               1044
-#define TX_LDR_PE_INFOLEVEL         1045
-#define SB_LDR_PE_INFOLEVEL         1046
-#define TX_LDR_PE_MODULES           1047
-#define TX_LDR_PE_MODULES_VAL       1048
-#define CK_LDR_PE_ONEOBJECT         1049
+#define GR_LDR_ELF                  1240
+#define CB_LDR_ELF_ENABLED          1241
+#define TX_LDR_ELF_INFOLEVEL        1242
+#define SB_LDR_ELF_INFOLEVEL        1243
+#define TX_LDR_ELF_MODULES          1244
+#define TX_LDR_ELF_MODULES_VAL      1245
 
-#define GR_LDR_ELF                  1060
-#define CB_LDR_ELF_ENABLED          1061
-#define TX_LDR_ELF_INFOLEVEL        1062
-#define SB_LDR_ELF_INFOLEVEL        1063
-#define TX_LDR_ELF_MODULES          1064
-#define TX_LDR_ELF_MODULES_VAL      1065
+#define CB_LDR_SHELL_SCRIPTS        1260
+#define CB_LDR_JAVA                 1270
+#define CB_LDR_REXX                 1280
 
-#define CB_LDR_SHELL_SCRIPTS        1070
-#define CB_LDR_JAVA                 1080
-#define CB_LDR_REXX                 1090
+/*
+ * The Heaps page.
+ */
+#define DL_HEAPS_PAGE               1300
+#define GR_HEAPS                    1301
 
+#define GR_HEAP_RES                 1310
+#define TX_HEAP_RES_MAX             1311
+#define SB_HEAP_RES_MAX             1312
+#define TX_HEAP_RES_INIT            1313
+#define TX_HEAP_RES_INIT_VAL        1314
+#define TX_HEAP_RES_USED            1315
+#define TX_HEAP_RES_USED_VAL        1316
+#define TX_HEAP_RES_FREE            1317
+#define TX_HEAP_RES_FREE_VAL        1318
+#define TX_HEAP_RES_SIZE            1319
+#define TX_HEAP_RES_SIZE_VAL        1320
+#define TX_HEAP_RES_USED_BLOCKS     1321
+#define TX_HEAP_RES_USED_BLOCKS_VAL 1322
+#define TX_HEAP_RES_FREE_BLOCKS     1323
+#define TX_HEAP_RES_FREE_BLOCKS_VAL 1324
 
-#define GR_HEAPS                    1150
+#define GR_HEAP_SWP                 1340
+#define TX_HEAP_SWP_MAX             1341
+#define SB_HEAP_SWP_MAX             1342
+#define TX_HEAP_SWP_INIT            1343
+#define TX_HEAP_SWP_INIT_VAL        1344
+#define TX_HEAP_SWP_USED            1345
+#define TX_HEAP_SWP_USED_VAL        1346
+#define TX_HEAP_SWP_FREE            1347
+#define TX_HEAP_SWP_FREE_VAL        1348
+#define TX_HEAP_SWP_SIZE            1349
+#define TX_HEAP_SWP_SIZE_VAL        1350
+#define TX_HEAP_SWP_USED_BLOCKS     1351
+#define TX_HEAP_SWP_USED_BLOCKS_VAL 1352
+#define TX_HEAP_SWP_FREE_BLOCKS     1353
+#define TX_HEAP_SWP_FREE_BLOCKS_VAL 1354
 
-#define GR_HEAP_RES                 1160
-#define TX_HEAP_RES_MAX             1161
-#define SB_HEAP_RES_MAX             1162
-#define TX_HEAP_RES_INIT            1163
-#define TX_HEAP_RES_INIT_VAL        1164
-#define TX_HEAP_RES_USED            1165
-#define TX_HEAP_RES_USED_VAL        1166
-#define TX_HEAP_RES_FREE            1167
-#define TX_HEAP_RES_FREE_VAL        1168
-#define TX_HEAP_RES_SIZE            1169
-#define TX_HEAP_RES_SIZE_VAL        1170
-#define TX_HEAP_RES_USED_BLOCKS     1171
-#define TX_HEAP_RES_USED_BLOCKS_VAL 1172
-#define TX_HEAP_RES_FREE_BLOCKS     1173
-#define TX_HEAP_RES_FREE_BLOCKS_VAL 1174
+/*
+ * The Status page.
+ */
+#define PG_WIN32K_INFO_PAGE         1400
+#define GR_W32K                     1401
+#define TX_W32K_VERSION             1402
+#define TX_W32K_VERSION_VAL         1403
+#define TX_W32K_BUILD_DATETIME      1404
+#define TX_W32K_BUILD_DATETIME_VAL  1405
+#define TX_W32K_SYMBOLFILE          1406
+#define TX_W32K_SYMBOLFILE_VAL      1407
+#define TX_W32K_KERNELBUILD         1408
+#define TX_W32K_KERNELBUILD_VAL     1409
 
-
-#define GR_HEAP_SWP                 1180
-#define TX_HEAP_SWP_MAX             1181
-#define SB_HEAP_SWP_MAX             1182
-#define TX_HEAP_SWP_INIT            1183
-#define TX_HEAP_SWP_INIT_VAL        1184
-#define TX_HEAP_SWP_USED            1185
-#define TX_HEAP_SWP_USED_VAL        1186
-#define TX_HEAP_SWP_FREE            1187
-#define TX_HEAP_SWP_FREE_VAL        1188
-#define TX_HEAP_SWP_SIZE            1189
-#define TX_HEAP_SWP_SIZE_VAL        1190
-#define TX_HEAP_SWP_USED_BLOCKS     1191
-#define TX_HEAP_SWP_USED_BLOCKS_VAL 1192
-#define TX_HEAP_SWP_FREE_BLOCKS     1193
-#define TX_HEAP_SWP_FREE_BLOCKS_VAL 1194
-
-#define GR_W32K                     1200
-#define TX_W32K_VERSION             1201
-#define TX_W32K_VERSION_VAL         1202
-#define TX_W32K_BUILD_DATETIME      1203
-#define TX_W32K_BUILD_DATETIME_VAL  1204
-#define TX_W32K_SYMBOLFILE          1205
-#define TX_W32K_SYMBOLFILE_VAL      1206
-#define TX_W32K_KERNELBUILD         1207
-#define TX_W32K_KERNELBUILD_VAL     1208
-
-#define GR_LDRFIX                   1250
-#define CB_LDRFIX_DLLFIXES          1251
-#define CB_LDRFIX_EXEFIXES          1252
-#define CB_LDRFIX_FORCEPRELOAD      1253
+/*
+ * The Loader/OS extention Page
+ */
+#define DL_LDRFIX_PAGE              1500
+#define GR_LDRFIX                   1501
+#define CB_LDRFIX_DLLFIXES          1502
+#define CB_LDRFIX_EXEFIXES          1503
+#define CB_LDRFIX_FORCEPRELOAD      1504
 
