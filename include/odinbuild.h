0c9e02fc02ac7badb98470a7c338fe0a088ecbfd
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-02-21 03:26:57 +0000

    Made a change to stop the build since win32k.sys is still broken ;-).

--- include/odinbuild.h
+++ include/odinbuild.h
@@ -1,4 +1,4 @@
-/* $Id: odinbuild.h,v 1.22 2001-02-20 06:03:27 bird Exp $
+/* $Id: odinbuild.h,v 1.23 2001-02-21 03:26:57 bird Exp $
  *
  * Odin32 version number and build numbers.
  *
@@ -15,30 +15,11 @@
  * Current Odin version and build number.
  * Used for bldlevel info.
  */
-#define ODIN32_VERSION_MAJOR 0
-#define ODIN32_VERSION_MINOR 5
+#define ODIN32_VERSION_MAJOR    0
+#define ODIN32_VERSION_MINOR    5
 #define ODIN32_VERSION       "0.5"
 #define ODIN32_BUILD_NR       330        /* This started at 300 on the 17th Des 2000. */
 
+
 #endif
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
