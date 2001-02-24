8ccd134475dbdebef8a56aaeda4410cd402bbbe7
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-02-24 04:37:15 +0000

    Updated method.

--- trunk/src/win32k/mmf/mmf.cpp
+++ trunk/src/win32k/mmf/mmf.cpp
@@ -1,4 +1,4 @@
-/* $Id: mmf.cpp,v 1.1 2000-10-31 17:35:00 bird Exp $
+/* $Id: mmf.cpp,v 1.2 2001-02-24 04:37:15 bird Exp $
  *
  * Memory Mapped Files.
  *
@@ -34,33 +34,16 @@ in these forms:
 
 @subsection     Loader Exploits (Overloads)
 
-The Ring-0 part will overload the handling of the Ring-3 DLL, that's the
-reason why I will insist on having a separate MMF DLL. When the first call to
-the MMF is issued we'll find the MMF DLL and "hook" it's handle. This
-"hooking" will enable us to make speciall processing in LDRGetPage,
-LDRFreeTask and maybe ldrAllocateObjects. All of these LDR functions will have
-to be overloaded with my own functions - LDRGetPage will be written in
-assembly to minimize latency.<p>
+The Ring-0 part will create pseudo MTEs for each file handle given in the
+openmapping call. These MTEs will not be linked into the mte list, but will
+be linked into a private MMF list. By doing it this way we'll be able to use
+the LDRGetPage function without overloading it. We just have to feed the
+loader with valid pagelists.<p>
 
+We'll have to do cleanups of this internal MTEs by overloading LDRFreeTask.<p>
 
-@subsubsection  LDRGetPage
-
-As you will see somewhere else (when this text is completed) we'll allocate
-objects my self and give the hMTE of the MMF DLL. The block number will be
-relative to the start of each mapping if there is a filebacking of the
-object.<p>
-
-So, what we'll actually do is 1st to check that the call isn't for the real
-LX objects of the MMF DLL. Since we're overloading the processing of the
-MMF DLL we will have to distiguish between calls related to the processing
-of the DLL and the processing of our memorymapped objects. Then, if this
-is a request for a page in the one of the objects of the MMF DLL we'll simply
-call(/jump) to the original LDRGetPage.<p>
-
-If this is a request for a page in one of the memory mapped objects, we'll
-have to get that page. Starts out by finding the mapping this is for. If it's
-a mapping without filebacking we'll simply return a zero'ed page. Else, we'll
-have to read it from the disk. And that's about all we'll have to do.
+Objects are allocated by us self using ldrAllocObjects and ldrSetVMFlags will
+be overloaded to set the correct flags.
 
 
 @subsubsection  LDRFreeTask
@@ -83,7 +66,7 @@ and yet the Ring-0 would not trust the Ring-3 very much since someone may
 call the DosIOCtls them selves. These are the proposed interfaces:
 <ul>
     <li>MMFCreating  - Create a mapping handle.
-    <li>MMFDuplicate - Duplicates an mapping handle.
+    <li>MMFDuplicate - Duplicates a mapping handle.
     <li>MMFOpen      - Open an existing mapping.
     <li>MMFViewMap   - Creates a view for a part of the file.
     <li>MMFViewUnmap - Flushes and destroys a view.
@@ -104,3 +87,5 @@ This will roughly be the exported "native" APIs too.
 To be written some other day.
 
  */
+
+
