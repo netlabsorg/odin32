6cb4bee95b30498c124424ac4cc78cda8c4a8521
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-05-19 13:17:34 +0000

    added/implemented functions

--- trunk/src/shlwapi/path.c
+++ trunk/src/shlwapi/path.c
@@ -28,6 +28,15 @@
 #include "debugtools.h"
 
 
+#ifdef __WIN32OS2__
+#undef FIXME
+#ifdef DEBUG
+#define FIXME WriteLog("FIXME %s", __FUNCTION__); WriteLog
+#else
+#define FIXME 1 ? (void)0 : (void)((int (*)(char *, ...)) NULL)
+#endif
+#endif
+
 DEFAULT_DEBUG_CHANNEL(shell);
 
 #define isSlash(x) ((x)=='\\' || (x)=='/')
