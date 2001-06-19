3aee4435c860f79ba870c321ca5562ed8de29e6f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-06-19 10:50:26 +0000

    semaphore rewrite (not activated)

--- src/kernel32/hmmutex.cpp
+++ src/kernel32/hmmutex.cpp
@@ -1,9 +1,16 @@
-/* $Id: hmmutex.cpp,v 1.3 2000-02-16 14:24:00 sandervl Exp $ */
+/* $Id: hmmutex.cpp,v 1.4 2001-06-19 10:50:25 sandervl Exp $ */
 
 /*
+ * Win32 Mutex Semaphore implementation
+ *
+ * TODO: Inheritance
+ * TODO: No inheritance when CreateMutex is called for existing named event semaphore?
+ *       (see HMCreateMutex in handlemanager.cpp)
+ * TODO: Name collisions with files & mutex not allowed. Check if this can happen in OS/2
+ *
  * Project Odin Software License can be found in LICENSE.TXT
- * Win32 Unified Handle Manager for OS/2
- * Copyright 1999 Patrick Haller (haller@zebra.fh-weingarten.de)
+ *
+ * Copyright 2001 Sander van Leeuwen (sandervl@xs4all.nl)
  */
 
 #undef DEBUG_LOCAL
@@ -21,11 +28,20 @@
  * Includes                                                                  *
  *****************************************************************************/
 
+#ifdef USE_OS2SEMAPHORES
+#define INCL_DOSSEMAPHORES
+#include <os2wrap.h>
+#include <win32type.h>
+#include <win32api.h>
+#include <winconst.h>
+#else
 #include <os2win.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 #include "unicode.h"
 #include "misc.h"
+#include "oslibdos.h"
 
 #include "HandleManager.H"
 #include "HMMutex.h"
@@ -63,6 +79,33 @@ DWORD HMDeviceMutexClass::CreateMutex(PHMHANDLEDATA         pHMHandleData,
                                       BOOL                  fInitialOwner,
                                       LPCTSTR               lpszMutexName)
 {
+#ifdef USE_OS2SEMAPHORES
+  APIRET rc;
+  HMTX htmx;
+  char szSemName[CCHMAXPATH];
+
+  dprintf(("KERNEL32: HandleManager::Mutex::CreateMutex(%08xh,%08xh,%08xh,%s)\n",
+           pHMHandleData,
+           lpsa,
+           fInitialOwner,
+           lpszMutexName));
+
+  if(lpszMutexName) {
+      strcpy(szSemName, "\\SEM32\\");
+      strcat(szSemName, lpszMutexName);
+      lpszMutexName = szSemName;
+  }
+  rc = DosCreateMutexSem(lpszMutexName, &htmx, 0, fInitialOwner);
+
+  if(rc) {
+      dprintf(("DosCreateMutexSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      return error2WinError(rc);
+  }
+  pHMHandleData->dwAccess  = MUTEX_ALL_ACCESS_W;
+  pHMHandleData->hHMHandle = htmx;
+  return ERROR_SUCCESS_W;
+#else
   HANDLE hOpen32;
 
   dprintf(("KERNEL32: HandleManager::Mutex::CreateMutex(%08xh,%08xh,%08xh,%s)\n",
@@ -82,6 +125,7 @@ DWORD HMDeviceMutexClass::CreateMutex(PHMHANDLEDATA         pHMHandleData,
   }
   else
     return (O32_GetLastError());
+#endif
 }
 
 
@@ -101,6 +145,32 @@ DWORD HMDeviceMutexClass::OpenMutex(PHMHANDLEDATA         pHMHandleData,
                                     BOOL                  fInheritHandle,
                                     LPCTSTR               lpszMutexName)
 {
+#ifdef USE_OS2SEMAPHORES
+  HMTX   hmtx;
+  APIRET rc;
+  char szSemName[CCHMAXPATH];
+
+  dprintf(("KERNEL32: HandleManager::Mutex::OpenMutex(%08xh,%08xh,%s)\n",
+           pHMHandleData,
+           fInheritHandle,
+           lpszMutexName));
+
+  if(lpszMutexName == NULL) {
+      pHMHandleData->hHMHandle = 0;
+      return ERROR_INVALID_PARAMETER_W;
+  }
+
+  strcpy(szSemName, "\\SEM32\\");
+  strcat(szSemName, lpszMutexName);
+  rc = DosOpenMutexSem(szSemName, &hmtx);
+  if(rc) {
+      dprintf(("DosOpenMutexSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      return error2WinError(rc);
+  }
+  pHMHandleData->hHMHandle = hmtx;
+  return ERROR_SUCCESS_W;
+#else
   HANDLE hOpen32;
 
   dprintf(("KERNEL32: HandleManager::Mutex::OpenMutex(%08xh,%08xh,%s)\n",
@@ -119,8 +189,88 @@ DWORD HMDeviceMutexClass::OpenMutex(PHMHANDLEDATA         pHMHandleData,
   }
   else
     return (O32_GetLastError());
+#endif
 }
 
+/*****************************************************************************
+ * Name      : HMDeviceMutexClass::CloseHandle
+ * Purpose   : close the handle
+ * Parameters: PHMHANDLEDATA pHMHandleData
+ * Variables :
+ * Result    : API returncode
+ * Remark    :
+ * Status    :
+ *
+ * Author    : 
+ *****************************************************************************/
+
+#ifdef USE_OS2SEMAPHORES
+BOOL HMDeviceMutexClass::CloseHandle(PHMHANDLEDATA pHMHandleData)
+{
+  APIRET rc;
+
+  if(pHMHandleData->hHMHandle) {
+      rc = DosCloseMutexSem((HEV)pHMHandleData->hHMHandle);
+      if(rc) {
+          dprintf(("DosCloseMutexSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+          SetLastError(error2WinError(rc));
+          return FALSE;
+      }
+  }
+  return TRUE;
+}
+#endif
+
+/*****************************************************************************
+ * Name      : HMDeviceMutexClass::DuplicateHandle
+ * Purpose   : 
+ * Parameters:
+ *             various parameters as required
+ * Variables :
+ * Result    :
+ * Remark    : the standard behaviour is to return an error code for non-
+ *             existant request codes
+ * Status    :
+ *
+ * Author    : 
+ *****************************************************************************/
+#ifdef USE_OS2SEMAPHORES
+BOOL HMDeviceMutexClass::DuplicateHandle(PHMHANDLEDATA pHMHandleData, HANDLE  srcprocess,
+                               PHMHANDLEDATA pHMSrcHandle,
+                               HANDLE  destprocess,
+                               PHANDLE desthandle,
+                               DWORD   fdwAccess,
+                               BOOL    fInherit,
+                               DWORD   fdwOptions,
+                               DWORD   fdwOdinOptions)
+{
+  APIRET rc; 
+  HMTX hmtx;
+  
+  dprintf(("KERNEL32:HandleManager::DuplicateHandle %s(%08x,%08x,%08x,%08x,%08x) - NOT IMPLEMENTED!!!!!!!!\n",
+           lpHMDeviceName,
+           pHMHandleData,
+           srcprocess, pHMSrcHandle, destprocess, desthandle));
+
+  if(srcprocess != destprocess) {
+      DebugInt3();
+      SetLastError(ERROR_ACCESS_DENIED_W);
+      return FALSE;
+  }
+  hmtx = (HMTX)pHMSrcHandle->hHMHandle;
+  rc = DosOpenMutexSem(NULL, &hmtx);
+  if(rc) {
+      dprintf(("DosOpenMutexSem %x failed with rc %d", pHMSrcHandle->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      SetLastError(error2WinError(rc));
+      return FALSE;
+  }
+  pHMHandleData->dwAccess  = fdwAccess;
+  pHMHandleData->hHMHandle = hmtx;
+  SetLastError(ERROR_SUCCESS_W);
+  return TRUE;
+}
+#endif
 
 /*****************************************************************************
  * Name      : HMReleaseMutex
@@ -136,9 +286,25 @@ DWORD HMDeviceMutexClass::OpenMutex(PHMHANDLEDATA         pHMHandleData,
 
 BOOL HMDeviceMutexClass::ReleaseMutex(PHMHANDLEDATA pHMHandleData)
 {
+#ifdef USE_OS2SEMAPHORES
+  APIRET rc;
+  
+  dprintf(("KERNEL32: HandleManager::Mutex::ReleaseMutex(%08xh)\n",
+           pHMHandleData->hHMHandle));
+
+  rc = DosReleaseMutexSem(pHMHandleData->hHMHandle);
+  if(rc) {
+      dprintf(("DosReleaseMutexSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      SetLastError(error2WinError(rc));
+      return FALSE;
+  }
+  SetLastError(ERROR_SUCCESS_W);
+  return TRUE;
+#else
   dprintf(("KERNEL32: HandleManager::Mutex::ReleaseMutex(%08xh)\n",
            pHMHandleData->hHMHandle));
 
   return (O32_ReleaseMutex(pHMHandleData->hHMHandle));
+#endif
 }
 
--- src/kernel32/hmsemaphore.cpp
+++ src/kernel32/hmsemaphore.cpp
@@ -1,9 +1,18 @@
-/* $Id: hmsemaphore.cpp,v 1.3 2000-02-16 14:24:00 sandervl Exp $ */
+/* $Id: hmsemaphore.cpp,v 1.4 2001-06-19 10:50:25 sandervl Exp $ */
 
 /*
+ * Win32 Semaphore implementation
+ *
+ * TODO: Inheritance
+ * TODO: Does DCE_POSTONE work in Warp 3 or 4 with no FP applied?
+ * TODO: No inheritance when CreateSemaphore is called for existing named event semaphore?
+ *       (see HMCreateSemaphore in handlemanager.cpp)
+ * TODO: Name collisions with files & mutex not allowed. Check if this can happen in OS/2
+ * TODO: Does NOT work for sharing semaphores between processes!!
+ *
  * Project Odin Software License can be found in LICENSE.TXT
- * Win32 Unified Handle Manager for OS/2
- * Copyright 1999 Patrick Haller (haller@zebra.fh-weingarten.de)
+ *
+ * Copyright 2001 Sander van Leeuwen (sandervl@xs4all.nl)
  */
 
 #undef DEBUG_LOCAL
@@ -21,7 +30,15 @@
  * Includes                                                                  *
  *****************************************************************************/
 
+#ifdef USE_OS2SEMAPHORES
+#define INCL_DOSSEMAPHORES
+#include <os2wrap.h>
+#include <win32type.h>
+#include <win32api.h>
+#include <winconst.h>
+#else
 #include <os2win.h>
+#endif
 #include <stdlib.h>
 #include <string.h>
 #include "unicode.h"
@@ -29,9 +46,18 @@
 
 #include "HandleManager.H"
 #include "HMSemaphore.h"
+#include "oslibdos.h"
 
 #define DBG_LOCALLOG	DBG_hmsemaphore
 #include "dbglocal.h"
+
+#ifndef DCE_AUTORESET
+#define DCE_AUTORESET   0x1000  /* DosCreateEventSem option to auto-reset  */
+                                /* event semaphore on post.                */
+#define DCE_POSTONE     0x0800  /* DosCreateEventSem option to post only   */
+                                /* waiter and auto-reset the semaphore when*/
+                                /* there are multiple waiters.             */
+#endif
 
 
 /*****************************************************************************
@@ -65,6 +91,44 @@ DWORD HMDeviceSemaphoreClass::CreateSemaphore(PHMHANDLEDATA         pHMHandleDat
                                               LONG                  lMaximumCount,
                                               LPCTSTR               lpszSemaphoreName)
 {
+#ifdef USE_OS2SEMAPHORES
+  APIRET rc;
+  HEV hev;
+  char szSemName[CCHMAXPATH];
+
+
+  dprintf(("KERNEL32: HandleManager::Semaphore::CreateSemaphore(%08xh,%08xh,%08xh,%08xh,%s)\n",
+           pHMHandleData,
+           lpsa,
+           lInitialCount,
+           lMaximumCount,
+           lpszSemaphoreName));
+
+  if(lMaximumCount <= 0 || lInitialCount < 0 || lInitialCount > lMaximumCount) {
+      dprintf(("ERROR: invalid parameter"));
+      return ERROR_INVALID_PARAMETER_W;
+  }
+  
+  if(lpszSemaphoreName) {
+      strcpy(szSemName, "\\SEM32\\");
+      strcat(szSemName, lpszSemaphoreName);
+      lpszSemaphoreName = szSemName;
+  }
+  //Manual reset means all threads waiting on the event semaphore will be
+  //unblocked and the app must manually reset the event semaphore
+  //Automatic reset -> only one waiting thread unblocked & state reset
+  rc = DosCreateEventSem(lpszSemaphoreName, &hev, DCE_POSTONE, lInitialCount);
+
+  if(rc) {
+      dprintf(("DosCreateEventSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      return error2WinError(rc);
+  }
+  pHMHandleData->dwAccess  = SEMAPHORE_ALL_ACCESS_W;
+  pHMHandleData->dwFlags   = lMaximumCount;
+  pHMHandleData->hHMHandle = hev;
+  return ERROR_SUCCESS_W;
+#else
   HANDLE hOpen32;
 
   dprintf(("KERNEL32: HandleManager::Semaphore::CreateSemaphore(%08xh,%08xh,%08xh,%08xh,%s)\n",
@@ -86,6 +150,7 @@ DWORD HMDeviceSemaphoreClass::CreateSemaphore(PHMHANDLEDATA         pHMHandleDat
   }
   else
     return (O32_GetLastError());
+#endif
 }
 
 
@@ -105,6 +170,32 @@ DWORD HMDeviceSemaphoreClass::OpenSemaphore(PHMHANDLEDATA         pHMHandleData,
                                             BOOL                  fInheritHandle,
                                             LPCTSTR               lpszSemaphoreName)
 {
+#ifdef USE_OS2SEMAPHORES
+  HEV    hev;
+  APIRET rc;
+  char szSemName[CCHMAXPATH];
+
+  dprintf(("KERNEL32: HandleManager::Semaphore::OpenSemaphore(%08xh,%08xh,%s)\n",
+           pHMHandleData,
+           fInheritHandle,
+           lpszSemaphoreName));
+
+  if(lpszSemaphoreName == NULL) {
+      pHMHandleData->hHMHandle = 0;
+      return ERROR_INVALID_PARAMETER_W;
+  }
+
+  strcpy(szSemName, "\\SEM32\\");
+  strcat(szSemName, lpszSemaphoreName);
+  rc = DosOpenEventSem(szSemName, &hev);
+  if(rc) {
+      dprintf(("DosOpenEventSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      return error2WinError(rc);
+  }
+  pHMHandleData->hHMHandle = hev;
+  return ERROR_SUCCESS_W;
+#else
   HANDLE hOpen32;
 
   dprintf(("KERNEL32: HandleManager::Semaphore::OpenSemaphore(%08xh,%08xh,%s)\n",
@@ -123,8 +214,90 @@ DWORD HMDeviceSemaphoreClass::OpenSemaphore(PHMHANDLEDATA         pHMHandleData,
   }
   else
     return (O32_GetLastError());
+#endif
 }
 
+/*****************************************************************************
+ * Name      : HMDeviceEventClass::CloseHandle
+ * Purpose   : close the handle
+ * Parameters: PHMHANDLEDATA pHMHandleData
+ * Variables :
+ * Result    : API returncode
+ * Remark    :
+ * Status    :
+ *
+ * Author    : 
+ *****************************************************************************/
+
+#ifdef USE_OS2SEMAPHORES
+BOOL HMDeviceSemaphoreClass::CloseHandle(PHMHANDLEDATA pHMHandleData)
+{
+  APIRET rc;
+
+  if(pHMHandleData->hHMHandle) {
+      rc = DosCloseEventSem((HEV)pHMHandleData->hHMHandle);
+      if(rc) {
+          dprintf(("DosCloseEventSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+          SetLastError(error2WinError(rc));
+          return FALSE;
+      }
+  }
+  return TRUE;
+}
+#endif
+
+
+/*****************************************************************************
+ * Name      : HMDeviceEventClass::DuplicateHandle
+ * Purpose   : 
+ * Parameters:
+ *             various parameters as required
+ * Variables :
+ * Result    :
+ * Remark    : the standard behaviour is to return an error code for non-
+ *             existant request codes
+ * Status    :
+ *
+ * Author    : 
+ *****************************************************************************/
+#ifdef USE_OS2SEMAPHORES
+BOOL HMDeviceSemaphoreClass::DuplicateHandle(PHMHANDLEDATA pHMHandleData, HANDLE  srcprocess,
+                               PHMHANDLEDATA pHMSrcHandle,
+                               HANDLE  destprocess,
+                               PHANDLE desthandle,
+                               DWORD   fdwAccess,
+                               BOOL    fInherit,
+                               DWORD   fdwOptions,
+                               DWORD   fdwOdinOptions)
+{
+  APIRET rc; 
+  HEV hev;
+  
+  dprintf(("KERNEL32:HandleManager::DuplicateHandle %s(%08x,%08x,%08x,%08x,%08x)",
+           lpHMDeviceName,
+           pHMHandleData,
+           srcprocess, pHMSrcHandle, destprocess, desthandle));
+
+  if(srcprocess != destprocess) {
+      DebugInt3();
+      SetLastError(ERROR_ACCESS_DENIED_W);
+      return FALSE;
+  }
+  hev = (HEV)pHMSrcHandle->hHMHandle;
+  rc = DosOpenEventSem(NULL, &hev);
+  if(rc) {
+      dprintf(("DosOpenEventSem %x failed with rc %d", pHMSrcHandle->hHMHandle, rc));
+      pHMHandleData->hHMHandle = 0;
+      SetLastError(error2WinError(rc));
+      return FALSE;
+  }
+  pHMHandleData->dwAccess  = fdwAccess;
+  pHMHandleData->dwFlags   = pHMSrcHandle->dwFlags; //lMaximumCount;
+  pHMHandleData->hHMHandle = hev;
+  SetLastError(ERROR_SUCCESS_W);
+  return TRUE;
+}
+#endif
 
 /*****************************************************************************
  * Name      : HMReleaseSemaphore
@@ -142,6 +315,31 @@ BOOL HMDeviceSemaphoreClass::ReleaseSemaphore(PHMHANDLEDATA pHMHandleData,
                                               LONG          cReleaseCount,
                                               LPLONG        lpPreviousCount)
 {
+#ifdef USE_OS2SEMAPHORES
+  APIRET rc;
+  ULONG count;  
+  
+  dprintf2(("KERNEL32: HandleManager::Semaphore::ReleaseSemaphore(%08xh,%08xh,%08xh)\n",
+            pHMHandleData->hHMHandle,
+            cReleaseCount,
+            lpPreviousCount));
+
+  if(!(pHMHandleData->dwAccess & SEMAPHORE_MODIFY_STATE_W) )
+  {
+      dprintf(("ERROR: Access denied!!"));
+      SetLastError(ERROR_ACCESS_DENIED_W);
+      return FALSE;
+  }
+
+  rc = DosResetEventSem(pHMHandleData->hHMHandle, &count);
+  if(rc) {
+      dprintf(("DosResetEventSem %x failed with rc %d", pHMHandleData->hHMHandle, rc));
+      SetLastError(error2WinError(rc));
+      return FALSE;
+  }
+  SetLastError(ERROR_SUCCESS_W);
+  return TRUE;
+#else
   dprintf(("KERNEL32: HandleManager::Semaphore::ReleaseSemaphore(%08xh,%08xh,%08xh)\n",
            pHMHandleData->hHMHandle,
            cReleaseCount,
@@ -150,5 +348,6 @@ BOOL HMDeviceSemaphoreClass::ReleaseSemaphore(PHMHANDLEDATA pHMHandleData,
   return (O32_ReleaseSemaphore(pHMHandleData->hHMHandle,
                                cReleaseCount,
                                lpPreviousCount));
+#endif
 }
 
