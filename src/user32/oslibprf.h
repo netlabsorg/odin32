2006f8acc9e5daebadd5e969eb98f9f9198fa303
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-06-23 07:20:55 +0000

    added OS/2 INI file access library

---
+++ src/user32/oslibprf.h
@@ -0,0 +1,25 @@
+/* $Id: oslibprf.h,v 1.1 2001-06-23 07:20:55 achimha Exp $ */
+/*
+ * Profile API wrappers for OS/2
+ *
+ *
+ * Copyright 2001 Achim Hasenmueller <achimha@innotek.de>
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+
+#ifndef __OSLIBPRF_H__
+#define __OSLIBPRF_H__
+
+typedef ULONG                    OSLIB_HINI;
+#define OSLIB_HINI_PROFILE       NULL
+#define OSLIB_HINI_USERPROFILE   -1L
+#define OSLIB_HINI_SYSTEMPROFILE -2L
+#define OSLIB_HINI_USER          OSLIB_HINI_USERPROFILE
+#define OSLIB_HINI_SYSTEM        OSLIB_HINI_SYSTEMPROFILE
+
+LONG OSLibPrfQueryProfileInt(OSLIB_HINI hini, char *pszApp, char *pszKey, LONG sDefault);
+
+#endif
