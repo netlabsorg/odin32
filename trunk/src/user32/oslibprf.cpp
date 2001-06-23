6a1853e3e7d17885e530256aa8be46ec66ca1dd4
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-06-23 07:20:55 +0000

    added OS/2 INI file access library

---
+++ trunk/src/user32/oslibprf.cpp
@@ -0,0 +1,24 @@
+/* $Id: oslibprf.cpp,v 1.1 2001-06-23 07:20:55 achimha Exp $ */
+/*
+ * Profile API wrappers for OS/2
+ *
+ *
+ * Copyright 2001 Achim Hasenmueller <achimha@innotek.de>
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#define  INCL_WIN
+#define  INCL_PM
+#define  INCL_WINSWITCHLIST
+#include <os2wrap.h>
+
+#include <win32type.h>
+#include <winconst.h>
+#include "oslibprf.h"
+
+LONG OSLibPrfQueryProfileInt(OSLIB_HINI hini, char *pszApp, char *pszKey, LONG sDefault)
+{
+    return PrfQueryProfileInt(hini, pszApp, pszKey, sDefault);
+}
