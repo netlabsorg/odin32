4c214fac110dc7477c0f38db938f5700d702358b
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-07-03 13:23:32 +0000

    GetKeyboardState fixes

--- trunk/src/user32/winkeyboard.cpp
+++ trunk/src/user32/winkeyboard.cpp
@@ -1,4 +1,4 @@
-/* $Id: winkeyboard.cpp,v 1.8 2001-02-19 21:43:18 sandervl Exp $ */
+/* $Id: winkeyboard.cpp,v 1.9 2001-07-03 13:23:32 sandervl Exp $ */
 /*
  * Win32 <-> PM key translation
  *
@@ -13,9 +13,24 @@
 #include <winkeyboard.h>
 #include "oslibwin.h"
 #include <heapstring.h>
+#include <pmscan.h>
 
 #define DBG_LOCALLOG	DBG_winkeyboard
 #include "dbglocal.h"
+
+BOOL OPEN32API _O32_GetKeyboardState( PBYTE lpKeyState );
+
+inline BOOL O32_GetKeyboardState(PBYTE lpKeyState)
+{
+ BOOL yyrc;
+ USHORT sel = RestoreOS2FS();
+
+    yyrc = _O32_GetKeyboardState(lpKeyState);
+    SetFS(sel);
+
+    return yyrc;
+} 
+
 
 BYTE abPMScanToWinVKey[256] =
 /****************************************************************************/
@@ -50,7 +65,7 @@ BYTE abPMScanToWinVKey[256] =
 /* 0x1A PMSCAN_BRACKETLEFT  */ , VK_BRACKETLEFT
 /* 0x1B PMSCAN_BRACKETRIGHT */ , VK_BRACKETRIGHT
 /* 0x1C PMSCAN_ENTER        */ , VK_RETURN
-/* 0x1D PMSCAN_CTRLLEFT     */ , VK_CONTROL
+/* 0x1D PMSCAN_CTRLLEFT     */ , VK_LCONTROL
 /* 0x1E PMSCAN_A            */ , VK_A
 /* 0x1F PMSCAN_S            */ , VK_S
 /* 0x20 PMSCAN_D            */ , VK_D
@@ -63,7 +78,7 @@ BYTE abPMScanToWinVKey[256] =
 /* 0x27 PMSCAN_SEMICOLON    */ , VK_SEMICOLON
 /* 0x28 PMSCAN_QUOTESINGLE  */ , VK_QUOTESINGLE
 /* 0x29 PMSCAN_GRAVE        */ , VK_GRAVE
-/* 0x2A PMSCAN_SHIFTLEFT    */ , VK_SHIFT
+/* 0x2A PMSCAN_SHIFTLEFT    */ , VK_LSHIFT
 /* 0x2B PMSCAN_BACKSLASH    */ , VK_BACKSLASH
 /* 0x2C PMSCAN_Z            */ , VK_Z
 /* 0x2D PMSCAN_X            */ , VK_X
@@ -75,9 +90,9 @@ BYTE abPMScanToWinVKey[256] =
 /* 0x33 PMSCAN_COMMA        */ , VK_COMMA
 /* 0x34 PMSCAN_PERIOD       */ , VK_PERIOD
 /* 0x35 PMSCAN_SLASH        */ , VK_SLASH
-/* 0x36 PMSCAN_SHIFTRIGHT   */ , VK_SHIFT
+/* 0x36 PMSCAN_SHIFTRIGHT   */ , VK_RSHIFT
 /* 0x37 PMSCAN_PADASTERISK  */ , VK_MULTIPLY
-/* 0x38 PMSCAN_ALTLEFT      */ , VK_MENU
+/* 0x38 PMSCAN_ALTLEFT      */ , VK_LMENU
 /* 0x39 PMSCAN_SPACE        */ , VK_SPACE
 /* 0x3A PMSCAN_CAPSLOCK     */ , VK_CAPITAL
 /* 0x3B PMSCAN_F1           */ , VK_F1
@@ -115,7 +130,7 @@ BYTE abPMScanToWinVKey[256] =
 /* 0x5B PMSCAN_CTRLRIGHT    */ , VK_CONTROL
 /* 0x5C PMSCAN_PADSLASH     */ , VK_DIVIDE
 /* 0x5D PMSCAN_PRINT        */ , VK_PRINT
-/* 0x5E PMSCAN_ALTRIGHT     */ , VK_MENU
+/* 0x5E PMSCAN_ALTRIGHT     */ , VK_RMENU
 /* 0x5F PMSCAN_PAUSE        */ , VK_PAUSE
 /* 0x60 PMSCAN_HOME         */ , VK_HOME
 /* 0x61 PMSCAN_UP           */ , VK_UP
@@ -279,25 +294,286 @@ BYTE abPMScanToWinVKey[256] =
 /* 0xFF                     */ , 0x00
                                };
 
-//******************************************************************************
-//******************************************************************************
-BYTE WIN32API KeyTranslatePMToWin(BYTE key)
-{
-   return abPMScanToWinVKey[key];
-}
+BYTE abWinVKeyToPMScan[256] =
+/****************************************/
+/* Vkey                   *    Scancode */
+/****************************************/
+/* 0x00                   */ { 0x00
+/* 0x01 VK_LBUTTON        */ , 0x00
+/* 0x02 VK_RBUTTON        */ , 0x00
+/* 0x03 VK_CANCEL         */ , PMSCAN_BREAK
+/* 0x04 VK_MBUTTON        */ , 0x00
+/* 0x05                   */ , 0x00
+/* 0x06                   */ , 0x00
+/* 0x07                   */ , 0x00
+/* 0x08 VK_BACK           */ , PMSCAN_BACKSPACE
+/* 0x09 VK_TAB            */ , PMSCAN_TAB
+/* 0x0A                   */ , 0x00
+/* 0x0B                   */ , 0x00
+/* 0x0C VK_CLEAR          */ , PMSCAN_PAD5
+/* 0x0D VK_RETURN         */ , PMSCAN_ENTER
+/* 0x0E                   */ , 0x00
+/* 0x0F                   */ , 0x00
+/* 0x10 VK_SHIFT          */ , PMSCAN_SHIFTLEFT
+/* 0x11 VK_CONTROL        */ , PMSCAN_CTRLLEFT
+/* 0x12 VK_MENU           */ , PMSCAN_ALTLEFT
+/* 0x13 VK_PAUSE          */ , PMSCAN_PAUSE
+/* 0x14 VK_CAPITAL        */ , PMSCAN_CAPSLOCK
+/* 0x15                   */ , 0x00
+/* 0x16                   */ , 0x00
+/* 0x17                   */ , 0x00
+/* 0x18                   */ , 0x00
+/* 0x19                   */ , 0x00
+/* 0x1A                   */ , 0x00
+/* 0x1B VK_ESCAPE         */ , PMSCAN_ESC
+/* 0x1C                   */ , 0x00
+/* 0x1D                   */ , 0x00
+/* 0x1E                   */ , 0x00
+/* 0x1F                   */ , 0x00
+/* 0x20 VK_SPACE          */ , PMSCAN_SPACE
+/* 0x21 VK_PRIOR          */ , PMSCAN_PAGEUP
+/* 0x22 VK_NEXT           */ , PMSCAN_PAGEDOWN
+/* 0x23 VK_END            */ , PMSCAN_END
+/* 0x24 VK_HOME           */ , PMSCAN_HOME
+/* 0x25 VK_LEFT           */ , PMSCAN_LEFT
+/* 0x26 VK_UP             */ , PMSCAN_UP
+/* 0x27 VK_RIGHT          */ , PMSCAN_RIGHT
+/* 0x28 VK_DOWN           */ , PMSCAN_DOWN
+/* 0x29 VK_SELECT         */ , 0x00
+/* 0x2A VK_PRINT          */ , PMSCAN_PRINT
+/* 0x2B VK_EXECUTE        */ , 0x00
+/* 0x2C VK_SNAPSHOT       */ , 0x00
+/* 0x2D VK_INSERT         */ , PMSCAN_INSERT
+/* 0x2E VK_DELETE         */ , PMSCAN_DELETE
+/* 0x2F VK_HELP           */ , PMSCAN_HELP
+/* 0x30 VK_0              */ , PMSCAN_ZERO
+/* 0x31 VK_1              */ , PMSCAN_ONE
+/* 0x32 VK_2              */ , PMSCAN_TWO
+/* 0x33 VK_3              */ , PMSCAN_THREE
+/* 0x34 VK_4              */ , PMSCAN_FOUR
+/* 0x35 VK_5              */ , PMSCAN_FIVE
+/* 0x36 VK_6              */ , PMSCAN_SIX
+/* 0x37 VK_7              */ , PMSCAN_SEVEN
+/* 0x38 VK_8              */ , PMSCAN_EIGHT
+/* 0x39 VK_9              */ , PMSCAN_NINE
+/* 0x3A                   */ , 0x00
+/* 0x3B                   */ , 0x00
+/* 0x3C                   */ , 0x00
+/* 0x3D                   */ , 0x00
+/* 0x3E                   */ , 0x00
+/* 0x3F                   */ , 0x00
+/* 0x40                   */ , 0x00
+/* 0x41 VK_A              */ , PMSCAN_A
+/* 0x42 VK_B              */ , PMSCAN_B
+/* 0x43 VK_C              */ , PMSCAN_C
+/* 0x44 VK_D              */ , PMSCAN_D
+/* 0x45 VK_E              */ , PMSCAN_E
+/* 0x46 VK_F              */ , PMSCAN_F
+/* 0x47 VK_G              */ , PMSCAN_G
+/* 0x48 VK_H              */ , PMSCAN_H
+/* 0x49 VK_I              */ , PMSCAN_I
+/* 0x4A VK_J              */ , PMSCAN_J
+/* 0x4B VK_K              */ , PMSCAN_K
+/* 0x4C VK_L              */ , PMSCAN_L
+/* 0x4D VK_M              */ , PMSCAN_M
+/* 0x4E VK_N              */ , PMSCAN_N
+/* 0x4F VK_O              */ , PMSCAN_O
+/* 0x50 VK_P              */ , PMSCAN_P
+/* 0x51 VK_Q              */ , PMSCAN_Q
+/* 0x52 VK_R              */ , PMSCAN_R
+/* 0x53 VK_S              */ , PMSCAN_S
+/* 0x54 VK_T              */ , PMSCAN_T
+/* 0x55 VK_U              */ , PMSCAN_U
+/* 0x56 VK_V              */ , PMSCAN_V
+/* 0x57 VK                */ , PMSCAN_W
+/* 0x58 VK_X              */ , PMSCAN_X
+/* 0x59 VK_Y              */ , PMSCAN_Y
+/* 0x5A VK_Z              */ , PMSCAN_Z
+/* 0x5B VK_LWIN           */ , PMSCAN_WINLEFT
+/* 0x5C VK_RWIN           */ , PMSCAN_WINRIGHT
+/* 0x5D VK_APPS           */ , PMSCAN_APPLICATION
+/* 0x5E                   */ , 0x00
+/* 0x5F                   */ , 0x00
+/* 0x60 VK_NUMPAD0        */ , PMSCAN_PAD0
+/* 0x61 VK_NUMPAD1        */ , PMSCAN_PAD1
+/* 0x62 VK_NUMPAD2        */ , PMSCAN_PAD2
+/* 0x63 VK_NUMPAD3        */ , PMSCAN_PAD3
+/* 0x64 VK_NUMPAD4        */ , PMSCAN_PAD4
+/* 0x65 VK_NUMPAD5        */ , PMSCAN_PAD5
+/* 0x66 VK_NUMPAD6        */ , PMSCAN_PAD6
+/* 0x67 VK_NUMPAD7        */ , PMSCAN_PAD7
+/* 0x68 VK_NUMPAD8        */ , PMSCAN_PAD8
+/* 0x69 VK_NUMPAD9        */ , PMSCAN_PAD9
+/* 0x6A VK_MULTIPLY       */ , PMSCAN_PADASTERISK
+/* 0x6B VK_ADD            */ , PMSCAN_PADPLUS
+/* 0x6C VK_SEPARATOR      */ , 0x00
+/* 0x6D VK_SUBTRACT       */ , PMSCAN_PADMINUS
+/* 0x6E VK_DECIMAL        */ , PMSCAN_PADPERIOD
+/* 0x6F VK_DIVIDE         */ , PMSCAN_PADSLASH
+/* 0x70 VK_F1             */ , PMSCAN_F1
+/* 0x71 VK_F2             */ , PMSCAN_F2
+/* 0x72 VK_F3             */ , PMSCAN_F3
+/* 0x73 VK_F4             */ , PMSCAN_F4
+/* 0x74 VK_F5             */ , PMSCAN_F5
+/* 0x75 VK_F6             */ , PMSCAN_F6
+/* 0x76 VK_F7             */ , PMSCAN_F7
+/* 0x77 VK_F8             */ , PMSCAN_F8
+/* 0x78 VK_F9             */ , PMSCAN_F9
+/* 0x79 VK_F10            */ , PMSCAN_F10
+/* 0x7A VK_F11            */ , PMSCAN_F11
+/* 0x7B VK_F12            */ , PMSCAN_F12
+/* 0x7C VK_F13            */ , PMSCAN_F13
+/* 0x7D VK_F14            */ , PMSCAN_F14
+/* 0x7E VK_F15            */ , PMSCAN_F15
+/* 0x7F VK_F16            */ , PMSCAN_F16
+/* 0x80 VK_F17            */ , PMSCAN_F17
+/* 0x81 VK_F18            */ , PMSCAN_F18
+/* 0x82 VK_F19            */ , PMSCAN_F19
+/* 0x83 VK_F20            */ , PMSCAN_F20
+/* 0x84 VK_F21            */ , PMSCAN_F21
+/* 0x85 VK_F22            */ , PMSCAN_F22
+/* 0x86 VK_F23            */ , PMSCAN_F23
+/* 0x87 VK_F24            */ , PMSCAN_F24
+/* 0x88                   */ , 0x00
+/* 0x89                   */ , 0x00
+/* 0x8A                   */ , 0x00
+/* 0x8B                   */ , 0x00
+/* 0x8C                   */ , 0x00
+/* 0x8D                   */ , 0x00
+/* 0x8E                   */ , 0x00
+/* 0x8F                   */ , 0x00
+/* 0x90 VK_NUMLOCK        */ , PMSCAN_NUMLOCK
+/* 0x91 VK_SCROLL         */ , PMSCAN_SCROLLLOCK
+/* 0x92                   */ , 0x00
+/* 0x93                   */ , 0x00
+/* 0x94                   */ , 0x00
+/* 0x95                   */ , 0x00
+/* 0x96                   */ , 0x00
+/* 0x97                   */ , 0x00
+/* 0x98                   */ , 0x00
+/* 0x99                   */ , 0x00
+/* 0x9A                   */ , 0x00
+/* 0x9B                   */ , 0x00
+/* 0x9C                   */ , 0x00
+/* 0x9D                   */ , 0x00
+/* 0x9E                   */ , 0x00
+/* 0x9F                   */ , 0x00
+/* 0xA0 VK_LSHIFT         */ , PMSCAN_SHIFTLEFT
+/* 0xA1 VK_RSHIFT         */ , PMSCAN_SHIFTRIGHT
+/* 0xA2 VK_LCONTROL       */ , PMSCAN_CTRLLEFT
+/* 0xA3 VK_RCONTROL       */ , PMSCAN_CTRLRIGHT
+/* 0xA4 VK_LMENU          */ , PMSCAN_ALTLEFT
+/* 0xA5 VK_RMENU          */ , PMSCAN_ALTRIGHT
+/* 0xA6                   */ , 0x00
+/* 0xA7                   */ , 0x00
+/* 0xA8                   */ , 0x00
+/* 0xA9                   */ , 0x00
+/* 0xAA                   */ , 0x00
+/* 0xAB                   */ , 0x00
+/* 0xAC                   */ , 0x00
+/* 0xAD                   */ , 0x00
+/* 0xAE                   */ , 0x00
+/* 0xAF                   */ , 0x00
+/* 0xB0                   */ , 0x00
+/* 0xB1                   */ , 0x00
+/* 0xB2                   */ , 0x00
+/* 0xB3                   */ , 0x00
+/* 0xB4                   */ , 0x00
+/* 0xB5                   */ , 0x00
+/* 0xB6                   */ , 0x00
+/* 0xB7                   */ , 0x00
+/* 0xB8                   */ , 0x00
+/* 0xB9                   */ , 0x00
+/* 0xBA VK_SEMICOLON      */ , PMSCAN_SEMICOLON
+/* 0xBB VK_EQUAL          */ , PMSCAN_EQUAL
+/* 0xBC VK_COMMA          */ , PMSCAN_COMMA
+/* 0xBD VK_HYPHEN         */ , PMSCAN_HYPHEN
+/* 0xBE VK_PERIOD         */ , PMSCAN_PERIOD
+/* 0xBF VK_SLASH          */ , PMSCAN_SLASH
+/* 0xC0 VK_GRAVE          */ , PMSCAN_GRAVE
+/* 0xC1 VK_FFC1           */ , PMSCAN_JEXTRA
+/* 0xC2 VK_YEN            */ , PMSCAN_YEN
+/* 0xC3                   */ , 0x00
+/* 0xC4                   */ , 0x00
+/* 0xC5                   */ , 0x00
+/* 0xC6                   */ , 0x00
+/* 0xC7                   */ , 0x00
+/* 0xC8                   */ , 0x00
+/* 0xC9                   */ , 0x00
+/* 0xCA                   */ , 0x00
+/* 0xCB                   */ , 0x00
+/* 0xCC                   */ , 0x00
+/* 0xCD                   */ , 0x00
+/* 0xCE                   */ , 0x00
+/* 0xCF                   */ , 0x00
+/* 0xD0                   */ , 0x00
+/* 0xD1                   */ , 0x00
+/* 0xD2                   */ , 0x00
+/* 0xD3                   */ , 0x00
+/* 0xD4                   */ , 0x00
+/* 0xD5                   */ , 0x00
+/* 0xD6                   */ , 0x00
+/* 0xD7                   */ , 0x00
+/* 0xD8                   */ , 0x00
+/* 0xD9                   */ , 0x00
+/* 0xDA                   */ , 0x00
+/* 0xDB VK_BRACKETLEFT    */ , PMSCAN_BRACKETLEFT
+/* 0xDC VK_BACKSLASH      */ , PMSCAN_BACKSLASH
+/* 0xDD VK_BRACKETRIGHT   */ , PMSCAN_BRACKETRIGHT
+/* 0xDE VK_QUOTESINGLE    */ , PMSCAN_QUOTESINGLE
+/* 0xDF                   */ , 0x00
+/* 0xE0                   */ , 0x00
+/* 0xE1                   */ , 0x00
+/* 0xE2 VK_EXTRA          */ , PMSCAN_EXTRA
+/* 0xE3                   */ , 0x00
+/* 0xE4                   */ , 0x00
+/* 0xE5                   */ , 0x00
+/* 0xE6                   */ , 0x00
+/* 0xE7                   */ , 0x00
+/* 0xE8                   */ , 0x00
+/* 0xE9                   */ , 0x00
+/* 0xEA                   */ , 0x00
+/* 0xEB                   */ , 0x00
+/* 0xEC                   */ , 0x00
+/* 0xED                   */ , 0x00
+/* 0xEE                   */ , 0x00
+/* 0xEF                   */ , 0x00
+/* 0xF0                   */ , 0x00
+/* 0xF1                   */ , 0x00
+/* 0xF2                   */ , 0x00
+/* 0xF3                   */ , 0x00
+/* 0xF4                   */ , 0x00
+/* 0xF5                   */ , 0x00
+/* 0xF6 VK_ATTN           */ , 0x00
+/* 0xF7 VK_CRSEL          */ , 0x00
+/* 0xF8 VK_EXSEL          */ , 0x00
+/* 0xF9 VK_EREOF          */ , PMSCAN_ERASEEOF
+/* 0xFA VK_PLAY           */ , 0x00
+/* 0xFB VK_ZOOM           */ , 0x00
+/* 0xFC VK_NONAME         */ , 0x00
+/* 0xFD VK_PA1            */ , PMSCAN_PA1
+/* 0xFE VK_OEM_CLEAR      */ , 0x00
+/* 0xFF                   */ , 0x00
+                             };
+
 //******************************************************************************
 //******************************************************************************
 void WIN32API KeyTranslatePMToWinBuf(BYTE *pmkey, BYTE *winkey, int nrkeys)
 {
-   for(int i=0;i<nrkeys;i++) {
-    	winkey[i] = abPMScanToWinVKey[pmkey[i]];
+   for(int i=1;i<nrkeys;i++) {
+       if(abWinVKeyToPMScan[i]) {
+            winkey[i] = pmkey[OSLibWinTranslateChar(abWinVKeyToPMScan[i])];
+       }
    }
+   winkey[VK_SHIFT]   = winkey[VK_LSHIFT] | winkey[VK_RSHIFT];
+   winkey[VK_CONTROL] = winkey[VK_LCONTROL] | winkey[VK_RCONTROL];
+   winkey[VK_MENU]    = winkey[VK_LMENU] | winkey[VK_RMENU];
 }
 //******************************************************************************
 //******************************************************************************
 int WIN32API GetKeyboardType( int nTypeFlag)
 {
-    dprintf(("USER32:  GetKeyboardType\n"));
+    dprintf(("USER32: GetKeyboardType %x", nTypeFlag));
     return O32_GetKeyboardType(nTypeFlag);
 }
 //******************************************************************************
@@ -308,11 +584,25 @@ BOOL WIN32API GetKeyboardState(PBYTE lpKeyState)
  BOOL   rc;
 
   dprintf(("USER32: GetKeyboardState %x", lpKeyState));
-  rc = OSLibWinGetKeyboardStateTable((PBYTE)&PMKeyState );
-
+  memset(PMKeyState, 0, sizeof(PMKeyState));
+  memset(lpKeyState, 0, 256);
+  rc = OSLibWinGetKeyboardStateTable((PBYTE)&PMKeyState[0] );
+////  rc = O32_GetKeyboardState(lpKeyState);
   if(rc == TRUE) 
   {
-    	KeyTranslatePMToWinBuf((BYTE *)&PMKeyState, lpKeyState, 256);
+    	KeyTranslatePMToWinBuf((BYTE *)&PMKeyState[0], lpKeyState, 256);
+#ifdef DEBUG
+        for(int i=0;i<256;i++) {
+            if(PMKeyState[i] & 0x80) {
+                dprintf2(("PM  key 0x%0x = %x", i, PMKeyState[i]));
+            }
+        }
+        for(i=0;i<256;i++) {
+            if(lpKeyState[i] & 0x80) {
+                dprintf2(("Win key 0x%0x = %x", i, lpKeyState[i]));
+            }
+        }
+#endif
     	return TRUE;
   }
   return FALSE;
@@ -440,12 +730,8 @@ int WIN32API ToAscii(UINT   uVirtKey,
                      LPWORD lpwTransKey,
                      UINT   fuState)
 {
-  dprintf(("USER32:ToAscii (%u,%u,%08xh,%08xh,%u) not implemented.\n",
-         uVirtKey,
-         uScanCode,
-         lpbKeyState,
-         lpwTransKey,
-         fuState));
+  dprintf(("USER32:ToAscii (%u,%u,%08xh,%08xh,%u) partially implemented",
+           uVirtKey, uScanCode, lpbKeyState, lpwTransKey,  fuState));
 
   INT ret;
 
@@ -460,6 +746,7 @@ int WIN32API ToAscii(UINT   uVirtKey,
         dprintf2(("Key UP, doing nothing"));
         return 0;
   }
+#if 0
   /* We have a special case to handle : Shift + arrow, shift + home, ...
      X returns a char for it, but Windows doesn't. Let's eat it. */
   if(!(lpbKeyState[VK_NUMLOCK] & 0x01)  /* NumLock is off */
@@ -470,6 +757,7 @@ int WIN32API ToAscii(UINT   uVirtKey,
         ret = 0;
   }
   else        
+#endif
   /* We have another special case for delete key (XK_Delete) on an
      extended keyboard. X returns a char for it, but Windows doesn't */
   if (uVirtKey == VK_DELETE)
@@ -478,7 +766,10 @@ int WIN32API ToAscii(UINT   uVirtKey,
        ret = 0;
   }
   else {
-       *(char*)lpwTransKey = uVirtKey;
+       if(uVirtKey >= VK_A && uVirtKey <= VK_Z && !(lpbKeyState[VK_SHIFT] & 0x80)) {
+             *(char*)lpwTransKey = uVirtKey + 32; //translate to lower case
+       }
+       else  *(char*)lpwTransKey = uVirtKey;
        ret = 1;
   }
   return ret;
