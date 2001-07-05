5d4fe0f7047bf1fe9a039edd8f5391bee0339dcd
achimha <achimha@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-07-05 19:26:18 +0000

    don't drink and code

--- trunk/src/ws2_32/socket.cpp
+++ trunk/src/ws2_32/socket.cpp
@@ -107,25 +107,25 @@ int WINAPI WSAEventSelect(SOCKET s, WSAEVENT hEvent, LONG lEvent)
     // log all event event bits set
     char tmpbuf[300];
     strcpy(tmpbuf, "");
-    if (lEvent | FD_READ)
+    if (lEvent & FD_READ)
         strcat(tmpbuf, " FD_READ");
-    if (lEvent | FD_WRITE)
+    if (lEvent & FD_WRITE)
         strcat(tmpbuf, " FD_WRITE");
-    if (lEvent | FD_OOB)
+    if (lEvent & FD_OOB)
         strcat(tmpbuf, " FD_OOB");    
-    if (lEvent | FD_ACCEPT)
+    if (lEvent & FD_ACCEPT)
         strcat(tmpbuf, " FD_ACCEPT");
-    if (lEvent | FD_CONNECT)
+    if (lEvent & FD_CONNECT)
         strcat(tmpbuf, " FD_CONNECT");
-    if (lEvent | FD_CLOSE)
+    if (lEvent & FD_CLOSE)
         strcat(tmpbuf, " FD_CLOSE");
-    if (lEvent | FD_QOS)
+    if (lEvent & FD_QOS)
         strcat(tmpbuf, " FD_QOS");
-    if (lEvent | FD_GROUP_QOS)
+    if (lEvent & FD_GROUP_QOS)
         strcat(tmpbuf, " FD_GROUP_QOS");
-    if (lEvent | FD_ROUTING_INTERFACE_CHANGE)
+    if (lEvent & FD_ROUTING_INTERFACE_CHANGE)
         strcat(tmpbuf, " FD_ROUTING_INTERFACE_CHANGE");
-    if (lEvent | FD_ADDRESS_LIST_CHANGE)
+    if (lEvent & FD_ADDRESS_LIST_CHANGE)
         strcat(tmpbuf, " FD_ADDRESS_LIST_CHANGE");
     dprintf(("event bits:%s", tmpbuf));
 #endif
