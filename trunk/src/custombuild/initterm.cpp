a26047beb3f37d8e5cafc1e6d04a7ab70bed857e
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-07-20 15:41:43 +0000

    created

---
+++ trunk/src/custombuild/initterm.cpp
@@ -0,0 +1,170 @@
+/*
+ * DLL entry point
+ *
+ * Copyright 1998 Sander van Leeuwen
+ * Copyright 1998 Peter Fitzsimmons
+ *
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+
+/*-------------------------------------------------------------*/
+/* INITERM.C -- Source for a custom dynamic link library       */
+/*              initialization and termination (_DLL_InitTerm) */
+/*              function.                                      */
+/*                                                             */
+/* When called to perform initialization, this sample function */
+/* gets storage for an array of integers, and initializes its  */
+/* elements with random integers.  At termination time, it     */
+/* frees the array.  Substitute your own special processing.   */
+/*-------------------------------------------------------------*/
+
+
+/* Include files */
+#define  INCL_DOSMODULEMGR
+#define  INCL_DOSMISC
+#define  INCL_DOSPROCESS
+#include <os2wrap.h>    //Odin32 OS/2 api wrappers
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <odin.h>
+#include <win32type.h>
+#include <winconst.h>
+#include <odinlx.h>
+#include <misc.h>       /*PLF Wed  98-03-18 23:18:15*/
+#include <initdll.h>
+#include <exitlist.h>
+
+BOOL  fVersionWarp3 = FALSE;
+
+#ifdef __IBMCPP__
+extern "C" {
+
+/*-------------------------------------------------------------------*/
+/* A clean up routine registered with DosExitList must be used if    */
+/* runtime calls are required and the runtime is dynamically linked. */
+/* This will guarantee that this clean up routine is run before the  */
+/* library DLL is terminated.                                        */
+/*-------------------------------------------------------------------*/
+static void APIENTRY cleanup(ULONG reason);
+}
+/****************************************************************************/
+/* _DLL_InitTerm is the function that gets called by the operating system   */
+/* loader when it loads and frees this DLL for each process that accesses   */
+/* this DLL.  However, it only gets called the first time the DLL is loaded */
+/* and the last time it is freed for a particular process.  The system      */
+/* linkage convention MUST be used because the operating system loader is   */
+/* calling this function.                                                   */
+/****************************************************************************/
+ULONG DLLENTRYPOINT_CCONV DLLENTRYPOINT_NAME(ULONG hModule, ULONG ulFlag)
+{
+   size_t i;
+   APIRET rc;
+   ULONG  version[2];
+
+   /*-------------------------------------------------------------------------*/
+   /* If ulFlag is zero then the DLL is being loaded so initialization should */
+   /* be performed.  If ulFlag is 1 then the DLL is being freed so            */
+   /* termination should be performed.                                        */
+   /*-------------------------------------------------------------------------*/
+
+   switch (ulFlag) {
+      case 0 :
+         /*******************************************************************/
+         /* The C run-time environment initialization function must be      */
+         /* called before any calls to C run-time functions that are not    */
+         /* inlined.                                                        */
+         /*******************************************************************/
+
+         if (_CRT_init() == -1)
+            return 0UL;
+         ctordtorInit();
+
+         rc = DosQuerySysInfo(QSV_VERSION_MAJOR, QSV_VERSION_MINOR, version, sizeof(version));
+         if(rc == 0){
+             if(version[0] >= 20 && version[1] <= 30) {
+                 fVersionWarp3 = TRUE;
+             }
+         }
+
+         /*******************************************************************/
+         /* A DosExitList routine must be used to clean up if runtime calls */
+         /* are required and the runtime is dynamically linked.             */
+         /*******************************************************************/
+         rc = DosExitList(EXITLIST_ODINCRT|EXLST_ADD, cleanup);
+         if(rc)
+                return 0UL;
+
+         CheckVersionFromHMOD(PE2LX_VERSION, hModule); /*PLF Wed  98-03-18 05:28:48*/
+
+         rc = inittermKernel32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermUser32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermWinmm(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermRpcrt4(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermOle32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermComctl32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermShell32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         rc = inittermComdlg32(hModule, ulFlag);
+         if(rc == 0) 
+                return 0UL;
+
+         break;
+      case 1 :
+         inittermComdlg32(hModule, ulFlag);
+         inittermShell32(hModule, ulFlag);
+         inittermComctl32(hModule, ulFlag);
+         inittermOle32(hModule, ulFlag);
+         inittermRpcrt4(hModule, ulFlag);
+         inittermWinmm(hModule, ulFlag);
+         inittermUser32(hModule, ulFlag);
+         inittermKernel32(hModule, ulFlag);
+         break;
+
+      default  :
+         return 0UL;
+   }
+
+   /***********************************************************/
+   /* A non-zero value must be returned to indicate success.  */
+   /***********************************************************/
+   return 1UL;
+}
+//******************************************************************************
+//******************************************************************************
+static void APIENTRY cleanup(ULONG ulReason)
+{
+   cleanupUser32(ulReason);
+   cleanupKernel32(ulReason);
+   ctordtorTerm();
+   _CRT_term();
+   DosExitList(EXLST_EXIT, cleanup);
+   return ;
+}
+//******************************************************************************
+//******************************************************************************
+#else
+#error message("compiler is not supported");
+#endif
