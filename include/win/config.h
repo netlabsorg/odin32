ead21f5d2a9e50b00295e2fc878752e5642b7855
nobody <nobody@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-08-08 21:54:46 +0000

    This commit was manufactured by cvs2svn to create branch 'mini'.

--- include/win/config.h
+++
@@ -1,6 +0,0 @@
-/* $Id: config.h,v 1.1 1999-05-24 20:19:10 ktk Exp $ */
-
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#endif
--- src/gdi32/region.h
+++
@@ -1,9 +0,0 @@
-//$Id: region.h,v 1.2 2000-09-08 18:06:33 sandervl Exp $
-#ifndef __REGION_H__
-#define __REGION_H__
-
-BOOL InitRegionSpace();
-
-BOOL WIN32API OSLibDeleteRegion(HANDLE hRegion);
-
-#endif //__REGION_H__
