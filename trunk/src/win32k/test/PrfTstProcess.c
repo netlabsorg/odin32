ff0ba164016f16764c319b48c4609ec5c7c24588
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-08-08 21:54:45 +0000

    Made useable NT version. Result from Citrix Server.

--- trunk/src/win32k/test/PrfTstProcess.c
+++ trunk/src/win32k/test/PrfTstProcess.c
@@ -1,4 +1,4 @@
-/* $Id: PrfTstProcess.c,v 1.2 2001-07-30 01:56:28 bird Exp $
+/* $Id: PrfTstProcess.c,v 1.3 2001-08-08 21:54:45 bird Exp $
  *
  * Test program which checks how long it takes to execute another
  * instance of itself and run
@@ -43,6 +43,18 @@
  *   6501.000000 - 0.005449 sec (pid=0)
  *   6496.000000 - 0.005444 sec (pid=0)
  *
+ *  Citrix Server (NT4) (SMP) Pentium III 800Mhz - Watcom -Otx:
+ *   4380885.000000 - 0.005493 sec (pid=0) (Hz: 797510000)
+ *   4411477.000000 - 0.005532 sec (pid=0)
+ *   4339893.000000 - 0.005442 sec (pid=0)
+ *   4349910.000000 - 0.005454 sec (pid=0)
+ *
+ *  Citrix Server (NT4) (SMP) Pentium III 800Mhz - Watcom (no optimization):
+ *   4351488.000000 - 0.005456 sec (pid=0) (Hz: 797510000)
+ *   4411035.000000 - 0.005531 sec (pid=0)
+ *   4362396.000000 - 0.005470 sec (pid=0)
+ *   4374720.000000 - 0.005485 sec (pid=0)
+ *
  *  OS/2 WS4eB 4.5 FP 2 (SMP) Pentium III 600Mhz - VAC365:
  *   6743.000000 - 0.005651 sec (pid=0)
  *   6694.000000 - 0.005610 sec (pid=0)
@@ -61,10 +73,23 @@
  *   31699.000000 - 0.026567 sec (pid=1c85)
  *   33570.000000 - 0.028135 sec (pid=1c88)
  *
+ * @subsection  Compilation OS/2
+ * Just as normal odin apps:<br>
+ * nmake -f prftstprocess.mak
+ *
+ * @subsection  Complation NT
+ * This works from OS/2 and NT:
+ * wcl386 -d__WINNT__=1 -bt=nt /lnt -I%WATCOM\h\nt PrfTstProcess.c   kernel32.lib
+ *
+ * Optimized:
+ * wcl386 -Otx -d__WINNT__=1 -bt=nt /lnt -I%WATCOM\h\nt PrfTstProcess.c   kernel32.lib
+ *
  */
 
 #include <stdio.h>
+#if !defined(__WINNT__)
 #include <sys/time.h>
+#endif
 #ifndef __NOTPC__
 #include <process.h>
 #endif
@@ -87,8 +112,12 @@ unsigned getHz(void)
     return ul;
 }
 
+void printSystemInfo(void)
+{
+}
 
 #elif defined(__WINNT__)
+#include <windows.h>
 /*
  * Windows
  */
@@ -96,14 +125,117 @@ unsigned long __stdcall GetTickCount(void);
 
 long double gettime(void)
 {
-    return (long double)GetTickCount();
+    //return (long double)GetTickCount();
+    LARGE_INTEGER ullCounter;
+    ullCounter.QuadPart = 0;
+    QueryPerformanceCounter(&ullCounter);
+    return (long double)ullCounter.QuadPart;
 }
 
 unsigned getHz(void)
 {
-    return 1000;
+    //return 1000;
+    LARGE_INTEGER ullFrequency;
+    ullFrequency.QuadPart = 0;
+    QueryPerformanceFrequency(&ullFrequency);
+    return (unsigned)ullFrequency.QuadPart;
 }
 
+void printSystemInfo(void)
+{
+    LONG        lrc;
+    SYSTEM_INFO si;
+    HKEY        hProcessor0;
+    char        szMhz[16];
+    szMhz[0] = '\0';
+
+    lrc = RegOpenKey(HKEY_LOCAL_MACHINE,
+                     "HARDWARE\\DESCRIPTION\\System\\CentralProcessor\\0",
+                     &hProcessor0);
+    if (!lrc)
+    {
+        LONG    cchMhz = sizeof(szMhz);
+        DWORD   iKey;
+        CHAR    szValueName[256];
+        DWORD   cchValueName = sizeof(szValueName);
+        DWORD   dwType;
+        BYTE    abData[256];
+        DWORD   cbData;
+
+        iKey = 0;
+        szMhz[0] = '\0';
+        while ((lrc = RegEnumValue(hProcessor0,
+                                   iKey,
+                                   szValueName,
+                                   &cchValueName,
+                                   NULL,
+                                   &dwType,
+                                   &abData,
+                                   &cbData)) == 0
+               || lrc == ERROR_MORE_DATA)
+        {
+            switch (dwType)
+            {
+                case REG_SZ:
+                case REG_EXPAND_SZ:
+                    /*
+                    printf("%-24s =  type: %1x  namesize: %2d  data size: %3d bytes\n          %s\n",
+                           szValueName, dwType, cchValueName, cbData, &abData[0]);
+                    */
+                    break;
+
+                default:
+                {
+                    /*
+                    int i,j;
+                    printf("%-24s =  type: %1x  namesize: %2d  data size: %3d bytes\n",
+                           szValueName, dwType, cchValueName, cbData);
+                    for (i = 0; i < cbData; i += 16)
+                    {
+                        printf("    %04x ", i);
+                        for (j = 0; j < 16; j++)
+                        {
+                            if (j + i < cbData)
+                                printf(j == 8 ? "-  %02x " : "%02x ",
+                                       (BYTE)abData[i+j]);
+                            else
+                                printf(j == 8 ? "    " : "   ");
+                        }
+                        putc(' ', stdout);
+                        for (j = 0; j < 16; j++)
+                        {
+                            if (j+i < cbData)
+                                putc(isprint(abData[j]) ? abData[j] : '.', stdout);
+                            else
+                                putc(' ', stdout);
+                        }
+                        putc('\n', stdout);
+                    }
+                    */
+                    if (   !szMhz[0]
+                        && stricmp(szValueName, "~MHz") == 0
+                        && dwType == REG_DWORD
+                        && cbData == sizeof(DWORD))
+                        sprintf(szMhz, "%d", *(PDWORD)&abData[0]);
+                }
+            }
+
+            /* next */
+            iKey++;
+            dwType = 0;
+            cchValueName = sizeof(szValueName);
+            szValueName[0] = '\0';
+        }
+        RegCloseKey(hProcessor0);
+    }
+
+    GetSystemInfo(&si);
+    printf("  %d %d CPUs  %s Mhz\n",
+           si.dwNumberOfProcessors,
+           si.dwProcessorType,
+           szMhz
+           );
+}
 
 #else
 
@@ -120,6 +252,10 @@ long double gettime(void)
 unsigned getHz(void)
 {
     return 1;//000000;
+}
+
+void printSystemInfo(void)
+{
 }
 
 #endif
@@ -160,6 +296,6 @@ int main(int argc, char **argv)
     rdEnd = gettime();
     printf("%Lf - %Lf sec (pid=%x)\n", rdEnd - rdStart, (rdEnd - rdStart) / getHz(), pid);
     printf("(start: %Lf  end: %Lf  Hz: %d\n", rdStart, rdEnd, getHz());
-
+    printSystemInfo();
     return 0;
 }
