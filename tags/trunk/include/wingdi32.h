8a3eb63743caab674d271adcc94664b6144b646b
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-08-26 14:20:54 +0000

    header updates

--- tags/trunk/include/wingdi32.h
+++ tags/trunk/include/wingdi32.h
@@ -1,5 +1,3 @@
-/* $Id: wingdi32.h,v 1.4 2001-08-02 14:49:03 sandervl Exp $ */
-
 /*
  * Internal functions exported by gdi32
  *
@@ -9,8 +7,11 @@
 #ifndef __WINGDI32_H__
 #define __WINGDI32_H__
 
+#include <dcdata.h>
+
 BOOL WIN32API setWinDeviceRegionFromPMDeviceRegion(HRGN winHrgn, HRGN pmHrgn, pDCData pHpsPmHrgn, HWND hwndPmHrgn);
 VOID WIN32API UnselectGDIObjects(HDC hdc);
 BOOL OSLibGpiSetCp(HDC hdc, ULONG codepage);
+void WIN32API ConvertRGB555to565(LPVOID dest, LPVOID src, UINT imgsize);
 
 #endif //__WINGDI32_H__
