79edebae118b817982383a72051278c61d5e8837
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-08-31 20:22:56 +0000

    fix ODINPROCEDURE4

--- include/odinwrap.h
+++ include/odinwrap.h
@@ -287,7 +287,7 @@ extern unsigned long int WIN32API GetCurrentThreadId(); //kernel32
     ODIN_##cName(a1,a2,a3,a4); \
     dprintf(("%s: void "#cName"() leave\n",   \
              pszOdinDebugChannel));           \
-    FNEPILOGUE       \
+  FNEPILOGUE(#cName)       \
   }                               \
                                   \
   void ODIN_INTERNAL ODIN_##cName (t1 a1,t2 a2,t3 a3,t4 a4)
