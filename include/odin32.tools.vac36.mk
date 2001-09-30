7d7cd81ba6a68588a2e48124f6ba1194efa66001
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-09-30 00:51:35 +0000

    Use VAC3 version since it's identical.

--- include/odin32.tools.vac36.mk
+++ include/odin32.tools.vac36.mk
@@ -1,4 +1,4 @@
-# $Id: odin32.tools.vac36.mk,v 1.5 2001-09-30 00:49:58 bird Exp $
+# $Id: odin32.tools.vac36.mk,v 1.6 2001-09-30 00:51:35 bird Exp $
 
 #
 # Odin32 API
@@ -7,94 +7,8 @@
 #       and inference rules.
 #
 
-
 #
-# VAC36 Specific tools
+# This is currently identical to VAC3.
 #
-MAPSYM      = mapsym
-
-
-#
-# Make command definition.
-#
-MAKE_CMD    = nmake /nologo
-!ifdef DEBUG
-MAKE_CMD    = $(MAKE_CMD) DEBUG=1
-!endif
-!ifdef PROFILE
-MAKE_CMD    = $(MAKE_CMD) DEBUG=1 PROFILE=1
-!endif
-!ifdef NODEBUGINFO
-MAKE_CMD    = $(MAKE_CMD) NODEBUGINFO=1
-!endif
-!ifdef NODEP
-MAKE_CMD    = $(MAKE_CMD) NODEP=1
-!endif
-
-
-#
-# Interference rules.
-#
-.SUFFIXES: .orc_asm .orc .pre-c .pre-cpp
-
-!ifdef MULTIJOBS
-.SILENT :
-!endif
-
-.c{$(OBJDIR)}.obj:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -Fo$(OBJDIR)\$(@B).obj -c $<
-!else
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES) -Fo$(OBJDIR)\$(@B).obj -c $<
-!endif
-
-.c.obj:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -Fo$(OBJDIR)\$(@B).obj -c $<
-!else
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES) -Fo$(OBJDIR)\$(@B).obj -c $<
-!endif
-
-.c.pre-c:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -P+ -Pd -Pc+ -c $< > $@
-!else
-    $(CMDQD_SUB) $(CC) $(CFLAGS) $(CINCLUDES) $(CDEFINES) -P+ -Pd -Pc+ -c $< > $@
-!endif
-
-
-.cpp{$(OBJDIR)}.obj:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -Fo$(OBJDIR)\$(@B).obj -c $<
-!else
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES) -Fo$(OBJDIR)\$(@B).obj -c $<
-!endif
-
-.cpp.obj:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -Fo$(OBJDIR)\$(@B).obj -c $<
-!else
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES) -Fo$(OBJDIR)\$(@B).obj -c $<
-!endif
-
-.cpp.pre-cpp:
-!ifdef USERAPP
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES_WIN32APP) -P+ -Pd -Pc+ -c $< > $@
-!else
-    $(CMDQD_SUB) $(CXX) $(CXXFLAGS) $(CINCLUDES) $(CDEFINES) -P+ -Pd -Pc+ -c $< > $@
-!endif
-
-
-.asm{$(OBJDIR)}.obj:
-    $(CMDQD_SUB) $(AS) $(ASFLAGS) -Fdo:$(OBJDIR) $<
-
-.orc{$(OBJDIR)}.orc_asm:
-    $(CMDQD_SUB) $(RC) $(RCFLAGS) $(CINCLUDES) -o $(OBJDIR)\$(@B).orc_asm $<
-
-.orc{$(OBJDIR)}.obj:
-    $(RC) $(RCFLAGS) $(CINCLUDES) -o $(OBJDIR)\$(@B).orc_asm $<
-    $(AS) $(ASFLAGS) -Fdo:$(OBJDIR) $(OBJDIR)\$(@B).orc_asm
-
-.rc{$(OBJDIR)}.res:
-    $(CMDQD_SUB) $(OS2RC) $(OS2RCFLAGS) $(CINCLUDES:-I=-i ) $< $@
+!include $(ODIN32_INCLUDE)/odin32.tools.vac3.mk
 
