c50a58113703f0809ea72c586b24a9c798a3639e
phaller <phaller@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-10 16:22:20 +0000

    .

--- trunk/src/iphlpapi/iphlpapi.cpp
+++ trunk/src/iphlpapi/iphlpapi.cpp
@@ -1,27 +1,75 @@
-/* $Id: iphlpapi.cpp,v 1.1 2001-05-24 17:35:34 sandervl Exp $ */
+/* $Id: iphlpapi.cpp,v 1.2 2001-10-10 16:22:19 phaller Exp $ */
 /*
  *	IPHLPAPI library
  *
  */
 
+
+/****************************************************************************
+ * includes
+ ****************************************************************************/
+
+#include <odin.h>
+#include <odinwrap.h>
+#include <os2sel.h>
+
 #include <os2win.h>
 #include <odinwrap.h>
 #include <winversion.h>
 
+#include "iphlpapi.h"
+
+ODINDEBUGCHANNEL(IPHLPAPI-IPHLPAPI)
+
 
 //******************************************************************************
 //******************************************************************************
-DWORD WIN32API GetAdaptersInfo(DWORD arg1, DWORD arg2)
+ODINFUNCTION2(DWORD,            GetAdaptersInfo,
+              PIP_ADAPTER_INFO, pAdapterInfo,
+              PULONG,           pOutBufLen)
 {
-  dprintf(("GetAdaptersInfo %x %x - STUB", arg1, arg2));
+  dprintf(("GetAdaptersInfo not implemented"));
+  
+  static PIP_ADAPTER_INFO pipAdapters = NULL;
+  static ULONG ulAdaptersSize = 0;
+  
+  if (NULL == pipAdapters)
+  {
+    // gather the information and save it
+    
+    // determine number of IP adapters (interfaces) in the system
+    
+    // os2_ioctl() SIOSTATIF42
+  }
+  
+  // OK, just copy over the information as far as possible
+  
+  return ERROR_SUCCESS;
+}
+
+
+//******************************************************************************
+//******************************************************************************
+ODINFUNCTION2(DWORD,       GetNetworkParams,
+              PFIXED_INFO, pFixedInfo,
+              PULONG,      pOutBufLen)
+{
+  dprintf(("GetNetworkParams not implemented"));
   return ERROR_NOT_SUPPORTED; //NT returns this 
 }
 //******************************************************************************
 //******************************************************************************
-DWORD WIN32API GetNetworkParams(DWORD arg1, DWORD arg2)
-{
-  dprintf(("GetNetworkParams %x %x - STUB", arg1, arg2));
-  return ERROR_NOT_SUPPORTED; //NT returns this 
-}
-//******************************************************************************
-//******************************************************************************
+
+
+DWORD AddIPAddress(IPAddr Address, // IP address to add
+                   IPMask IpMask, // subnet mask for IP address
+                   DWORD IfIndex, // index of adapter
+                   PULONG NTEContext, // Net Table Entry context
+                   PULONG NTEInstance // Net Table Entry Instance
+                  );
+// SIOCAIFADDR
+
+DWORD DeleteIPAddress(
+                      ULONG NTEContext // net table entry context
+                     );
+// SIOCDIFADDR
