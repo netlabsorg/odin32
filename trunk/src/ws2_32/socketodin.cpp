1141c2ba379b53e26c39b4b3c24f5491758d959f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-11 19:24:38 +0000

    getsockopt update

--- trunk/src/ws2_32/socketodin.cpp
+++ trunk/src/ws2_32/socketodin.cpp
@@ -1,4 +1,4 @@
-/* $Id: socketodin.cpp,v 1.1 2001-10-10 17:19:34 sandervl Exp $ */
+/* $Id: socketodin.cpp,v 1.2 2001-10-11 19:24:38 sandervl Exp $ */
 
 #include <odin.h>
 #include <winsock2.h>
@@ -31,7 +31,7 @@ ODINFUNCTION5(int,WS2setsockopt,
        case IP_ADD_MEMBERSHIP_WS2:   /* add an IP group membership */
        case IP_DROP_MEMBERSHIP_WS2:  /* drop an IP group membership */
        case IP_DONTFRAGMENT_WS2:     /* don't fragment IP datagrams */
-           optname += WS2_IPPROTO_OFFSET;
+           optname = WS2_IPPROTO_OPT(optname);
            break;
 
        default:
@@ -41,6 +41,41 @@ ODINFUNCTION5(int,WS2setsockopt,
        }
    }
    ret = setsockopt(s, level, optname, (char *)optval, optlen);
+   return ret;
+}
+//******************************************************************************
+//******************************************************************************
+ODINFUNCTION5(int,WS2getsockopt,
+              SOCKET, s,
+              int, level,
+              int, optname,
+              char *, optval,
+              int *,optlen)
+{
+   int ret;
+
+   if(level == IPPROTO_IP) {
+       switch(optname) {
+       case IP_OPTIONS_WS2:	     /* set/get IP options */
+       case IP_HDRINCL_WS2:	     /* header is included with data */
+       case IP_TOS_WS2:		     /* IP type of service and preced*/
+       case IP_TTL_WS2:	    	     /* IP time to live */
+       case IP_MULTICAST_IF_WS2:     /* set/get IP multicast i/f  */
+       case IP_MULTICAST_TTL_WS2:    /* set/get IP multicast ttl */
+       case IP_MULTICAST_LOOP_WS2:   /* set/get IP multicast loopback */
+       case IP_ADD_MEMBERSHIP_WS2:   /* add an IP group membership */
+       case IP_DROP_MEMBERSHIP_WS2:  /* drop an IP group membership */
+       case IP_DONTFRAGMENT_WS2:     /* don't fragment IP datagrams */
+           optname = WS2_IPPROTO_OPT(optname);
+           break;
+
+       default:
+           dprintf(("getsockopt: IPPROTO_IP, unknown option %x", optname));
+       	   WSASetLastError(WSAENOPROTOOPT);
+      	   return SOCKET_ERROR;
+       }
+   }
+   ret = getsockopt(s, level, optname, (char *)optval, optlen);
    return ret;
 }
 //******************************************************************************
