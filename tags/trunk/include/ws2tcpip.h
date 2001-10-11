c700cad10dba2bcdf242067f325c5e5157f29f94
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-11 19:25:20 +0000

    *** empty log message ***

--- tags/trunk/include/ws2tcpip.h
+++ tags/trunk/include/ws2tcpip.h
@@ -16,6 +16,21 @@
 #define	IP_DROP_MEMBERSHIP_WS2     13 /* drop an IP group membership */
 #define IP_DONTFRAGMENT_WS2        14 /* don't fragment IP datagrams */
 
+#define IP_OPTIONS_OS2              1    /* buf/ip_opts; set/get IP options */
+#define IP_MULTICAST_IF_OS2         2    /* u_char; set/get IP multicast i/f  */
+#define IP_MULTICAST_TTL_OS2        3    /* u_char; set/get IP multicast ttl */
+#define IP_MULTICAST_LOOP_OS2       4    /* u_char; set/get IP multicast loopback */
+#define IP_ADD_MEMBERSHIP_OS2       5    /* ip_mreq; add an IP group membership */
+#define IP_DROP_MEMBERSHIP_OS2      6    /* ip_mreq; drop an IP group membership */
+#define IP_HDRINCL_OS2              7    /* int; header is included with data */
+#define IP_TOS_OS2                  8    /* int; IP type of service and preced. */
+#define IP_TTL_OS2                  9    /* int; IP time to live */
+#define IP_RECVOPTS_OS2             10   /* bool; receive all IP opts w/dgram */
+#define IP_RECVRETOPTS_OS2          11   /* bool; receive IP opts for response */
+#define IP_RECVDSTADDR_OS2          12   /* bool; receive IP dst addr w/dgram */
+#define IP_RETOPTS_OS2              13   /* ip_opts; set/get IP options */
+#define IP_RECVTRRI_OS2             14   /* bool; receive token ring routing inf */
+
 #endif	/* _WS2TCPIP_H_ */
 
 
