04d90525c6be2af2dcd04bd5e913864f9a2bf21f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-16 14:49:07 +0000

    update

--- include/custombuild.h
+++ include/custombuild.h
@@ -6,6 +6,7 @@ extern BOOL fCustomBuild;
 void InitDirectoriesCustom(char *szSystemDir, char *szWindowsDir);
 
 void DisableOdinIni();
+void DisableOdinSysMenuItems();
 
 #endif  /*__CUSTOMBUILD_H__*/
 
