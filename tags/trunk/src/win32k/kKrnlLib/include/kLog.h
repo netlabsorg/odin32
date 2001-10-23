77a772c2f4fcd1911e403ef83e5afbefade7a10a
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-23 02:12:14 +0000

    Added lots macros for logging. Printf fix working. Mostly working it seems.

--- tags/trunk/src/win32k/kKrnlLib/include/kLog.h
+++ tags/trunk/src/win32k/kKrnlLib/include/kLog.h
@@ -1,4 +1,4 @@
-/* $Id: kLog.h,v 1.4 2001-10-19 01:48:36 bird Exp $
+/* $Id: kLog.h,v 1.5 2001-10-23 02:12:14 bird Exp $
  *
  * kLog - Generic Logging and Trace Routines.
  *
@@ -52,14 +52,38 @@ extern "C" {
 #define KLOG_FLAGS_AS_ABOVE     0xffff  /* Inherit options from above option level. */
 
 /*
- * Define default file and function names.
+ * Define defaults.
  */
+#ifndef KLOGFILEDEF
+    #define KLOGFILEDEF     __FILE__
+#endif
+#ifndef KLOGFUNCTIONDEF
+    #define KLOGFUNCTIONDEF __FUNCTION__
+#endif
+
 #ifndef KLOGFILE
-#   define KLOGFILE         __FILE__
+    #ifndef NOFILEID
+        #define KLOGFILE    szFileId
+    #else
+        #define KLOGFILE    KLOGFUNCTIONDEF
+    #endif
 #endif
 #ifndef KLOGFUNCTION
-#   define KLOGFUNCTION     __FUNCTION__
+    #define KLOGFUNCTION    szkLogFunctionName
 #endif
+#ifndef KLOGRETURNTYPE
+    #define KLOGRETURNTYPE  szkLogReturnType
+#endif
+#ifndef KLOGMODHANDLE
+    #define KLOGMODHANDLE   hkLogMod
+#endif
+#ifndef KLOGSEQHANDLE
+    #define KLOGSEQHANDLE   kLogSeqHandle
+#endif
+
+/*
+ * Position parameters.
+ */
 #define KLOGPOS_EXT         KLOGFILE, __LINE__, KLOGFUNCTION    /* external call */
 #define KLOGPOS_INT         pszFile, iLine, pszFunction         /* Internal call */
 #define KLOGPOS_DECL        const char *pszFile, int iLine, const char *pszFunction
@@ -205,17 +229,121 @@ KBOOL       KLIBCALL kLogInitBuffer(unsigned long cbBufferSize);
 #ifdef _kAVL_h_
 HKLOGMOD    KLIBCALL kLogInitMod(PKLOGMODDATA pMod, KLOGPOS_DECL);
 #endif
-void        KLIBCALL kLogEntry(HKLOGMOD hLogMod, KLOGPOS_DECL, const char *pszFuncProto, KBOOL fOptlink, ...);
-void        KLIBCALL kLogExit(HKLOGMOD hLogMod, KLOGPOS_DECL, const char *pszReturnType, unsigned uReturn);
+void        KLIBCALL kLogTermMod(HKLOGMOD hLogMod, KLOGPOS_DECL);
+
+int         KLIBCALL kLogStart(HKLOGMOD hLogMod, KLOGPOS_DECL, PHKLOGSEQ phSeq, const char *pszFuncProto, KBOOL fOptlink, ...);
+int         KLIBCALL kLogEntry(HKLOGMOD hLogMod, KLOGPOS_DECL, const char *pszFuncProto, KBOOL fOptlink, ...);
+void        KLIBCALL kLogExit(HKLOGMOD hLogMod, KLOGPOS_DECL, const char *pszReturnType, ...);
+void        KLIBCALL kLogStop(HKLOGMOD hLogMod, KLOGPOS_DECL, PHKLOGSEQ phSeq, const char *pszReturnType, ...);
+
 void        KLIBCALL kLog(HKLOGMOD hLogMod, KLOGPOS_DECL, int iType, ...);
 #if defined(va_arg) || defined(va_list) || defined(va_start) || defined(va_end)
 void        KLIBCALL kLogv(HKLOGMOD hLogMod, KLOGPOS_DECL, int iType, va_list args);
-va_list     KLIBCALL kLogFixPrintf(const char *pszFormat, ...); /* a hack! */
 #endif
 
-#define KLOGINITMOD(pMod)                   kLogInitMod(pMod, KLOGPOS_EXT)
-#define KLOGENTRY(hLogMod, pszProto, parg0) kLogEntry(hLogMod, KLOGPOS_EXT, pszFuncProto, FALSE, parg0)
-#define KLOGEXIT(hLogMod, pszType, uValue)  kLogExit(hLogMod, KLOGPOS_EXT, pszType, uValue)
+int         KLIBCALL kLogFixPrintf1(const char *pszFormat, ...);
+void        KLIBCALL kLogFixPrintf2(HKLOGMOD hLogMod, KLOGPOS_DECL, int iType, int iBuffer);
+
+
+
+/*******************************************************************************
+*   Macros                                                                     *
+*******************************************************************************/
+#if defined(DEBUG) || defined(LOGGING_ENABLED)
+    #define KLOGINITMOD(pMod)                                           KLOGMODHANDLE = kLogInitMod(pMod, KLOGPOS_EXT)
+    #define KLOGTERMMOD()                                               kLogTermMod(KLOGMODHANDLE, KLOGPOS_EXT)
+
+    #define KLOGSTARTPARMS                                              KLOGMODHANDLE, KLOGPOS_EXT, &KLOGSEQHANDLE
+    #define KLOGENTRYPARMS                                              KLOGMODHANDLE, KLOGPOS_EXT
+    #define KLOGSTARTVARS(pszRetType)                                   static const char   KLOGFUNCTION[] = KLOGFUNCTIONDEF; \
+                                                                        static const char   KLOGRETURNTYPE[] = pszRetType; \
+                                                                        HLOGSEQ             KLOGSEQHANDLE; \
+                                                                        int                 kLogDummyVariable
+    #define KLOGENTRYVARS(pszRetType)                                   static const char   KLOGFUNCTION[] = KLOGFUNCTIONDEF; \
+                                                                        static const char   KLOGRETURNTYPE[] = pszRetType; \
+                                                                        int                 kLogDummyVariable
+
+    #define KLOGSTART( pszRetType, pszProto, arg0)                      KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, FALSE, &(arg0))
+    #define KLOGSTART0(pszRetType)                                      KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, NULL, TRUE)
+    #define KLOGSTART1(pszRetType, pszProto, a0)                        KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0)
+    #define KLOGSTART2(pszRetType, pszProto, a0,a1)                     KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1)
+    #define KLOGSTART3(pszRetType, pszProto, a0,a1,a2)                  KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2)
+    #define KLOGSTART4(pszRetType, pszProto, a0,a1,a2,a3)               KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2,a3)
+    #define KLOGSTART5(pszRetType, pszProto, a0,a1,a2,a3,a4)            KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2,a2,a4)
+    #define KLOGSTART6(pszRetType, pszProto, a0,a1,a2,a3,a4,a5)         KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5)
+    #define KLOGSTART7(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6)      KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5,a6)
+    #define KLOGSTART8(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6,a7)   KLOGSTARTVARS(pszRetType) = kLogStart(KLOGSTARTPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5,a6,a7)
+
+    #define KLOGENTRY( pszRetType, pszProto, arg0)                      KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, FALSE, &(arg0))
+    #define KLOGENTRY0(pszRetType)                                      KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, NULL, TRUE)
+    #define KLOGENTRY1(pszRetType, pszProto, a0)                        KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0)
+    #define KLOGENTRY2(pszRetType, pszProto, a0,a1)                     KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1)
+    #define KLOGENTRY3(pszRetType, pszProto, a0,a1,a2)                  KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2)
+    #define KLOGENTRY4(pszRetType, pszProto, a0,a1,a2,a3)               KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2,a3)
+    #define KLOGENTRY5(pszRetType, pszProto, a0,a1,a2,a3,a4)            KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2,a2,a4)
+    #define KLOGENTRY6(pszRetType, pszProto, a0,a1,a2,a3,a4,a5)         KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5)
+    #define KLOGENTRY7(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6)      KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5,a6)
+    #define KLOGENTRY8(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6,a7)   KLOGENTRYVARS(pszRetType) = kLogEntry(KLOGENTRYPARMS, pszProto, TRUE,a0,a1,a2,a2,a4,a5,a6,a7)
+
+    #define KLOGEXIT(uValue)                                            kLogExit( KLOGMODHANDLE, KLOGPOS_EXT,                 KLOGRETURNTYPE, uValue)
+    #define KLOGEXITVOID()                                              kLogExit( KLOGMODHANDLE, KLOGPOS_EXT,                 NULL,         NULL)
+
+    #define KLOGSTOP(uValue)                                            kLogStop( KLOGMODHANDLE, KLOGPOS_EXT, &KLOGSEQHANDLE, KLOGRETURNTYPE, uValue)
+    #define KLOGSTOPVOID(uValue)                                        kLogStop( KLOGMODHANDLE, KLOGPOS_EXT, &KLOGSEQHANDLE, NULL,         NULL)
+
+    #ifdef KLOG_USE_RETURN
+        #if KLOG_USE_RETURN
+            #define return(value)                                       KLOGEXIT(value); return (value)
+        #else
+            #define return(value)                                       KLOGSTOP(value); return (value)
+        #endif
+    #endif
+
+    #define dprintf(a)                                                  kLogFixPrintf2(KLOGMODHANDLE, KLOGPOS_EXT, KLOG_TYPE_PRINTF, kLogFixPrintf1 a)
+    #define kprintf(a)                                                  dprintf(a)
+
+#else
+
+    #define KLOGINITMOD(pMod)                                           ((HKLOGMOD)-1)
+    #define KLOGTERMMOD()                                               ((void)0)
+
+    #define KLOGSTART( pszRetType, pszProto, arg0)                      ((void)0)
+    #define KLOGSTART0(pszRetType)                                      ((void)0)
+    #define KLOGSTART1(pszRetType, pszProto, a0)                        ((void)0)
+    #define KLOGSTART2(pszRetType, pszProto, a0,a1)                     ((void)0)
+    #define KLOGSTART3(pszRetType, pszProto, a0,a1,a2)                  ((void)0)
+    #define KLOGSTART4(pszRetType, pszProto, a0,a1,a2,a3)               ((void)0)
+    #define KLOGSTART5(pszRetType, pszProto, a0,a1,a2,a3,a4)            ((void)0)
+    #define KLOGSTART6(pszRetType, pszProto, a0,a1,a2,a3,a4,a5)         ((void)0)
+    #define KLOGSTART7(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6)      ((void)0)
+    #define KLOGSTART8(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6,a7)   ((void)0)
+
+    #define KLOGENTRY( pszRetType, pszProto, arg0)                      ((void)0)
+    #define KLOGENTRY0(pszRetType)                                      ((void)0)
+    #define KLOGENTRY1(pszRetType, pszProto, a0)                        ((void)0)
+    #define KLOGENTRY2(pszRetType, pszProto, a0,a1)                     ((void)0)
+    #define KLOGENTRY3(pszRetType, pszProto, a0,a1,a2)                  ((void)0)
+    #define KLOGENTRY4(pszRetType, pszProto, a0,a1,a2,a3)               ((void)0)
+    #define KLOGENTRY5(pszRetType, pszProto, a0,a1,a2,a3,a4)            ((void)0)
+    #define KLOGENTRY6(pszRetType, pszProto, a0,a1,a2,a3,a4,a5)         ((void)0)
+    #define KLOGENTRY7(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6)      ((void)0)
+    #define KLOGENTRY8(pszRetType, pszProto, a0,a1,a2,a3,a4,a5,a6,a7)   ((void)0)
+
+    #define KLOGEXIT(uValue)                                            ((void)0)
+    #define KLOGEXITVOID()                                              ((void)0)
+
+    #define KLOGSTOP(uValue)                                            ((void)0)
+    #define KLOGSTOPVOID()                                              ((void)0)
+
+    #define kprintf(a)                                                  ((void)0)
+    #define dprintf(a)                                                  ((void)0)
+#endif
+
+
+/*******************************************************************************
+*   Global Variables                                                           *
+*******************************************************************************/
+extern HKLOGMOD KLOGMODHANDLE;   /* Define this on the behalf of the kKL user. */
 
 #ifdef __cplusplus
 }
