6724709415dd426cc49dec755d69725375c8f402
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-27 17:27:44 +0000

    *** empty log message ***

--- trunk/include/custombuild.h
+++ trunk/include/custombuild.h
@@ -1,6 +1,10 @@
 #ifndef __CUSTOMBUILD_H__
 #define __CUSTOMBUILD_H__
 
+//HKEY_LOCAL_MACHINE
+#define CUSTOM_BUILD_OPTIONS_KEY  "System\\CustomBuild"
+#define DISABLE_AUDIO_KEY         "DisableAudio"
+
 extern BOOL fCustomBuild;
 
 void InitDirectoriesCustom(char *szSystemDir, char *szWindowsDir);
