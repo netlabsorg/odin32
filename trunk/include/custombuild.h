c2269391f29eb4823b3fd4d205925b3b6673b522
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-28 15:23:39 +0000

    update

--- trunk/include/custombuild.h
+++ trunk/include/custombuild.h
@@ -23,5 +23,15 @@ typedef BOOL   (* WIN32API PFNDRVIOCTL)(HANDLE hDevice, DWORD dwIoControlCode,
 BOOL WIN32API RegisterCustomDriver(PFNDRVOPEN pfnDriverOpen, PFNDRVCLOSE pfnDriverClose, 
                                    PFNDRVIOCTL pfnDriverIOCtl, LPCSTR lpDeviceName);
 
+
+//SetDialogHook can be used by a custom Odin build to register a hook procedure
+//that gets called before or after dialog creation
+
+#define HCUSTOM_PREDIALOGCREATION	0
+#define HCUSTOM_POSTDIALOGCREATION	1
+
+BOOL WIN32API SetDialogHook(HOOKPROC pfnDialogProc);
+BOOL WIN32API ClearDialogHook();
+
 #endif  /*__CUSTOMBUILD_H__*/
 
