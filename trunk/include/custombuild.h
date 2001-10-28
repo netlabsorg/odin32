2fa57facb8dcdac85ef6cc204d0df20dec5b53da
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-10-28 12:59:22 +0000

    *** empty log message ***

--- trunk/include/custombuild.h
+++ trunk/include/custombuild.h
@@ -12,5 +12,16 @@ void InitDirectoriesCustom(char *szSystemDir, char *szWindowsDir);
 void DisableOdinIni();
 void DisableOdinSysMenuItems();
 
+
+typedef HANDLE (* WIN32API PFNDRVOPEN)(DWORD dwAccess, DWORD dwShare);
+typedef void   (* WIN32API PFNDRVCLOSE)(HANDLE hDevice);
+typedef BOOL   (* WIN32API PFNDRVIOCTL)(HANDLE hDevice, DWORD dwIoControlCode,
+                       LPVOID lpInBuffer, DWORD nInBufferSize,
+                       LPVOID lpOutBuffer, DWORD nOutBufferSize,
+                       LPDWORD lpBytesReturned, LPOVERLAPPED lpOverlapped);
+
+BOOL WIN32API RegisterCustomDriver(PFNDRVOPEN pfnDriverOpen, PFNDRVCLOSE pfnDriverClose, 
+                                   PFNDRVIOCTL pfnDriverIOCtl, LPCSTR lpDeviceName);
+
 #endif  /*__CUSTOMBUILD_H__*/
 
