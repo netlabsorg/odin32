92cf65daff37d937e33f73b725295838fbf6467a
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2001-11-19 12:32:25 +0000

    created

---
+++ trunk/src/guidlib/guid.c
@@ -0,0 +1,44 @@
+/* $Id: guid.c,v 1.1 2001-11-19 12:32:25 sandervl Exp $ */
+#define ICOM_CINTERFACE 1
+#include <odin.h>
+
+#define CINTERFACE 1
+#define INITGUID
+#include <wine\obj_oleview.h>
+#include "initguid.h"
+
+#include "shlwapi.h"
+#include "shlguid.h"
+#include "shlobj.h"
+#include "docobj.h"
+#include "olectl.h"
+//#include "..\shell32\shellfolder.h"
+
+#include "wine/obj_inplace.h"
+#include "wine/obj_oleobj.h"
+#include "wine/obj_surrogate.h"
+#include "wine/obj_errorinfo.h"
+#include "wine/obj_oleview.h"
+#include "wine/obj_clientserver.h"
+#include "wine/obj_cache.h"
+#include "wine\obj_oleaut.h"
+#include "wine\obj_olefont.h"
+
+#include <wine/obj_oleview.h>
+#include <wine/obj_dragdrop.h>
+#include <wine/obj_inplace.h>
+#include <wine/obj_control.h>
+#include <wine/obj_shellfolder.h>
+#include <wine/obj_shelllink.h>
+#include <wine/obj_contextmenu.h>
+#include <wine/obj_commdlgbrowser.h>
+#include <wine/obj_extracticon.h>
+#include <wine/obj_shellextinit.h>
+#include <wine/obj_shellbrowser.h>
+#include <wine/obj_serviceprovider.h>
+#include <wine/unicode.h>
+
+#include <misc.h>
+
+DEFINE_GUID(IID_ISFHelper, 0x1fe68efbL, 0x1874, 0x9812, 0x56, 0xdc, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00);
+DEFINE_GUID(GUID_NULL,   0,0,0,0,0,0,0,0,0,0,0);
