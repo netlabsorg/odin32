0918b0137a064874ec088cd1a034c9b4d11c3399
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-01-05 11:15:29 +0000

    minor update

--- src/pe2lx/icon.h
+++ src/pe2lx/icon.h
@@ -1,6 +1,6 @@
-/* $Id: icon.h,v 1.4 1999-07-20 17:50:19 sandervl Exp $ */
+/* $Id: icon.h,v 1.5 2002-01-05 11:15:29 sandervl Exp $ */
 
-/* $Header: /home/ktk/tmp/odin/2007/netlabs.cvs/odin32/src/pe2lx/icon.h,v 1.4 1999-07-20 17:50:19 sandervl Exp $
+/* $Header: /home/ktk/tmp/odin/2007/netlabs.cvs/odin32/src/pe2lx/icon.h,v 1.5 2002-01-05 11:15:29 sandervl Exp $
  *
  * Declarations, protypes and defintions for conversion of icons.
  *
