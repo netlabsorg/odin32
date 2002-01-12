1e125592610742c5c87edcb4d502924c34c91164
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-01-12 09:58:36 +0000

    CreateFakeWindowEx change

--- trunk/include/winuser32.h
+++ trunk/include/winuser32.h
@@ -40,7 +40,7 @@ HICON WIN32API GetOS2Icon(HICON hIcon);
 //Turns native OS/2 window into window recognized by Odin (with only very
 //limited functionality)
 //Useful for creating an Odin window with an OS/2 window as parent.
-HWND WIN32API CreateFakeWindowEx(HWND hwndOS2);
+HWND WIN32API CreateFakeWindowEx(HWND hwndOS2, ATOM classAtom);
 
 #define WIN32_APPEARANCE	0
 #define OS2_APPEARANCE 		1  //OS/2 look with win32 system menu
