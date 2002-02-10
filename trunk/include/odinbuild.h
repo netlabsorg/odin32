369b212efdf572b55d6fada8567ea48daf1983ac
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-02-10 04:57:05 +0000

    Daily build - 20020210

--- trunk/include/odinbuild.h
+++ trunk/include/odinbuild.h
@@ -1,4 +1,4 @@
-/* $Id: odinbuild.h,v 1.268 2002-01-30 07:54:44 bird Exp $
+/* $Id: odinbuild.h,v 1.269 2002-02-10 04:57:05 bird Exp $
  *
  * Odin32 version number and build numbers.
  *
@@ -23,9 +23,10 @@ int     WIN32API Odin32GetBuildNumber(void);
 #define ODIN32_VERSION_MAJOR    0
 #define ODIN32_VERSION_MINOR    5
 #define ODIN32_VERSION       "0.5"
-#define ODIN32_BUILD_NR       572        /* This started at 300 on the 17th Des 2000. */
+#define ODIN32_BUILD_NR       573        /* This started at 300 on the 17th Des 2000. */
 
 #endif
+
 
 
 
