0f702fc263f3e369c463cded495522085c4c4e62
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-02-12 07:05:28 +0000

    Updated to 3.23.42-rel.

--- tags/trunk/tools/database/mysql/mysql_com.h
+++ tags/trunk/tools/database/mysql/mysql_com.h
@@ -1,5 +1,19 @@
-/* Copyright Abandoned 1996 TCX DataKonsult AB & Monty Program KB & Detron HB
-   This file is public domain and comes with NO WARRANTY of any kind */
+/* Copyright (C) 2000 MySQL AB & MySQL Finland AB & TCX DataKonsult AB
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+   MA 02111-1307, USA */
 
 /*
 ** Common definition between mysql server & client
@@ -7,34 +21,28 @@
 
 #ifndef _mysql_com_h
 #define _mysql_com_h
-#ifdef	__cplusplus
-extern "C" {
-#endif
+
 
 #define NAME_LEN	64		/* Field/table name length */
 #define HOSTNAME_LENGTH 60
 #define USERNAME_LENGTH 16
+#define SERVER_VERSION_LENGTH 60
 
 #define LOCAL_HOST	"localhost"
 #define LOCAL_HOST_NAMEDPIPE "."
 
-#define MYSQL_PORT	3306		/* Alloced by ISI for MySQL */
-#define MYSQL_UNIX_ADDR "/tmp/mysql.sock"
-#if defined(__EMX__) || defined(__OS2__)
-#undef MYSQL_UNIX_ADDR
-#define MYSQL_OS2_ADDR "\\socket\\MySQL"
-#define MYSQL_UNIX_ADDR MYSQL_OS2_ADDR
-#endif
-#ifdef WIN32
+#if defined(__WIN__) && !defined( _CUSTOMCONFIG_)
 #define MYSQL_NAMEDPIPE "MySQL"
 #define MYSQL_SERVICENAME "MySql"
-#endif
+#endif /* __WIN__ */
 
 enum enum_server_command {COM_SLEEP,COM_QUIT,COM_INIT_DB,COM_QUERY,
 			  COM_FIELD_LIST,COM_CREATE_DB,COM_DROP_DB,COM_REFRESH,
 			  COM_SHUTDOWN,COM_STATISTICS,
 			  COM_PROCESS_INFO,COM_CONNECT,COM_PROCESS_KILL,
-			  COM_DEBUG,COM_PING,COM_TIME,COM_DELAYED_INSERT};
+			  COM_DEBUG,COM_PING,COM_TIME,COM_DELAYED_INSERT,
+			  COM_CHANGE_USER, COM_BINLOG_DUMP,
+                          COM_TABLE_DUMP, COM_CONNECT_OUT};
 
 #define NOT_NULL_FLAG	1		/* Field can't be NULL */
 #define PRI_KEY_FLAG	2		/* Field is part of a primary key */
@@ -49,6 +57,7 @@ enum enum_server_command {COM_SLEEP,COM_QUIT,COM_INIT_DB,COM_QUERY,
 #define AUTO_INCREMENT_FLAG 512		/* field is a autoincrement field */
 #define TIMESTAMP_FLAG	1024		/* Field is a timestamp */
 #define SET_FLAG	2048		/* field is a set */
+#define NUM_FLAG	32768		/* Field is num (for clients) */
 #define PART_KEY_FLAG	16384		/* Intern; Part of some key */
 #define GROUP_FLAG	32768		/* Intern: Group field */
 #define UNIQUE_FLAG	65536		/* Intern: Used by sql_yacc */
@@ -58,6 +67,14 @@ enum enum_server_command {COM_SLEEP,COM_QUIT,COM_INIT_DB,COM_QUERY,
 #define REFRESH_TABLES		4	/* close all tables */
 #define REFRESH_HOSTS		8	/* Flush host cache */
 #define REFRESH_STATUS		16	/* Flush status variables */
+#define REFRESH_THREADS		32	/* Flush status variables */
+#define REFRESH_SLAVE           64      /* Reset master info and restart slave
+					   thread */
+#define REFRESH_MASTER          128     /* Remove all bin logs in the index
+					   and truncate the index */
+
+/* The following can't be set with mysql_refresh() */
+#define REFRESH_READ_LOCK	16384	/* Lock tables for read */
 #define REFRESH_FAST		32768	/* Intern flag */
 
 #define CLIENT_LONG_PASSWORD	1	/* new more secure passwords */
@@ -65,32 +82,50 @@ enum enum_server_command {COM_SLEEP,COM_QUIT,COM_INIT_DB,COM_QUERY,
 #define CLIENT_LONG_FLAG	4	/* Get all column flags */
 #define CLIENT_CONNECT_WITH_DB	8	/* One can specify db on connect */
 #define CLIENT_NO_SCHEMA	16	/* Don't allow database.table.column */
-#define CLIENT_COMPRESS		32	/* Can use compression protcol */
+#define CLIENT_COMPRESS		32	/* Can use compression protocol */
 #define CLIENT_ODBC		64	/* Odbc client */
 #define CLIENT_LOCAL_FILES	128	/* Can use LOAD DATA LOCAL */
 #define CLIENT_IGNORE_SPACE	256	/* Ignore spaces before '(' */
+#define CLIENT_CHANGE_USER	512	/* Support the mysql_change_user() */
+#define CLIENT_INTERACTIVE	1024	/* This is an interactive client */
+#define CLIENT_SSL              2048     /* Switch to SSL after handshake */
+#define CLIENT_IGNORE_SIGPIPE   4096     /* IGNORE sigpipes */
+#define CLIENT_TRANSACTIONS	8192	/* Client knows about transactions */
+
+#define SERVER_STATUS_IN_TRANS  1	/* Transaction has started */
+#define SERVER_STATUS_AUTOCOMMIT 2	/* Server in auto_commit mode */
 
 #define MYSQL_ERRMSG_SIZE	200
 #define NET_READ_TIMEOUT	30		/* Timeout on read */
 #define NET_WRITE_TIMEOUT	60		/* Timeout on write */
 #define NET_WAIT_TIMEOUT	8*60*60		/* Wait for new query */
 
-enum enum_net_type { NET_TYPE_TCPIP, NET_TYPE_SOCKET, NET_TYPE_NAMEDPIPE };
+#ifndef Vio_defined
+#define Vio_defined
+#ifdef HAVE_VIO
+class Vio;					/* Fill Vio class in C++ */
+#else
+struct st_vio;					/* Only C */
+typedef struct st_vio Vio;
+#endif
+#endif
 
 typedef struct st_net {
-  enum enum_net_type nettype;
-#ifdef WIN32
-  HANDLE hPipe;
-#endif
-  my_socket fd;
+  Vio* vio;
+  my_socket fd;					/* For Perl DBI/dbd */
   int fcntl;
   unsigned char *buff,*buff_end,*write_pos,*read_pos;
   char last_error[MYSQL_ERRMSG_SIZE];
   unsigned int last_errno,max_packet,timeout,pkt_nr;
-  my_bool error,return_errno,compress;
-
+  unsigned char error;
+  my_bool return_errno,compress;
+  my_bool no_send_ok; /* needed if we are doing several
+   queries in one command ( as in LOAD TABLE ... FROM MASTER ),
+   and do not want to confuse the client with OK at the wrong time
+		      */
   unsigned long remain_in_buf,length, buf_length, where_b;
-  my_bool more;
+  unsigned int *return_status;
+  unsigned char reading_or_writing;
   char save_char;
 } NET;
 
@@ -122,8 +157,7 @@ extern unsigned long net_buffer_length;
 
 #define net_new_transaction(net) ((net)->pkt_nr=0)
 
-int	my_net_init(NET *net,  enum enum_net_type nettype, my_socket fd,
-		    void *);
+int	my_net_init(NET *net, Vio* vio);
 void	net_end(NET *net);
 void	net_clear(NET *net);
 int	net_flush(NET *net);
@@ -168,17 +202,25 @@ typedef struct st_udf_init
 
   /* Prototypes to password functions */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+  
 void randominit(struct rand_struct *,unsigned long seed1,
 		unsigned long seed2);
 double rnd(struct rand_struct *);
 void make_scrambled_password(char *to,const char *password);
 void get_salt_from_password(unsigned long *res,const char *password);
+void make_password_from_salt(char *to, unsigned long *hash_res);
 char *scramble(char *to,const char *message,const char *password,
 	       my_bool old_ver);
 my_bool check_scramble(const char *, const char *message,
 		       unsigned long *salt,my_bool old_ver);
 char *get_tty_password(char *opt_message);
 void hash_password(unsigned long *result, const char *password);
+#ifdef __cplusplus
+}
+#endif
 
 /* Some other useful functions */
 
@@ -188,13 +230,12 @@ void load_defaults(const char *conf_file, const char **groups,
 
 #define NULL_LENGTH ((unsigned long) ~0) /* For net_store_length */
 
-#ifdef WIN32
+#ifdef __WIN__
 #define socket_errno WSAGetLastError()
+#elif defined(OS2)
+#define socket_errno sock_errno()
 #else
 #define socket_errno errno
 #endif
 
-#ifdef	__cplusplus
-}
-#endif
 #endif
