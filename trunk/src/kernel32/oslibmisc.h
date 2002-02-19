5294277e44b1bd29f1e376991ab7e8da4e6f5ca3
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-02-19 13:08:19 +0000

    country updates

--- trunk/src/kernel32/oslibmisc.h
+++ trunk/src/kernel32/oslibmisc.h
@@ -1,4 +1,4 @@
-/* $Id: oslibmisc.h,v 1.8 2002-02-19 11:49:20 sandervl Exp $ */
+/* $Id: oslibmisc.h,v 1.9 2002-02-19 13:08:19 sandervl Exp $ */
 /*
  * Misc util. procedures
  *
@@ -44,40 +44,47 @@ ULONG OSLibAllocThreadLocalMemory(int nrdwords);
 ULONG OSLibWinInitialize();
 ULONG OSLibWinQueryMsgQueue(ULONG hab);
 
-//#define CC_NONE                        0
-#define CC_USA                         1
-#define CC_CANADA                      2
-#define CC_LATIN_AMERICA               3
-#define CC_RUSSIA                      7
-#define CC_GREECE                     30
-#define CC_NETHERLANDS                31
-#define CC_BELGIUM                    32
-#define CC_FRANCE                     33
-#define CC_SPAIN                      34
-#define CC_ITALY                      39
-#define CC_SWITZERLAND                41
-#define CC_AUSTRIA                    43
-#define CC_UNITED_KINGDOM             44
-#define CC_DENMARK                    45
-#define CC_SWEDEN                     46
-#define CC_NORWAY                     47
-#define CC_POLAND                     48
-#define CC_WEST_GERMANY               49
-#define CC_MEXICO                     52
-#define CC_BRAZIL                     55
-#define CC_AUSTRALIA                  61
-#define CC_NEW_ZEALAND                64
-#define CC_JAPAN                      81
-#define CC_KOREA                      82
-#define CC_CHINA                      86
-#define CC_TAIWAN                     88
-#define CC_TURKEY                     90
-#define CC_PORTUGAL                  351
-#define CC_LUXEMBOURG                352
-#define CC_ICELAND                   354
-#define CC_FINLAND                   358
-#define CC_CZECH_REPUBLIC            421
-#define CC_SLOVAK_REPUBLIC           422
+//#define CTRY_NONE                        0
+#define CTRY_USA                         1
+#define CTRY_CANADA                      2
+#define CTRY_LATIN_AMERICA               3
+#define CTRY_RUSSIA                      7
+#define CTRY_GREECE                     30
+#define CTRY_NETHERLANDS                31
+#define CTRY_BELGIUM                    32
+#define CTRY_FRANCE                     33
+#define CTRY_SPAIN                      34
+#define CTRY_ITALY                      39
+#define CTRY_SWITZERLAND                41
+#define CTRY_AUSTRIA                    43
+#define CTRY_UNITED_KINGDOM             44
+#define CTRY_DENMARK                    45
+#define CTRY_SWEDEN                     46
+#define CTRY_NORWAY                     47
+#define CTRY_POLAND                     48
+#define CTRY_GERMANY                    49
+#define CTRY_MEXICO                     52
+#define CTRY_BRAZIL                     55
+#define CTRY_AUSTRALIA                  61
+#define CTRY_NEW_ZEALAND                64
+#define CTRY_SINGAPORE                  65
+#define CTRY_JAPAN                      81
+#define CTRY_KOREA                      82
+#define CTRY_CHINA                      86
+#define CTRY_TAIWAN                     88
+#define CTRY_TURKEY                     90
+#define CTRY_PORTUGAL                  351
+#define CTRY_LUXEMBOURG                352
+#define CTRY_IRELAND                   353
+#define CTRY_ICELAND                   354
+#define CTRY_FINLAND                   358
+#define CTRY_BULGARIA                  359
+#define CTRY_CROATIA                   385
+#define CTRY_SLOVENIA                  386
+#define CTRY_CZECH_REPUBLIC            421
+#define CTRY_SLOVAK_REPUBLIC           422
+#define CTRY_HONG_KONG                 852
+//#define CTRY_TAIWAN                    886
 
 ULONG OSLibQueryCountry();
 
