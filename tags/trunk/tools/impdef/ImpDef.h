885c1e2ebd8d2ca4b06920b6a0edc196190562d4
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-02-24 02:44:40 +0000

    New kFile* classes.

--- tags/trunk/tools/impdef/ImpDef.h
+++ tags/trunk/tools/impdef/ImpDef.h
@@ -1,4 +1,4 @@
-/* $Id: ImpDef.h,v 1.2 1999-09-08 07:30:10 bird Exp $ */
+/* $Id: ImpDef.h,v 1.3 2002-02-24 02:44:40 bird Exp $ */
 /*
  * ImpDef - Create export file which use internal names and ordinals.
  *
@@ -9,18 +9,18 @@
 #define _ImpDef_h_
     typedef struct _Options
     {
-        BOOL fSimilarToExported;   /* Merges the names of the exported and    *
-                                    * internal function when possible/needed. */
-        BOOL ulOrdStartInternalFunctions;
-                                   /* Ordinal number where internal functions *
-                                    * starts. API functions have lower        *
-                                    * ordinal values that this number.        */
-        BOOL fRemoveOS2APIPrefix;  /* For API exports any OS2 prefix is       *
-                                    * removed (APIs have ordinals less than   *
-                                    * ORD_START_INTERNAL_FUNCTIONS).          */
-        BOOL fInternalFnExportStdCallsIntName;
-                                   /* Fix! Export internal names for internal *
-                                    * stdcall functions.                      */
+        KBOOL fSimilarToExported;    /* Merges the names of the exported and    *
+                                      * internal function when possible/needed. */
+        KBOOL ulOrdStartInternalFunctions;
+                                     /* Ordinal number where internal functions *
+                                      * starts. API functions have lower        *
+                                      * ordinal values that this number.        */
+        KBOOL fRemoveOS2APIPrefix;   /* For API exports any OS2 prefix is       *
+                                      * removed (APIs have ordinals less than   *
+                                      * ORD_START_INTERNAL_FUNCTIONS).          */
+        KBOOL fInternalFnExportStdCallsIntName;
+                                     /* Fix! Export internal names for internal *
+                                      * stdcall functions.                      */
     } OPTIONS, *POPTIONS;
 #endif
 
