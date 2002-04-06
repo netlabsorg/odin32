5c598abb281969a8a6594d18e3d679a66481af2c
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-04-06 20:25:06 +0000

    Moved from tools\make.

---
+++ trunk/make/setup.os2prfvac365.mk
@@ -0,0 +1,6 @@
+# $Id: setup.os2prfvac365.mk,v 1.1 2002-04-06 20:25:05 bird Exp $
+
+# ---OS2, PROFILE, VAC365-------------------------
+ENV_NAME="OS/2, Profile, IBM VisualAge for C++ 3.6.5"
+# ---OS2, PROFILE, VAC365-------------------------
+
