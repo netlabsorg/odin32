81448d1c682f67788d5b520cf0d24a95f822e22f
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-04-30 14:54:07 +0000

    Implemented WH_MOUSE_LL hook; removed DInput hack for keyboard & mouse message translation

--- tags/trunk/src/user32/winmouse.cpp
+++ tags/trunk/src/user32/winmouse.cpp
@@ -1,4 +1,4 @@
-/* $Id: winmouse.cpp,v 1.22 2002-02-11 16:06:00 sandervl Exp $ */
+/* $Id: winmouse.cpp,v 1.23 2002-04-30 14:54:06 sandervl Exp $ */
 /*
  * Win32 mouse functions
  *
@@ -48,88 +48,6 @@ ODINDEBUGCHANNEL(USER32-WINMOUSE)
  * local variables
  ****************************************************************************/
 
-LPMOUSE_EVENT_PROC mouseHandler = NULL;
-WNDPROC            keyboardHandler = NULL;
-
-
-//******************************************************************************
-//******************************************************************************
-VOID WIN32API MOUSE_Enable(LPMOUSE_EVENT_PROC lpMouseEventProc)
-{
-  if(lpMouseEventProc == (LPMOUSE_EVENT_PROC)-1)
-    mouseHandler = NULL;
-  else	
-    mouseHandler = lpMouseEventProc;
-}
-//******************************************************************************
-//******************************************************************************
-VOID WIN32API KEYBOARD_Enable(WNDPROC handler)
-{
-  keyboardHandler = handler;
-}
-//******************************************************************************
-//******************************************************************************
-BOOL DInputKeyBoardHandler(MSG *msg)
-{
-  if(!ISKDB_CAPTURED())
-  	return FALSE;
-
-  return keyboardHandler(msg->hwnd, msg->message, msg->wParam, msg->lParam);
-}
-//******************************************************************************
-//******************************************************************************
-BOOL DInputMouseHandler(HWND hwnd, ULONG msg, ULONG x, ULONG y)
-{
- WINE_MOUSEEVENT mouseEvent;
- DWORD dwFlags = MOUSEEVENTF_ABSOLUTE;
-
-  if(!ISMOUSE_CAPTURED())
-	return FALSE;
-
-  mouseEvent.magic = WINE_MOUSEEVENT_MAGIC;
-  mouseEvent.hWnd  = hwnd;
-  mouseEvent.time  = OSLibWinQueryMsgTime();
-  mouseEvent.keyState = 0; //not used in dinput right now
-  switch(msg) 
-  {
-  case WM_NCLBUTTONDOWN:
-  case WM_LBUTTONDOWN:
-	dwFlags |= MOUSEEVENTF_LEFTDOWN;
-        break;
-  case WM_NCLBUTTONUP:
-  case WM_LBUTTONUP:
-	dwFlags |= MOUSEEVENTF_LEFTUP;
-        break;
-  case WM_NCRBUTTONUP:
-  case WM_RBUTTONUP:
-	dwFlags |= MOUSEEVENTF_RIGHTUP;
-        break;
-  case WM_NCRBUTTONDOWN:
-  case WM_RBUTTONDOWN:
-	dwFlags |= MOUSEEVENTF_RIGHTDOWN;
-        break;
-  case WM_NCMBUTTONUP:
-  case WM_MBUTTONUP:
-	dwFlags |= MOUSEEVENTF_MIDDLEUP;
-        break;
-  case WM_NCMBUTTONDOWN:
-  case WM_MBUTTONDOWN:
-	dwFlags |= MOUSEEVENTF_MIDDLEDOWN;
-        break;
-  case WM_MOUSEMOVE:
-  case WM_NCMOUSEMOVE:
-	dwFlags |= MOUSEEVENTF_MOVE;
- 	break;
-  default:
-	//TODO: handle double clicks???
-	return FALSE;
-  }
-
-  x = (((long)x << 16) + ScreenWidth-1)  / ScreenWidth;
-  y = (((long)y << 16) + ScreenHeight-1) / ScreenHeight;
-
-  return mouseHandler(dwFlags, x, y, 0, (DWORD)&mouseEvent);
-}
 //******************************************************************************
 //******************************************************************************
 
@@ -289,6 +207,7 @@ VOID WIN32API mouse_event(DWORD dwFlags, DWORD dx, DWORD dy, DWORD cButtons,
  *             0 if the input was already blocked by another thread
  * Remark    :
  * Status    : UNTESTED STUB
+ *             TODO: call hooks!!
  *
  * Author    : Patrick Haller [Thu, 1998/02/26 11:55]
  *****************************************************************************/
