ae925ac7eb094d4f28f32e5126bca6a515f8ad5f
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-04-30 19:44:31 +0000

    Configured 16-bit watcom.

---
+++ trunk/make/setup.os2debwat11-16.mk
@@ -0,0 +1,104 @@
+# $Id: setup.os2debwat11-16.mk,v 1.1 2002-04-30 19:44:31 bird Exp $
+
+# ---OS2, DEBUG, WAT11-------------------------
+ENV_NAME="OS/2, Debug, Watcom C/C++ v11.0c 16-bit"
+ENV_STATUS=OK
+!if "$(ENV_ENVS)" == ""
+ENV_ENVS=vac308 watcomc11c-16
+!else
+ENV_ENVS_FORCE=vac308 watcomc11c-16
+!endif
+ENV_16BIT = 16
+
+
+#
+# Include some shared standard stuff: ALP, VAC optional stuff.
+#
+!include $(PATH_MAKE)\setup.os2debalp.mk
+!include $(PATH_MAKE)\setup.optional.watcom11x.mk
+
+
+#
+# The tools
+#
+AR=ilib.exe
+CC=wcc.exe
+CXX=wpp.exe
+LINK=wlink.exe
+IMPLIB=implib.exe
+RC=rc.exe
+RL=rc.exe
+EXEPACK=lxlite.exe
+
+
+#
+# The flags
+#
+AR_FLAGS=/nologo /noignorecase
+AR_CMD=$(AR) $(AR_FLAGS) $@ @$(TARGET_LNK)
+_AR_LNK1= "$(TARGET_OBJS: ="&^
+")"
+AR_LNK1= $(_AR_LNK1:""=)
+AR_LNK2= $(@R).lst
+
+_CC_FLAGS_OS =
+
+CC_FLAGS=-bt=os2 -dOS2 -dDEBUG -d__16BIT__ -5 -zq -bm -ze -w4 -d2 -hc -zc $(_CC_OPTIONAL) $(CC_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CC_INCLUDES:-I=-i=) $(ALL_INCLUDES:-I=-i=) -i=$(PATH_INCLUDES) -i=$(WATCOM)\h
+CC_FLAGS_EXE=$(CC_FLAGS)
+CC_FLAGS_DLL=$(CC_FLAGS) -bd
+CC_FLAGS_SYS=$(CC_FLAGS) -s -zfp -zgp -zu
+CC_FLAGS_VDD=$(CC_FLAGS_SYS)
+CC_FLAGS_IFS=$(CC_FLAGS_SYS) -bd
+CC_OBJ_OUT=-fo=
+CC_LST_OUT=
+CC_PC_2_STDOUT=-pc
+
+CXX_FLAGS=-bt=os2 -dOS2 -dDEBUG -d__16BIT__ -5 -zq -bm -ze -w4 -d2 -hc -zc (_CXX_OPTIONAL)  $(CXX_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CXX_INCLUDES:-I=-i=) $(ALL_INCLUDES:-I=-i=) -i=$(PATH_INCLUDES) -i=$(WATCOM)\h
+CXX_FLAGS_EXE=$(CXX_FLAGS)
+CXX_FLAGS_DLL=$(CXX_FLAGS) -bd
+CXX_FLAGS_SYS=$(CXX_FLAGS) -s -zfp -zgp -zu
+CXX_FLAGS_VDD=$(CXX_FLAGS_SYS)
+CXX_FLAGS_IFS=$(CXX_FLAGS_SYS) -bd
+CXX_OBJ_OUT=-fo=
+CXX_LST_OUT=
+CXX_PC_2_STDOUT=-pc
+
+IMPLIB_FLAGS=/NOI /Nologo
+
+LINK_FLAGS=format os2 lx pmcompatible option quiet, stack=$(TARGET_STACKSIZE)
+LINK_FLAGS_EXE=$(LINK_FLAGS)
+LINK_FLAGS_DLL=$(LINK_FLAGS)
+LINK_FLAGS_SYS=format os2 physdevice option int, dosseg, map, eliminate, mang, tog sort global d codeview
+LINK_FLAGS_VDD=format os2 virtdevice option int, dosseg, map, eliminate, mang, tog sort global d codeview
+LINK_FLAGS_IFS=$(LINK_FLAGS_DLL)
+LINK_CMD_EXE=$(LINK) $(LINK_FLAGS_EXE) @$(TARGET_LNK)
+LINK_CMD_DLL=$(LINK) $(LINK_FLAGS_DLL) @$(TARGET_LNK)
+LINK_CMD_SYS=$(LINK) $(LINK_FLAGS_SYS) @$(TARGET_LNK)
+LINK_CMD_VDD=$(LINK) $(LINK_FLAGS_VDD) @$(TARGET_LNK)
+LINK_CMD_IFS=$(LINK) $(LINK_FLAGS_IFS) @$(TARGET_LNK)
+LINK_LNK1=file $(TARGET_OBJS: =^
+file )
+LINK_LNK2=libpath $(WATCOM)\lib286;$(WATCOM)\lib286\os2;
+LINK_LNK3=option map=$(TARGET_MAP)
+LINK_LNK4=library $(TARGET_LIBS: =^, )
+LINK_LNK5=name $(PATH_TARGET)\$(TARGET_NAME).$(TARGET_EXT)
+
+RC_FLAGS=-r -n -i $(PATH_INCLUDES:;= -i ) $(RC_DEFINES) $(RC_INCLUDES)
+RL_FLAGS=-x2 -n
+
+
+#
+# Libraries and object files.
+# TODO - DLL and RTDLL doesn't work.
+#        Probably needing code to be compiled with far code. I.e. MODEL hugh, large or medium.
+#
+LIB_OS      = os2286.lib
+LIB_C_OBJ   = clib$(_OBJ_MODEL).lib
+LIB_C_DLL   = clib$(_OBJ_MODEL).lib # clibdll.lib
+LIB_C_RTDLL = clib$(_OBJ_MODEL).lib # clibdll.lib
+LIB_C_NRE   = clib$(_OBJ_MODEL).lib
+LIB_C_DMNGL =
+OBJ_PROFILE =
+
+# ---OS2, DEBUG, WAT11-------------------------
+
