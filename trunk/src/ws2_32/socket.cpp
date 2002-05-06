11e30332de2b492a52a51fe606382107ab6cde26
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-05-06 09:38:52 +0000

    added WSAAccept

--- trunk/src/ws2_32/socket.cpp
+++ trunk/src/ws2_32/socket.cpp
@@ -1,4 +1,4 @@
-/* $Id: socket.cpp,v 1.12 2002-03-08 11:36:11 sandervl Exp $ */
+/* $Id: socket.cpp,v 1.13 2002-05-06 09:38:52 sandervl Exp $ */
 /*
  * based on Windows Sockets 1.1 specs
  * (ftp.microsoft.com:/Advsys/winsock/spec11/WINSOCK.TXT)
@@ -103,6 +103,65 @@ ODINFUNCTION6(SOCKET, WSASocketW,
          af, type, protocol, lpProtocolInfo, g, dwFlags );
 
    return ( socket (af, type, protocol) );
+}
+/***********************************************************************
+ *              WSAAccept                        (WS2_32.26)
+ */
+SOCKET WINAPI WSAAccept( SOCKET s, struct WS_sockaddr *addr, LPINT addrlen,
+               LPCONDITIONPROC lpfnCondition, DWORD dwCallbackData)
+{
+
+       int ret = 0, size = 0;
+       WSABUF CallerId, CallerData, CalleeId, CalleeData;
+       /*        QOS SQOS, GQOS; */
+       GROUP g;
+       SOCKET cs;
+       SOCKADDR src_addr, dst_addr;
+
+       TRACE("Socket  %u, sockaddr %p, addrlen %p, fnCondition %p, dwCallbackData %ld\n",
+               s, addr, addrlen, lpfnCondition, dwCallbackData);
+
+       
+       size = sizeof(src_addr);
+       cs = accept(s, &src_addr, &size);
+       
+       if (cs == SOCKET_ERROR) return SOCKET_ERROR;
+
+       CallerId.buf = (char *)&src_addr;
+       CallerId.len = sizeof(src_addr);  
+
+       CallerData.buf = NULL;
+       CallerData.len = (ULONG)NULL;   
+
+       getsockname(cs, &dst_addr, &size);
+
+       CalleeId.buf = (char *)&dst_addr;
+       CalleeId.len = sizeof(dst_addr);  
+       
+       ret = (*lpfnCondition)(&CallerId, &CallerData, NULL, NULL,
+                       &CalleeId, &CalleeData, &g, dwCallbackData); 
+
+       switch (ret)
+       {
+               case CF_ACCEPT:
+                       if (addr && addrlen)
+                               addr = (struct WS_sockaddr *)memcpy(addr, &src_addr, (*addrlen > size) ?  size : *addrlen );
+                       return cs;
+               case CF_DEFER:
+                       SetLastError(WSATRY_AGAIN);
+                       return SOCKET_ERROR;
+               case CF_REJECT:
+                       closesocket(cs);
+                       SetLastError(WSAECONNREFUSED);
+                       return SOCKET_ERROR;
+               default:
+                       FIXME("Unknown return type from Condition function\n");
+                       SetLastError(WSAENOTSOCK);
+                       return SOCKET_ERROR;
+               }
+
+       SetLastError(WSAENOTSOCK);
+       return SOCKET_ERROR;
 }
 //******************************************************************************
 //******************************************************************************
