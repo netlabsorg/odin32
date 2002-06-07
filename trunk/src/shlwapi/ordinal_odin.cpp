7484d2f5083ad2d78069b0e46bc5fefb617c9fa6
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-06-07 08:02:20 +0000

    resynced with latest Wine

--- trunk/src/shlwapi/ordinal_odin.cpp
+++ trunk/src/shlwapi/ordinal_odin.cpp
@@ -1,4 +1,4 @@
-/* $Id: ordinal_odin.cpp,v 1.5 2002-02-06 20:18:27 sandervl Exp $ */
+/* $Id: ordinal_odin.cpp,v 1.6 2002-06-07 08:02:17 sandervl Exp $ */
 
 /*
  * Win32 Lightweight SHELL32 for OS/2
@@ -54,7 +54,6 @@
 #include "win/wine/obj_base.h"
 #include "shellapi.h"
 #include "shlobj.h"
-#include "wine/undocshell.h"
 
 #include "shlwapi.h"
 #include "shlwapi_odin.h"
@@ -64,341 +63,6 @@
  * Local Variables                                                           *
  *****************************************************************************/
 
-ODINDEBUGCHANNEL(SHLWAPI-ORDINAL)
-
-
-#if 0
-/*****************************************************************************
- * Prototypes                                                                *
- *****************************************************************************/
-
-HANDLE WIN32API SHLWAPI_11(HANDLE, DWORD, DWORD, DWORD, DWORD);
-
-
-
-/*****************************************************************************
- * Name      : ???
- * Purpose   : Unknown (used by explorer.exe)
- * Parameters: Unknown (wrong)
- * Variables :
- * Result    : Unknown
- * Remark    :
- * Status    : UNTESTED STUB
- *
- * Author    : Christoph Bratschi [Wed, 2000/03/29 19:47]
- *****************************************************************************/
-
-// return characters in protocol part
-static INT extractProtocolFromURL(LPSTR  pszURL,
-                                  LPSTR* ppszColon)
-{
-  int i = 0;  // number of characters scanned
-  LPSTR s = pszURL;
-
-  *ppszColon = NULL;
-
-  if (*pszURL == 0)
-    return 0;
-
-
-  // scan through the string for the 1st colon
-  while(*s)
-  {
-    // filter non-printable characters
-    if (*s < ' ')
-      return 0;
-    else
-      if (*s > 0x80)
-        return 0;     
-
-    // Note: actually, the original code has a table with
-    // "forbidden" characters, we don't reproduce this here yet.
-
-    if (*s == ':')
-    {
-       // yes, first colon found!
-       if ( (pszURL[0] == 'U' || pszURL[0] == 'u') &&
-            (pszURL[1] == 'R' || pszURL[1] == 'r') &&
-            (pszURL[2] == 'L' || pszURL[2] == 'l') )
-       {
-         // restart scan!
-         pszURL = s+1;
-         i = 0;
-       }
-       else
-       {
-         // OK, protocol extracted
-         *ppszColon = s;
-         return i;
-       }
-    }
-
-    // skip to next character
-    i++;
-    s++;
-  }  
-
-  return 0;
-}
-
-typedef struct tabProtocolTableEntry
-{
-  LPSTR  pszName; // Note: original is unicode
-  DWORD  ID;
-  DWORD  dwNameLength;
-  DWORD  dwCharacteristic;
-} PROTOCOLTABLEENTRY, *LPPROTOCOLTABLEENTRY;
-
-
-#define PROTOCOL_HTTP 2
-#define PROTOCOL_FILE 9
-
-static PROTOCOLTABLEENTRY tabProtocolTable[] =
-{
-  { "http",        PROTOCOL_HTTP,  4, 0x0a },
-  { "file",        PROTOCOL_FILE,  4, 0x08 },
-  { "ftp",         1,  3, 0x0a },
-  { "https",      11,  5, 0x0a },
-  { "news",        5,  4, 0x0a },
-  { "mailto",      0,  6, 0x01 }, 
-  { "gopher",      3,  6, 0x0a },
-  { "nntp",        6,  4, 0x0a },
-  { "telnet",      7,  6, 0x0a },
-  { "wais",        8,  4, 0x00 },
-  { "mk",         10,  2, 0x04 },
-  { "shell",      12,  5, 0x01 },
-  { "local",      14,  5, 0x00 },
-  { "javascript", 15, 10, 0x05 },
-  { "vbscript",   16,  8, 0x05 },
-  { "snews",      13,  5, 0x0a },
-  { "about",      17,  5, 0x05 },
-  { "res",        18,  3, 0x04 }
-};
-#define END_OF_PROTOCOLTABLE (tabProtocolTable + (sizeof(tabProtocolTable) / sizeof(PROTOCOLTABLEENTRY)) )
-
-
-
-static DWORD getProtocolTableEntry(LPSTR pszProtocol, DWORD dwNameLength)
-{
-  LPPROTOCOLTABLEENTRY lpEntry = tabProtocolTable;
-
-  for(;;)
-  {
-    if (lpEntry->dwNameLength == dwNameLength)
-      if (lstrncmpiA( lpEntry->pszName, pszProtocol, dwNameLength) == 0)
-      {
-        return lpEntry->ID; 
-      }
-
-    lpEntry++;
-
-    // if scanning beyond end of the table,
-    // abort and return null
-    if (lpEntry > END_OF_PROTOCOLTABLE)
-      return 0;
-  }
-}
-
-typedef struct tagProtocolHandlerA
-{
-  DWORD  dwSize;
-  DWORD  dwProtocolNameLength;
-  LPSTR  lpszProtocolName;
-  LPSTR  lpszURL;
-  DWORD  dwURLNameLength;
-  DWORD  ProtocolID;
-} PROTOCOLHANDLERA, *LPPROTOCOLHANDLERA;
-
-typedef struct tagProtocolHandlerW
-{
-  DWORD  dwSize;
-  DWORD  dwProtocolNameLength;
-  LPWSTR lpszProtocolName;
-  LPWSTR lpszURL;
-  DWORD  dwURLNameLength;
-  DWORD  ProtocolID;
-} PROTOCOLHANDLERW, *LPPROTOCOLHANDLERW;
-
-
-ODINFUNCTION2(DWORD,   SHLWAPI_1,
-              LPSTR,   lpszURL,
-              LPPROTOCOLHANDLERA, lpHandler)
-{
-  dprintf(("not implemented"));
-
-  if (NULL == lpszURL)
-    return E_INVALIDARG;
-
-  if (NULL == lpHandler)
-    return E_INVALIDARG;
-
-  if (lpHandler->dwSize != sizeof( PROTOCOLHANDLERA) )
-    return E_INVALIDARG;
-
-  dprintf(("SHLWAPI-SHLWAPI1: URL=%s",
-    lpszURL));
-  
-  LPSTR lpszColon;
-  INT iProtocolLength = extractProtocolFromURL(lpszURL,
-                                               &lpszColon);
-  lpHandler->dwProtocolNameLength = iProtocolLength;
-  if (0 == iProtocolLength)
-    return 0x80041001; // unknown error constant
-
-  lpHandler->lpszProtocolName = lpszURL;
-
-  DWORD ID = getProtocolTableEntry(lpszURL,
-                                   iProtocolLength);
-  lpHandler->ProtocolID = ID;
-  lpHandler->lpszURL    = (LPSTR)(lpszURL + iProtocolLength + 1);
-  
-  if (ID == PROTOCOL_FILE)
-  {
-    // cut off leading slashes as required
-    if (lpHandler->lpszURL[0] == '/' &&
-        lpHandler->lpszURL[1] == '/')
-      lpHandler->lpszURL = lpHandler->lpszURL + 2;
-
-    if (lpHandler->lpszURL[0] == '/')
-      lpHandler->lpszURL = lpHandler->lpszURL + 1;
-  }
-
-  lpHandler->dwURLNameLength = lstrlenA(lpHandler->lpszURL);
-
-  return NO_ERROR;
-}
-#endif
-
-
-
-/*****************************************************************************
- * Name      : SHLWAPI_3
- * Purpose   :
- * Parameters:
- * Variables :
- * Result    :
- * Remark    :
- * Status    : STUB UNTESTED
- *
- * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
- *****************************************************************************/
-
-ODINFUNCTION2(BOOL,  SHLWAPI_3,
-              LPSTR, lpPath,
-              LPSTR, lpFile)
-{
-  dprintf(("not properly implemented"));
-
-  return TRUE;
-
-#if 0
-  if (lpPath == NULL)                return FALSE;
-  if (PathIsUNCServerA(lpPath))      return FALSE;
-  if (PathIsUNCServerShareA(lpPath)) return FALSE;
-
-  if (lpFile == NULL)
-    return PathFileExistsA(lpPath);
-  else
-  {
-    // PathFindExtensionA
-    // lstrlenA
-    // lstrcpyA
-    // PathFileExistsA
-    return FALSE;
-  }
-
-  return FALSE;
-#endif
-}
-
-
-/*****************************************************************************
- * Name      : SHLWAPI_4
- * Purpose   :
- * Parameters:
- * Variables :
- * Result    :
- * Remark    :
- * Status    : STUB UNTESTED
- *
- * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
- *****************************************************************************/
-
-ODINFUNCTION2(BOOL,   SHLWAPI_4,
-              LPWSTR, lpPath,
-              LPWSTR, lpFile)
-{
-  dprintf(("not properly implemented"));
-
-  return TRUE;
-
-#if 0
-  if (lpPath == NULL)                return FALSE;
-  if (PathIsUNCServerW(lpPath))      return FALSE;
-  if (PathIsUNCServerShareW(lpPath)) return FALSE;
-
-  if (lpFile == NULL)
-    return PathFileExtensionW(lpPath);
-  else
-  {
-    // PathFindExtensionW
-    // lstrlenW
-    // lstrcpyW
-    // PathFileExistsW
-    return FALSE;
-  }
-
-  return FALSE;
-#endif
-}
-
-
-/*****************************************************************************
- * Name      : ???
- * Purpose   : extended version of PathFindOnPathA
- * Parameters: Unknown (wrong)
- * Variables :
- * Result    : Unknown
- * Remark    :
- * Status    : UNTESTED STUB
- *
- * Author    : Patrick Haller [Sun, 2000/06/09 04:47]
- *****************************************************************************/
-
-ODINFUNCTION3(DWORD, SHLWAPI_5,
-              LPSTR, lpFile,
-              LPSTR, lpDirectories,
-              DWORD, dwUnknown)
-
-{
-  dprintf(("not implemented"));
-
-  return 0;
-}
-
-
-/*****************************************************************************
- * Name      : ???
- * Purpose   : extended version of PathFindOnPathA
- * Parameters: Unknown (wrong)
- * Variables :
- * Result    : Unknown
- * Remark    :
- * Status    : UNTESTED STUB
- *
- * Author    : Patrick Haller [Sun, 2000/06/09 04:47]
- *****************************************************************************/
-
-ODINFUNCTION3(DWORD, SHLWAPI_6,
-              DWORD, arg0,
-              DWORD, arg1,
-              DWORD, arg2)
-
-{
-  dprintf(("not implemented"));
-
-  return 0;
-}
 
 
 
@@ -416,9 +80,7 @@ ODINFUNCTION3(DWORD, SHLWAPI_6,
  * Author    : Patrick Haller [Sun, 2000/06/09 04:47]
  *****************************************************************************/
 
-ODINFUNCTION2(DWORD,SHLWAPI_12,
-              DWORD,arg0,
-              DWORD,arg1)
+DWORD WIN32API SHLWAPI_12(DWORD arg0, DWORD arg1)
 {
   dprintf(("not implemented, explorer.exe will trap now"));
 
@@ -440,9 +102,7 @@ ODINFUNCTION2(DWORD,SHLWAPI_12,
  * Author    : Patrick Haller [Sun, 2000/06/09 04:47]
  *****************************************************************************/
 
-ODINFUNCTION2(DWORD,SHLWAPI_17,
-              DWORD, arg0,
-              DWORD, arg1)
+DWORD WIN32API SHLWAPI_17(DWORD arg0, DWORD arg1)
 {
   dprintf(("not implemented, explorer.exe will trap now"));
 
@@ -464,9 +124,7 @@ ODINFUNCTION2(DWORD,SHLWAPI_17,
  * Author    : Patrick Haller [Sun, 2000/06/09 04:47]
  *****************************************************************************/
 
-ODINFUNCTION2(DWORD,SHLWAPI_20,
-              DWORD, arg0,
-              DWORD, arg1)
+DWORD WIN32API SHLWAPI_20(DWORD arg0, DWORD arg1)
 {
   dprintf(("not implemented, explorer.exe will trap now"));
 
@@ -492,9 +150,7 @@ ODINFUNCTION2(DWORD,SHLWAPI_20,
  * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
  *****************************************************************************/
 
-ODINFUNCTION2(DWORD,  SHLWAPI_160,
-              DWORD,  arg0,
-              DWORD,  arg1)
+DWORD WIN32API SHLWAPI_160(DWORD arg0, DWORD arg1)
 {
   dprintf(("not implemented.\n"));
   return 0;
@@ -516,13 +172,8 @@ ODINFUNCTION2(DWORD,  SHLWAPI_160,
  * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
  *****************************************************************************/
 
-ODINFUNCTION6(DWORD,   SHLWAPI_185,
-              DWORD, arg0,
-              LPSTR, lpStr1,
-              LPSTR, lpStr2,
-              DWORD, arg3,
-              DWORD, dwDefault,
-              LPSTR, lpstrValueName)
+DWORD WIN32API SHLWAPI_185(DWORD arg0, LPSTR lpStr1, LPSTR lpStr2,
+                           DWORD arg3, DWORD dwDefault, LPSTR lpstrValueName)
 {
   BOOL  fDontShow;
   WCHAR szValueNameW[256];
@@ -586,13 +237,8 @@ ODINFUNCTION6(DWORD,   SHLWAPI_185,
  * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
  *****************************************************************************/
 
-ODINFUNCTION6(DWORD,  SHLWAPI_191,
-              HWND,   hwndParent,
-              DWORD,  arg1,
-              DWORD,  arg2,
-              DWORD,  arg3,
-              DWORD,  dwDefault,
-              LPWSTR, lpstrDialog)
+DWORD WIN32API SHLWAPI_191(HWND hwndParent, DWORD arg1, DWORD arg2,
+                           DWORD arg3, DWORD dwDefault, LPWSTR lpstrDialog)
 {
   BOOL rc = SHRegGetBoolUSValueW((LPCWSTR)L"Software\\Microsoft\\Windows\\CurrentVersion"
                                  L"\\Explorer\\DontShowMeThisDialogAgain",
@@ -643,10 +289,7 @@ ODINFUNCTION6(DWORD,  SHLWAPI_191,
  * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
  *****************************************************************************/
 
-ODINFUNCTION3(BOOL,         SHLWAPI_197,
-              HDC,          hdc,
-              CONST RECT*,  lprc,
-              COLORREF,     crColor)
+BOOL WIN32API SHLWAPI_197(HDC hdc, CONST RECT*lprc, COLORREF crColor)
 {
   COLORREF crOld;
   BOOL     res;
@@ -679,12 +322,8 @@ ODINFUNCTION3(BOOL,         SHLWAPI_197,
  * Author    : Patrick Haller [Sun, 2000/06/10 04:02]
  *****************************************************************************/
 
-ODINFUNCTION5(DWORD, SHLWAPI_243,
-              DWORD, arg0,
-              DWORD, arg1,
-              DWORD, arg2,
-              DWORD, arg3,
-              DWORD, arg4)
+DWORD WIN32API SHLWAPI_243(DWORD arg0, DWORD arg1, DWORD arg2, DWORD arg3,
+                           DWORD arg4)
 {
   dprintf(("not implementes.\n"));
   return 0;
@@ -707,8 +346,7 @@ ODINFUNCTION5(DWORD, SHLWAPI_243,
 #define REG_OPTIONS_ENTERPRISE  "Enterprise"
 #define REG_OPTIONS_DATACENTER  "DataCenter"
 
-ODINFUNCTION1(DWORD,  SHLWAPI_437,
-              DWORD,  nFunction)
+DWORD WIN32API SHLWAPI_437(DWORD  nFunction)
 {
   static BOOL           flagProductOptionsInitialized = FALSE;
   static BOOL           flagIsProductEnterprise       = FALSE;
@@ -866,46 +504,4 @@ ODINFUNCTION1(DWORD,  SHLWAPI_437,
   return 0;
 }
 
-
-/*****************************************************************************
- * Name      : SHIsLowMemoryMachine
- * Purpose   :
- * Parameters: BOOL fRetest - TRUE if testing shall be repeated
- *                          - FALSE if cached result is to be used
- * Variables :
- * Result    : 0 - machine is not memory-constrained
- *             1 - machine is memory-constrained
- * Remark    : SHLWAPI.584
- * Status    : COMPLETELY IMPLEMENTED ? UNTESTED
- *
- * Author    : Patrick Haller [Mon, 2000/06/11 02:02]
- *****************************************************************************/
-
-ODINFUNCTION1(int,     SHIsLowMemoryMachine,
-              BOOL,    fRetest)
-{
-         MEMORYSTATUS memBuffer;
-  static int          flagIsLowMemoryMachine = -1;
-         ULONG        ulMem;
-
-  // use cached result?
-  if ( (fRetest == TRUE) ||
-       (flagIsLowMemoryMachine == -1) )
-  {
-    // determine and store result
-    GlobalMemoryStatus(&memBuffer);
-    if (VERSION_OsIsUnicode())
-      ulMem = 0x1000000; // unicode operation mode
-    else
-      ulMem = 0x0800000; // ascii operation mode
-
-    // enough memory?
-    if (memBuffer.dwTotalPhys <= ulMem)
-      flagIsLowMemoryMachine = 1;
-    else
-      flagIsLowMemoryMachine = 0;
-  }
-
-  return flagIsLowMemoryMachine;
-}
 
