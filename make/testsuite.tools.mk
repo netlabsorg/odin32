e27de08024a5f7054aa9419314019a4623388019
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-06-19 02:17:07 +0000

    Added some more tools.

--- make/testsuite.tools.mk
+++ make/testsuite.tools.mk
@@ -1,4 +1,4 @@
-# $Id: testsuite.tools.mk,v 1.1 2002-06-19 01:00:53 bird Exp $
+# $Id: testsuite.tools.mk,v 1.2 2002-06-19 02:17:07 bird Exp $
 
 #
 # Tools for the testsuite.
@@ -14,7 +14,11 @@ TS_PEC          = pec.exe
 TS_TEST         = test.exe
 TS_TEST_FILE_Z  = $(TS_TEST) ! -s
 TS_TEST_FILE_NZ = $(TS_TEST) -s
+TS_TEST_DIR     = $(TS_TEST) -d
 TS_SED          = sed.exe
 TS_CMP          = cmp.exe
 # UNIX or 4OS2 will not be used in append mode so no '-' vs. '/' problems.
 TS_TEE          = tee
+
+# dir (files only)
+TS_DIR_B        = dir /B /ON
