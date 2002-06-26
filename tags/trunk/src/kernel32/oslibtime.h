bd49ee7cb79b1f9c2eeb0145dec01fefbcb7ee0c
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-06-26 07:14:18 +0000

    debug updates + added OSLibDosGetTickCount

---
+++ tags/trunk/src/kernel32/oslibtime.h
@@ -0,0 +1,15 @@
+/* $Id: oslibtime.h,v 1.1 2002-06-26 07:14:18 sandervl Exp $ */
+/*
+ * OS/2 time procedures
+ *
+ * Copyright 2002 Sander van Leeuwen (sandervl@xs4all.nl)
+ *
+ * Project Odin Software License can be found in LICENSE.TXT
+ *
+ */
+#ifndef __OSLIBTIME_H__
+#define __OSLIBTIME_H__
+
+ULONG OSLibDosGetTickCount();
+
+#endif //__OSLIBTIME_H__
