3a073f4d0796a4bb715a1426053b2af0f310bb43
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-08-15 10:14:30 +0000

    added

---
+++ src/user32/user32api.h
@@ -0,0 +1,9 @@
+/* $Id: user32api.h,v 1.1 2002-08-15 10:14:30 sandervl Exp $ */
+//Internal user32 functions
+
+#ifndef __USER32API_H_
+#define __USER32API_H_
+
+HWND WINAPI GetAncestor( HWND hwnd, UINT type );
+
+#endif
