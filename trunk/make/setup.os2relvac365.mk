5cfc687bce22355f02519cf29cc4a05544a96145
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-08-24 04:56:14 +0000

    Removed /Gi+ and /Gf+ not secure optimizations. Removed /Optfunc as it removed public names without removing the code; which is just confusing. Include wrc setup.

--- trunk/make/setup.os2relvac365.mk
+++ trunk/make/setup.os2relvac365.mk
@@ -1,4 +1,4 @@
-# $Id: setup.os2relvac365.mk,v 1.12 2002-08-20 21:20:10 bird Exp $
+# $Id: setup.os2relvac365.mk,v 1.13 2002-08-24 04:56:14 bird Exp $
 
 # ---OS2, RELEASE, VAC365-------------------------
 ENV_NAME="OS/2, Release, IBM VisualAge for C++ 3.6.5"
@@ -15,6 +15,7 @@ ENV_ENVS_FORCE=vac365
 # Include some shared standard stuff: ALP, VAC optional stuff.
 #
 !include $(PATH_MAKE)\setup.os2relalp.mk
+!include $(PATH_MAKE)\setup.os2relwrc.mk
 !include $(PATH_MAKE)\setup.optional.vac3xx.mk
 
 
@@ -45,7 +46,7 @@ AR_LNK3= $(_AR_LNK3:+""&^
 AR_LNK4= "$(@R).lst";
 
 
-CC_FLAGS=/Q /DOS2 /D__32BIT__ /D__i386__ /DMODEL=FLAT /Ti+ /O+ /G5 /qtune=pentium2 /Gf+ /Gi+ /Ss+ /C+ $(_CC_OPTIONAL) $(CC_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CC_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
+CC_FLAGS=/Q /DOS2 /D__32BIT__ /D__i386__ /DMODEL=FLAT /Ti+ /O+ /G5 /qtune=pentium2 /Ss+ /C+ $(_CC_OPTIONAL) $(CC_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CC_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
 CC_FLAGS_EXE=$(CC_FLAGS) /Gm+ /Ge+
 CC_FLAGS_DLL=$(CC_FLAGS) /Gm+ /Ge-
 CC_FLAGS_SYS=$(CC_FLAGS) /Rn  /Ge+ /DRING0 /Gr+ /Gs- /Wall+ppt-ppc-inl-cnv-gnr-vft-gen-uni-ext-
@@ -55,7 +56,7 @@ CC_OBJ_OUT=/Fo
 CC_LST_OUT=/Fa
 CC_PC_2_STDOUT=/Pd+ /P+
 
-CXX_FLAGS=/Q /DOS2 /D__i386__ /DMODEL=FLAT /Ti+ /O+ /G5 /qtune=pentium2 /Gf+ /Gi+ /Ss+ /C+ $(_CXX_OPTIONAL) $(CXX_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CXX_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
+CXX_FLAGS=/Q /DOS2 /D__i386__ /DMODEL=FLAT /Ti+ /O+ /G5 /qtune=pentium2 /Ss+ /C+ $(_CXX_OPTIONAL) $(CXX_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CXX_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
 CXX_FLAGS_EXE=$(CXX_FLAGS) /Gm+ /Ge+
 CXX_FLAGS_DLL=$(CXX_FLAGS) /Gm+ /Ge-
 CXX_FLAGS_SYS=$(CXX_FLAGS) /Rn  /Ge+ /DRING0 /Gr+ /Gs- /Wall+ppt-ppc-inl-cnv-gnr-vft- /Gx+
@@ -89,7 +90,7 @@ CXX_FLAGS_IFS=$(CC_FLAGS_IFS) /Tdc
 
 IMPLIB_FLAGS=/NOI /Nologo
 
-LINK_FLAGS=/nofree /nologo /map /linenumbers /NOIgnorecase /NOE /NOD /Optfunc /PACKCODE /PACKDATA /EXEPACK:2 /NODEBUG
+LINK_FLAGS=/nofree /nologo /map /linenumbers /NOIgnorecase /NOE /NOD /PACKCODE /PACKDATA /EXEPACK:2 /NODEBUG
 LINK_FLAGS_EXE=$(LINK_FLAGS) /EXECutable /BASE:0x10000 /STACK:$(TARGET_STACKSIZE)
 LINK_FLAGS_DLL=$(LINK_FLAGS) /DLL
 LINK_FLAGS_SYS=$(LINK_FLAGS) /PDD /Align:16
