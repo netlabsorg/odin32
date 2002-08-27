5e3bc80e8c4c824823311a8c6d49851cc0ea1aa3
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-08-27 03:02:52 +0000

    Made debug format changeable.

--- make/setup.os2prfalp.mk
+++ make/setup.os2prfalp.mk
@@ -1,4 +1,4 @@
-# $Id: setup.os2prfalp.mk,v 1.3 2002-05-16 11:37:02 bird Exp $
+# $Id: setup.os2prfalp.mk,v 1.4 2002-08-27 03:02:52 bird Exp $
 
 #
 # The tool(s)
@@ -9,7 +9,7 @@ AS          = alp
 # The flags
 #
 !include $(PATH_MAKE)\setup.optional.alp.mk
-AS_FLAGS    = -D:OS2 -D:DEBUG $(_AS_OPTIONAL) -Mb +Od $(AS_DEFINES:-D=-D:) $(ALL_INCLUDES:-I=-i:) $(AS_INCLUDES:-I=-i:) $(ALL_DEFINES:-D=-D:) -i:$(PATH_INCLUDES) $(BUILD_DEFINES:-D=-D:)
+AS_FLAGS    = -D:OS2 -D:DEBUG $(_AS_OPTIONAL) -Mb $(AS_DEFINES:-D=-D:) $(ALL_INCLUDES:-I=-i:) $(AS_INCLUDES:-I=-i:) $(ALL_DEFINES:-D=-D:) -i:$(PATH_INCLUDES) $(BUILD_DEFINES:-D=-D:)
 AS_FLAGS_SYS= $(AS_FLAGS) -D:RING0 -i:$(PATH_DDK)\base\inc
 AS_OBJ_OUT  = -Fo:
 
