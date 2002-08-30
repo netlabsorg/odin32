449670b7270d994f7c97cc4830b6c32412b9fce0
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-08-30 20:28:18 +0000

    added -z (Display source line for each error message).

--- trunk/make/setup.os2allmasm510.mk
+++ trunk/make/setup.os2allmasm510.mk
@@ -1,4 +1,4 @@
-# $Id: setup.os2allmasm510.mk,v 1.1 2002-08-30 18:27:22 bird Exp $
+# $Id: setup.os2allmasm510.mk,v 1.2 2002-08-30 20:28:18 bird Exp $
 
 #
 # The tool(s)
@@ -11,8 +11,8 @@ AS              = masm
 AS_FLAGS        = -DOS2 \
 !if "$(BUILD_MODE)" != "RELEASE"
                   -DDEBUG \
-!endif                  
-                  -Zd -Zi -Mx -t $(BUILD_DEFINES) $(ALL_DEFINES) $(AS_DEFINES) \
+!endif
+                  -z -Zd -Zi -Mx -t $(BUILD_DEFINES) $(ALL_DEFINES) $(AS_DEFINES) \
                   $(AS_INCLUDES) $(ALL_INCLUDES) -I$(PATH_INCLUDES:;= -I)
 AS_FLAGS_SYS    = $(AS_FLAGS) -DRING0
 
