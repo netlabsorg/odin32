7d7d4045ac624fe34faff9d623c4aaac0d181297
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-09-14 23:35:27 +0000

    Improved dependencies on the makefiles.

--- make/setup.os2as.mk
+++ make/setup.os2as.mk
@@ -1,13 +1,15 @@
-# $Id: setup.os2as.mk,v 1.2 2002-08-30 18:27:22 bird Exp $
+# $Id: setup.os2as.mk,v 1.3 2002-09-14 23:35:27 bird Exp $
 
 #
 # Assembler fanout.
 #
 !ifdef AS_USE_MASM5
 # Alternate assembler: MASM v5.10
+MAKE_INCLUDE_AS_SETUP = $(PATH_MAKE)\setup.$(SHT_TRGPLTFRM)allmasm510.mk
 ! include $(PATH_MAKE)\setup.$(SHT_TRGPLTFRM)allmasm510.mk
 !else
 # Default assembler:   ALP
-! include $(PATH_MAKE)\setup.$(SHT_TRGPLTFRM)allalp.mk
+MAKE_INCLUDE_AS_SETUP = $(PATH_MAKE)\setup.$(SHT_TRGPLTFRM)allalp.mk
 !endif
+!include $(MAKE_INCLUDE_AS_SETUP)
 
