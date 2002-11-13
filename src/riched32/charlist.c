fa8d64d89d74456e6eb800ca19bf493122df6a29
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-11-13 12:17:19 +0000

    Wine resync

--- src/riched32/charlist.c
+++ src/riched32/charlist.c
@@ -1,10 +1,22 @@
-/* $Id: charlist.c,v 1.2 2000-08-02 14:58:40 bird Exp $ */
 /*
  *
  *  Character List
  *
  *  Copyright (c) 2000 by Jean-Claude Batista
  *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <stdio.h>
@@ -12,8 +24,13 @@
 #include <stddef.h>
 #include <ctype.h>
 #include <stdlib.h>
+
 #include "charlist.h"
-#include "windows.h"
+#include "windef.h"
+#include "winbase.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(richedit);
 
 extern HANDLE RICHED32_hHeap;
 
@@ -22,6 +39,8 @@ void CHARLIST_Enqueue( CHARLIST* pCharList, char myChar )
     CHARLISTENTRY* pNewEntry = HeapAlloc(RICHED32_hHeap, 0,sizeof(CHARLISTENTRY));
     pNewEntry->pNext = NULL;
     pNewEntry->myChar = myChar;
+
+    TRACE("\n");
 
     if(pCharList->pTail == NULL)
     {
@@ -39,6 +58,8 @@ void CHARLIST_Enqueue( CHARLIST* pCharList, char myChar )
 void CHARLIST_Push( CHARLIST* pCharList, char myChar)
 {
     CHARLISTENTRY* pNewEntry = malloc(sizeof(CHARLISTENTRY));
+
+    TRACE("\n");
 
     pNewEntry->myChar = myChar;
 
@@ -62,6 +83,8 @@ char CHARLIST_Dequeue(CHARLIST* pCharList)
     CHARLISTENTRY* pCurrent;
     char myChar;
 
+    TRACE("\n");
+
     if(pCharList->nCount == 0)
       return 0;
 
@@ -84,19 +107,25 @@ char CHARLIST_Dequeue(CHARLIST* pCharList)
 
 int CHARLIST_GetNbItems(CHARLIST* pCharList)
 {
+    TRACE("\n");
+
     return pCharList->nCount;
 }
 
 void CHARLIST_FreeList(CHARLIST* pCharList){
+    TRACE("\n");
+
     while(pCharList->nCount)
         CHARLIST_Dequeue(pCharList);
 }
 
-/* this function count the number of occurences of a caracter */
+/* this function counts the number of occurrences of a caracter */
 int CHARLIST_CountChar(CHARLIST* pCharList, char myChar)
 {
     CHARLISTENTRY *pCurrent;
     int nCount = 0;
+
+    TRACE("\n");
 
     for(pCurrent =pCharList->pHead ;pCurrent;pCurrent=pCurrent->pNext)
     	if(pCurrent->myChar == myChar)
@@ -107,6 +136,8 @@ int CHARLIST_CountChar(CHARLIST* pCharList, char myChar)
 
 int CHARLIST_toBuffer(CHARLIST* pCharList, char* pBuffer, int nBufferSize)
 {
+
+   TRACE("\n");
 
    /* we add one to store a NULL caracter */
    if(nBufferSize < pCharList->nCount + 1)
