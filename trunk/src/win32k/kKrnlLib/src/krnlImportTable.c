fd080738e73d422b79f2e8a319802abf66dfcd47
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-12-16 00:47:33 +0000

    Cleanup...

--- trunk/src/win32k/kKrnlLib/src/krnlImportTable.c
+++ trunk/src/win32k/kKrnlLib/src/krnlImportTable.c
@@ -1,24 +1,39 @@
-/* $Id: krnlImportTable.c,v 1.2 2002-04-01 12:47:04 bird Exp $
+/* $Id: krnlImportTable.c,v 1.3 2002-12-16 00:46:23 bird Exp $
  *
- * krnlImportTable - the import table (move from ProbKrnl).
+ * krnlImportTable - the import table.
  *
- * Copyright (c) 1998-2002 knut st. osmundsen (bird@anduin.net)
+ * Copyright (c) 1998-2003 knut st. osmundsen <bird@anduin.net>
  *
- * Project Odin Software License can be found in LICENSE.TXT
+ *
+ * This file is part of kKrnlLib.
+ *
+ * kKrnlLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * kKrnlLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kKrnlLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
 
 /*******************************************************************************
 *   Defined Constants And Macros                                               *
 *******************************************************************************/
-#define FALSE 0
-#define TRUE 0
+#define FALSE   0
+#define TRUE    1
+
 
 /*******************************************************************************
 *   Header Files                                                               *
 *******************************************************************************/
-#include "devSegDf.h"
-#include "ProbKrnl.h"
+#include "krnlImportTable.h"
 
 
 /*******************************************************************************
@@ -36,7 +51,8 @@
  *            We'll now generate both of these files from this table.
  *
  */
-IMPORTKRNLSYM DATA16_GLOBAL aImportTab[NBR_OF_KRNLIMPORTS] =
+#pragma info(notrd)
+IMPORTKRNLSYM aImportTab[NBR_OF_KRNLIMPORTS] =
 {/* iOrdinal    iObject   achName                      offObject  usSel  chOpcode    */
  /*       fFound    cchName                    achExtra    ulAddress  cProlog  fType */
     /* Ordinals 1-999 is reserved for kKrnlLib helper functions */
