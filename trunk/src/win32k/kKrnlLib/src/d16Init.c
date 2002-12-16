f20b051c07e6f531a00b3286c87e43d09fd0ccdd
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2002-12-16 01:53:55 +0000

    devErrors.c

--- trunk/src/win32k/kKrnlLib/src/d16Init.c
+++ trunk/src/win32k/kKrnlLib/src/d16Init.c
@@ -1,15 +1,28 @@
-/* $Id: d16Init.c,v 1.2 2002-03-31 19:01:13 bird Exp $
+/* $Id: d16Init.c,v 1.3 2002-12-16 01:53:25 bird Exp $
  *
  * d16init - init routines for both drivers.
  *
- * IMPORTANT! Code and data defined here will be discarded after init is
- *            completed. CodeEnd and DataEnd should not be set here.?
+ * Copyright (c) 1999-2002 knut st. osmundsen <bird@anduin.net>
  *
- * Copyright (c) 1999-2001 knut st. osmundsen (kosmunds@csc.com)
  *
- * Project Odin Software License can be found in LICENSE.TXT
+ * This file is part of kKrnlLib.
+ *
+ * kKrnlLib is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * kKrnlLib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with kKrnlLib; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
+
 
 /*******************************************************************************
 *   Defined Constants                                                          *
@@ -35,19 +48,61 @@
 #include <string.h>
 #include <memory.h>
 
-#include "devSegDf.h"
-#undef  DATA16_INIT
-#define DATA16_INIT
-#undef  CODE16_INIT
-#define CODE16_INIT
 #include "kKLInitHlp.h"
-#include "probkrnl.h"
+#include "devErrors.h"
 #include "dev1632.h"
 #include "dev16.h"
 #include "d16vprintf.h"
 #include "kKLlog.h"
 #include "options.h"
-#include "ProbKrnlErrors.h"
+
+
+
+/*******************************************************************************
+*   Internal Functions                                                         *
+*******************************************************************************/
+int             DoDevIOCtl(KKLR0INITPARAM  *pParam, KKLR0INITDATA *pData);
+
+
+/**
+ * Does the dev ioctl call to the helper driver for calling
+ * a 32-bit Ring-0 worker.
+ * @returns error code from DosOpen or DosDevIOCtl.
+ * @param   pParam  Pointer to parameter buffer.
+ * @param   pData   Pointer to data buffer.
+ * @author  knut st. osmundsen (kosmunds@csc.com)
+ */
+int             DoDevIOCtl(KKLR0INITPARAM  *pParam, KKLR0INITDATA *pData)
+{
+    APIRET          rc;
+    HFILE           hDev0 = 0;
+    USHORT          usAction = 0;
+
+    /* Open the kKrnlHlp device driver. */
+    rc = DosOpen(KKL_DEVICE_NAME, &hDev0, &usAction, 0UL, FILE_NORMAL,
+                 OPEN_ACTION_FAIL_IF_NEW | OPEN_ACTION_OPEN_IF_EXISTS,
+                 OPEN_SHARE_DENYNONE | OPEN_ACCESS_READONLY,
+                 0UL);
+    if (rc == NO_ERROR)
+    {
+        pData->ulRc = 0;
+        rc = DosDevIOCtl(pData, pParam, KKL_IOCTL_RING0INIT, KKL_IOCTL_CAT, hDev0);
+        DosClose(hDev0);
+        if (rc != NO_ERROR)
+        {
+            dprintf(("DosDevIOCtl failed with rc=%d\n", rc));
+            rc = ERROR_D16_IOCTL_FAILED;
+        }
+    }
+    else
+    {
+        dprintf(("DosOpen Failed with rc=%d\n", rc));
+        rc = ERROR_D16_OPEN_DEV_FAILED;
+    }
+
+    return rc;
+}
+
 
 
 /**
@@ -73,8 +128,8 @@ USHORT NEAR dev0Init(PRPINITIN pRpIn, PRPINITOUT pRpOut)
         printf16("kKrnlLib - kKrnlHlp: initGetDosTableData failed with rc=%d\n", rc);
 
     pRpOut->BPBArray = NULL;
-    pRpOut->CodeEnd = (USHORT)&CODE16_INITSTART;
-    pRpOut->DataEnd = (USHORT)&DATA16_INITSTART;
+    pRpOut->CodeEnd = (USHORT)&CODE16END;
+    pRpOut->DataEnd = (USHORT)&DATA16_ENDEND;
     pRpOut->Unit     = 0;
     pRpOut->rph.Status = STATUS_DONE;
     return STATUS_DONE;
@@ -95,43 +150,33 @@ USHORT NEAR dev1Init(PRPINITIN pRpIn, PRPINITOUT pRpOut)
     APIRET          rc;
     KKLR0INITPARAM  param;
     KKLR0INITDATA   data = {0UL};
-    HFILE           hDev0 = 0;
-    USHORT          usAction = 0;
-
+    RP32INIT        rp32init;
+    RP32INIT FAR *  fprp32init = &rp32init;
 
     /*
-     * Probe kernel data.
+     * Create new 32-bit init packet - Parameter pointer is thunked.
      */
-    rc = ProbeKernel(pRpIn);
-    if (rc == NO_ERROR)
-    {
-        /*
-         * Create new 32-bit init packet - Parameter pointer is thunked.
-         */
-        RP32INIT        rp32init;
-        RP32INIT FAR *  fprp32init = &rp32init;
-
-        _fmemcpy(fprp32init, pRpIn, sizeof(RPINITIN));
-        if ((   pRpIn->InitArgs == NULL
-             || !(rc = DevHelp_VirtToLin(SELECTOROF(rp32init.InitArgs), OFFSETOF(rp32init.InitArgs), (PLIN)&rp32init.InitArgs))
-             )
-            &&  !(rc = DevHelp_VirtToLin(SELECTOROF(fprp32init), OFFSETOF(fprp32init), (PLIN)&param.ulParam))
-           )
-        {   /* call 32-bit init routine and set 32 bit rc. */
-            param.pfn = GetR0InitPtr();
-            rc = DoDevIOCtl(&param, &data);
-            if (rc == NO_ERROR)
-                rc = (USHORT)data.ulRc;
-        }
-        else
-            rc = ERROR_D16_THUNKING_FAILED;
+    _fmemcpy(fprp32init, pRpIn, sizeof(RPINITIN));
+    if ((   pRpIn->InitArgs == NULL
+         || !(rc = DevHelp_VirtToLin(SELECTOROF(rp32init.InitArgs), OFFSETOF(rp32init.InitArgs), (PLIN)&rp32init.InitArgs))
+         )
+        &&  !(rc = DevHelp_VirtToLin(SELECTOROF(fprp32init), OFFSETOF(fprp32init), (PLIN)&param.ulParam))
+       )
+    {   /* call 32-bit init routine and set 32 bit rc. */
+        param.pfn = GetR0InitPtr();
+        rc = DoDevIOCtl(&param, &data);
+        if (rc == NO_ERROR)
+            rc = (USHORT)data.ulRc;
     }
+    else
+        rc = ERROR_D16_THUNKING_FAILED;
+
 
     /*
      * Fill return data.
      */
-    pRpOut->CodeEnd = (USHORT)&CODE16_INITSTART;
-    pRpOut->DataEnd = (USHORT)&DATA16_INITSTART;
+    pRpOut->CodeEnd = (USHORT)&CODE16_ENDEND;
+    pRpOut->DataEnd = (USHORT)&DATA16_ENDEND;
     pRpOut->BPBArray= NULL;
     pRpOut->Unit    = 0;
 
@@ -147,7 +192,7 @@ USHORT NEAR dev1Init(PRPINITIN pRpIn, PRPINITOUT pRpOut)
     }
     else
     {
-        char *psz = GetErrorMsg(rc);
+        char *psz = devGetErrorMsg(rc);
         printf16("kKrnlLib.sys init failed with rc=0x%x (%d)\n", rc, rc);
         if (psz)
             printf16("Explanation: %s\n\n", psz);
@@ -168,14 +213,11 @@ USHORT NEAR dev1Init(PRPINITIN pRpIn, PRPINITOUT pRpOut)
 
 /**
  * Gets the data we need from the DosTables.
- * This data is TKSSBase16, R0FlatCS16 and R0FlatDS16.
+ * (pulTKSSBase32, R0FlatCS16 and R0FlatDS16)
+ *
  * @returns   Same as DevHelp_GetDosVar.
  * @status    completely implemented.
  * @author    knut st. osmundsen (knut.stange.osmundsen@mynd.no)
- * @remark    If you are not sure if TKSSBase16 is set or not, call this.
- *            After R0Init16 is called TKSSBase16 _is_ set.
- *            IMPORTANT! This function must _not_ be called after the initiation of the second device driver!!!
- *                       (Since this is init code not present after init...)
  */
 USHORT NEAR  initGetDosTableData(void)
 {
@@ -183,7 +225,7 @@ USHORT NEAR  initGetDosTableData(void)
     PDOSTABLE  pDT;
     PDOSTABLE2 pDT2;
 
-    if (TKSSBase16 != 0)
+    if (pulTKSSBase32 != 0)
         return NO_ERROR;
     /*
     _asm {
@@ -196,7 +238,7 @@ USHORT NEAR  initGetDosTableData(void)
     if (rc == NO_ERROR)
     {
         pDT2 = (PDOSTABLE2)((char FAR *)pDT + pDT->cul*4 + 1);
-        TKSSBase16 = (ULONG)pDT2->pTKSSBase;
+        pulTKSSBase32 = (ULONG)pDT2->pTKSSBase;
         R0FlatCS16 = (USHORT)pDT2->R0FlatCS;
         R0FlatDS16 = (USHORT)pDT2->R0FlatDS;
 
