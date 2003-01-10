75eec657fd1b18a4aacc232cad8765f67b57ca23
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-01-10 13:14:44 +0000

    removed old version

--- trunk/src/crtdll/asmhlp.h
+++
@@ -1,9 +0,0 @@
-// $Id: asmhlp.h,v 1.2 2000-04-13 18:46:15 sandervl Exp $
-#ifndef __ASMHLP_H__
-#define __ASMHLP_H__
-
-void CDECL POP_FPU(double &fpval);
-
-void CDECL SetEDX(ULONG edx);
-
-#endif
