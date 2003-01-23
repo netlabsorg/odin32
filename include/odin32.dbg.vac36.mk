1aa41892eca8fccd589ff785fd70fd08929fc300
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-01-23 12:32:35 +0000

    Don't use include\incl_vac for statically linked targets

--- include/odin32.dbg.vac36.mk
+++ include/odin32.dbg.vac36.mk
@@ -1,4 +1,4 @@
-# $Id: odin32.dbg.vac36.mk,v 1.8 2003-01-22 17:05:17 sandervl Exp $
+# $Id: odin32.dbg.vac36.mk,v 1.9 2003-01-23 12:32:34 sandervl Exp $
 
 #
 # Odin32 API.
@@ -66,7 +66,10 @@ CXXFLAGS         = -c -Q -Si -Ti -Tm+ -Ss+ -W3 -Gm+ /Gn+ $(CXXTARGETFLAGS) /Gx+
 CXXFLAGS_ODINCRT = -c -Q -Si -Ti -Tm+ -Ss+ -W3 -Gm+ /Gn- $(CXXTARGETFLAGS) /Gx+
 CFLAGS_WIN32APP  = -c -Q -Si -Ti -Tm+ -Ss+ -W3 -Gm+ /Gn+ $(CTARGETFLAGS)
 CXXFLAGS_WIN32APP= -c -Q -Si -Ti -Tm+ -Ss+ -W3 -Gm+ /Gn+ $(CXXTARGETFLAGS)
-CINCLUDES        = -I$(ODIN32_INCLUDE)\incl_vac -I$(ODIN32_INCLUDE)\Win -I. -I$(ODIN32_INCLUDE) 
+CINCLUDES        = -I$(ODIN32_INCLUDE)\Win -I. -I$(ODIN32_INCLUDE) 
+!ifndef STATIC_CRT
+CINCLUDES        = -I$(ODIN32_INCLUDE)\incl_vac $(CINCLUDES) 
+!endif
 CDEFINES_WIN32APP= -DDEBUG -D__WIN32OS2__ -D__i386__
 CDEFINES_ODINCRT = -DDEBUG -D__WIN32OS2__ -D__i386__ -D__WINE__
 CDEFINES         = -DDEBUG -D__WIN32OS2__ -D__i386__ -D__WINE__ -DTCPV40HDRS -DCOMCTL32UNDOC \
