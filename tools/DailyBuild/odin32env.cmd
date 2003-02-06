3f5d3da1481f6d515e161cffd8eb1c662f8989f2
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-02-06 20:46:18 +0000

    gcc303 and EMX.

--- tools/DailyBuild/odin32env.cmd
+++ tools/DailyBuild/odin32env.cmd
@@ -1,4 +1,4 @@
-/* $Id: odin32env.cmd,v 1.27 2002-12-06 03:38:02 bird Exp $
+/* $Id: odin32env.cmd,v 1.28 2003-02-06 20:46:18 bird Exp $
  *
  * Sets the build environment.
  *
@@ -8,10 +8,11 @@
  *
  */
 
-call BuildEnv 'warpin mysql perl cvs ddkbase mscv6-16 emx vac365õ watcomc11cõ vac40õ vac308 toolkit40 debug'
+call BuildEnv 'warpin mysql perl cvs ddkbase mscv6-16 emx gcc303 vac365õ watcomc11cõ vac40õ vac308 toolkit40 debug'
 'Set CVSROOT=:pserver:bird@www.netlabs.org:/netlabs.cvs/odin32';
 'Set MULTIJOBS=1';
 'Set BUILD_SETUP_MAK=';
+'Set EMX=%PATH_EMXPGCC%';
 
 exit(0);
 
