6c18c85adc3cffd29a8bdc40392986e292646408
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-02-11 15:06:36 +0000

    perl is not required.

--- tools/DailyBuild/odin32env.cmd
+++ tools/DailyBuild/odin32env.cmd
@@ -1,4 +1,4 @@
-/* $Id: odin32env.cmd,v 1.29 2003-02-06 21:05:38 bird Exp $
+/* $Id: odin32env.cmd,v 1.30 2003-02-11 15:06:36 bird Exp $
  *
  * Sets the build environment.
  *
@@ -17,7 +17,7 @@ sDir = filespec('drive', sSrc) || filespec('path', sSrc);
 /* base env */
 sOldDir = directory();
 call directory sSrc||'\bin';
-call BuildEnv 'warpin mysql perl cvs ddkbase mscv6-16 emx gcc303 vac365õ watcomc11cõ vac40õ vac308 toolkit40 debug'
+call BuildEnv 'warpin mysql perl~ cvs ddkbase mscv6-16 emx gcc303 vac365õ watcomc11cõ vac40õ vac308 toolkit40 debug'
 call directory sOldDir;
 
 /* minor adjustments. */
