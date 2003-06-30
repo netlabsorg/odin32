fae7476386e3fc75e453937d1aeb40c8f3358e75
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-06-30 13:18:25 +0000

    Special optimization config for IFSes. Corrected CC optims check.

--- tags/trunk/make/setup.os2relwat11-16.mk
+++ tags/trunk/make/setup.os2relwat11-16.mk
@@ -1,4 +1,4 @@
-# $Id: setup.os2relwat11-16.mk,v 1.16 2002-09-20 04:10:06 bird Exp $
+# $Id: setup.os2relwat11-16.mk,v 1.17 2003-06-30 13:18:25 bird Exp $
 
 #
 # Note! Watcom is unable to do debug info release builds.
@@ -43,7 +43,7 @@ CXX=wpp.exe
 #
 # The flags
 #
-!if "$(_CXX_OPTIM)" == ""
+!if "$(_CC_OPTIM)" == ""
 _CC_OPTIM_ = -omlinear
 _CC_OPTIM_IFS = -out
 !else
@@ -58,16 +58,18 @@ CC_FLAGS_VDD=$(CC_FLAGS_SYS)
 CC_FLAGS_IFS=$(CC_FLAGS) $(_CC_OPTIM_IFS) -s -zff -zgf -zu -bd
 
 !if "$(_CXX_OPTIM)" == ""
-_CXX_OPTIM_ = -omlinear
+_CXX_OPTIM_    = -omlinear
+_CXX_OPTIM_IFS = -out
 !else
-_CXX_OPTIM_ =
+_CXX_OPTIM_    =
+_CXX_OPTIM_IFS =
 !endif
 CXX_FLAGS=-bt=os2 -dOS2 -d__16BIT__ -zq -fr=nul -bm -ze -w4 -zld $(_CXX_OPTIONAL)  $(CXX_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CXX_INCLUDES:-I=-i=) $(ALL_INCLUDES:-I=-i=) -i=$(PATH_INCLUDES) -i=$(WATCOM)\h
 CXX_FLAGS_EXE=$(CXX_FLAGS) $(_CXX_OPTIM_) -zc
 CXX_FLAGS_DLL=$(CXX_FLAGS) $(_CXX_OPTIM_) -zc -bd
 CXX_FLAGS_SYS=$(CXX_FLAGS) $(_CXX_OPTIM_) -s -zff -zgf -zu
 CXX_FLAGS_VDD=$(CXX_FLAGS_SYS)
-CXX_FLAGS_IFS=$(CXX_FLAGS) $(_CXX_OPTIM_)  -s -zff -zgf -zdp -zu -bd
+CXX_FLAGS_IFS=$(CXX_FLAGS) $(_CXX_OPTIM_IFS)  -s -zff -zgf -zdp -zu -bd
 
 !if "$(CC_AS_CXX)" != ""
 ! if "$(CXX_AS_CC)" != ""
