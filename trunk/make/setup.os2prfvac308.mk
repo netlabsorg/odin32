82261b74b9c16c54ac4d24f2420b3a7b0b55c25c
bird <bird@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-06-30 13:28:59 +0000

    No stack probes needed for Ring-0.

--- trunk/make/setup.os2prfvac308.mk
+++ trunk/make/setup.os2prfvac308.mk
@@ -1,4 +1,4 @@
-# $Id: setup.os2prfvac308.mk,v 1.21 2002-10-04 04:32:41 bird Exp $
+# $Id: setup.os2prfvac308.mk,v 1.22 2003-06-30 13:28:58 bird Exp $
 
 # ---OS2, PROFILE, VAC308-------------------------
 ENV_NAME="OS/2, Profile, IBM VisualAge for C++ 3.08"
@@ -36,7 +36,7 @@ CXX=icc.exe
 CC_FLAGS=/Q /DDEBUG /DOS2 /D__32BIT__ /D__i386__ /DMODEL=FLAT /Ti+ /O- /Gh+ /Ss+ /C+ $(_CC_OPTIONAL) $(CC_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CC_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
 CC_FLAGS_EXE=$(CC_FLAGS) /Gm+ /Ge+
 CC_FLAGS_DLL=$(CC_FLAGS) /Gm+ /Ge-
-CC_FLAGS_SYS=$(CC_FLAGS) /Tm- /Rn  /Ge+ /DRING0 /Gr+ /Gs- /Wall+ppt-ppc-inl-cnv-gnr-vft-gen-uni-ext-
+CC_FLAGS_SYS=$(CC_FLAGS) /Tm- /Rn  /Ge+ /DRING0 /Gr+ /Gs+ /Wall+ppt-ppc-inl-cnv-gnr-vft-gen-uni-ext-
 CC_FLAGS_VDD=$(CC_FLAGS_SYS)
 CC_FLAGS_IFS=$(CC_FLAGS_SYS) /Ge-
 CC_OBJ_OUT=/Fo
@@ -47,7 +47,7 @@ CC_PC_2_STRIPPED_STDOUT=/Pd+ /P+ /Pc- /Pe+
 CXX_FLAGS=/Q /DDEBUG /DOS2 /D__i386__ /DMODEL=FLAT /Ti+ /O- /Gh+ /Ss+ /C+ $(_CXX_OPTIONAL) $(CXX_DEFINES) $(ALL_DEFINES) $(BUILD_DEFINES) $(CXX_INCLUDES) $(ALL_INCLUDES) /I$(PATH_INCLUDES)
 CXX_FLAGS_EXE=$(CXX_FLAGS) /Gm+ /Ge+
 CXX_FLAGS_DLL=$(CXX_FLAGS) /Gm+ /Ge-
-CXX_FLAGS_SYS=$(CXX_FLAGS) /Tm- /Rn  /Ge+ /DRING0 /Gr+ /Gs- /Wall+ppt-ppc-inl-cnv-gnr-vft- /Gx+
+CXX_FLAGS_SYS=$(CXX_FLAGS) /Tm- /Rn  /Ge+ /DRING0 /Gr+ /Gs+ /Wall+ppt-ppc-inl-cnv-gnr-vft- /Gx+
 CXX_FLAGS_VDD=$(CXX_FLAGS_SYS)
 CXX_FLAGS_IFS=$(CXX_FLAGS_SYS) /Ge-
 CXX_OBJ_OUT=/Fo
