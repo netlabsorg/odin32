d0af216302b8fbaba1785e143eff15a5eadba471
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-08-08 15:59:48 +0000

    link fix

--- src/NTDLL/initterm.cpp
+++ src/NTDLL/initterm.cpp
@@ -24,7 +24,7 @@
 /* Include files */
 #define  INCL_DOSMODULEMGR
 #define  INCL_DOSPROCESS
-#include <os2wrap.h>    //Odin32 OS/2 api wrappers
+#include <os2.h>    //Odin32 OS/2 api wrappers
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
