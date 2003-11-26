bc76f6b7e055e7a32dd7d9518d51e5e54a5b3cbb
sandervl <sandervl@3ae8c665-79e0-dc11-ae90-00e081727c95>
2003-11-26 15:52:49 +0000

    update

--- include/dcdata.h
+++ include/dcdata.h
@@ -28,8 +28,7 @@ typedef struct _tagXFORM
     FLOAT eDy;
 } XFORM_W, *PXFORM_W, *LPXFORM_W;
 
-#ifndef USING_OPEN32
-#ifndef GPI_INCLUDED
+#ifndef INCL_GPI
    typedef struct _SIZEF
    {
       FIXED cx;
@@ -37,6 +36,7 @@ typedef struct _tagXFORM
    } SIZEF;
    typedef SIZEF *PSIZEF;
 
+#ifndef OS2DEF_INCLUDED
    typedef struct _MATRIXLF
    {
       FIXED fxM11;
@@ -52,6 +52,7 @@ typedef struct _tagXFORM
    typedef MATRIXLF *PMATRIXLF;
 
    typedef ULONG   HPS;
+#endif
 
    typedef struct _CHARBUNDLE
    {
@@ -193,139 +194,5 @@ typedef struct _DCData
    struct      _DCData *nextHPS_HDC;
 } tDCData, *pDCData;
 #pragma pack()
-#else
-
-#ifndef GPI_INCLUDED
-   typedef struct _SIZEF
-   {
-      FIXED cx;
-      FIXED cy;
-   } SIZEF;
-   typedef SIZEF *PSIZEF;
-
-   typedef struct _MATRIXLF
-   {
-      FIXED fxM11;
-      FIXED fxM12;
-      LONG  lM13;
-      FIXED fxM21;
-      FIXED fxM22;
-      LONG  lM23;
-      LONG  lM31;
-      LONG  lM32;
-      LONG  lM33;
-   } MATRIXLF;
-   typedef MATRIXLF *PMATRIXLF;
-
-   typedef ULONG   HPS;
-#endif
-
-typedef struct _DCData
-{
-   HDC         hdc;
-   HWND        hwnd;
-   HPS         hps;
-
-   UINT        lastBrushKey;
-   PVOID       lastBrushObject;
-   UINT        lastPenHandle;
-   struct _penobject *lastPenObject;
-   UINT        lastFontHandle;
-   PVOID       lastFontObject;
-   UINT        lastBitmapHandle;
-   PVOID       lastBitmapObject;
-   UINT        lastPaletteHandle;
-   PVOID       lastPaletteObject;
-
-   UINT        nullBitmapHandle;
-
-   ULONG       BkColor;
-   ULONG       TextColor;
-   ULONG       BkColor_PM;
-   ULONG       TextColor_PM;
-
-   int         BkMode;
-   ULONG       BkMode_PM;
-
-   int         ROP2Mode;
-   int         ROP2Mode_PM;
-
-   unsigned    isMemoryPS:1;
-   unsigned    isMetaPS:1;
-   unsigned    isPrinter:1;
-   unsigned    isFrameWindow:1;
-   unsigned    isOD_QUEUED:1;
-   unsigned    isOD_INFO:1;
-   unsigned    isClient:1;
-   unsigned    isClientArea:1;
-   unsigned    isLeftLeft:1;
-   unsigned    isTopTop:1;
-   unsigned    isWideLine:1;
-   unsigned    alignUpdateCP:1;
-   unsigned    isCacheable:1;
-   unsigned    penIsExtPen:1;
-   unsigned    isValid:1;
-   unsigned    inPath:1;
-   unsigned    isStartDoc:1;
-   unsigned    resetStockFonts:1;
-   unsigned    unused:14;
-
-   ULONG       MapMode;
-   HBITMAP     bitmapHandle;
-   ULONG       bitmapHeight;
-   ULONG       bitmapWidth;
-   ULONG       hMeta;
-   PVOID       pMetaFileObject;
-   int         polyFillMode;
-   int         arcDirection;
-   int         stretchBltMode;
-   int         graphicsMode;
-   HRGN        hrgnHDC;
-
-   PS_Type     psType;
-
-   HDC_Type    hdcType;
-   USHORT      usFiller;
-   POINTL      viewportOrg;
-   double      viewportXExt;
-   double      viewportYExt;
-   POINTL      windowOrg;
-   SIZEL       windowExt;
-   HRGN        hrgnVis;
-   POINTL      ptlOrigin;
-   ULONG       printPageHeight;
-   PVOID       printerObject;
-
-   LONG        taMode;
-   XFORM_W     xform;
-
-   INT         worldYDeltaFor1Pixel;
-   INT         worldXDeltaFor1Pixel;
-   ULONG       colorMode;
-   PULONG      pLogColorTable;
-
-   ULONG       lcidBitfield;
-
-   HWND        hwndRealize;
-   ULONG       cpeMap;
-
-   LONG        lTechnology;
-
-   LONG        lWndXExtSave, lWndYExtSave,
-               lVwpXExtSave, lVwpYExtSave;
-
-   int         height;
-
-   POINTL      brushOrgPoint;
-
-   PVOID       pEnhMetaPalette;
-   PVOID       lpAbortProc;
-   ULONG       HPStoHDCInversionHeight;
-
-   int         saveLevel;
-
-   struct      _DCData *nextDCData;
-} tDCData, *pDCData;
-#endif
 
 #endif //__DCDATA_H__
