7587f3843b4c37d34343a2cb0fe19167bbcb2937
sao2l02 <sao2l02@3ae8c665-79e0-dc11-ae90-00e081727c95>
2005-06-26 18:42:46 +0000

    .

--- tags/trunk/src/kernel32/Fileio.cpp
+++ tags/trunk/src/kernel32/Fileio.cpp
@@ -1,4 +1,4 @@
-/* $Id: Fileio.cpp,v 1.74 2005-06-26 18:40:58 sao2l02 Exp $ */
+/* $Id: Fileio.cpp,v 1.75 2005-06-26 18:42:46 sao2l02 Exp $ */
 
 /*
  * Win32 File IO API functions for OS/2
@@ -43,7 +43,6 @@ ODINDEBUGCHANNEL(KERNEL32-FILEIO)
 
 #include <ctype.h>
 #include "fileio.h"
-//#include <win/file.h>
 #include "file.h"
 
 
