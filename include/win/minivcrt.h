53f7ff78efff2955e0e55c1f8e4262de864fac9f
Dmitriy Kuminov <coding@dmik.org>
2010-12-03 11:03:21 +0000

    minivcrt: Added min and max macros.

--- include/win/minivcrt.h
+++ include/win/minivcrt.h
@@ -21,6 +21,13 @@ extern "C" {
 
 #define FILENAME_MAX      260
 
+#ifndef max
+#define max(a,b) ( (a>b) ? a : b )
+#endif
+#ifndef min
+#define min(a,b) ( (a<b) ? a : b )
+#endif
+
 typedef unsigned long _fsize_t;
 
 struct _wfinddata_t {
