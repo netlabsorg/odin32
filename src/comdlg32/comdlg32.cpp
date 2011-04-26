d9dd6df347d64ca0931d5061b759a8f0f72ee17f
Dmitriy Kuminov <coding@dmik.org>
2011-04-26 19:00:45 +0000

    comdlg32: Made PrintDlg() use the native PM print dialog instead of the Win32 clone. This looks much more native and gives access to all printer settings. Note that PageSetupDlg() is a no-op now (always returns FALSE) but it's not really necessary since thew native dialog gives access to page settings as well.

--- src/comdlg32/comdlg32.cpp
+++ src/comdlg32/comdlg32.cpp
@@ -43,49 +43,75 @@ ODINDEBUGCHANNEL(COMDLG32-COMDLG32)
   } \
   a->hwndOwner = Win32ToOS2Handle(a->hwndOwner);
 
+// from gdi32/devcontext.cpp
+BOOL WIN32API GetPMQueueName(LPSTR pDeviceName, LPSTR lpszPMQueue, INT cbPMQueue);
+
+// USING_OPEN32 seems to be not necessary since the WGSS GlobalAlloc API appears
+// to be in line with the Odin32 GlobalAlloc API (and the current version of
+// WGSS doesn't export this API anyway)
+
 #ifdef USING_OPEN32
 
 #define FLAG_TO_OPEN32	     0
 #define FLAG_FROM_OPEN32     1
 
-//******************************************************************************
-//******************************************************************************
-HGLOBAL GlobalCopy(HGLOBAL hDest, HGLOBAL hSource, BOOL fToOpen32)
+static HGLOBAL GlobalCopy(HGLOBAL hDest, HGLOBAL hSource, BOOL fToOpen32)
 {
- LPVOID src;
- LPVOID dest;
- ULONG  size;
+    LPVOID src;
+    LPVOID dest;
+    ULONG  size;
 
-      if(fToOpen32 == FLAG_TO_OPEN32) {
-          src = GlobalLock(hSource);
-          if(src) {
-              size = GlobalSize(hSource);
-              if(hDest == NULL) {
-                  hDest = O32_GlobalAlloc(GHND, size);
-              }
-              dest  = O32_GlobalLock(hDest);
-              memcpy(dest, src, size);
-              O32_GlobalUnlock(hDest);
-          }
-          GlobalUnlock(hSource);
-      }
-      else {
-          src = O32_GlobalLock(hSource);
-          if(src) {
-              size = O32_GlobalSize(hSource);
-              if(hDest == NULL) {
-                  hDest = GlobalAlloc(GHND, size);
-              }
-              dest  = GlobalLock(hDest);
-              memcpy(dest, src, size);
-              GlobalUnlock(hDest);
-          }
-          O32_GlobalUnlock(hSource);
-      }
-      return hDest;
+    if (fToOpen32 == FLAG_TO_OPEN32)
+    {
+        src = GlobalLock(hSource);
+        if (src)
+        {
+            size = GlobalSize(hSource);
+            if(hDest == NULL)
+            {
+                hDest = O32_GlobalAlloc(GHND, size);
+            }
+            dest = O32_GlobalLock(hDest);
+            memcpy(dest, src, size);
+            O32_GlobalUnlock(hDest);
+        }
+        GlobalUnlock(hSource);
+    }
+    else
+    {
+        src = O32_GlobalLock(hSource);
+        if (src)
+        {
+            size = O32_GlobalSize(hSource);
+            if(hDest == NULL)
+            {
+                hDest = GlobalAlloc(GHND, size);
+            }
+            dest  = GlobalLock(hDest);
+            memcpy(dest, src, size);
+            GlobalUnlock(hDest);
+        }
+        O32_GlobalUnlock(hSource);
+    }
+    return hDest;
 }
+
+#define MyGlobalAlloc   O32_GlobalAlloc
+#define MyGlobalLock    O32_GlobalLock
+#define MyGlobalUnlock  O32_GlobalUnlock
+#define MyGlobalFree    O32_GlobalFree
+#define MyGlobalSize    O32_GlobalSize
+
+#else
+
+#define MyGlobalAlloc   GlobalAlloc
+#define MyGlobalLock    GlobalLock
+#define MyGlobalUnlock  GlobalUnlock
+#define MyGlobalFree    GlobalFree
+#define MyGlobalSize    GlobalSize
+
 #endif
-#if 1
+
 /*****************************************************************************
  * Name      :
  * Purpose   :
@@ -101,52 +127,128 @@ HGLOBAL GlobalCopy(HGLOBAL hDest, HGLOBAL hSource, BOOL fToOpen32)
 ODINFUNCTION1(BOOL, PrintDlgA,
               LPPRINTDLGA, lppd)
 {
-  BOOL      ret;
+    BOOL      ret;
 #ifdef USING_OPEN32
-  HGLOBAL   hDevmode = 0, hDevNames = 0;
-  HGLOBAL   hOrgDevmode = 0, hOrgDevNames = 0;
-  DEVMODEA *devmode, *devmodeorg;
-  DEVNAMES *devnames, *devnamesorg;
-  ULONG     size;
+    HGLOBAL   hOrgDevMode = 0, hOrgDevNames = 0;
 #endif
+    LPSTR     origDevice = NULL;
 
-  COMDLG32_CHECKHOOK2(lppd, PD_ENABLEPRINTHOOK, LPPRINTHOOKPROC,lpfnPrintHook)
-  COMDLG32_CHECKHOOK2(lppd, PD_ENABLESETUPHOOK, LPSETUPHOOKPROC,lpfnSetupHook)
+    COMDLG32_CHECKHOOK2(lppd, PD_ENABLEPRINTHOOK, LPPRINTHOOKPROC,lpfnPrintHook)
+    COMDLG32_CHECKHOOK2(lppd, PD_ENABLESETUPHOOK, LPSETUPHOOKPROC,lpfnSetupHook)
 
 #ifdef USING_OPEN32
-  if(lppd->hDevMode) {
-      hOrgDevmode = lppd->hDevMode;
-      lppd->hDevMode = GlobalCopy(NULL, hOrgDevmode, FLAG_TO_OPEN32);
-  }
-  if(lppd->hDevNames) {
-      hOrgDevNames = lppd->hDevNames;
-      lppd->hDevNames = GlobalCopy(NULL, hOrgDevNames, FLAG_TO_OPEN32);
-  }
+    if (lppd->hDevMode)
+    {
+        hOrgDevMode = lppd->hDevMode;
+        lppd->hDevMode = GlobalCopy(NULL, hOrgDevMode, FLAG_TO_OPEN32);
+    }
+    if (lppd->hDevNames)
+    {
+        hOrgDevNames = lppd->hDevNames;
+        lppd->hDevNames = GlobalCopy(NULL, hOrgDevNames, FLAG_TO_OPEN32);
+    }
 #endif
-  HWND hwndOwner = lppd->hwndOwner;
-  if(lppd->hwndOwner) {
-      lppd->hwndOwner = Win32ToOS2Handle(lppd->hwndOwner);
-  }
-  ret = O32_PrintDlg(lppd);
-  lppd->hwndOwner = hwndOwner;
+
+    if (lppd->hDevNames)
+    {
+        LPDEVNAMES devNames = (LPDEVNAMES)MyGlobalLock(lppd->hDevNames);
+
+        LPSTR driver = (LPSTR)devNames + devNames->wDriverOffset;
+        LPSTR device = (LPSTR)devNames + devNames->wDeviceOffset;
+        LPSTR output = (LPSTR)devNames + devNames->wOutputOffset;
+        int driverLen = lstrlenA(driver) + 1;
+        int deviceLen = lstrlenA(device) + 1;
+        int outputLen = lstrlenA(output) + 1;
+
+        // get the PM queue name for the device (which is usually the full
+        // PM printer's WPS object name): WGSS expects it and will correctly
+        // pre-select the printer specified in hDevNames. Note that we don't
+        // perform the opposite conversion from the PM queue to the device name
+        // after calling O32_PrintDlg() since this seems to work as it is:
+        // the dialog returns the correct DC when OK is pressed despite the name
+        // mismatch
+
+        CHAR queue[256];
+        if (GetPMQueueName(device, queue, sizeof(queue)))
+        {
+            dprintf(("PrintDlgA: renamed device {%s} to PM queue {%s}",
+                     device, queue));
+            device = queue;
+            deviceLen = lstrlenA(device) + 1;
+
+            HGLOBAL hDevNames2 =
+                MyGlobalAlloc(GHND, MyGlobalSize(lppd->hDevNames) +
+                              lstrlenA(queue) - (deviceLen - 1));
+
+            LPDEVNAMES devNames2 = (LPDEVNAMES)MyGlobalLock(hDevNames2);
+
+            devNames2->wDriverOffset = sizeof(DEVNAMES);
+            memcpy((LPSTR)devNames2 + devNames2->wDriverOffset, driver, driverLen);
+            devNames2->wDeviceOffset = devNames2->wDriverOffset + driverLen;
+            memcpy((LPSTR)devNames2 + devNames2->wDeviceOffset, device, deviceLen);
+            devNames2->wOutputOffset = devNames2->wDeviceOffset + deviceLen;
+            memcpy((LPSTR)devNames2 + devNames2->wOutputOffset, output, outputLen);
+            devNames2->wDefault = devNames->wDefault;
+
+            MyGlobalUnlock(hDevNames2);
+
+            MyGlobalUnlock(lppd->hDevNames);
+            MyGlobalFree(lppd->hDevNames);
+            lppd->hDevNames = hDevNames2;
+        }
+        else
+            MyGlobalUnlock(lppd->hDevNames);
+    }
+
+    HWND hwndOwner = lppd->hwndOwner;
+    if (lppd->hwndOwner)
+    {
+        lppd->hwndOwner = Win32ToOS2Handle(lppd->hwndOwner);
+    }
+
+    ret = O32_PrintDlg(lppd);
+
+    lppd->hwndOwner = hwndOwner;
+
 #ifdef USING_OPEN32
-  if(ret == TRUE) {
-      if(lppd->hDevMode) {
-          hDevmode = lppd->hDevMode;
-          lppd->hDevMode = GlobalCopy(hOrgDevmode, lppd->hDevMode, FLAG_FROM_OPEN32);
-          O32_GlobalFree(hDevmode);
-      }
-      if(lppd->hDevNames) {
-          hDevNames = lppd->hDevNames;
-          lppd->hDevNames = GlobalCopy(hOrgDevNames, lppd->hDevNames, FLAG_FROM_OPEN32);
-          O32_GlobalFree(hDevNames);
-      }
-  }
+    if (ret == TRUE)
+    {
+        if (lppd->hDevMode)
+        {
+            HGLOBAL hDevMode = lppd->hDevMode;
+            lppd->hDevMode = GlobalCopy(hOrgDevMode, lppd->hDevMode,
+                                        FLAG_FROM_OPEN32);
+            O32_GlobalFree(hDevMode);
+        }
+        if (lppd->hDevNames)
+        {
+            HGLOBAL hDevNames = lppd->hDevNames;
+            lppd->hDevNames = GlobalCopy(hOrgDevNames, lppd->hDevNames,
+                                         FLAG_FROM_OPEN32);
+            O32_GlobalFree(hDevNames);
+        }
+    }
+    else
+    {
+        if (lppd->hDevMode)
+        {
+            O32_GlobalFree(lppd->hDevMode);
+            lppd->hDevMode = hOrgDevMode;
+        }
+        if (lppd->hDevNames)
+        {
+            O32_GlobalFree(lppd->hDevNames);
+            lppd->hDevNames = hOrgDevNames;
+        }
+    }
 #endif
-  if(lppd->hDC) {
-      OSLibGpiSetCp(lppd->hDC, GetDisplayCodepage());
-  }
-  return ret;
+
+    if(lppd->hDC)
+    {
+        OSLibGpiSetCp(lppd->hDC, GetDisplayCodepage());
+    }
+
+    return ret;
 }
 
 
@@ -165,48 +267,224 @@ ODINFUNCTION1(BOOL, PrintDlgA,
 ODINFUNCTION1(BOOL, PrintDlgW,
               LPPRINTDLGW, lppd)
 {
+    PRINTDLGA pd;
+    BOOL      bResult;
 
-  PRINTDLGA pd;
-  BOOL      bResult;
+    memcpy(&pd,          // make binary copy first to save all the fields
+           lppd,
+           sizeof(pd));
 
-  //SvL: TODO: hDevMode & hDevNames
-  dprintf(("PrintDlgW: ERROR NOT COMPLETE (UNICODE TRANSLATION)!!"));
+    if (lppd->hwndOwner)
+    {
+        pd.hwndOwner = Win32ToOS2Handle(lppd->hwndOwner);
+    }
 
-  memcpy(&pd,          // make binary copy first to save all the fields
-         lppd,
-         sizeof(pd));
+    if (lppd->hDevMode)
+    {
+        LPDEVMODEW devMode = (LPDEVMODEW)GlobalLock(lppd->hDevMode);
 
-  // convert to ASCII string
-  if ((lppd->Flags & PD_ENABLEPRINTTEMPLATE) &&
-      (lppd->lpPrintTemplateName != NULL))
-    pd.lpPrintTemplateName = UnicodeToAsciiString((WCHAR*)lppd->lpPrintTemplateName);
-  else
-    pd.lpPrintTemplateName = NULL;
+        dprintf(("PrintDlgW: in devMode{%.*ls,%.*ls}",
+                 CCHDEVICENAME, devMode->dmDeviceName,
+                 CCHFORMNAME, devMode->dmFormName));
 
-  if ((lppd->Flags & PD_ENABLESETUPTEMPLATE) &&
-      (lppd->lpSetupTemplateName != NULL))
-    pd.lpSetupTemplateName = UnicodeToAsciiString((WCHAR*)lppd->lpSetupTemplateName);
-  else
-    pd.lpSetupTemplateName = NULL;
+        pd.hDevMode = MyGlobalAlloc(GHND, sizeof(DEVMODEA));
 
-  COMDLG32_CHECKHOOK2((&pd), PD_ENABLEPRINTHOOK, LPPRINTHOOKPROC,lpfnPrintHook)
-  COMDLG32_CHECKHOOK2((&pd), PD_ENABLESETUPHOOK, LPSETUPHOOKPROC,lpfnSetupHook)
+        LPDEVMODEA devModeA = (LPDEVMODEA)MyGlobalLock(pd.hDevMode);
 
-  bResult = O32_PrintDlg(&pd); // call ASCII API
+        // convert to ASCII string
+        UnicodeToAsciiN((LPWSTR)devMode->dmDeviceName, (LPSTR)devModeA->dmDeviceName,
+                        CCHDEVICENAME);
+        UnicodeToAsciiN((LPWSTR)devMode->dmFormName, (LPSTR)devModeA->dmFormName,
+                        CCHFORMNAME);
 
-  if (pd.lpPrintTemplateName != NULL) FreeAsciiString((char*)pd.lpPrintTemplateName);
-  if (pd.lpSetupTemplateName != NULL) FreeAsciiString((char*)pd.lpSetupTemplateName);
+        MyGlobalLock(pd.hDevMode);
+        GlobalLock(lppd->hDevMode);
+    }
 
-  // copy back result
-  lppd->Flags     = pd.Flags;
-  lppd->nFromPage = pd.nFromPage;
-  lppd->nToPage   = pd.nToPage;
-  lppd->nMinPage  = pd.nMinPage;
-  lppd->nMaxPage  = pd.nMaxPage;
-  lppd->nCopies   = pd.nCopies;
-  //@@@PH: all pass-back fields ?
+    if (lppd->hDevNames)
+    {
+        LPDEVNAMES devNames = (LPDEVNAMES)GlobalLock(lppd->hDevNames);
 
-  return bResult;
+        dprintf(("PrintDlgW: in devNames{%ls,%ls,%ls}",
+                 (LPCWSTR)devNames + devNames->wDriverOffset,
+                 (LPCWSTR)devNames + devNames->wDeviceOffset,
+                 (LPCWSTR)devNames + devNames->wOutputOffset));
+
+        // convert to ASCII string
+        LPSTR driver = UnicodeToAsciiString((LPCWSTR)devNames + devNames->wDriverOffset);
+        LPSTR device = UnicodeToAsciiString((LPCWSTR)devNames + devNames->wDeviceOffset);
+        LPSTR output = UnicodeToAsciiString((LPCWSTR)devNames + devNames->wOutputOffset);
+        int driverLen = lstrlenA(driver) + 1;
+        int deviceLen = lstrlenA(device) + 1;
+        int outputLen = lstrlenA(output) + 1;
+
+        // get the PM queue name for the device (which is usually the full
+        // PM printer's WPS object name): WGSS expects it and will correctly
+        // pre-select the printer specified in hDevNames. Note that we don't
+        // perform the opposite conversion from the PM queue to the device name
+        // after calling O32_PrintDlg() since this seems to work as it is:
+        // the dialog returns the correct DC when OK is pressed despite the name
+        // mismatch
+
+        CHAR queue[256];
+        if (GetPMQueueName(device, queue, sizeof(queue)))
+        {
+            dprintf(("PrintDlgW: renamed device {%s} to PM queue {%s}",
+                     device, queue));
+            device = queue;
+            deviceLen = lstrlenA(device) + 1;
+        }
+
+        pd.hDevNames = MyGlobalAlloc(GHND, sizeof(DEVNAMES) +
+                                     driverLen + deviceLen + outputLen);
+
+        LPDEVNAMES devNamesA = (LPDEVNAMES)MyGlobalLock(pd.hDevNames);
+
+        devNamesA->wDriverOffset = sizeof(DEVNAMES);
+        memcpy((LPSTR)devNamesA + devNamesA->wDriverOffset, driver, driverLen);
+        devNamesA->wDeviceOffset = devNamesA->wDriverOffset + driverLen;
+        memcpy((LPSTR)devNamesA + devNamesA->wDeviceOffset, device, deviceLen);
+        devNamesA->wOutputOffset = devNamesA->wDeviceOffset + deviceLen;
+        memcpy((LPSTR)devNamesA + devNamesA->wOutputOffset, output, outputLen);
+        devNamesA->wDefault = devNames->wDefault;
+
+        FreeAsciiString(output);
+        if (device != queue)
+            FreeAsciiString(device);
+        FreeAsciiString(driver);
+
+        MyGlobalUnlock(pd.hDevNames);
+        GlobalUnlock(lppd->hDevNames);
+    }
+
+    // convert to ASCII string
+    if ((lppd->Flags & PD_ENABLEPRINTTEMPLATE) &&
+        (lppd->lpPrintTemplateName != NULL))
+        pd.lpPrintTemplateName = UnicodeToAsciiString((WCHAR*)lppd->lpPrintTemplateName);
+    else
+        pd.lpPrintTemplateName = NULL;
+
+    if ((lppd->Flags & PD_ENABLESETUPTEMPLATE) &&
+        (lppd->lpSetupTemplateName != NULL))
+        pd.lpSetupTemplateName = UnicodeToAsciiString((WCHAR*)lppd->lpSetupTemplateName);
+    else
+        pd.lpSetupTemplateName = NULL;
+
+    COMDLG32_CHECKHOOK2((&pd), PD_ENABLEPRINTHOOK, LPPRINTHOOKPROC, lpfnPrintHook)
+    COMDLG32_CHECKHOOK2((&pd), PD_ENABLESETUPHOOK, LPSETUPHOOKPROC, lpfnSetupHook)
+
+    bResult = O32_PrintDlg(&pd); // call ASCII API
+
+    if (pd.lpPrintTemplateName != NULL)
+        FreeAsciiString((char*)pd.lpPrintTemplateName);
+    if (pd.lpSetupTemplateName != NULL)
+        FreeAsciiString((char*)pd.lpSetupTemplateName);
+
+    if (pd.hDevNames)
+    {
+        LPDEVNAMES devNames = (LPDEVNAMES)MyGlobalLock(pd.hDevNames);
+
+        dprintf(("PrintDlgW: out devNames{%s,%s,%s}",
+                 (LPCSTR)devNames + devNames->wDriverOffset,
+                 (LPCSTR)devNames + devNames->wDeviceOffset,
+                 (LPCSTR)devNames + devNames->wOutputOffset));
+
+        LPSTR realDevice = (LPSTR)devNames + devNames->wDeviceOffset;
+
+        // convert from ASCII string
+        LPWSTR driver = AsciiToUnicodeString((LPCSTR)devNames + devNames->wDriverOffset);
+        LPWSTR device = AsciiToUnicodeString(realDevice);
+        LPWSTR output = AsciiToUnicodeString((LPCSTR)devNames + devNames->wOutputOffset);
+        int driverLen = (lstrlenW(driver) + 1) * sizeof(WCHAR);
+        int deviceLen = (lstrlenW(device) + 1) * sizeof(WCHAR);
+        int outputLen = (lstrlenW(output) + 1) * sizeof(WCHAR);
+
+        if (lppd->hDevNames)
+            lppd->hDevNames = GlobalReAlloc(lppd->hDevNames,
+                                            sizeof(DEVNAMES) +
+                                            driverLen + deviceLen + outputLen,
+                                            GHND);
+        else
+            lppd->hDevNames = GlobalAlloc(GHND, sizeof(DEVNAMES) +
+                                          driverLen + deviceLen + outputLen);
+
+        LPDEVNAMES devNamesW = (LPDEVNAMES)GlobalLock(lppd->hDevNames);
+
+        devNamesW->wDriverOffset = sizeof(DEVNAMES) / sizeof(WCHAR);
+        memcpy((LPWSTR)devNamesW + devNamesW->wDriverOffset, driver, driverLen);
+        devNamesW->wDeviceOffset = devNamesW->wDriverOffset + (driverLen / sizeof(WCHAR));
+        memcpy((LPWSTR)devNamesW + devNamesW->wDeviceOffset, device, deviceLen);
+        devNamesW->wOutputOffset = devNamesW->wDeviceOffset + (deviceLen / sizeof(WCHAR));
+        memcpy((LPWSTR)devNamesW + devNamesW->wOutputOffset, output, outputLen);
+        devNamesW->wDefault = devNames->wDefault;
+
+        FreeAsciiString(output);
+        FreeAsciiString(device);
+        FreeAsciiString(driver);
+
+        GlobalUnlock(lppd->hDevNames);
+        MyGlobalUnlock(pd.hDevNames);
+
+        MyGlobalFree(pd.hDevNames);
+    }
+    else
+    {
+         if (lppd->hDevNames)
+            MyGlobalFree(lppd->hDevNames);
+         lppd->hDevNames = NULL;
+    }
+
+    if (pd.hDevMode)
+    {
+        LPDEVMODEA devMode = (LPDEVMODEA)MyGlobalLock(pd.hDevMode);
+
+        dprintf(("PrintDlgW: out devMode{%.*s,%.*s}",
+                 CCHDEVICENAME, devMode->dmDeviceName,
+                 CCHFORMNAME, devMode->dmFormName));
+
+        if (!lppd->hDevMode)
+            lppd->hDevMode = GlobalAlloc(GHND, sizeof(DEVMODEW));
+
+        LPDEVMODEW devModeW = (LPDEVMODEW)GlobalLock(lppd->hDevMode);
+
+        // convert from ASCII string
+        AsciiToUnicodeN((LPSTR)devMode->dmDeviceName, (LPWSTR)devModeW->dmDeviceName,
+                        CCHDEVICENAME);
+        AsciiToUnicodeN((LPSTR)devMode->dmFormName, (LPWSTR)devModeW->dmFormName,
+                        CCHFORMNAME);
+
+        memcpy(&devModeW->dmSpecVersion, &devMode->dmSpecVersion,
+               offsetof(DEVMODEW, dmFormName) - offsetof(DEVMODEW, dmSpecVersion));
+        memcpy(&devModeW->dmLogPixels, &devMode->dmLogPixels,
+               sizeof(DEVMODEW) - offsetof(DEVMODEW, dmLogPixels));
+
+        GlobalUnlock(lppd->hDevMode);
+        MyGlobalUnlock(pd.hDevMode);
+
+        MyGlobalFree(pd.hDevMode);
+    }
+    else
+    {
+         if (lppd->hDevNames)
+            MyGlobalFree(lppd->hDevNames);
+         lppd->hDevNames = NULL;
+    }
+
+    // copy back the rest
+    lppd->hDC       = pd.hDC;
+    lppd->Flags     = pd.Flags;
+    lppd->nFromPage = pd.nFromPage;
+    lppd->nToPage   = pd.nToPage;
+    lppd->nMinPage  = pd.nMinPage;
+    lppd->nMaxPage  = pd.nMaxPage;
+    lppd->nCopies   = pd.nCopies;
+
+    if (lppd->hDC)
+    {
+        OSLibGpiSetCp(lppd->hDC, GetDisplayCodepage());
+    }
+
+    return bResult;
 }
 
 
@@ -257,4 +535,3 @@ ODINFUNCTION1(BOOL, PageSetupDlgW,
   return(FALSE);
 }
 
-#endif
