40fb8026042609f156c694e5f1e22805d7bc67cc
Dmitriy Kuminov <coding@dmik.org>
2011-08-24 21:38:23 +0000

    Better makeall sequence.

--- makeall.cmd
+++ makeall.cmd
@@ -1,5 +1,9 @@
-nmake clean
+nmake RELEASE=1 clean
 nmake dep
-nmake DEBUG=1 /a /i
 nmake RELEASE=1 /a /i
-nmake PROFILE=1 DEBUG=1 /a /i
+
+nmake DEBUG=1 clean
+nmake DEBUG=1 /a /i
+
+@rem nmake PROFILE=1 DEBUG=1 clean
+@rem nmake PROFILE=1 DEBUG=1 /a /i
