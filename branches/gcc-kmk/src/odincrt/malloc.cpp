1bcf098f64bb3196cd73f90d5ab8a29aa9aefcc8
dmik <dmik@3ae8c665-79e0-dc11-ae90-00e081727c95>
2011-11-07 21:12:04 +0000

    Add malloc/free wrappers to ODINCRT.

--- branches/gcc-kmk/src/odincrt/malloc.cpp
+++ branches/gcc-kmk/src/odincrt/malloc.cpp
@@ -1,203 +1,49 @@
-/* $Id: malloc.cpp,v 1.9 2003-01-23 12:33:04 sandervl Exp $ */
-/*
+/** @file
+ * Memory RTL function wrappers for GCC.
+ *
  * Project Odin Software License can be found in LICENSE.TXT
- * Memory RTL function wrappers
- *
- * Copyright 1999 Sander van Leeuwen
- *
  */
 
-#define ORIGINAL_VAC_FUNCTIONS
-#include <malloc.h>
-#include <umalloc.h>
 #include <os2sel.h>
 
-void * _IMPORT _LNK_CONV _debug_calloc( size_t, size_t, const char *, size_t );
-void   _IMPORT _LNK_CONV _debug_free( void *, const char *, size_t );
-void * _IMPORT _LNK_CONV _debug_malloc( size_t, const char *, size_t );
-void * _IMPORT _LNK_CONV _debug_realloc( void *, size_t, const char *, size_t );
-void * _IMPORT _LNK_CONV _debug_umalloc(Heap_t , size_t , const char *,size_t);
-void * _IMPORT _LNK_CONV _debug_ucalloc(Heap_t , size_t, size_t ,const char *,size_t);
-
-#ifdef DEBUG
-unsigned long nrcalls_malloc = 0;
-unsigned long nrcalls_free   = 0;
-unsigned long totalmemalloc  = 0;
-
-void _LNK_CONV getcrtstat(unsigned long *pnrcalls_malloc,
-                          unsigned long *pnrcalls_free,
-                          unsigned long *ptotalmemalloc)
+extern "C"
 {
-    *pnrcalls_malloc = nrcalls_malloc;
-    *pnrcalls_free   = nrcalls_free;
-    *ptotalmemalloc  = totalmemalloc;
-}
 
-#endif
+// these are GCC kLIBC exports
+void _std_malloc(size_t sz);
+void *_std_realloc(void *ptr, size_t sz);
+void *_std_calloc(size_t cnt, size_t sz);
+void _std_free(void *ptr);
 
-void * _LNK_CONV CRTWRAP(calloc)( size_t a, size_t b )
+void malloc(size_t sz)
 {
     unsigned short sel = RestoreOS2FS();
-    void *rc;
+    void *ptr = _std_malloc(sz);
+	SetFS(sel);
+	return ptr;
+}
 
-#ifdef DEBUG
-        totalmemalloc += a*b;
-        nrcalls_malloc++;
-#endif
-	rc = calloc(a,b);
+void *realloc(void *ptr, size_t sz)
+{
+    unsigned short sel = RestoreOS2FS();
+    void *newPtr = _std_realloc(ptr, sz);
+	SetFS(sel);
+	return newPtr;
+}
+
+void *calloc(size_t cnt, size_t sz)
+{
+    unsigned short sel = RestoreOS2FS();
+    void *ptr = _std_calloc(cnt, sz);
 	SetFS(sel);
 	return rc;
 }
 
-void   _LNK_CONV CRTWRAP(free)( void *a )
+void free(void *ptr)
 {
     unsigned short sel = RestoreOS2FS();
-
-#ifdef DEBUG
-        nrcalls_free++;
-        totalmemalloc -= _msize(a);
-#endif
-	free(a);
+	_std_free(ptr);
 	SetFS(sel);
 }
 
-void * _LNK_CONV CRTWRAP(malloc)( size_t a)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-
-#ifdef DEBUG
-        totalmemalloc += a;
-        nrcalls_malloc++;
-#endif
-	rc = malloc(a);
-	SetFS(sel);
-	return rc;
 }
-
-void * _LNK_CONV CRTWRAP(realloc)( void *a, size_t b)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += (b - _msize(a));
-#endif
-
-	rc = realloc(a, b);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_debug_calloc)( size_t a, size_t b, const char *c, size_t d)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += a*b;
-        nrcalls_malloc++;
-#endif
-
-	rc = _debug_calloc(a,b,c,d);
-	SetFS(sel);
-	return rc;
-}
-
-void   _LNK_CONV CRTWRAP(_debug_free)( void *a, const char *b, size_t c)
-{
-    unsigned short sel = RestoreOS2FS();
-
-
-#ifdef DEBUG
-        nrcalls_free++;
-        totalmemalloc -= _msize(a);
-#endif
-	_debug_free(a,b,c);
-	SetFS(sel);
-}
-
-void * _LNK_CONV CRTWRAP(_debug_malloc)( size_t a, const char *b, size_t c)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += a;
-        nrcalls_malloc++;
-#endif
-	rc = _debug_calloc(1,a,b,c);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_debug_realloc)( void *a, size_t b, const char *c, size_t d)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += (b - _msize(a));
-#endif
-	rc = _debug_realloc(a,b,c,d);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_umalloc)(Heap_t a, size_t b)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += b;
-        nrcalls_malloc++;
-#endif
-	rc = _umalloc(a,b);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_ucalloc)(Heap_t a, size_t b, size_t c)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += b*c;
-        nrcalls_malloc++;
-#endif
-	rc = _ucalloc(a,b,c);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_debug_umalloc)(Heap_t a, size_t b, const char *c, size_t d)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += b;
-        nrcalls_malloc++;
-#endif
-	rc = _debug_ucalloc(a, 1, b,c,d);
-	SetFS(sel);
-	return rc;
-}
-
-void * _LNK_CONV CRTWRAP(_debug_ucalloc)(Heap_t a, size_t b, size_t c, const char *d, size_t e)
-{
-    unsigned short sel = RestoreOS2FS();
-    void *rc;
-
-#ifdef DEBUG
-        totalmemalloc += b*c;
-        nrcalls_malloc++;
-#endif
-	rc = _debug_ucalloc(a,b,c,d,e);
-	SetFS(sel);
-	return rc;
-}
-
