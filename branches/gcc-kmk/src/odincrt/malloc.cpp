ee853b749cff09bcf236c11ad53f4ff7caa7b65b
dmik <dmik@3ae8c665-79e0-dc11-ae90-00e081727c95>
2011-11-25 21:18:08 +0000

    odincrt: Actually build malloc funciton replacements.

--- branches/gcc-kmk/src/odincrt/malloc.cpp
+++ branches/gcc-kmk/src/odincrt/malloc.cpp
@@ -4,18 +4,37 @@
  * Project Odin Software License can be found in LICENSE.TXT
  */
 
+#include <odin.h>
 #include <os2sel.h>
+
+#include <memory.h>
+
+#ifdef DEBUG
+unsigned long nrcalls_malloc = 0;
+unsigned long nrcalls_free   = 0;
+unsigned long totalmemalloc  = 0;
+
+// currently, it's a dumb stub
+void _LNK_CONV getcrtstat(unsigned long *pnrcalls_malloc,
+                          unsigned long *pnrcalls_free,
+                          unsigned long *ptotalmemalloc)
+{
+    *pnrcalls_malloc = nrcalls_malloc;
+    *pnrcalls_free   = nrcalls_free;
+    *ptotalmemalloc  = totalmemalloc;
+}
+#endif
 
 extern "C"
 {
 
 // these are GCC kLIBC exports
-void _std_malloc(size_t sz);
+void *_std_malloc(size_t sz);
 void *_std_realloc(void *ptr, size_t sz);
 void *_std_calloc(size_t cnt, size_t sz);
 void _std_free(void *ptr);
 
-void malloc(size_t sz)
+void *malloc(size_t sz)
 {
     unsigned short sel = RestoreOS2FS();
     void *ptr = _std_malloc(sz);
@@ -36,7 +55,7 @@ void *calloc(size_t cnt, size_t sz)
     unsigned short sel = RestoreOS2FS();
     void *ptr = _std_calloc(cnt, sz);
 	SetFS(sel);
-	return rc;
+	return ptr;
 }
 
 void free(void *ptr)
