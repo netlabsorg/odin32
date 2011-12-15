4dfbcb47bf33a4f1502f6161619fcd7ab3b977bd
dmik <dmik@3ae8c665-79e0-dc11-ae90-00e081727c95>
2011-12-15 12:11:04 +0000

    Port OLEACC, OLECLI32, OLEDLG, OLEPRO32 and OLESVR32 to GCC/kBuild.

--- branches/gcc-kmk/src/oleacc/initterm.cpp
+++ branches/gcc-kmk/src/oleacc/initterm.cpp
@@ -1,28 +1,13 @@
 /* $Id: initterm.cpp,v 1.17 2001/09/05 10:26:30 bird Exp $
  *
- * DLL entry point
+ * OLEACC DLL entry point
  *
  * Copyright 1998 Sander van Leeuwen
  * Copyright 1998 Peter Fitzsimmons
  *
- *
  * Project Odin Software License can be found in LICENSE.TXT
- *
  */
 
-/*-------------------------------------------------------------*/
-/* INITERM.C -- Source for a custom dynamic link library       */
-/*              initialization and termination (_DLL_InitTerm) */
-/*              function.                                      */
-/*                                                             */
-/* When called to perform initialization, this sample function */
-/* gets storage for an array of integers, and initializes its  */
-/* elements with random integers.  At termination time, it     */
-/* frees the array.  Substitute your own special processing.   */
-/*-------------------------------------------------------------*/
-
-
-/* Include files */
 #define  INCL_DOSMODULEMGR
 #define  INCL_DOSPROCESS
 #include <os2wrap.h>    //Odin32 OS/2 api wrappers
@@ -30,52 +15,71 @@
 #include <stdio.h>
 #include <string.h>
 #include <odin.h>
-#include <win32type.h>
+#include <win32api.h>
 #include <winconst.h>
 #include <odinlx.h>
-#include <misc.h>       /*PLF Wed  98-03-18 23:18:15*/
+#include <cpuhlp.h>
+#include <dbglog.h>
 #include <initdll.h>
 
+// Win32 resource table (produced by wrc)
+extern DWORD oleacc_PEResTab;
 
-/****************************************************************************/
-/* _DLL_InitTerm is the function that gets called by the operating system   */
-/* loader when it loads and frees this DLL for each process that accesses   */
-/* this DLL.  However, it only gets called the first time the DLL is loaded */
-/* and the last time it is freed for a particular process.  The system      */
-/* linkage convention MUST be used because the operating system loader is   */
-/* calling this function.                                                   */
-/****************************************************************************/
-ULONG SYSTEM _DLL_InitTerm(ULONG hModule, ULONG ulFlag)
+static HMODULE dllHandle = 0;
+
+BOOL WINAPI OleaccDllMain(HINSTANCE hInst, DWORD fdwReason, PVOID pvReserved);
+
+static BOOL WINAPI OdinLibMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID fImpLoad)
 {
-   size_t i;
-   APIRET rc;
+    BOOL ret;
 
-   /*-------------------------------------------------------------------------*/
-   /* If ulFlag is zero then the DLL is being loaded so initialization should */
-   /* be performed.  If ulFlag is 1 then the DLL is being freed so            */
-   /* termination should be performed.                                        */
-   /*-------------------------------------------------------------------------*/
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        return OleaccDllMain(hinstDLL, fdwReason, fImpLoad);
 
-   switch (ulFlag) {
-      case 0 :
-          ctordtorInit();
-          CheckVersionFromHMOD(PE2LX_VERSION, hModule); /*PLF Wed  98-03-18 05:28:48*/
-          return inittermOleacc(hModule, ulFlag);
-         break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+        return OleaccDllMain(hinstDLL, fdwReason, fImpLoad);
 
-
-      case 1 :
-          return inittermOleacc(hModule, ulFlag);
-         break;
-
-      default  :
-         return 0UL;
-   }
-
-   /***********************************************************/
-   /* A non-zero value must be returned to indicate success.  */
-   /***********************************************************/
-   return 1UL;
+    case DLL_PROCESS_DETACH:
+        ret = OleaccDllMain(hinstDLL, fdwReason, fImpLoad);
+        return ret;
+    }
+    return FALSE;
 }
-//******************************************************************************
-//******************************************************************************
+
+ULONG SYSTEM DLL_InitMSCms(ULONG hModule)
+{
+    if (!InitializeKernel32())
+        return 0;
+
+    CheckVersionFromHMOD(PE2LX_VERSION, hModule); /*PLF Wed  98-03-18 05:28:48*/
+
+    dllHandle = RegisterLxDll(hModule, OdinLibMain, (PVOID)&oleacc_PEResTab);
+    if (dllHandle == 0)
+        return -1;
+
+    dprintf(("oleacc init %s %s (%x)", __DATE__, __TIME__, DLL_InitMSCms));
+
+    return 0;
+}
+
+void SYSTEM DLL_TermMSCms(ULONG hModule)
+{
+    if (dllHandle)
+       UnregisterLxDll(dllHandle);
+}
+
+ULONG SYSTEM DLL_Init(ULONG hModule)
+{
+    if (DLL_InitDefault(hModule) == -1)
+        return -1;
+    return DLL_InitMSCms(hModule);
+}
+
+void SYSTEM DLL_Term(ULONG hModule)
+{
+    DLL_TermMSCms(hModule);
+    DLL_TermDefault(hModule);
+}
