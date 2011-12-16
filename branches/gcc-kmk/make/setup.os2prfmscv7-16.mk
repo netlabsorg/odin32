5d9121766429ddb13861f4a6e58487ed215a03b7
dmik <dmik@3ae8c665-79e0-dc11-ae90-00e081727c95>
2011-12-16 14:52:14 +0000

    Remove makefiles and all stuff related to the old make system.
    Since kBuild is now used, this stuff is not needed and misleading.
    Note that for historical purposes (and for the purposes of further porting
    of components not yet ported) the old make system files will be available
    under the "pre-gcc-kmk" branch containing what used to be the trunk before
    merging the "gcc-kmk" branch in it .

--- branches/gcc-kmk/make/setup.os2prfmscv7-16.mk
+++
@@ -1,8 +0,0 @@
-# $Id: setup.os2prfmscv7-16.mk,v 1.1 2002-09-14 16:03:38 bird Exp $
-
-# ---OS2, PROFILE, MSCV7-------------------------
-!include $(PATH_MAKE)\setup.os2debmscv7-16.mk
-ENV_NAME="OS/2, Profile, Microsoft C v7.0 16-bit"
-
-# ---OS2, PROFILE, MSCV7-------------------------
-
--- branches/gcc-kmk/make/setup.os2prfwat11-16.mk
+++
@@ -1,8 +0,0 @@
-# $Id: setup.os2prfwat11-16.mk,v 1.1 2002-05-16 11:37:04 bird Exp $
-
-# ---OS2, PROFILE, WAT11-------------------------
-!include $(PATH_MAKE)\setup.os2debwat11-16.mk
-ENV_NAME="OS/2, Profile, Watcom C/C++ v11.0c 16-bit"
-
-# ---OS2, PROFILE, WAT11-------------------------
-
