5705bf51f4c4602d576d22575c34aec443ff8205
dmik <dmik@3ae8c665-79e0-dc11-ae90-00e081727c95>
2011-12-16 15:13:35 +0000

    Remove more unneeded scripts.

--- branches/gcc-kmk/bin/dbg.cmd
+++
@@ -1,4 +0,0 @@
-SET PMDPATH=f:\source\odin32\src\peldr;f:\source\odin32\src\advapi32;f:\source\odin32\src\comdlg32;f:\source\odin32\src\gdi32;f:\source\odin32\src\user32;f:\source\odin32\src\kernel32;f:\source\odin32\src\winmm;f:\source\odin32\src\version;f:\source\odin32\src\shell32;f:\source\odin32\src\ole32;f:\source\odin32\src\ddraw;f:\source\odin32\src\dsound;f:\source\odin32\src\comctl32;
-del \os2\*.@*
-ipmd %1 %2 %3 %4 %5
-
